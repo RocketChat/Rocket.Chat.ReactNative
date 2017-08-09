cf20dae1f919d6714b605d039af873fb215a163d
Rodrigo Nascimento <yiweigao0226@gmail.com>
2017-08-09 15:01:54 -0300

    Sort room's list

--- app/components/RoomItem.js
+++ app/components/RoomItem.js
@@ -1,20 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		flexDirection: 'row',
+		alignItems: 'center'
+	},
+	number: {
+		width: 20,
+		lineHeight: 20,
+		borderRadius: 5,
+		backgroundColor: 'green',
+		color: '#fff',
+		textAlign: 'center',
+		overflow: 'hidden',
+		marginRight: 15
+	},
 	roomItem: {
 		lineHeight: 18,
 		borderTopWidth: 2,
 		borderColor: '#aaa',
-		padding: 14
+		padding: 14,
+		flexGrow: 1
 	}
 });
 
 export default class RoomItem extends React.PureComponent {
 	static propTypes = {
 		onPressItem: PropTypes.func.isRequired,
-		title: PropTypes.string.isRequired,
+		item: PropTypes.object.isRequired,
 		id: PropTypes.string.isRequired
 	}
 
@@ -22,9 +38,22 @@ export default class RoomItem extends React.PureComponent {
 		this.props.onPressItem(this.props.id);
 	};
 
+	renderNumber = (item) => {
+		if (item.unread) {
+			return (
+				<Text style={styles.number}>
+					{ item.unread }
+				</Text>
+			);
+		}
+	}
+
 	render() {
 		return (
-			<Text onPress={this._onPress} style={styles.roomItem}>{ this.props.title }</Text>
+			<View style={styles.container}>
+				<Text onPress={this._onPress} style={styles.roomItem}>{ this.props.item.name }</Text>
+				{this.renderNumber(this.props.item)}
+			</View>
 		);
 	}
 }
