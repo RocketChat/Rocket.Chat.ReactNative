43c3fbcbd1a7ada23e8404c7d28e3440e94ee821
Guilherme Gazzo <yiweigao0226@gmail.com>
2017-08-10 14:43:31 -0300

    fix icons

--- android/app/src/main/java/com/rocketchatrn/MainApplication.java
+++ android/app/src/main/java/com/rocketchatrn/MainApplication.java
@@ -3,6 +3,7 @@ package com.rocketchatrn;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.balthazargronon.RCTZeroconf.ZeroconfReactPackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SvgPackage(),
             new VectorIconsPackage(),
             new RNFetchBlobPackage(),
             new ZeroconfReactPackage(),
