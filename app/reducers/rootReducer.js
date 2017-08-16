9553ecbcafd75d203772eb72403fda6442f46652
Guilherme Gazzo <guilhermegazzo@gmail.com>
2017-08-16 20:29:12 -0300

    login saga (incomplete)

--- app/reducers/rootReducer.js
+++ app/reducers/rootReducer.js
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux';
 import * as reducers from './reducers';
+import * as login from './login';
 
 const rootReducer = combineReducers({
-	...reducers
+	...reducers, ...login
 });
 
 export default rootReducer;
