ad8ccdf596446faee9504dfa527c80e3865ee61c
Guilherme Gazzo <yiweigao0226@gmail.com>
2017-08-17 17:15:24 -0300

    auto login connect

--- app/sagas/login.js
+++ app/sagas/login.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import { take, put, call, takeLast, fork, select } from 'redux-saga/effects';
 import * as types from '../actions/actionsTypes';
-import { loginSuccess, loginFailure, logout } from '../actions/login';
+import { loginRequest, loginSuccess, loginFailure, logout } from '../actions/login';
 import RocketChat from '../lib/rocketchat';
 
-const getUser = state => state.login;
+const getUser = state => state.login.user;
 function loginCall(args) {
-	return RocketChat.loginWithPassword(args);
+	return args.resume ? RocketChat.login(args) : RocketChat.loginWithPassword(args);
 }
 const auto = function* auto() {
 	while (true) {
+		yield take(types.LOGOUT);
 		yield take(types.METEOR.SUCCESS);
 		const user = yield select(getUser);
 		if (user.token) {
-			RocketChat.login({ resume: user.token });
+			yield put(loginRequest({ resume: user.token }));
 		}
 	}
 };
@@ -21,22 +22,21 @@ const watchLoginRequest = function* watchLoginRequest() {
 	while (true) {
 		try {
 			yield take(types.METEOR.SUCCESS);
-			console.log('\n\n[LOGIN METEOR CONNECTED]\n\n');
+			// console.log('\n\n[LOGIN METEOR CONNECTED]\n\n');
 			const payload = yield take(types.LOGIN.REQUEST);
 			try {
 				const response = yield call(loginCall, payload);
-				console.log(response);
 				yield put(loginSuccess(response));
 				console.log('\n\n[LOGIN SUCCESS]\n\n');
 			} catch (err) {
-				console.log('\n\n[LOGIN FAILURE]\n\n', err);
+				// console.log('\n\n[LOGIN FAILURE]\n\n', err);
 				yield put(loginFailure(err.status));
 			}
 			yield take(types.METEOR.DISCONNECT);
 			console.log('\n\n[METEOR DISCONNECT LOGOUT]\n\n');
 			yield put(logout());
 		} catch (e) {
-			console.log(e);
+			console.error(e);
 		}
 	}
 };
@@ -45,4 +45,4 @@ const root = function* root() {
 	yield fork(watchLoginRequest);
 	yield fork(auto);
 };
-export default watchLoginRequest;
+export default root;
