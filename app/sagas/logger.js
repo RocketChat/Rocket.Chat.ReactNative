5b4fc5ddf83640a533bf260d2a88d961712fc532
Guilherme Gazzo <guilhermegazzo@gmail.com>
2017-08-17 17:15:24 -0300

    auto login connect

--- app/sagas/logger.js
+++ app/sagas/logger.js
@@ -1,12 +1,12 @@
 import { select, takeEvery } from 'redux-saga/effects';
 
 const root = function* watchAndLog() {
-	yield takeEvery('*', function* logger(action) {
-		const state = yield select();
-		const tmp = { ...state };
-		delete tmp.settings;
-		console.log('action', action);
-		console.log('state after', tmp);
-	});
+	// yield takeEvery('*', function* logger(action) {
+	// 	const state = yield select();
+	// 	const tmp = { ...state };
+	// 	delete tmp.settings;
+	// 	console.log('action', action);
+	// 	console.log('state after', tmp);
+	// });
 };
 export default root;
