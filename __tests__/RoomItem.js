4a0600e32c86b201fe004b001702e30bec669d35
Diego Sampaio <chinello@gmail.com>
2017-08-22 09:01:53 -0300

    Reduce test lines of code

--- __tests__/RoomItem.js
+++ __tests__/RoomItem.js
@@ -11,64 +11,34 @@ jest.mock('react-native-img-cache', () => {
 	}
 });
 
+const component = props => <RoomItem {...props} />
+
 it('renders correctly', () => {
-	const tree = renderer.create(
-		<RoomItem
-			type="d"
-			name="name"
-		/>
-	).toJSON();
+	const tree = renderer.create(component({ type: 'd', name: 'name' })).toJSON();
 	expect(tree).toMatchSnapshot();
 });
 
 it('render unread', () => {
-	const tree = renderer.create(
-		<RoomItem
-			type="d"
-			name="name"
-			unread={1}
-		/>
-	).toJSON();
+	const tree = renderer.create(component({ type: 'd', name: 'name', unread: 1 })).toJSON();
 	expect(tree).toMatchSnapshot();
 });
 
 it('render unread +999', () => {
-	const tree = renderer.create(
-		<RoomItem
-			type="d"
-			name="name"
-			unread={1000}
-		/>
-	).toJSON();
+	const tree = renderer.create(component({ type: 'd', name: 'name', unread: 1000 })).toJSON();
 	expect(tree).toMatchSnapshot();
 });
 
 it('render no icon', () => {
-	const tree = renderer.create(
-		<RoomItem
-			type="X"
-			name="name"
-		/>
-	).toJSON();
+	const tree = renderer.create(component({ type: 'X', name: 'name' })).toJSON();
 	expect(tree).toMatchSnapshot();
 });
 
 it('render private group', () => {
-	const tree = renderer.create(
-		<RoomItem
-			type="g"
-			name="private-group"
-		/>
-	).toJSON();
+	const tree = renderer.create(component({ type: 'g', name: 'private-group' })).toJSON();
 	expect(tree).toMatchSnapshot();
 });
 
 it('render channel', () => {
-	const tree = renderer.create(
-		<RoomItem
-			type="c"
-			name="general"
-		/>
-	).toJSON();
+	const tree = renderer.create(component({ type: 'c', name: 'general' })).toJSON();
 	expect(tree).toMatchSnapshot();
 });
