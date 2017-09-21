2457c14a58dd37f3b9a9fd43eb7b6a640b04a151
Guilherme Gazzo <guilhermegazzo@gmail.com>
2017-09-21 13:12:33 -0300

    Update PublicRoutes.js

--- app/containers/routes/PublicRoutes.js
+++ app/containers/routes/PublicRoutes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, TouchableOpacity } from 'react-native';
+import { TouchableOpacity } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
