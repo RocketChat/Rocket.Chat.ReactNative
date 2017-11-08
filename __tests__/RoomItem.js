f87ac58ca3a67abd238c3a9d0f8092a22f5d60b4
Rodrigo Nascimento <yiweigao0226@gmail.com>
2017-11-08 18:46:59 -0200

    Fix tests

--- __tests__/RoomItem.js
+++ __tests__/RoomItem.js
@@ -5,28 +5,30 @@ import RoomItem from '../app/presentation/RoomItem';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+const date = new Date(2017, 10, 10, 10);
+
 jest.mock('react-native-img-cache', () => { return { CachedImage: 'View' } });
 
 it('renders correctly', () => {
-	expect(renderer.create(<RoomItem type="d" name="name" />).toJSON()).toMatchSnapshot();
+	expect(renderer.create(<RoomItem type="d" _updatedAt={date} name="name" />).toJSON()).toMatchSnapshot();
 });
 
 it('render unread', () => {
-	expect(renderer.create(<RoomItem type="d" name="name" unread={1} />).toJSON()).toMatchSnapshot();
+	expect(renderer.create(<RoomItem type="d" _updatedAt={date} name="name" unread={1} />).toJSON()).toMatchSnapshot();
 });
 
 it('render unread +999', () => {
-	expect(renderer.create(<RoomItem type="d" name="name" unread={1000} />).toJSON()).toMatchSnapshot();
+	expect(renderer.create(<RoomItem type="d" _updatedAt={date} name="name" unread={1000} />).toJSON()).toMatchSnapshot();
 });
 
 it('render no icon', () => {
-	expect(renderer.create(<RoomItem type="X" name="name" />).toJSON()).toMatchSnapshot();
+	expect(renderer.create(<RoomItem type="X" _updatedAt={date} name="name" />).toJSON()).toMatchSnapshot();
 });
 
 it('render private group', () => {
-	expect(renderer.create(<RoomItem type="g" name="private-group" /> ).toJSON()).toMatchSnapshot();
+	expect(renderer.create(<RoomItem type="g" _updatedAt={date} name="private-group" /> ).toJSON()).toMatchSnapshot();
 });
 
 it('render channel', () => {
-	expect(renderer.create(<RoomItem type="c" name="general" />).toJSON()).toMatchSnapshot();
+	expect(renderer.create(<RoomItem type="c" _updatedAt={date} name="general" />).toJSON()).toMatchSnapshot();
 });
