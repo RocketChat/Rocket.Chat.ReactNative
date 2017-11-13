77af55ca9ef08ee54a5a0e08f35a410acb1dc28a
Diego Mello <yiweigao0226@gmail.com>
2017-11-13 11:19:24 -0200

    set token on app resume (#66)

--- app/sagas/init.js
+++ app/sagas/init.js
@@ -2,16 +2,23 @@ import { AsyncStorage } from 'react-native';
 import { call, put, take } from 'redux-saga/effects';
 import * as actions from '../actions';
 import { setServer } from '../actions/server';
+import { restoreToken } from '../actions/login';
 import { APP } from '../actions/actionsTypes';
 
 const restore = function* restore() {
 	try {
 		yield take(APP.INIT);
+		const token = yield call([AsyncStorage, 'getItem'], 'reactnativemeteor_usertoken');
+		if (token) {
+			yield put(restoreToken(token));
+		}
+
 		const currentServer = yield call([AsyncStorage, 'getItem'], 'currentServer');
-		yield put(actions.appReady({}));
 		if (currentServer) {
 			yield put(setServer(currentServer));
 		}
+
+		yield put(actions.appReady({}));
 	} catch (e) {
 		console.log(e);
 	}
