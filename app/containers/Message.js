926e4df596aed91e82d67d10a74cb0f48592cc78
Diego Mello <diegolmello@gmail.com>
2017-11-14 09:54:33 -0200

    react-native-actionsheet installed

--- app/containers/Message.js
+++ app/containers/Message.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
 import { emojify } from 'react-emojione';
 import Markdown from 'react-native-easy-markdown';
+import ActionSheet from 'react-native-actionsheet';
 
 import Card from './message/Card';
 import Avatar from './Avatar';
 import User from './message/User';
+
+const title = 'Message actions';
+const options = ['Cancel', 'Reply', 'Edit', 'Permalink', 'Copy', 'Quote', 'Star Message', 'Delete'];
+const CANCEL_INDEX = 0;
+const DESTRUCTIVE_INDEX = 7;
 
 const styles = StyleSheet.create({
 	content: {
@@ -29,12 +35,26 @@ export default class Message extends React.PureComponent {
 		Message_TimeFormat: PropTypes.string.isRequired
 	}
 
+	constructor(props) {
+		super(props);
+		this.handleActionPress = this.handleActionPress.bind(this);
+		this.showActions = this.showActions.bind(this);
+	}
+
 	attachments() {
 		return this.props.item.attachments.length ? (
 			<Card
 				data={this.props.item.attachments[0]}
 			/>
 		) : null;
+	}
+
+	showActions() {
+		this.ActionSheet.show();
+	}
+
+	handleActionPress = (i) => {
+		console.log(i);
 	}
 
 	render() {
@@ -49,26 +69,36 @@ export default class Message extends React.PureComponent {
 		const username = item.alias || item.u.username;
 
 		return (
-			<View style={[styles.message, extraStyle]}>
-				<Avatar
-					style={{ marginRight: 10 }}
-					text={item.avatar ? '' : username}
-					size={40}
-					baseUrl={this.props.baseUrl}
-					avatar={item.avatar}
-				/>
-				<View style={[styles.content]}>
-					<User
-						onPress={this._onPress}
-						item={item}
-						Message_TimeFormat={this.props.Message_TimeFormat}
+			<TouchableOpacity onLongPress={() => this.showActions()}>
+				<View style={[styles.message, extraStyle]}>
+					<Avatar
+						style={{ marginRight: 10 }}
+						text={item.avatar ? '' : username}
+						size={40}
+						baseUrl={this.props.baseUrl}
+						avatar={item.avatar}
 					/>
-					{this.attachments()}
-					<Markdown>
-						{msg}
-					</Markdown>
+					<View style={[styles.content]}>
+						<User
+							onPress={this._onPress}
+							item={item}
+							Message_TimeFormat={this.props.Message_TimeFormat}
+						/>
+						{this.attachments()}
+						<Markdown>
+							{msg}
+						</Markdown>
+					</View>
+					<ActionSheet
+						ref={o => this.ActionSheet = o}
+						title={title}
+						options={options}
+						cancelButtonIndex={CANCEL_INDEX}
+						destructiveButtonIndex={DESTRUCTIVE_INDEX}
+						onPress={this.handleActionPress}
+					/>
 				</View>
-			</View>
+			</TouchableOpacity>
 		);
 	}
 }
