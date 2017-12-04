2dcf7a485ba4411de2db81b4e6dc65c459252d3e
Diego Mello <yiweigao0226@gmail.com>
2017-12-04 16:24:21 -0200

    Set user presence (online/away) (#127)
    * - User presence away when app goes to background
    - User presence online when app comes back to foreground
    
    * test if user is connected before call userpresence
    
    * Update state.js

--- app/sagas/state.js
+++ app/sagas/state.js
@@ -1,8 +1,22 @@
-import { takeLatest } from 'redux-saga/effects';
+import { takeLatest, select } from 'redux-saga/effects';
 import { FOREGROUND, BACKGROUND, INACTIVE } from 'redux-enhancer-react-native-appstate';
 
+import RocketChat from '../lib/rocketchat';
+
 const appHasComeBackToForeground = function* appHasComeBackToForeground() {
-	yield console.log('appHasComeBackToForeground');
+	const auth = yield select(state => state.login.isAuthenticated);
+	if (!auth) {
+		return;
+	}
+	return yield RocketChat.setUserPresenceOnline();
+};
+
+const appHasComeBackToBackground = function* appHasComeBackToBackground() {
+	const auth = yield select(state => state.login.isAuthenticated);
+	if (!auth) {
+		return;
+	}
+	return yield RocketChat.setUserPresenceAway();
 };
 
 const root = function* root() {
@@ -12,11 +26,11 @@ const root = function* root() {
 	);
 	yield takeLatest(
 		BACKGROUND,
-		appHasComeBackToForeground
+		appHasComeBackToBackground
 	);
 	yield takeLatest(
 		INACTIVE,
-		appHasComeBackToForeground
+		appHasComeBackToBackground
 	);
 };
 
