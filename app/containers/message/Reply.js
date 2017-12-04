74c0914517d687e01fe6eafd2bfe7dd5c49c092f
Diego Mello <diegolmello@gmail.com>
2017-12-04 09:51:17 -0200

    Support <http://link|Text>

--- app/containers/message/Reply.js
+++ app/containers/message/Reply.js
@@ -57,6 +57,14 @@ const onPress = (attachment) => {
 	}
 	Linking.openURL(attachment.title_link || attachment.author_link);
 };
+
+// Support <http://link|Text>
+const formatText = text =>
+	text.replace(
+		new RegExp('(?:<|<)((?:https|http):\\/\\/[^\\|]+)\\|(.+?)(?=>|>)(?:>|>)', 'gm'),
+		(match, url, title) => `[${ title }](${ url })`
+	);
+
 const Reply = ({ attachment, timeFormat }) => {
 	if (!attachment) {
 		return null;
@@ -86,7 +94,7 @@ const Reply = ({ attachment, timeFormat }) => {
 	};
 
 	const renderText = () => (
-		attachment.text ? <Markdown msg={attachment.text} /> : null
+		attachment.text ? <Markdown msg={formatText(attachment.text)} /> : null
 	);
 
 	const renderFields = () => {
