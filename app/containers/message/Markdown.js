0e747ee6e9355a88d8bc3716efd769d14fa661bc
Diego Mello <diegolmello@gmail.com>
2017-12-14 14:58:16 -0200

    Block code working

--- app/containers/message/Markdown.js
+++ app/containers/message/Markdown.js
@@ -1,9 +1,30 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, Platform } from 'react-native';
 import PropTypes from 'prop-types';
 import EasyMarkdown from 'react-native-easy-markdown'; // eslint-disable-line
 import SimpleMarkdown from 'simple-markdown';
 import { emojify } from 'react-emojione';
+
+const codeStyle = {
+	...Platform.select({
+		ios: { fontFamily: 'Courier New' },
+		android: { fontFamily: 'Roboto' }
+	}),
+	backgroundColor: '#f8f8f8',
+	borderColor: '#cccccc',
+	borderWidth: 1,
+	borderRadius: 5,
+	padding: 5
+};
+
+const BlockCode = ({ node, state }) => (
+	<Text
+		key={state.key}
+		style={codeStyle}
+	>
+		{node.content}
+	</Text>
+);
 
 const rules = {
 	username: {
@@ -45,6 +66,37 @@ const rules = {
 				)
 			}
 		})
+	},
+	fence: {
+		order: -5,
+		match: SimpleMarkdown.blockRegex(/^ *(`{3,}|~{3,}) *(\S+)? *\n([\s\S]+?)\s*\1 *(?:\n *)+\n/),
+		parse: capture => ({
+			lang: capture[2] || undefined,
+			content: capture[3]
+		}),
+		react: (node, output, state) => ({
+			type: 'custom',
+			key: state.key,
+			props: {
+				children: (
+					<BlockCode key={state.key} node={node} state={state} />
+				)
+			}
+		})
+	},
+	blockCode: {
+		order: -6,
+		match: SimpleMarkdown.blockRegex(/^(```)\s*([\s\S]*?[^`])\s*\1(?!```)/),
+		parse: capture => ({ content: capture[2] }),
+		react: (node, output, state) => ({
+			type: 'custom',
+			key: state.key,
+			props: {
+				children: (
+					<BlockCode key={state.key} node={node} state={state} />
+				)
+			}
+		})
 	}
 };
 
@@ -53,11 +105,23 @@ const Markdown = ({ msg }) => {
 		return null;
 	}
 	msg = emojify(msg, { output: 'unicode' });
-	return <EasyMarkdown rules={rules}>{msg}</EasyMarkdown>;
+	return (
+		<EasyMarkdown
+			style={{ marginBottom: 0 }}
+			rules={rules}
+			markdownStyles={{ code: codeStyle }}
+		>{msg}
+		</EasyMarkdown>
+	);
 };
 
 Markdown.propTypes = {
 	msg: PropTypes.string.isRequired
+};
+
+BlockCode.propTypes = {
+	node: PropTypes.object,
+	state: PropTypes.object
 };
 
 export default Markdown;
