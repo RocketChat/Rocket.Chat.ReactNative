b485bcfbcdc6a1fe70e698523772506dc54ece20
Diego Mello <diegolmello@gmail.com>
2017-12-14 10:38:55 -0200

    Username, room and code markdown

--- app/containers/message/Markdown.js
+++ app/containers/message/Markdown.js
@@ -1,14 +1,59 @@
 import React from 'react';
+import { Text } from 'react-native';
 import PropTypes from 'prop-types';
 import EasyMarkdown from 'react-native-easy-markdown'; // eslint-disable-line
+import SimpleMarkdown from 'simple-markdown';
 import { emojify } from 'react-emojione';
+
+const rules = {
+	username: {
+		order: -1,
+		match: SimpleMarkdown.inlineRegex(/@[0-9a-zA-Z-_.]+/),
+		parse: capture => ({ content: capture[0] }),
+		react: (node, output, state) => ({
+			type: 'custom',
+			key: state.key,
+			props: {
+				children: (
+					<Text
+						key={state.key}
+						style={{ color: '#13679a' }}
+						onPress={() => alert('Username')}
+					>
+						{node.content}
+					</Text>
+				)
+			}
+		})
+	},
+	heading: {
+		order: -2,
+		match: SimpleMarkdown.inlineRegex(/#[0-9a-zA-Z-_.]+/),
+		parse: capture => ({ content: capture[0] }),
+		react: (node, output, state) => ({
+			type: 'custom',
+			key: state.key,
+			props: {
+				children: (
+					<Text
+						key={state.key}
+						style={{ color: '#13679a' }}
+						onPress={() => alert('Room')}
+					>
+						{node.content}
+					</Text>
+				)
+			}
+		})
+	}
+};
 
 const Markdown = ({ msg }) => {
 	if (!msg) {
 		return null;
 	}
 	msg = emojify(msg, { output: 'unicode' });
-	return <EasyMarkdown>{msg}</EasyMarkdown>;
+	return <EasyMarkdown rules={rules}>{msg}</EasyMarkdown>;
 };
 
 Markdown.propTypes = {
