11d9a667524ba2f474f94a39c65337e3d6ee4a4a
Diego Mello <diegolmello@gmail.com>
2017-12-20 18:12:49 -0200

    Splash screen (#153)
    * Splash working

--- ios/RocketChatRN/AppDelegate.m
+++ ios/RocketChatRN/AppDelegate.m
@@ -12,6 +12,7 @@
 #import <React/RCTPushNotificationManager.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import "SplashScreen.h"
 
 @implementation AppDelegate
 
@@ -32,6 +33,7 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [SplashScreen show];
   return YES;
 }
 
