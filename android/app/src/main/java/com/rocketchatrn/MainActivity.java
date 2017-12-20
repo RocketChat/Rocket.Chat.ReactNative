b79aff6e93e4a7ea42b341f0ad7a14a89127bbf4
Diego Mello <diegolmello@gmail.com>
2017-12-20 13:23:52 -0200

    Comment removed

--- android/app/src/main/java/com/rocketchatrn/MainActivity.java
+++ android/app/src/main/java/com/rocketchatrn/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends ReactActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen.show(this);  // here
+        SplashScreen.show(this);
         super.onCreate(savedInstanceState);
     }
 }
