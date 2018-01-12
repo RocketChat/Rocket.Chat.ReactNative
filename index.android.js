7714c388fc0a0226f8d0e12f170e7a8059bde3ca
Saket Kumar <saketkumar95@users.noreply.github.com>
2018-01-13 01:38:31 +0530

    Configures Reactotron with the project (#177)

--- index.android.js
+++ index.android.js
@@ -5,7 +5,7 @@ import { AppRegistry } from 'react-native';
 import './app/push';
 import RocketChat from './app/index';
 
-
+// import './app/ReactotronConfig';
 // import { AppRegistry } from 'react-native';
 // import Routes from './app/routes';
 //
