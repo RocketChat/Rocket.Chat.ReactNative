ec1782e5040bfb56db6b433b723d93b478a8bad5
Martin Schoeler <martin.schoeler@rocket.chat>
2018-01-15 16:44:20 -0200

    [FIX] Unread not going away when receiving a message from inside a room (#194)
    * Fix Unread inside Room

--- app/actions/room.js
+++ app/actions/room.js
@@ -29,9 +29,29 @@ export function openRoom(room) {
 	};
 }
 
+export function closeRoom() {
+	return {
+		type: types.ROOM.CLOSE
+	};
+}
+
 export function userTyping(status = true) {
 	return {
 		type: types.ROOM.USER_TYPING,
 		status
+	};
+}
+
+export function roomMessageReceived(message) {
+	return {
+		type: types.ROOM.MESSAGE_RECEIVED,
+		message
+	};
+}
+
+export function setLastOpen(date = new Date()) {
+	return {
+		type: types.ROOM.SET_LAST_OPEN,
+		date
 	};
 }
