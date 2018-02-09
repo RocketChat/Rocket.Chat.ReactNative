79fe772a320bcc95999dd74a9f9c9fe2c9640c61
Diego Mello <diegolmello@gmail.com>
2018-02-09 10:08:40 -0200

    test removed

--- rc-cli.config.js
+++
@@ -1,7 +0,0 @@
-const blacklist = require('metro/src/blacklist'); // eslint-disable-line
-
-module.exports = {
-	getBlacklistRE() {
-		return blacklist([/react-native\/local-cli\/core\/__fixtures__.*/]);
-	}
-};
