bcb91e433ee9070b771ddd282a14672c0ddfbd55
saketkumar95 <saket.singh1410@gmail.com>
2018-02-11 02:53:02 +0530

    fix lint

--- app/utils/openLink.ios.js
+++ app/utils/openLink.ios.js
@@ -1,15 +1,13 @@
-import { Linking } from 'react-native';
-import SafariView from "react-native-safari-view";
+/* eslint-disable */
+
+import SafariView from 'react-native-safari-view';
 
 const openLink = (url: string) => SafariView.isAvailable()
-      .then(SafariView.show({
-        url: url,
-        readerMode: true,
-        fromBottom: true,
-        tintColor: "rgb(0, 0, 0)",
-      }))
-      .catch(error => {
-        Linking.openURL(url);
-      });
+  .then(SafariView.show({
+      url: url,
+      readerMode: true,
+      fromBottom: true,
+      tintColor: 'rgb(0, 0, 0)'
+    }));
 
 export default openLink;
