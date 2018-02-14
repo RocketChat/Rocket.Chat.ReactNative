167509b4727bce597e695dbff6d7b0c8a2980f7f
Diego Mello <diegolmello@gmail.com>
2018-02-14 09:46:19 -0200

    - Reader mode and tint color removed - Lint fixed

--- app/utils/openLink.ios.js
+++ app/utils/openLink.ios.js
@@ -1,13 +1,5 @@
-/* eslint-disable */
-
 import SafariView from 'react-native-safari-view';
 
-const openLink = (url: string) => SafariView.isAvailable()
-  .then(SafariView.show({
-      url: url,
-      readerMode: true,
-      fromBottom: true,
-      tintColor: 'rgb(0, 0, 0)'
-    }));
+const openLink = url => SafariView.show({ url, fromBottom: true });
 
 export default openLink;
