c2c548d5fbed6b5db456cb2be16edc20960f1bb2
Diego Mello <diegolmello@gmail.com>
2018-03-07 13:52:59 -0300

    Reactotron working

--- app/ReactotronConfig.js
+++ app/ReactotronConfig.js
@@ -1,20 +1,13 @@
 /* eslint-disable */
-import Reactotron, { asyncStorage, networking, openInEditor } from 'reactotron-react-native';
-import { NativeModules } from 'react-native';
-
+import Reactotron from 'reactotron-react-native';
 import { reactotronRedux } from 'reactotron-redux';
+import sagaPlugin from 'reactotron-redux-saga'
 
-// uncomment the following lines to test on a device
-// let scriptHostname;
-// if (__DEV__) {
-//   const scriptURL = NativeModules.SourceCode.scriptURL;
-//   scriptHostname = scriptURL.split('://')[1].split(':')[0];
-// }
-
-Reactotron.configure(/*{ host: scriptHostname }*/) // controls connection & communication settings
-  .useReactNative() // add all built-in react native plugins
-  .use(reactotronRedux())
-  .use(asyncStorage())
-  .use(networking())
-  .use(openInEditor())
-  .connect();
+if (__DEV__) {
+    Reactotron
+        .configure()
+        .useReactNative()
+        .use(reactotronRedux())
+        .use(sagaPlugin())
+        .connect();
+}
