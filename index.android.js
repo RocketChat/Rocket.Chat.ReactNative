012063e41aabed9f8e2dc8bce4625b0af37441d4
Diego Mello <yiweigao0226@gmail.com>
2018-03-23 12:55:40 -0400

    Reactotron working (#249)

--- index.android.js
+++ index.android.js
@@ -2,13 +2,10 @@ import 'babel-polyfill';
 import 'regenerator-runtime/runtime';
 import { AppRegistry, UIManager } from 'react-native';
 
+import './app/ReactotronConfig';
 import './app/push';
 import RocketChat from './app/index';
 
 UIManager.setLayoutAnimationEnabledExperimental(true);
 
-// import './app/ReactotronConfig';
-// import { AppRegistry } from 'react-native';
-// import Routes from './app/routes';
-//
 AppRegistry.registerComponent('RocketChatRN', () => RocketChat);
