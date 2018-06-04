9bee772bdb9c849ef9aa04c0a80ec365b8eba102
Diego Mello <diegolmello@gmail.com>
2018-06-04 09:58:58 -0300

    Drawer layout

--- app/containers/Sidebar.js
+++ app/containers/Sidebar.js
@@ -1,44 +1,83 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { ScrollView, Text, View, StyleSheet, FlatList, TouchableHighlight } from 'react-native';
+import { ScrollView, Text, View, StyleSheet, FlatList, LayoutAnimation } from 'react-native';
 import { connect } from 'react-redux';
-import { DrawerActions } from 'react-navigation';
+import { DrawerActions, SafeAreaView } from 'react-navigation';
+import FastImage from 'react-native-fast-image';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import database from '../lib/realm';
 import { setServer } from '../actions/server';
 import { logout } from '../actions/login';
+import Avatar from '../containers/Avatar';
+import Status from '../containers/status';
+import Touch from '../utils/touch';
+import { STATUS_COLORS } from '../constants/colors';
+import RocketChat from '../lib/rocketchat';
+import log from '../utils/log';
 
 const styles = StyleSheet.create({
-	scrollView: {
-		paddingTop: 20
+	selected: {
+		backgroundColor: 'rgba(0, 0, 0, .04)'
 	},
-	imageContainer: {
-		width: '100%',
+	item: {
+		flexDirection: 'row',
 		alignItems: 'center'
 	},
-	image: {
-		width: 200,
-		height: 200,
-		borderRadius: 100
+	itemLeft: {
+		marginHorizontal: 16,
+		width: 24,
+		alignItems: 'center'
 	},
-	serverTitle: {
-		fontSize: 16,
-		color: 'grey',
-		padding: 10,
-		width: '100%'
+	itemLeftOpacity: {
+		opacity: 0.62
 	},
-	serverItem: {
-		backgroundColor: 'white',
-		padding: 10,
-		flex: 1
+	itemText: {
+		margin: 16,
+		fontWeight: 'bold',
+		color: '#292E35'
 	},
-	selectedServer: {
-		backgroundColor: '#eeeeee'
+	separator: {
+		borderBottomWidth: StyleSheet.hairlineWidth,
+		borderColor: '#ddd',
+		marginVertical: 4
+	},
+	serverImage: {
+		width: 24,
+		height: 24,
+		borderRadius: 4
+	},
+	header: {
+		borderBottomWidth: StyleSheet.hairlineWidth,
+		borderColor: '#ddd',
+		marginBottom: 4,
+		paddingVertical: 16,
+		flexDirection: 'row',
+		alignItems: 'center'
+	},
+	headerTextContainer: {
+		flex: 1,
+		flexDirection: 'column',
+		alignItems: 'flex-start'
+	},
+	headerUsername: {
+		flexDirection: 'row',
+		alignItems: 'center'
+	},
+	avatar: {
+		marginHorizontal: 10
+	},
+	status: {
+		borderRadius: 12,
+		width: 12,
+		height: 12,
+		marginRight: 5
 	}
 });
 const keyExtractor = item => item.id;
 @connect(state => ({
-	server: state.server.server
+	server: state.server.server,
+	user: state.login.user
 }), dispatch => ({
 	selectServer: server => dispatch(setServer(server)),
 	logout: () => dispatch(logout())
@@ -53,7 +92,23 @@ export default class Sidebar extends Component {
 
 	constructor(props) {
 		super(props);
-		this.state = { servers: [] };
+		this.state = {
+			servers: [],
+			status: [{
+				id: 'online',
+				name: 'Online'
+			}, {
+				id: 'busy',
+				name: 'Busy'
+			}, {
+				id: 'away',
+				name: 'Away'
+			}, {
+				id: 'offline',
+				name: 'Invisible'
+			}],
+			showServers: false
+		};
 	}
 
 	componentDidMount() {
@@ -82,58 +137,178 @@ export default class Sidebar extends Component {
 		this.props.navigation.dispatch(DrawerActions.closeDrawer());
 	}
 
-	renderItem = ({ item, separators }) => (
+	toggleServers = () => {
+		LayoutAnimation.easeInEaseOut();
+		this.setState({ showServers: !this.state.showServers });
+	}
 
-		<TouchableHighlight
-			onShowUnderlay={separators.highlight}
-			onHideUnderlay={separators.unhighlight}
-			onPress={() => { this.onPressItem(item); }}
-			testID={`sidebar-${ item.id }`}
+	isRouteFocused = (route) => {
+		const { state } = this.props.navigation;
+		const activeItemKey = state.routes[state.index] ? state.routes[state.index].key : null;
+		return activeItemKey === route;
+	}
+
+	sidebarNavigate = (route) => {
+		const { navigate } = this.props.navigation;
+		if (!this.isRouteFocused(route)) {
+			navigate(route);
+		}
+	}
+
+	renderSeparator = key => <View key={key} style={styles.separator} />;
+
+	renderItem = ({
+		text, left, selected, onPress
+	}) => (
+		<Touch
+			key={text}
+			onPress={onPress}
+			underlayColor='rgba(255, 255, 255, 0.5)'
+			activeOpacity={0.3}
 		>
-			<View style={[styles.serverItem, (item.id === this.props.server ? styles.selectedServer : null)]}>
-				<Text>
-					{item.id}
+			<View style={[styles.item, selected && styles.selected]}>
+				<View style={[styles.itemLeft, !selected && styles.itemLeftOpacity]}>
+					{left}
+				</View>
+				<Text style={styles.itemText}>
+					{text}
 				</Text>
 			</View>
-		</TouchableHighlight>
-	);
+		</Touch>
+	)
+
+	renderStatusItem = ({ item }) => (
+		this.renderItem({
+			text: item.name,
+			left: <View style={[styles.status, { backgroundColor: STATUS_COLORS[item.id] }]} />,
+			selected: this.props.user.status === item.id,
+			onPress: () => {
+				if (this.props.user.status === item.id) {
+					return this.closeDrawer();
+				}
+				try {
+					RocketChat.setUserPresenceDefaultStatus(item.id);
+				} catch (e) {
+					log('onPressModalButton', e);
+				}
+			}
+		})
+	)
+
+	renderServer = ({ item }) => (
+		this.renderItem({
+			text: item.id,
+			left: <FastImage
+				style={styles.serverImage}
+				source={{ uri: encodeURI(`${ item.id }/assets/favicon_32.png`) }}
+			/>,
+			selected: this.props.server === item.id,
+			onPress: () => {
+				if (this.props.server === item.id) {
+					return this.closeDrawer();
+				}
+				this.props.selectServer(item.id);
+			}
+		})
+	)
+
+	renderNavigation = () => (
+		[
+			this.renderItem({
+				text: 'Chats',
+				left: <Icon name='chat-bubble' size={20} />,
+				onPress: () => this.sidebarNavigate('Chats'),
+				selected: this.isRouteFocused('Chats')
+			}),
+			this.renderItem({
+				text: 'Profile',
+				left: <Icon name='person' size={20} />,
+				onPress: () => this.sidebarNavigate('ProfileView'),
+				selected: this.isRouteFocused('ProfileView')
+			}),
+			this.renderItem({
+				text: 'Settings',
+				left: <Icon name='settings' size={20} />,
+				onPress: () => this.sidebarNavigate('SettingsView'),
+				selected: this.isRouteFocused('SettingsView')
+			}),
+			this.renderSeparator('separator-logout'),
+			this.renderItem({
+				text: 'Logout',
+				left: <Icon
+					name='exit-to-app'
+					size={20}
+				/>,
+				onPress: () => this.props.logout()
+			})
+		]
+	)
+
+	renderServers = () => (
+		[
+			<FlatList
+				key='status-list'
+				data={this.state.status}
+				extraData={this.props.user}
+				renderItem={this.renderStatusItem}
+				keyExtractor={keyExtractor}
+			/>,
+			this.renderSeparator('separator-status'),
+			<FlatList
+				key='servers-list'
+				data={this.state.servers}
+				extraData={this.props.server}
+				renderItem={this.renderServer}
+				keyExtractor={keyExtractor}
+			/>,
+			this.renderSeparator('separator-add-server'),
+			this.renderItem({
+				text: 'Add Server',
+				left: <Icon
+					name='add'
+					size={20}
+				/>,
+				onPress: () => {
+					this.closeDrawer();
+					this.props.navigation.navigate({ key: 'AddServer', routeName: 'AddServer' });
+				}
+			})
+		]
+	)
 
 	render() {
+		const { user, server } = this.props;
 		return (
-			<ScrollView style={styles.scrollView}>
-				<View style={{ paddingBottom: 20 }} testID='sidebar'>
-					<FlatList
-						data={this.state.servers}
-						renderItem={this.renderItem}
-						keyExtractor={keyExtractor}
-					/>
-					<TouchableHighlight
-						onPress={() => {
-							this.closeDrawer();
-							this.props.logout();
-						}}
-						testID='sidebar-logout'
+			<ScrollView>
+				<SafeAreaView style={styles.container} forceInset={{ top: 'always', horizontal: 'never' }}>
+					<Touch
+						onPress={() => this.toggleServers()}
+						underlayColor='rgba(255, 255, 255, 0.5)'
+						activeOpacity={0.3}
 					>
-						<View style={styles.serverItem}>
-							<Text>
-								Logout
-							</Text>
+						<View style={styles.header}>
+							<Avatar
+								text={user.username}
+								size={30}
+								style={styles.avatar}
+							/>
+							<View style={styles.headerTextContainer}>
+								<View style={styles.headerUsername}>
+									<Status style={styles.status} id={user.id} />
+									<Text>{user.username}</Text>
+								</View>
+								<Text style={{ fontWeight: 'bold' }}>{server}</Text>
+							</View>
+							<Icon
+								name={this.state.showServers ? 'keyboard-arrow-up' : 'keyboard-arrow-down'}
+								size={30}
+							/>
 						</View>
-					</TouchableHighlight>
-					<TouchableHighlight
-						onPress={() => {
-							this.closeDrawer();
-							this.props.navigation.navigate({ key: 'AddServer', routeName: 'AddServer' });
-						}}
-						testID='sidebar-add-server'
-					>
-						<View style={styles.serverItem}>
-							<Text>
-								Add Server
-							</Text>
-						</View>
-					</TouchableHighlight>
-				</View>
+					</Touch>
+
+					{!this.state.showServers && this.renderNavigation()}
+					{this.state.showServers && this.renderServers()}
+				</SafeAreaView>
 			</ScrollView>
 		);
 	}
