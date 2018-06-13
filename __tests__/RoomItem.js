fa5e908a32154ff4f4e98ff19d96b08159f88639
Diego Mello <diegolmello@gmail.com>
2018-06-12 22:29:18 -0300

    Ddp fixes (#324)

--- __tests__/RoomItem.js
+++ __tests__/RoomItem.js
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux';
 
 import { createStore, combineReducers } from 'redux';
 
-const reducers = combineReducers({login:() => ({user: {}}), settings:() => ({})});
+const reducers = combineReducers({login:() => ({user: {}}), settings:() => ({}), meteor: () => ({ connected: true })});
 const store = createStore(reducers);
 
 import React from 'react';
