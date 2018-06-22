acf68db5ff018298ed4612c19be2805a39b80d3d
Diego Mello <diegolmello@gmail.com>
2018-06-22 17:05:38 -0300

    Polyfill

--- index.android.js
+++ index.android.js
@@ -1,5 +1,5 @@
-// import 'babel-polyfill';
-// import 'regenerator-runtime/runtime';
+import 'babel-polyfill';
+import 'regenerator-runtime/runtime';
 // import { AppRegistry, UIManager } from 'react-native';
 
 import './app/ReactotronConfig';
