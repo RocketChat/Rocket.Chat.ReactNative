e3db0602cd33e7a6f6a8b3aa3e54f8198e53ec39
diegolmello <diegolmello@gmail.com>
2018-09-21 10:37:59 -0300

    [NEW] Portuguese translation

--- app/i18n/index.js
+++ app/i18n/index.js
@@ -1,11 +1,12 @@
 import I18n from 'react-native-i18n';
 import en from './locales/en';
 import ru from './locales/ru';
+import pt from './locales/pt-BR';
 
 I18n.fallbacks = true;
 
 I18n.translations = {
-	en, ru
+	en, ru, pt
 };
 
 export default I18n;
