21847195ffd4f813cfacae85637b15bff0390d2b
Diego Mello <diegolmello@gmail.com>
2018-10-26 10:24:00 -0300

    Check if lastMessage has an attachment and show "User sent an attachment" at RoomsList (#510)

--- app/presentation/RoomItem.js
+++ app/presentation/RoomItem.js
@@ -189,8 +189,17 @@ export default class RoomItem extends React.Component {
 		}
 
 		let prefix = '';
+		const me = lastMessage.u.username === username;
 
-		if (lastMessage.u.username === username) {
+		if (!lastMessage.msg && Object.keys(lastMessage.attachments).length > 0) {
+			if (me) {
+				return I18n.t('User_sent_an_attachment', { user: I18n.t('You') });
+			} else {
+				return I18n.t('User_sent_an_attachment', { user: lastMessage.u.username });
+			}
+		}
+
+		if (me) {
 			prefix = I18n.t('You_colon');
 		}	else if (type !== 'd') {
 			prefix = `${ lastMessage.u.username }: `;
