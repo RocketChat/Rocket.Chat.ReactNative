2692f5222d692376a8fc764722eb396a2587e312
Diego Mello <diegolmello@gmail.com>
2018-11-19 16:18:15 -0200

    [FIX] Upload buttons on Android (#541)

--- app/containers/MessageBox/UploadModal.js
+++ app/containers/MessageBox/UploadModal.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {
-	View, Text, StyleSheet, Image, ScrollView, Platform
+	View, Text, StyleSheet, Image, ScrollView, Platform, TouchableHighlight
 } from 'react-native';
 import PropTypes from 'prop-types';
 import Modal from 'react-native-modal';
@@ -10,20 +10,24 @@ import equal from 'deep-equal';
 import TextInput from '../TextInput';
 import Button from '../Button';
 import I18n from '../../i18n';
+import sharedStyles from '../../views/Styles';
 
 const cancelButtonColor = '#f7f8fa';
 
 const styles = StyleSheet.create({
+	modal: {
+		alignItems: 'center'
+	},
 	titleContainer: {
 		flexDirection: 'row',
 		paddingHorizontal: 16,
 		paddingTop: 16
 	},
 	title: {
-		fontWeight: 'bold'
+		...sharedStyles.textBold
 	},
 	container: {
-		height: Platform.OS === 'ios' ? 404 : 430,
+		height: 430,
 		backgroundColor: '#ffffff',
 		flexDirection: 'column'
 	},
@@ -43,9 +47,20 @@ const styles = StyleSheet.create({
 		padding: 16,
 		backgroundColor: '#f7f8fa'
 	},
-	buttonMargin: {
-		margin: 0
+	button: {
+		marginBottom: 0
+	},
+	androidButton: {
+		paddingHorizontal: 15,
+		justifyContent: 'center',
+		height: 48,
+		borderRadius: 2
+	},
+	androidButtonText: {
+		fontSize: 18,
+		textAlign: 'center'
 	}
+
 });
 
 @responsive
@@ -75,10 +90,54 @@ export default class UploadModal extends Component {
 		return null;
 	}
 
-	_submit = () => {
+	submit = () => {
 		const { file, submit } = this.props;
 		const { name, description } = this.state;
 		submit({ ...file, name, description });
+	}
+
+	renderButtons = () => {
+		const { close } = this.props;
+		if (Platform.OS === 'ios') {
+			return (
+				<View style={styles.buttonContainer}>
+					<Button
+						title={I18n.t('Cancel')}
+						type='secondary'
+						backgroundColor={cancelButtonColor}
+						style={styles.button}
+						onPress={close}
+					/>
+					<Button
+						title={I18n.t('Send')}
+						type='primary'
+						style={styles.button}
+						onPress={this.submit}
+					/>
+				</View>
+			);
+		}
+		// FIXME: RNGH don't work well on Android modals: https://github.com/kmagiera/react-native-gesture-handler/issues/139
+		return (
+			<View style={styles.buttonContainer}>
+				<TouchableHighlight
+					onPress={close}
+					style={[styles.androidButton, { backgroundColor: cancelButtonColor }]}
+					underlayColor={cancelButtonColor}
+					activeOpacity={0.5}
+				>
+					<Text style={[styles.androidButtonText, { ...sharedStyles.textBold, color: '#1d74f5' }]}>{I18n.t('Cancel')}</Text>
+				</TouchableHighlight>
+				<TouchableHighlight
+					onPress={this.submit}
+					style={[styles.androidButton, { backgroundColor: '#1d74f5' }]}
+					underlayColor='#1d74f5'
+					activeOpacity={0.5}
+				>
+					<Text style={[styles.androidButtonText, { ...sharedStyles.textMedium, color: '#fff' }]}>{I18n.t('Send')}</Text>
+				</TouchableHighlight>
+			</View>
+		);
 	}
 
 	render() {
@@ -87,9 +146,9 @@ export default class UploadModal extends Component {
 		return (
 			<Modal
 				isVisible={isVisible}
-				style={{ alignItems: 'center' }}
-				onBackdropPress={() => close()}
-				onBackButtonPress={() => close()}
+				style={styles.modal}
+				onBackdropPress={close}
+				onBackButtonPress={close}
 				animationIn='fadeIn'
 				animationOut='fadeOut'
 				useNativeDriver
@@ -97,7 +156,7 @@ export default class UploadModal extends Component {
 			>
 				<View style={[styles.container, { width: width - 32 }]}>
 					<View style={styles.titleContainer}>
-						<Text style={styles.title}>Upload file?</Text>
+						<Text style={styles.title}>{I18n.t('Upload_file_question_mark')}</Text>
 					</View>
 
 					<ScrollView style={styles.scrollView}>
@@ -113,21 +172,7 @@ export default class UploadModal extends Component {
 							onChangeText={value => this.setState({ description: value })}
 						/>
 					</ScrollView>
-					<View style={styles.buttonContainer}>
-						<Button
-							title={I18n.t('Cancel')}
-							type='secondary'
-							backgroundColor={cancelButtonColor}
-							margin={styles.buttonMargin}
-							onPress={close}
-						/>
-						<Button
-							title={I18n.t('Send')}
-							type='primary'
-							margin={styles.buttonMargin}
-							onPress={this._submit}
-						/>
-					</View>
+					{this.renderButtons()}
 				</View>
 			</Modal>
 		);
