3f3ee544225a35bc73b7c972eb6fde41fba5ae21
diegolmello <diegolmello@gmail.com>
2018-12-03 10:54:17 -0200

    Sort prefs

--- app/sagas/init.js
+++ app/sagas/init.js
@@ -3,12 +3,10 @@ import { put, takeLatest, all } from 'redux-saga/effects';
 
 import * as actions from '../actions';
 import { selectServerRequest } from '../actions/server';
-import { restoreToken, setUser } from '../actions/login';
 import { setAllPreferences } from '../actions/sortPreferences';
 import { APP } from '../actions/actionsTypes';
 import RocketChat from '../lib/rocketchat';
 import log from '../utils/log';
-import I18n from '../i18n';
 
 const restore = function* restore() {
 	try {
@@ -24,26 +22,11 @@ const restore = function* restore() {
 		}
 
 		yield put(selectServerRequest(server));
-
-		// const userStringified = yield AsyncStorage.getItem(`${ RocketChat.TOKEN_KEY }-${ server }`);
-		// if (userStringified) {
-		// 	const user = JSON.parse(userStringified);
-		// 	yield put(restoreToken(token));
-		// 	// yield put(setUser(JSON.parse(user)));
-		// 	yield put(actions.appStart('inside'));
-		// 	yield put(selectServerRequest(server));
-		// 	// RocketChat.start({ server, token, user });
-		// 	// const sortPreferences = yield RocketChat.getSortPreferences();
-		// 	// yield put(setAllPreferences(sortPreferences));
-		// } else {
-		// 	yield put(actions.appStart('outside'));
-		// 	yield RocketChat.clearAsyncStorage(server);
-		// }
-
+		const sortPreferences = yield RocketChat.getSortPreferences();
+		yield put(setAllPreferences(sortPreferences));
 		yield put(actions.appReady({}));
 	} catch (e) {
-		alert(e)
-		// log('restore', e);
+		log('restore', e);
 	}
 };
 
