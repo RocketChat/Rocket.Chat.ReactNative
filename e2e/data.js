fc2e5a385a855b1bc79d5a8accf78c0a071a4b8d
diegolmello <diegolmello@gmail.com>
2019-01-24 16:54:51 -0200

    Update to RN 0.57.7

--- e2e/data.js
+++ e2e/data.js
@@ -1,13 +1,13 @@
 const random = require('./helpers/random');
 const value = random(20);
 const data = {
-    server: 'https://stable.rocket.chat',
+    server: 'http://localhost:3000',
     alternateServer: 'https://unstable.rocket.chat',
     user: `user${ value }`,
     password: `password${ value }`,
     alternateUser: 'detox',
     alternateUserPassword: '123',
-    alternateUserTOTPSecret: 'HJGECLDOH5RCKJSWMREXAKKENVZXKOJ6I5ZTKPSRIEQWGOK5K5KA',
+    alternateUserTOTPSecret: 'KFJW6SZMH5EUI5LHPJ2XCOKKGRHDA2ZDN5YD4YLBMMSSMVCEPJSQ',
     email: `diego.mello+e2e${ value }@rocket.chat`,
     random: value
 }
