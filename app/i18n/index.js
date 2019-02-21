3e1117d66b4763652ce907f141e9ac3755b0103e
AnBo83 <46653766+AnBo83@users.noreply.github.com>
2019-02-21 16:08:53 +0100

    Update index.js

--- app/i18n/index.js
+++ app/i18n/index.js
@@ -2,6 +2,7 @@ import I18n from 'react-native-i18n';
 import en from './locales/en';
 import ru from './locales/ru';
 import fr from './locales/fr';
+import de from './locales/de';
 import ptBR from './locales/pt-BR';
 import zhCN from './locales/zh-CN';
 
@@ -9,7 +10,7 @@ I18n.fallbacks = true;
 I18n.defaultLocale = 'en';
 
 I18n.translations = {
-	en, ru, 'pt-BR': ptBR, 'zh-CN': zhCN, fr
+	en, ru, 'pt-BR': ptBR, 'zh-CN': zhCN, fr, de
 };
 
 export default I18n;
