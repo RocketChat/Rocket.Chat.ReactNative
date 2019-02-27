e2ab7a0da78bdb15af467e4c2b69ed3b539a6fd2
diegolmello <diegolmello@gmail.com>
2019-02-27 16:41:41 -0300

    [FIX] Fetch rooms date

--- app/lib/methods/getRooms.js
+++ app/lib/methods/getRooms.js
@@ -1,17 +1,8 @@
-import database from '../realm';
-
-const lastMessage = () => {
-	const message = database
-		.objects('subscriptions')
-		.sorted('roomUpdatedAt', true)[0];
-	return message && new Date(message.roomUpdatedAt).toISOString();
-};
-
-export default function() {
-	const updatedSince = lastMessage();
+export default function(updatedSince) {
 	// subscriptions.get: Since RC 0.60.0
 	// rooms.get: Since RC 0.62.0
 	if (updatedSince) {
+		updatedSince = updatedSince.toISOString();
 		return Promise.all([this.sdk.get('subscriptions.get', { updatedSince }), this.sdk.get('rooms.get', { updatedSince })]);
 	}
 	return Promise.all([this.sdk.get('subscriptions.get'), this.sdk.get('rooms.get')]);
