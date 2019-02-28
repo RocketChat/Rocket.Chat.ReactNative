b6ab62ba417aad6c4999a506e7453a0b7161f8d4
Diego Mello <yiweigao0226@gmail.com>
2019-02-28 15:03:26 -0300

    [FIX] Select emoji error (#666)

--- app/containers/MessageBox/index.js
+++ app/containers/MessageBox/index.js
@@ -216,7 +216,7 @@ export default class MessageBox extends Component {
 			newText = `${ text }${ emoji }`;
 		}
 		this.setInput(newText);
-		this.showSend(true);
+		this.setShowSend(true);
 	}
 
 	get leftButtons() {
