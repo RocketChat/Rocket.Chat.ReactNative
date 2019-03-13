5f0a58496b9a885cb5cdf25541a0da59edf7965b
diegolmello <diegolmello@gmail.com>
2019-03-13 13:46:10 -0300

    update tests

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	presets: ['module:metro-react-native-babel-preset'],
+	plugins: [['@babel/plugin-proposal-decorators', { legacy: true }]],
+	env: {
+		production: {
+			plugins: ['transform-remove-console']
+		}
+	}
+};
