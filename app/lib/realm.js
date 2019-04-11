81d97015f32c0d97f333c6d2fd88cdfbd6fc50fc
diegolmello <diegolmello@gmail.com>
2019-04-11 15:36:55 -0300

    Revert "Refactor realm"
    This reverts commit ce0f07d43b0efce60a9e8c2d0eb248ebde42090d.

--- app/lib/realm.js
+++ app/lib/realm.js
@@ -197,51 +197,72 @@ const messagesEditedBySchema = {
 	}
 };
 
-// We reuse this object on threads
-const messageObj = {
-	_id: 'string',
-	msg: { type: 'string', optional: true },
-	t: { type: 'string', optional: true },
-	rid: { type: 'string', indexed: true },
-	ts: 'date',
-	u: 'users',
-	alias: { type: 'string', optional: true },
-	parseUrls: { type: 'bool', optional: true },
-	groupable: { type: 'bool', optional: true },
-	avatar: { type: 'string', optional: true },
-	attachments: { type: 'list', objectType: 'attachment' },
-	urls: { type: 'list', objectType: 'url', default: [] },
-	_updatedAt: { type: 'date', optional: true },
-	status: { type: 'int', optional: true },
-	pinned: { type: 'bool', optional: true },
-	starred: { type: 'bool', optional: true },
-	editedBy: 'messagesEditedBy',
-	reactions: { type: 'list', objectType: 'messagesReactions' },
-	role: { type: 'string', optional: true },
-	drid: { type: 'string', optional: true },
-	dcount: { type: 'int', optional: true },
-	dlm: { type: 'date', optional: true },
-	tmid: { type: 'string', optional: true },
-	tcount: { type: 'int', optional: true },
-	tlm: { type: 'date', optional: true }
-};
-
 const messagesSchema = {
 	name: 'messages',
 	primaryKey: '_id',
-	properties: messageObj
+	properties: {
+		_id: 'string',
+		msg: { type: 'string', optional: true },
+		t: { type: 'string', optional: true },
+		rid: { type: 'string', indexed: true },
+		ts: 'date',
+		u: 'users',
+		alias: { type: 'string', optional: true },
+		parseUrls: { type: 'bool', optional: true },
+		groupable: { type: 'bool', optional: true },
+		avatar: { type: 'string', optional: true },
+		attachments: { type: 'list', objectType: 'attachment' },
+		urls: { type: 'list', objectType: 'url', default: [] },
+		_updatedAt: { type: 'date', optional: true },
+		status: { type: 'int', optional: true },
+		pinned: { type: 'bool', optional: true },
+		starred: { type: 'bool', optional: true },
+		editedBy: 'messagesEditedBy',
+		reactions: { type: 'list', objectType: 'messagesReactions' },
+		role: { type: 'string', optional: true },
+		drid: { type: 'string', optional: true },
+		dcount: { type: 'int', optional: true },
+		dlm: { type: 'date', optional: true },
+		tmid: { type: 'string', optional: true },
+		tcount: { type: 'int', optional: true },
+		tlm: { type: 'date', optional: true }
+	}
 };
 
 const threadsSchema = {
 	name: 'threads',
 	primaryKey: '_id',
-	properties: messageObj
+	properties: {
+		_id: 'string',
+		msg: { type: 'string', optional: true },
+		rid: { type: 'string', indexed: true }
+	}
 };
 
 const threadMessagesSchema = {
 	name: 'threadMessages',
 	primaryKey: '_id',
-	properties: messageObj
+	properties: {
+		_id: 'string',
+		msg: { type: 'string', optional: true },
+		t: { type: 'string', optional: true },
+		rid: { type: 'string', indexed: true },
+		ts: 'date',
+		u: 'users',
+		alias: { type: 'string', optional: true },
+		parseUrls: { type: 'bool', optional: true },
+		groupable: { type: 'bool', optional: true },
+		avatar: { type: 'string', optional: true },
+		attachments: { type: 'list', objectType: 'attachment' },
+		urls: { type: 'list', objectType: 'url', default: [] },
+		_updatedAt: { type: 'date', optional: true },
+		status: { type: 'int', optional: true },
+		pinned: { type: 'bool', optional: true },
+		starred: { type: 'bool', optional: true },
+		editedBy: 'messagesEditedBy',
+		reactions: { type: 'list', objectType: 'messagesReactions' },
+		role: { type: 'string', optional: true }
+	}
 };
 
 const frequentlyUsedEmojiSchema = {
