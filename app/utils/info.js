0715f20ddac1b057c99b9d49f552fbc730e521a8
Pranav Pandey <pranavpandey1998official@gmail.com>
2019-04-24 11:52:45 +0530

    fix lint

--- app/utils/info.js
+++ app/utils/info.js
@@ -1,5 +1,5 @@
 import { Alert } from 'react-native';
 
-export const showAlert = (message: string) => Alert.alert( null, message, [{ text: 'OK', onPress: () => {} }], { cancelable: true });
+export const showAlert = (message: string) => Alert.alert(null, message, [{ text: 'OK', onPress: () => {} }], { cancelable: true });
 
 export const showErrorAlert = (message: string, title: string) => Alert.alert(title, message, [{ text: 'OK', onPress: () => {} }], { cancelable: true });
