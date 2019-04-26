30dba5e3ff2a133503a05cbcf4b76ee498fbc959
diegolmello <diegolmello@gmail.com>
2019-04-26 09:03:51 -0300

    [Regression] Search stopped working on Android after LastMessage refactor

--- app/views/RoomsListView/index.js
+++ app/views/RoomsListView/index.js
@@ -402,7 +402,7 @@ export default class RoomsListView extends LoggedView {
 					unread={item.unread}
 					userMentions={item.userMentions}
 					favorite={item.f}
-					lastMessage={JSON.parse(JSON.stringify(item.lastMessage))}
+					lastMessage={item.lastMessage ? JSON.parse(JSON.stringify(item.lastMessage)) : null}
 					name={this.getRoomTitle(item)}
 					_updatedAt={item.roomUpdatedAt}
 					key={item._id}
