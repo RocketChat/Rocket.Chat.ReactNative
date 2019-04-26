05bb8a54758d3bd9a50668d7760cf0bfbb5f66ea
diegolmello <diegolmello@gmail.com>
2019-04-26 09:56:41 -0300

    Support RC versions lower than 0.75.0

--- app/lib/methods/getCustomEmojis.js
+++ app/lib/methods/getCustomEmojis.js
@@ -1,4 +1,5 @@
 import { InteractionManager } from 'react-native';
+import semver from 'semver';
 
 import reduxStore from '../createStore';
 import database from '../realm';
@@ -10,51 +11,73 @@ const getUpdatedSince = () => {
 	return emoji && emoji._updatedAt.toISOString();
 };
 
+const create = (customEmojis) => {
+	if (customEmojis && customEmojis.length) {
+		customEmojis.forEach((emoji) => {
+			try {
+				database.create('customEmojis', emoji, true);
+			} catch (e) {
+				log('getEmojis create', e);
+			}
+		});
+	}
+};
+
+
 export default async function() {
 	try {
-		const params = {};
+		const serverVersion = reduxStore.getState().server.version;
 		const updatedSince = getUpdatedSince();
-		if (updatedSince) {
-			params.updatedSince = updatedSince;
-		}
 
-		// RC 0.75.0
-		const result = await this.sdk.get('emoji-custom.list', params);
+		// if server version is lower than 0.75.0, fetches from old api
+		if (semver.lt(serverVersion, '0.75.0')) {
+			// RC 0.61.0
+			const result = await this.sdk.get('emoji-custom');
 
-		if (!result.success) {
-			return;
-		}
+			InteractionManager.runAfterInteractions(() => {
+				let { emojis } = result;
+				emojis = emojis.filter(emoji => !updatedSince || emoji._updatedAt > updatedSince);
+				database.write(() => {
+					create(emojis);
+				});
+				reduxStore.dispatch(actions.setCustomEmojis(this.parseEmojis(result.emojis)));
+			});
+		} else {
+			const params = {};
+			if (updatedSince) {
+				params.updatedSince = updatedSince;
+			}
 
-		InteractionManager.runAfterInteractions(
-			() => database.write(() => {
-				const { emojis } = result;
-				if (emojis.update && emojis.update.length) {
-					emojis.update.forEach((emoji) => {
-						try {
-							database.create('customEmojis', emoji, true);
-						} catch (e) {
-							log('getEmojis create', e);
-						}
-					});
-				}
+			// RC 0.75.0
+			const result = await this.sdk.get('emoji-custom.list', params);
 
-				if (emojis.delete && emojis.delete.length) {
-					emojis.delete.forEach((emoji) => {
-						try {
-							const emojiRecord = database.objectForPrimaryKey('customEmojis', emoji._id);
-							if (emojiRecord) {
-								database.delete(emojiRecord);
+			if (!result.success) {
+				return;
+			}
+
+			InteractionManager.runAfterInteractions(
+				() => database.write(() => {
+					const { emojis } = result;
+					create(emojis.update);
+
+					if (emojis.delete && emojis.delete.length) {
+						emojis.delete.forEach((emoji) => {
+							try {
+								const emojiRecord = database.objectForPrimaryKey('customEmojis', emoji._id);
+								if (emojiRecord) {
+									database.delete(emojiRecord);
+								}
+							} catch (e) {
+								log('getEmojis delete', e);
 							}
-						} catch (e) {
-							log('getEmojis delete', e);
-						}
-					});
-				}
+						});
+					}
 
-				const allEmojis = database.objects('customEmojis');
-				reduxStore.dispatch(actions.setCustomEmojis(this.parseEmojis(allEmojis)));
-			})
-		);
+					const allEmojis = database.objects('customEmojis');
+					reduxStore.dispatch(actions.setCustomEmojis(this.parseEmojis(allEmojis)));
+				})
+			);
+		}
 	} catch (e) {
 		log('getCustomEmojis', e);
 	}
