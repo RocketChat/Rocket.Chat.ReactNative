e4fcf4273671c5f988560b5ef603466857736f52
diegolmello <diegolmello@gmail.com>
2019-04-30 09:51:22 -0300

    Remove thread listener from RightButtons

--- app/views/RoomView/Header/RightButtons.js
+++ app/views/RoomView/Header/RightButtons.js
@@ -47,6 +47,12 @@ class RightButtonsContainer extends React.PureComponent {
 		};
 	}
 
+	componentWillUnmount() {
+		if (this.thread && this.thread.removeAllListeners) {
+			this.thread.removeAllListeners();
+		}
+	}
+
 	updateThread = () => {
 		const { userId } = this.props;
 		this.setState({
