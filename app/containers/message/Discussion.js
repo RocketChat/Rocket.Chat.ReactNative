b16e227a30442694859ba5b84e3a20227c42a88a
diegolmello <diegolmello@gmail.com>
2019-05-07 10:35:26 -0300

    Discussion rerender

--- app/containers/message/Discussion.js
+++ app/containers/message/Discussion.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import Touchable from 'react-native-platform-touchable';
+import PropTypes from 'prop-types';
 
 import { formatLastMessage, formatMessageCount, BUTTON_HIT_SLOP } from './utils';
 import styles from './styles';
@@ -32,6 +33,24 @@ const Discussion = React.memo(({
 			</View>
 		</React.Fragment>
 	);
+}, (prevProps, nextProps) => {
+	if (prevProps.msg !== nextProps.msg) {
+		return false;
+	}
+	if (prevProps.dount !== nextProps.dount) {
+		return false;
+	}
+	if (prevProps.dlm !== nextProps.dlm) {
+		return false;
+	}
+	return true;
 });
+
+Discussion.propTypes = {
+	msg: PropTypes.string,
+	dcount: PropTypes.number,
+	dlm: PropTypes.string,
+	onDiscussionPress: PropTypes.func
+};
 
 export default Discussion;
