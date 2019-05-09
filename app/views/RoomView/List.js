e32c7823319ed20d4e3c014d50993cc0101f3b22
diegolmello <diegolmello@gmail.com>
2019-05-09 17:59:22 -0300

    EmptyRoom

--- app/views/RoomView/List.js
+++ app/views/RoomView/List.js
@@ -134,50 +134,28 @@ export class List extends React.PureComponent {
 		console.count(`${ this.constructor.name }.render calls`);
 		const { messages } = this.state;
 		return (
-			<FlatList
-				testID='room-view-messages'
-				ref={ref => this.list = ref}
-				keyExtractor={item => item._id}
-				data={messages}
-				extraData={this.state}
-				renderItem={this.renderItem}
-				contentContainerStyle={styles.contentContainer}
-				style={styles.list}
-				inverted
-				removeClippedSubviews
-				initialNumToRender={20}
-				onEndReached={this.onEndReached}
-				onEndReachedThreshold={0.5}
-				// maxToRenderPerBatch={5}
-				windowSize={21}
-				ListFooterComponent={this.renderFooter}
-				// legacyImplementation
-				{...scrollPersistTaps}
-			/>
+			<React.Fragment>
+				<EmptyRoom length={messages.length} />
+				<FlatList
+					testID='room-view-messages'
+					ref={ref => this.list = ref}
+					keyExtractor={item => item._id}
+					data={messages}
+					extraData={this.state}
+					renderItem={this.renderItem}
+					contentContainerStyle={styles.contentContainer}
+					style={styles.list}
+					inverted
+					removeClippedSubviews
+					initialNumToRender={20}
+					onEndReached={this.onEndReached}
+					onEndReachedThreshold={0.5}
+					maxToRenderPerBatch={5}
+					windowSize={21}
+					ListFooterComponent={this.renderFooter}
+					{...scrollPersistTaps}
+				/>
+			</React.Fragment>
 		);
-		// return (
-		// 	<React.Fragment>
-		// 		<EmptyRoom length={messages.length} />
-		// 		<FlatList
-		// 			testID='room-view-messages'
-		// 			ref={ref => this.list = ref}
-		// 			keyExtractor={item => item._id}
-		// 			data={messages}
-		// 			extraData={this.state}
-		// 			renderItem={this.renderItem}
-		// 			contentContainerStyle={styles.contentContainer}
-		// 			style={styles.list}
-		// 			inverted
-		// 			removeClippedSubviews
-		// 			initialNumToRender={20}
-		// 			onEndReached={this.onEndReached}
-		// 			onEndReachedThreshold={0.5}
-		// 			maxToRenderPerBatch={5}
-		// 			windowSize={21}
-		// 			ListFooterComponent={this.renderFooter}
-		// 			{...scrollPersistTaps}
-		// 		/>
-		// 	</React.Fragment>
-		// );
 	}
 }
