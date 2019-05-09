57d63031e624574a588a07e0520795989c92f6f7
diegolmello <diegolmello@gmail.com>
2019-05-09 18:12:39 -0300

    Emoji

--- app/containers/message/Emoji.js
+++ app/containers/message/Emoji.js
@@ -1,30 +1,29 @@
 import React from 'react';
-import { Text, ViewPropTypes } from 'react-native';
+import { Text } from 'react-native';
 import PropTypes from 'prop-types';
 import { emojify } from 'react-emojione';
 
 import CustomEmoji from '../EmojiPicker/CustomEmoji';
 import RocketChat from '../../lib/rocketchat';
 
-// FIXME: missing improvement
-export default class Emoji extends React.PureComponent {
-	static propTypes = {
-		content: PropTypes.string.isRequired,
-		baseUrl: PropTypes.string.isRequired,
-		standardEmojiStyle: Text.propTypes.style,
-		customEmojiStyle: ViewPropTypes.style
+const Emoji = React.memo(({
+	content, standardEmojiStyle, customEmojiStyle, baseUrl
+}) => {
+	const parsedContent = content.replace(/^:|:$/g, '');
+	const emojiExtension = RocketChat.getCustomEmojiFromLocal(parsedContent);
+	if (emojiExtension) {
+		const emoji = { extension: emojiExtension, content: parsedContent };
+		return <CustomEmoji key={content} baseUrl={baseUrl} style={customEmojiStyle} emoji={emoji} />;
 	}
+	return <Text style={standardEmojiStyle}>{ emojify(content, { output: 'unicode' }) }</Text>;
+}, () => true);
 
-	render() {
-		const {
-			content, standardEmojiStyle, customEmojiStyle, baseUrl
-		} = this.props;
-		const parsedContent = content.replace(/^:|:$/g, '');
-		const emojiExtension = RocketChat.getCustomEmojiFromLocal(parsedContent);
-		if (emojiExtension) {
-			const emoji = { extension: emojiExtension, content: parsedContent };
-			return <CustomEmoji key={content} baseUrl={baseUrl} style={customEmojiStyle} emoji={emoji} />;
-		}
-		return <Text style={standardEmojiStyle}>{ emojify(content, { output: 'unicode' }) }</Text>;
-	}
-}
+Emoji.propTypes = {
+	content: PropTypes.string,
+	standardEmojiStyle: PropTypes.object,
+	customEmojiStyle: PropTypes.object,
+	baseUrl: PropTypes.string
+};
+Emoji.displayName = 'MessageEmoji';
+
+export default Emoji;
