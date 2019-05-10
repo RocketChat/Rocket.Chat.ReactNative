c1003b1a9afebd7cd94e1c136a7d8d57dfe8dcd4
diegolmello <diegolmello@gmail.com>
2019-05-10 11:14:38 -0300

    List params

--- app/views/RoomView/List.js
+++ app/views/RoomView/List.js
@@ -147,11 +147,11 @@ export class List extends React.PureComponent {
 					style={styles.list}
 					inverted
 					removeClippedSubviews
-					initialNumToRender={20}
+					initialNumToRender={7}
 					onEndReached={this.onEndReached}
-					onEndReachedThreshold={0.5}
+					onEndReachedThreshold={5}
 					maxToRenderPerBatch={5}
-					windowSize={21}
+					windowSize={5}
 					ListFooterComponent={this.renderFooter}
 					{...scrollPersistTaps}
 				/>
