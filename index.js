db5c7aeaa33557b24cb3cd357f63a1fd22005792
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2019-05-21 16:28:02 -0300

    :art: Add store in Share container

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { AppRegistry } from 'react-native';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => require('./app/index').default);
-AppRegistry.registerComponent('ShareRocketChatRN', () => require('./Share').default);
+AppRegistry.registerComponent('ShareRocketChatRN', () => require('./app/share').default);
 
 // For storybook, comment everything above and uncomment below
 // import './storybook';
