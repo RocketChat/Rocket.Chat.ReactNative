76344d460d4886ab5d782b5fccaf4c3a0da61ffb
diegolmello <diegolmello@gmail.com>
2019-06-18 15:59:03 -0300

    Show webpage title on Android

--- app/utils/openLink.js
+++ app/utils/openLink.js
@@ -3,7 +3,10 @@ import * as WebBrowser from 'expo-web-browser';
 import { HEADER_TINT, HEADER_BACKGROUND } from '../constants/colors';
 
 const openLink = url => WebBrowser.openBrowserAsync(url, {
-	toolbarColor: HEADER_BACKGROUND, controlsColor: HEADER_TINT, collapseToolbar: true
+	toolbarColor: HEADER_BACKGROUND,
+	controlsColor: HEADER_TINT,
+	collapseToolbar: true,
+	showTitle: true
 });
 
 export default openLink;
