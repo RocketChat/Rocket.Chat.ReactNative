fbc3b20010a7a17a857bda8c0bc93041521f8f91
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2019-06-17 21:50:48 -0300

    :lipstick: Fix server item

--- app/views/RoomsListView/ServerDropdown.js
+++ app/views/RoomsListView/ServerDropdown.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {
-	View, Text, Animated, Easing, TouchableWithoutFeedback, TouchableOpacity, FlatList, Image
+	View, Text, Animated, Easing, TouchableWithoutFeedback, TouchableOpacity, FlatList
 } from 'react-native';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
@@ -13,11 +13,10 @@ import { selectServerRequest as selectServerRequestAction } from '../../actions/
 import { appStart as appStartAction } from '../../actions';
 import styles from './styles';
 import database, { safeAddListener } from '../../lib/realm';
-import Touch from '../../utils/touch';
 import RocketChat from '../../lib/rocketchat';
 import I18n from '../../i18n';
 import EventEmitter from '../../utils/events';
-import Check from '../../containers/Check';
+import ServerItem from '../../presentation/ServerItem';
 
 const ROW_HEIGHT = 68;
 const ANIMATION_DURATION = 200;
@@ -139,37 +138,13 @@ class ServerDropdown extends Component {
 
 	renderSeparator = () => <View style={styles.serverSeparator} />;
 
-	renderServer = ({ item }) => {
-		const { server } = this.props;
-
-		return (
-			<Touch onPress={() => this.select(item.id)} style={styles.serverItem} testID={`rooms-list-header-server-${ item.id }`}>
-				<View style={styles.serverItemContainer}>
-					{item.iconURL
-						? (
-							<Image
-								source={{ uri: item.iconURL }}
-								defaultSource={{ uri: 'logo' }}
-								style={styles.serverIcon}
-								onError={() => console.warn('error loading serverIcon')}
-							/>
-						)
-						: (
-							<Image
-								source={{ uri: 'logo' }}
-								style={styles.serverIcon}
-							/>
-						)
-					}
-					<View style={styles.serverTextContainer}>
-						<Text style={styles.serverName}>{item.name || item.id}</Text>
-						<Text style={styles.serverUrl}>{item.id}</Text>
-					</View>
-					{item.id === server ? <Check /> : null}
-				</View>
-			</Touch>
-		);
-	}
+	renderServer = ({ item }) => (
+		<ServerItem
+			onPress={() => this.select(item.id)}
+			item={item}
+			hasCheck
+		/>
+	);
 
 	render() {
 		const { servers } = this.state;
