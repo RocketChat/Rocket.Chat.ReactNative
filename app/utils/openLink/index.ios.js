67d6bd8cbf8b15305488fe84e9b9e816dbd46d42
diegolmello <diegolmello@gmail.com>
2019-06-18 15:28:20 -0300

    Working on android

--- app/utils/openLink/index.ios.js
+++
@@ -1,7 +0,0 @@
-import * as WebBrowser from 'expo-web-browser';
-
-import { HEADER_TINT, HEADER_BACKGROUND } from '../../constants/colors';
-
-const openLink = url => WebBrowser.openBrowserAsync(url, { toolbarColor: HEADER_BACKGROUND, controlsColor: HEADER_TINT, collapseToolbar: true });
-
-export default openLink;
