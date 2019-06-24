ba99428ada1bf099d4a8a0705109a2836c1d2b92
Camila Bressan <mila.bressan@hotmail.com>
2019-06-24 13:16:24 +0100

    adjusment eslint

--- app/i18n/index.js
+++ app/i18n/index.js
@@ -1,18 +1,23 @@
 import i18n from 'i18n-js';
 import { I18nManager } from 'react-native';
 import * as RNLocalize from 'react-native-localize';
-import {
-	de, en, fr, ptBR, ptPT, ru, zhCN, 
-} from './locales';
-
+import { de, en, fr, ptBR, ptPT, ru, zhCN } from './locales';
 
 i18n.translations = {
-	en, ru, 'pt-BR': ptBR, 'zh-CN': zhCN, fr, de, 'pt-PT': ptPT
+  en,
+  ru,
+  'pt-BR': ptBR,
+  'zh-CN': zhCN,
+  fr,
+  de,
+  'pt-PT': ptPT
 };
 i18n.fallbacks = true;
 
 const fallback = { languageTag: 'en', isRTL: false };
-const { languageTag, isRTL } = RNLocalize.findBestAvailableLanguage(Object.keys(i18n.translations)) || fallback;
+const { languageTag, isRTL } =
+  RNLocalize.findBestAvailableLanguage(Object.keys(i18n.translations)) ||
+  fallback;
 
 I18nManager.forceRTL(isRTL);
 i18n.locale = languageTag;
