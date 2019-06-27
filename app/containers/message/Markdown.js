dd6b237466b71e1ae08e470f3ffc7f51278cb056
IlarionHalushka <hilarion.galushka@gmail.com>
2019-06-27 19:15:33 +0300

    merge develop

--- app/containers/message/Markdown.js
+++ app/containers/message/Markdown.js
@@ -56,98 +56,99 @@ const Markdown = React.memo(({
 	let m = formatText(msg);
 	if (m) {
 		m = emojify(m, { output: 'unicode' });
-		m = m.replace(/^\[([^\]]*)\]\(([^)]*)\)/, '').trim();
-		if (numberOfLines > 0) {
-			m = m.replace(/[\n]+/g, '\n').trim();
-		}
-
-		if (!useMarkdown) {
-			return <Text style={styles.text} numberOfLines={numberOfLines}>{m}</Text>;
-		}
-
-		const isMessageContainsOnlyEmoji = isOnlyEmoji(m) && emojiCount(m) <= 3;
-
-		return (
-			<MarkdownRenderer
-				rules={{
-					paragraph: (node, children) => (
-						<Text key={node.key} style={styles.paragraph} numberOfLines={numberOfLines}>
-							{children}
-							{isEdited ? <Text style={styles.edited}> ({I18n.t('edited')})</Text> : null}
-						</Text>
-					),
-					mention: (node) => {
-						const { content, key } = node;
-						let mentionStyle = styles.mention;
-						if (content === 'all' || content === 'here') {
-							mentionStyle = {
-								...mentionStyle,
-								...styles.mentionAll
-							};
-						} else if (content === username) {
-							mentionStyle = {
-								...mentionStyle,
-								...styles.mentionLoggedUser
-							};
-						}
-						if (mentions && mentions.length && mentions.findIndex(mention => mention.username === content) !== -1) {
-							return (
-								<Text style={mentionStyle} key={key}>
-									&nbsp;{content}&nbsp;
-								</Text>
-							);
-						}
-						return `@${ content }`;
-					},
-					hashtag: (node) => {
-						const { content, key } = node;
-						if (channels && channels.length && channels.findIndex(channel => channel.name === content) !== -1) {
-							return (
-								<Text key={key} style={styles.mention}>
-									&nbsp;#{content}&nbsp;
-								</Text>
-							);
-						}
-						return `#${ content }`;
-					},
-					emoji: (node) => {
-						if (node.children && node.children.length && node.children[0].content) {
-							const { content } = node.children[0];
-							const emoji = getCustomEmoji && getCustomEmoji(content);
-							if (emoji) {
-								return (
-									<CustomEmoji
-										key={node.key}
-										baseUrl={baseUrl}
-										style={isMessageContainsOnlyEmoji ? styles.customEmojiBig : styles.customEmoji}
-										emoji={emoji}
-									/>
-								);
-							}
-							return <Text key={node.key}>:{content}:</Text>;
-						}
-						return null;
-					},
-					hardbreak: () => null,
-					blocklink: () => null,
-					image: node => (
-						<Image key={node.key} style={styles.inlineImage} source={{ uri: node.attributes.src }} />
-					),
-					...rules
-				}}
-				style={{
-					paragraph: styles.paragraph,
-					text: isMessageContainsOnlyEmoji ? styles.textBig : styles.text,
-					codeInline: styles.codeInline,
-					codeBlock: styles.codeBlock,
-					link: styles.link,
-					...style
-				}}
-				plugins={plugins}
-			>{m}
-			</MarkdownRenderer>
-		);
 	}
+
+	m = m.replace(/^\[([^\]]*)\]\(([^)]*)\)/, '').trim();
+	if (numberOfLines > 0) {
+		m = m.replace(/[\n]+/g, '\n').trim();
+	}
+
+	if (!useMarkdown) {
+		return <Text style={styles.text} numberOfLines={numberOfLines}>{m}</Text>;
+	}
+
+	const isMessageContainsOnlyEmoji = isOnlyEmoji(m) && emojiCount(m) <= 3;
+
+	return (
+		<MarkdownRenderer
+			rules={{
+				paragraph: (node, children) => (
+					<Text key={node.key} style={styles.paragraph} numberOfLines={numberOfLines}>
+						{children}
+						{isEdited ? <Text style={styles.edited}> ({I18n.t('edited')})</Text> : null}
+					</Text>
+				),
+				mention: (node) => {
+					const { content, key } = node;
+					let mentionStyle = styles.mention;
+					if (content === 'all' || content === 'here') {
+						mentionStyle = {
+							...mentionStyle,
+							...styles.mentionAll
+						};
+					} else if (content === username) {
+						mentionStyle = {
+							...mentionStyle,
+							...styles.mentionLoggedUser
+						};
+					}
+					if (mentions && mentions.length && mentions.findIndex(mention => mention.username === content) !== -1) {
+						return (
+							<Text style={mentionStyle} key={key}>
+								{content}&nbsp;
+							</Text>
+						);
+					}
+					return `@${ content }`;
+				},
+				hashtag: (node) => {
+					const { content, key } = node;
+					if (channels && channels.length && channels.findIndex(channel => channel.name === content) !== -1) {
+						return (
+							<Text key={key} style={styles.mention}>
+									&nbsp;#{content}&nbsp;
+							</Text>
+						);
+					}
+					return `#${ content }`;
+				},
+				emoji: (node) => {
+					if (node.children && node.children.length && node.children[0].content) {
+						const { content } = node.children[0];
+						const emoji = getCustomEmoji && getCustomEmoji(content);
+						if (emoji) {
+							return (
+								<CustomEmoji
+									key={node.key}
+									baseUrl={baseUrl}
+									style={isMessageContainsOnlyEmoji ? styles.customEmojiBig : styles.customEmoji}
+									emoji={emoji}
+								/>
+							);
+						}
+						return <Text key={node.key}>:{content}:</Text>;
+					}
+					return null;
+				},
+				hardbreak: () => null,
+				blocklink: () => null,
+				image: node => (
+					<Image key={node.key} style={styles.inlineImage} source={{ uri: node.attributes.src }} />
+				),
+				...rules
+			}}
+			style={{
+				paragraph: styles.paragraph,
+				text: isMessageContainsOnlyEmoji ? styles.textBig : styles.text,
+				codeInline: styles.codeInline,
+				codeBlock: styles.codeBlock,
+				link: styles.link,
+				...style
+			}}
+			plugins={plugins}
+		>{m}
+		</MarkdownRenderer>
+	);
 }, (prevProps, nextProps) => prevProps.msg === nextProps.msg);
 
 Markdown.propTypes = {
