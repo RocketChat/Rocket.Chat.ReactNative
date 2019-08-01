a4ef2cea59d2bca453e01f99eae7edd3c61abb83
IlarionHalushka <hilarion.galushka@gmail.com>
2019-08-02 00:26:12 +0300

    upload sourcemaps to bugsnag during ios-build job

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ import { AppRegistry } from 'react-native';
 import { Client } from 'bugsnag-react-native';
 import { name as appName, share as shareName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => require('./app/index').default);
-AppRegistry.registerComponent(shareName, () => require('./app/share').default);
-
 const bugsnag = new Client('72a0364cb361fc8f0fdc6cbf605f9963');
 bugsnag.notify(new Error('Test error'));
+
+AppRegistry.registerComponent(appName, () => require('./app/index').default);
+AppRegistry.registerComponent(shareName, () => require('./app/share').default);
 
 // For storybook, comment everything above and uncomment below
 // import './storybook';
