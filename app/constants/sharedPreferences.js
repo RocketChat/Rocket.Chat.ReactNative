ed07b94a54fb680281e6540a3d08a63003bf874c
Filipe Brito <filipedelimabrito@gmail.com>
2019-08-16 19:09:16 -0300

    Add Android sharedPreferences keys.

---
+++ app/constants/sharedPreferences.js
@@ -0,0 +1,6 @@
+export const SHARED_PREFERENCES_ACCOUNTS_KEY = 'ACCOUNTS_KEY';
+export const ANDROID_TOKEN = 'authToken';
+export const ANDROID_USER_ID = 'userId'; 
+export const ANDROID_SERVER_URL = 'serverUrl';
+export const ANDROID_SERVER_NAME = 'serverName';
+export const ANDROID_SERVER_ICON = 'serverLogo';
