048689379177bbfd7875f819b6f990f5cd9378f1
Filipe Brito <filipedelimabrito@gmail.com>
2019-08-20 10:46:02 -0300

    Checks if custom OAuth button should be show

--- app/lib/rocketchat.js
+++ app/lib/rocketchat.js
@@ -802,9 +802,11 @@ const RocketChat = {
 		}
 	},
 	_determineAuthType(services) {
-		const { name, custom, service } = services;
+		const {
+			name, custom, showButton = true, service
+		} = services;
 
-		if (custom) {
+		if (custom && showButton) {
 			return 'oauth_custom';
 		}
 
