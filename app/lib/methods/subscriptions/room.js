702bb3e9ef47c92a348aa0175c6f2f0dcaf9a31e
diegolmello <diegolmello@gmail.com>
2019-09-03 20:09:51 -0300

    Don't call updateMessages. Execute that entire logic for one message id instead.

--- app/lib/methods/subscriptions/room.js
+++ app/lib/methods/subscriptions/room.js
@@ -1,17 +1,18 @@
 import EJSON from 'ejson';
+import { sanitizedRaw } from '@nozbe/watermelondb/RawRecord';
+import { InteractionManager } from 'react-native';
 
 import log from '../../../utils/log';
 import protectedFunction from '../helpers/protectedFunction';
 import buildMessage from '../helpers/buildMessage';
 import database from '../../realm';
+import watermelondb from '../../database';
 import debounce from '../../../utils/debounce';
-import updateMessages from '../updateMessages';
 
 const unsubscribe = subscriptions => subscriptions.forEach(sub => sub.unsubscribe().catch(() => console.log('unsubscribeRoom')));
 const removeListener = listener => listener.stop();
 
 export default function subscribeRoom({ rid }) {
-    console.log('TCL: subscribeRoom -> rid', rid);
 	let promises;
 	let connectedListener;
 	let disconnectedListener;
@@ -101,41 +102,92 @@ export default function subscribeRoom({ rid }) {
 		}
 	});
 
-	const read = debounce(() => {
-		const [room] = database.objects('subscriptions').filtered('rid = $0', rid);
-		if (room && room._id) {
-			this.readMessages(rid);
-		}
-	}, 300);
-
 	const handleMessageReceived = protectedFunction(async(ddpMessage) => {
-		const message = EJSON.fromJSONValue(ddpMessage.fields.args[0]);
+		const message = buildMessage(EJSON.fromJSONValue(ddpMessage.fields.args[0]));
 		if (rid !== message.rid) {
 			return;
 		}
-		try {
-			await updateMessages(rid, [message]);
-		} catch (e) {
-			log(e);
-		}
-		// requestAnimationFrame(async() => {
-		// 	try {
-		// 		database.write(() => {
-		// 			database.create('messages', message, true);
-		// 			// if it's a thread "header"
-		// 			if (message.tlm) {
-		// 				database.create('threads', message, true);
-		// 			} else if (message.tmid) {
-		// 				message.rid = message.tmid;
-		// 				database.create('threadMessages', message, true);
-		// 			}
-		// 		});
+		InteractionManager.runAfterInteractions(async() => {
+			const watermelon = watermelondb.database;
+			const batch = [];
+			const subCollection = watermelon.collections.get('subscriptions');
+			const msgCollection = watermelon.collections.get('messages');
+			const threadsCollection = watermelon.collections.get('threads');
+			const threadMessagesCollection = watermelon.collections.get('thread_messages');
 
-		// 		read();
-		// 	} catch (e) {
-		// 		console.warn('handleMessageReceived', e);
-		// 	}
-		// });
+			// Create or update message
+			try {
+				const messageRecord = await msgCollection.find(message._id);
+				batch.push(
+					messageRecord.prepareUpdate((m) => {
+						Object.assign(m, message);
+					})
+				);
+			} catch (error) {
+				batch.push(
+					msgCollection.prepareCreate(protectedFunction((m) => {
+						m._raw = sanitizedRaw({ id: message._id }, msgCollection.schema);
+						m.subscription.id = rid;
+						Object.assign(m, message);
+					}))
+				);
+			}
+
+			// Create or update thread
+			if (message.tlm) {
+				try {
+					const threadRecord = await threadsCollection.find(message._id);
+					batch.push(
+						threadRecord.prepareUpdate((t) => {
+							Object.assign(t, message);
+						})
+					);
+				} catch (error) {
+					batch.push(
+						threadsCollection.prepareCreate(protectedFunction((t) => {
+							t._raw = sanitizedRaw({ id: message._id }, threadsCollection.schema);
+							t.subscription.id = rid;
+							Object.assign(t, message);
+						}))
+					);
+				}
+			}
+
+			// Create or update thread message
+			if (message.tmid) {
+				try {
+					const threadMessageRecord = await threadMessagesCollection.find(message._id);
+					batch.push(
+						threadMessageRecord.prepareUpdate((tm) => {
+							Object.assign(tm, message);
+						})
+					);
+				} catch (error) {
+					batch.push(
+						threadMessagesCollection.prepareCreate(protectedFunction((tm) => {
+							tm._raw = sanitizedRaw({ id: message._id }, threadMessagesCollection.schema);
+							tm.subscription.id = message.tmid;
+							Object.assign(tm, message);
+						}))
+					);
+				}
+			}
+
+			try {
+				await subCollection.find(rid);
+				this.readMessages(rid);
+			} catch (e) {
+				console.log('Subscription not found. We probably subscribed to a not joined channel. No need to mark as read.');
+			}
+
+			try {
+				await watermelon.action(async() => {
+					await watermelon.batch(...batch);
+				});
+			} catch (e) {
+				log(e);
+			}
+		});
 	});
 
 	const stop = () => {
