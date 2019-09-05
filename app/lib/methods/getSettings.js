1349d43b34067d26699bc1165264df1c6852cf92
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2019-09-05 11:23:31 -0300

    Improve getSettings

--- app/lib/methods/getSettings.js
+++ app/lib/methods/getSettings.js
@@ -1,16 +1,56 @@
-import { InteractionManager } from 'react-native';
+import { sanitizedRaw } from '@nozbe/watermelondb/RawRecord';
 
 import reduxStore from '../createStore';
-import * as actions from '../../actions';
-import log from '../../utils/log';
+// import * as actions from '../../actions';
 import settings from '../../constants/settings';
-import watermelon from '../database';
-import update	 from '../../utils/update';
+import log from '../../utils/log';
+import watermelondb from '../database';
+import protectedFunction from './helpers/protectedFunction';
+
+const serverInfoKeys = ['Site_Name', 'UI_Use_Real_Name', 'FileUpload_MediaTypeWhiteList', 'FileUpload_MaxFileSize'];
+
+const serverInfoUpdate = (serverInfo, iconSetting) => {
+	const { serversDB } = watermelondb.databases;
+	const serverId = reduxStore.getState().server.server;
+
+	let info = serverInfo.reduce((allSettings, setting) => {
+		if (setting._id === 'Site_Name') {
+			return { ...allSettings, name: setting.valueAsString };
+		}
+		if (setting._id === 'UI_Use_Real_Name') {
+			return { ...allSettings, useRealName: setting.valueAsBoolean };
+		}
+		if (setting._id === 'FileUpload_MediaTypeWhiteList') {
+			return { ...allSettings, FileUpload_MediaTypeWhiteList: setting.valueAsString };
+		}
+		if (setting._id === 'FileUpload_MaxFileSize') {
+			return { ...allSettings, FileUpload_MaxFileSize: setting.valueAsNumber };
+		}
+		return allSettings;
+	}, {});
+
+	if (iconSetting) {
+		const iconURL = `${ serverId }/${ iconSetting.value.url || iconSetting.value.defaultUrl }`;
+		info = { ...info, iconURL };
+	}
+
+	serversDB.action(async() => {
+		try {
+			const serversCollection = serversDB.collections.get('servers');
+			const server = await serversCollection.find(serverId);
+
+			await server.update((record) => {
+				Object.assign(record, info);
+			});
+		} catch (e) {
+			log(e);
+		}
+	});
+};
 
 export default async function() {
 	try {
-		const { serversDB } = watermelon.databases;
-		const serverId = reduxStore.getState().server.server;
+		const watermelon = watermelondb.database;
 		const settingsParams = JSON.stringify(Object.keys(settings));
 		// RC 0.60.0
 		const result = await fetch(`${ this.sdk.client.host }/api/v1/settings.public?query={"_id":{"$in":${ settingsParams }}}`).then(response => response.json());
@@ -21,36 +61,44 @@ export default async function() {
 		const data = result.settings || [];
 		const filteredSettings = this._prepareSettings(data.filter(item => item._id !== 'Assets_favicon_512'));
 
-		InteractionManager.runAfterInteractions(
-			() => filteredSettings.forEach(async(setting) => {
-				try {
-					await update(watermelon.database, 'settings', { ...setting, _updatedAt: new Date(), id: setting._id });
+		watermelon.action(async() => {
+			const settingsCollection = watermelon.collections.get('settings');
+			const allSettingsRecords = await settingsCollection.query().fetch();
 
-					if (setting._id === 'Site_Name') {
-						await update(serversDB, 'servers', { id: serverId, name: setting.valueAsString });
-					}
-					if (setting._id === 'UI_Use_Real_Name') {
-						await update(serversDB, 'servers', { id: serverId, useRealName: setting.valueAsBoolean });
-					}
-					if (setting._id === 'FileUpload_MediaTypeWhiteList') {
-						await update(serversDB, 'servers', { id: serverId, FileUpload_MediaTypeWhiteList: setting.valueAsString });
-					}
-					if (setting._id === 'FileUpload_MaxFileSize') {
-						await update(serversDB, 'servers', { id: serverId, FileUpload_MaxFileSize: setting.valueAsNumber });
-					}
-				} catch (e) {
-					log(e);
-				}
-			})
-		);
-		reduxStore.dispatch(actions.addSettings(this.parseSettings(filteredSettings)));
+			// filter settings
+			let settingsToCreate = filteredSettings.filter(i1 => !allSettingsRecords.find(i2 => i1._id === i2.id));
+			let settingsToUpdate = allSettingsRecords.filter(i1 => filteredSettings.find(i2 => i1.id === i2._id));
+			const serverInfo = filteredSettings.filter(i1 => serverInfoKeys.includes(i1._id));
+			const iconSetting = data.find(item => item._id === 'Assets_favicon_512');
 
-		const iconSetting = data.find(item => item._id === 'Assets_favicon_512');
-		if (iconSetting) {
-			const baseUrl = reduxStore.getState().server.server;
-			const iconURL = `${ baseUrl }/${ iconSetting.value.url || iconSetting.value.defaultUrl }`;
-			await update(serversDB, 'servers', { id: serverId, iconURL });
-		}
+			// Create
+			settingsToCreate = settingsToCreate.map(setting => settingsCollection.prepareCreate(protectedFunction((s) => {
+				s._raw = sanitizedRaw({ id: setting._id }, settingsCollection.schema);
+				Object.assign(s, setting);
+			})));
+
+			// Update
+			settingsToUpdate = settingsToUpdate.map((setting) => {
+				const newSetting = filteredSettings.find(s => s._id === setting.id);
+				return setting.prepareUpdate(protectedFunction((s) => {
+					Object.assign(s, newSetting);
+				}));
+			});
+
+			const allRecords = [
+				...settingsToCreate,
+				...settingsToUpdate
+			];
+
+			serverInfoUpdate(serverInfo, iconSetting);
+
+			try {
+				await watermelon.batch(...allRecords);
+			} catch (e) {
+				log(e);
+			}
+			return allRecords.length;
+		});
 	} catch (e) {
 		log(e);
 	}
