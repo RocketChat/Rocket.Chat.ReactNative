ff9a56eb825e9dec75522e96e685aac4510ea9ef
Filipe Brito <filipedelimabrito@gmail.com>
2019-09-10 16:23:25 -0300

    Improves push notification.

--- android/app/src/main/java/chat/rocket/reactnative/pushnotification/CustomPushNotification.kt
+++ android/app/src/main/java/chat/rocket/reactnative/pushnotification/CustomPushNotification.kt
@@ -5,16 +5,19 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
-import android.graphics.BitmapFactory
 import android.os.Build
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
 import chat.rocket.reactnative.R
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.wix.reactnativenotifications.core.AppLaunchHelper
 import com.wix.reactnativenotifications.core.AppLifecycleFacade
 import com.wix.reactnativenotifications.core.JsIOHelper
 import com.wix.reactnativenotifications.core.notification.PushNotification
 import kotlinx.serialization.UnstableDefault
 import kotlinx.serialization.json.Json
+import java.util.*
 
 class CustomPushNotification(
     context: Context,
@@ -23,52 +26,72 @@ class CustomPushNotification(
     appLaunchHelper: AppLaunchHelper,
     jsIoHelper: JsIOHelper
 ) : PushNotification(context, bundle, appLifecycleFacade, appLaunchHelper, jsIoHelper) {
+    private val smallIcon =
+        mContext.resources.getIdentifier("ic_notification", "mipmap", mContext.packageName)
+    private lateinit var notificationManager: NotificationManager
+    private lateinit var groupBuilder: NotificationCompat.Builder
+    private lateinit var title: String
+    private lateinit var message: String
+    private lateinit var ejson: Ejson
+    private lateinit var notificationId: String
+
+    @UnstableDefault
+    override fun buildNotification(intent: PendingIntent): Notification {
+        with(mNotificationProps.asBundle()) {
+            title = getString("title", "")
+            message = getString("message", "")
+            ejson = Json.nonstrict.parse(Ejson.serializer(), getString("ejson", "{}"))
+            notificationId = getString("notId", Random().nextInt().toString())
+        }
+
+        notificationManager =
+            (mContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
+
+        groupBuilder = NotificationCompat.Builder(mContext, ejson.host)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setSmallIcon(smallIcon)
+            .setStyle(NotificationCompat.InboxStyle().setSummaryText(title))
+            .setGroup(ejson.toString())
+            .setGroupSummary(true)
+
+        return getNotificationBuilder(intent).build()
+    }
 
     @UnstableDefault
     override fun getNotificationBuilder(intent: PendingIntent): Notification.Builder {
-        val res = mContext.resources
-        val packageName = mContext.packageName
-
-        val bundle = mNotificationProps.asBundle()
-        val title = bundle.getString("title")
-        val message = bundle.getString("message")
-        val ejson = Json.nonstrict.parse(
-            Ejson.serializer(),
-            bundle.getString("ejson", "{}")
-        )
-
         val notificationBuilder = Notification.Builder(mContext)
-            .setSmallIcon(res.getIdentifier("ic_notification", "mipmap", packageName))
             .setContentTitle(title)
             .setContentText(message)
-            .setGroup(ejson.toString())
-            .setGroupSummary(true)
-            .setStyle(Notification.BigTextStyle().bigText(message))
-            .setContentIntent(intent)
+            .setSmallIcon(smallIcon)
             .setLargeIcon(
-                BitmapFactory.decodeResource(
-                    res,
-                    res.getIdentifier("ic_launcher", "mipmap", packageName)
-                )
+                Glide.with(mContext)
+                    .asBitmap()
+                    .apply(RequestOptions.circleCropTransform())
+                    .load(ejson.avatarUri)
+                    .submit(100, 100)
+                    .get()
             )
+            .setGroup(ejson.toString())
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             notificationBuilder.setColor(mContext.getColor(R.color.notification_text))
         }
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            (mContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
-                .createNotificationChannel(
-                    NotificationChannel(
-                        ejson.rid,
-                        ejson.host,
-                        NotificationManager.IMPORTANCE_HIGH
-                    )
-                )
-
-            notificationBuilder.setChannelId(ejson.rid)
+            notificationManager.createNotificationChannel(
+                NotificationChannel(ejson.host, ejson.host, NotificationManager.IMPORTANCE_HIGH)
+            )
+            notificationBuilder.setChannelId(ejson.host)
         }
 
         return notificationBuilder
+    }
+
+    override fun postNotification(id: Int, notification: Notification) {
+        with(notificationManager) {
+            notify(Random().nextInt(), notification)
+            notify(notificationId.toInt(), groupBuilder.build())
+        }
     }
 }
--- android/app/src/main/java/chat/rocket/reactnative/pushnotification/Ejson.kt
+++ android/app/src/main/java/chat/rocket/reactnative/pushnotification/Ejson.kt
@@ -3,6 +3,17 @@ package chat.rocket.reactnative.pushnotification
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class Ejson(val host: String, val rid: String) {
+data class Ejson(val host: String, val rid: String, val sender: Sender) {
     override fun toString(): String = host + rid
+
+    val avatarUri: String = "${removeTrailingSlash(host)}/avatar/${sender.username}"
+
+    private fun removeTrailingSlash(baseUrl: String): String {
+        var url = baseUrl
+        if (url.endsWith('/')) url = url.dropLast(1)
+        return url
+    }
 }
+
+@Serializable
+data class Sender(val username: String)
