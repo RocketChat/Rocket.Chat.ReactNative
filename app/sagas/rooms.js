4315910895a206e9d6d143c51ca31b5aa6c2486a
diegolmello <diegolmello@gmail.com>
2019-09-13 18:06:16 -0300

    Fetch only subscriptions returned by fetch

--- app/sagas/rooms.js
+++ app/sagas/rooms.js
@@ -2,6 +2,7 @@ import {
 	put, select, race, take, fork, cancel, delay
 } from 'redux-saga/effects';
 import { BACKGROUND, INACTIVE } from 'redux-enhancer-react-native-appstate';
+import { Q } from '@nozbe/watermelondb';
 
 import { sanitizedRaw } from '@nozbe/watermelondb/RawRecord';
 import * as types from '../actions/actionsTypes';
@@ -20,22 +21,20 @@ const handleRoomsRequest = function* handleRoomsRequest() {
 		const serversCollection = serversDB.collections.get('servers');
 		const serverRecord = yield serversCollection.find(server);
 		const { roomsUpdatedAt } = serverRecord;
-		const [subscriptionsResult, roomsResult] = yield RocketChat.getRooms(
-			roomsUpdatedAt
-		);
-		const { subscriptions } = mergeSubscriptionsRooms(
-			subscriptionsResult,
-			roomsResult
-		);
+		const [subscriptionsResult, roomsResult] = yield RocketChat.getRooms(roomsUpdatedAt);
+		const { subscriptions } = mergeSubscriptionsRooms(subscriptionsResult, roomsResult);
 
 		const db = database.active;
 		yield db.action(async() => {
 			const subCollection = db.collections.get('subscriptions');
-			const existingSubs = await subCollection.query().fetch();
+			if (!subscriptions.length) {
+				return;
+			}
+
+			const subsIds = subscriptions.map(sub => sub.rid);
+			const existingSubs = await subCollection.query(Q.where('id', Q.oneOf(subsIds))).fetch();
 			const subsToUpdate = existingSubs.filter(i1 => subscriptions.find(i2 => i1._id === i2._id));
-			const subsToCreate = subscriptions.filter(
-				i1 => !existingSubs.find(i2 => i1._id === i2._id)
-			);
+			const subsToCreate = subscriptions.filter(i1 => !existingSubs.find(i2 => i1._id === i2._id));
 			// TODO: subsToDelete?
 
 			const allRecords = [
@@ -44,9 +43,7 @@ const handleRoomsRequest = function* handleRoomsRequest() {
 					return Object.assign(s, subscription);
 				})),
 				...subsToUpdate.map((subscription) => {
-					const newSub = subscriptions.find(
-						s => s._id === subscription._id
-					);
+					const newSub = subscriptions.find(s => s._id === subscription._id);
 					return subscription.prepareUpdate(() => {
 						Object.assign(subscription, newSub);
 					});
