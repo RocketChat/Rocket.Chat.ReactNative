2887ff04e78ff52c3ced8d50ebc6fb589f094cda
Diego Mello <diegolmello@gmail.com>
2019-09-17 16:24:47 -0300

    [CHORE] Update navigation lib (#1201)
    * Bump react-navigation
    
    * Bump react-native-screens
    
    * Bump react-navigation-drawer and react-native-gesture-handler
    
    * Update mocks
    
    * Add supportLibVersion

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import {
-	createStackNavigator, createAppContainer, createSwitchNavigator, createDrawerNavigator
-} from 'react-navigation';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createDrawerNavigator } from 'react-navigation-drawer';
 import { Provider } from 'react-redux';
 import { useScreens } from 'react-native-screens'; // eslint-disable-line import/no-unresolved
 import { Linking } from 'react-native';
