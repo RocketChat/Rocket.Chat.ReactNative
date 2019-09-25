37365375a987b43acbfbce7b8189f556d25d4df1
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2019-09-25 19:24:03 -0300

    Revert storybook

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-// import 'react-native-gesture-handler';
-// import 'react-native-console-time-polyfill';
-// import { AppRegistry } from 'react-native';
-// import joypixels from 'emoji-toolkit';
-// import { name as appName, share as shareName } from './app.json';
+import 'react-native-gesture-handler';
+import 'react-native-console-time-polyfill';
+import { AppRegistry } from 'react-native';
+import joypixels from 'emoji-toolkit';
+import { name as appName, share as shareName } from './app.json';
 
-// joypixels.ascii = true;
+joypixels.ascii = true;
 
-// if (__DEV__) {
-// 	require('./app/ReactotronConfig');
-// }
+if (__DEV__) {
+	require('./app/ReactotronConfig');
+}
 
-// AppRegistry.registerComponent(appName, () => require('./app/index').default);
-// AppRegistry.registerComponent(shareName, () => require('./app/share').default);
+AppRegistry.registerComponent(appName, () => require('./app/index').default);
+AppRegistry.registerComponent(shareName, () => require('./app/share').default);
 
 // For storybook, comment everything above and uncomment below
-import './storybook';
+// import './storybook';
