8ec582e38ac563a473ee4c935a7efb2ec0d0bba5
diegolmello <diegolmello@gmail.com>
2019-10-03 14:12:59 -0300

    Batch send message database calls

--- app/lib/methods/sendMessage.js
+++ app/lib/methods/sendMessage.js
@@ -5,16 +5,32 @@ import database from '../database';
 import log from '../../utils/log';
 import random from '../../utils/random';
 
-export const getMessage = async(rid, msg = '', tmid, user) => {
-	const _id = random(17);
-	const { id, username } = user;
+export function sendMessageCall(message) {
+	const {
+		id: _id, subscription: { id: rid }, msg, tmid
+	} = message;
+	// RC 0.60.0
+	return this.sdk.post('chat.sendMessage', {
+		message: {
+			_id, rid, msg, tmid
+		}
+	});
+}
+
+export default async function(rid, msg, tmid, user) {
 	try {
 		const db = database.active;
+		const subsCollection = db.collections.get('subscriptions');
 		const msgCollection = db.collections.get('messages');
-		let message;
-		await db.action(async() => {
-			message = await msgCollection.create((m) => {
-				m._raw = sanitizedRaw({ id: _id }, msgCollection.schema);
+		const threadMessagesCollection = db.collections.get('thread_messages');
+		const messageId = random(17);
+		const batch = [];
+		const message = {
+			id: messageId, subscription: { id: rid }, msg, tmid
+		};
+		batch.push(
+			msgCollection.prepareCreate((m) => {
+				m._raw = sanitizedRaw({ id: messageId }, msgCollection.schema);
 				m.subscription.id = rid;
 				m.msg = msg;
 				m.tmid = tmid;
@@ -22,62 +38,74 @@ export const getMessage = async(rid, msg = '', tmid, user) => {
 				m._updatedAt = new Date();
 				m.status = messagesStatus.TEMP;
 				m.u = {
-					_id: id || '1',
-					username
+					_id: user.id || '1',
+					username: user.username
 				};
-			});
-		});
-		return message;
-	} catch (error) {
-		console.warn('getMessage', error);
-	}
-};
+			})
+		);
 
-export async function sendMessageCall(message) {
-	const {
-		id: _id, subscription: { id: rid }, msg, tmid
-	} = message;
-	// RC 0.60.0
-	const data = await this.sdk.post('chat.sendMessage', {
-		message: {
-			_id, rid, msg, tmid
-		}
-	});
-	return data;
-}
-
-export default async function(rid, msg, tmid, user) {
-	try {
-		const db = database.active;
-		const subsCollections = db.collections.get('subscriptions');
-		const message = await getMessage(rid, msg, tmid, user);
-		if (!message) {
-			return;
+		if (tmid) {
+			batch.push(
+				threadMessagesCollection.prepareCreate((tm) => {
+					tm._raw = sanitizedRaw({ id: messageId }, threadMessagesCollection.schema);
+					tm.subscription.id = rid;
+					tm.rid = tmid;
+					tm.msg = msg;
+					tm.ts = new Date();
+					tm._updatedAt = new Date();
+					tm.status = messagesStatus.TEMP;
+					tm.u = {
+						_id: user.id || '1',
+						username: user.username
+					};
+				})
+			);
 		}
 
 		try {
-			const room = await subsCollections.find(rid);
-			await db.action(async() => {
-				await room.update((r) => {
-					r.draftMessage = null;
-				});
-			});
+			const room = await subsCollection.find(rid);
+			if (room.draftMessage) {
+				batch.push(
+					room.prepareUpdate((r) => {
+						r.draftMessage = null;
+					})
+				);
+			}
 		} catch (e) {
 			// Do nothing
 		}
 
 		try {
-			await sendMessageCall.call(this, message);
 			await db.action(async() => {
-				await message.update((m) => {
-					m.status = messagesStatus.SENT;
-				});
+				await db.batch(...batch);
 			});
 		} catch (e) {
-			await db.action(async() => {
-				await message.update((m) => {
+			log(e);
+			return;
+		}
+
+		try {
+			await sendMessageCall.call(this, message);
+		} catch (e) {
+			const errorBatch = [];
+			const messageRecord = await msgCollection.find(messageId);
+			errorBatch.push(
+				messageRecord.prepareUpdate((m) => {
 					m.status = messagesStatus.ERROR;
-				});
+				})
+			);
+
+			if (tmid) {
+				const threadMessageRecord = await threadMessagesCollection.find(messageId);
+				errorBatch.push(
+					threadMessageRecord.prepareUpdate((tm) => {
+						tm.status = messagesStatus.ERROR;
+					})
+				);
+			}
+
+			await db.action(async() => {
+				await db.batch(...errorBatch);
 			});
 		}
 	} catch (e) {
