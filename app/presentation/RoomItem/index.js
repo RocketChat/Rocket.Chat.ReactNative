06e584dfb61e85e072b5927df891d38b4aa83d5d
Prateek Jain <prateek93a@gmail.com>
2019-10-10 05:55:28 +0530

    initial-commit

--- app/presentation/RoomItem/index.js
+++ app/presentation/RoomItem/index.js
@@ -1,26 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View, Text, Animated } from 'react-native';
-import {
-	RectButton,
-	PanGestureHandler,
-	State
-} from 'react-native-gesture-handler';
+import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
-
+import { compose } from 'redux';
 import Avatar from '../../containers/Avatar';
 import I18n from '../../i18n';
 import styles, {
-	ROW_HEIGHT,
-	ACTION_WIDTH,
-	SMALL_SWIPE,
-	LONG_SWIPE
+	ROW_HEIGHT
 } from './styles';
 import UnreadBadge from './UnreadBadge';
 import TypeIcon from './TypeIcon';
 import LastMessage from './LastMessage';
 import { capitalize, formatDate } from '../../utils/room';
-import { LeftActions, RightActions } from './Actions';
+import withTouchableComponent from './withTouchableComponent';
 
 export { ROW_HEIGHT };
 
@@ -50,20 +42,11 @@ class RoomItem extends React.Component {
 		userMentions: PropTypes.number,
 		id: PropTypes.string,
 		prid: PropTypes.string,
-		onPress: PropTypes.func,
 		userId: PropTypes.string,
 		username: PropTypes.string,
 		token: PropTypes.string,
 		avatarSize: PropTypes.number,
-		testID: PropTypes.string,
-		width: PropTypes.number,
-		favorite: PropTypes.bool,
-		isRead: PropTypes.bool,
-		rid: PropTypes.string,
 		status: PropTypes.string,
-		toggleFav: PropTypes.func,
-		toggleRead: PropTypes.func,
-		hideChannel: PropTypes.func,
 		avatar: PropTypes.bool
 	}
 
@@ -71,22 +54,6 @@ class RoomItem extends React.Component {
 		avatarSize: 48
 	};
 
-	constructor(props) {
-		super(props);
-		this.dragX = new Animated.Value(0);
-		this.rowOffSet = new Animated.Value(0);
-		this.transX = Animated.add(
-			this.rowOffSet,
-			this.dragX
-		);
-		this.state = {
-			rowState: 0 // 0: closed, 1: right opened, -1: left opened
-		};
-		this._onGestureEvent = Animated.event(
-			[{ nativeEvent: { translationX: this.dragX } }]
-		);
-		this._value = 0;
-	}
 
 	shouldComponentUpdate(nextProps) {
 		const { _updatedAt } = this.props;
@@ -97,128 +64,9 @@ class RoomItem extends React.Component {
 		return attrs.some(key => nextProps[key] !== this.props[key]);
 	}
 
-	_onHandlerStateChange = ({ nativeEvent }) => {
-		if (nativeEvent.oldState === State.ACTIVE) {
-			this._handleRelease(nativeEvent);
-		}
-	};
-
-	_handleRelease = (nativeEvent) => {
-		const { translationX } = nativeEvent;
-		const { rowState } = this.state;
-		this._value = this._value + translationX;
-
-		let toValue = 0;
-		if (rowState === 0) { // if no option is opened
-			if (translationX > 0 && translationX < LONG_SWIPE) {
-				toValue = ACTION_WIDTH; // open left option if he swipe right but not enough to trigger action
-				this.setState({ rowState: -1 });
-			} else if (translationX >= LONG_SWIPE) {
-				toValue = 0;
-				this.toggleRead();
-			} else if (translationX < 0 && translationX > -LONG_SWIPE) {
-				toValue = -2 * ACTION_WIDTH; // open right option if he swipe left
-				this.setState({ rowState: 1 });
-			} else if (translationX <= -LONG_SWIPE) {
-				toValue = 0;
-				this.setState({ rowState: 0 });
-				this.hideChannel();
-			} else {
-				toValue = 0;
-			}
-		}
-
-		if (rowState === -1) { // if left option is opened
-			if (this._value < SMALL_SWIPE) {
-				toValue = 0;
-				this.setState({ rowState: 0 });
-			} else if (this._value > LONG_SWIPE) {
-				toValue = 0;
-				this.setState({ rowState: 0 });
-				this.toggleRead();
-			} else {
-				toValue = ACTION_WIDTH;
-			}
-		}
-
-		if (rowState === 1) { // if right option is opened
-			if (this._value > -2 * SMALL_SWIPE) {
-				toValue = 0;
-				this.setState({ rowState: 0 });
-			} else if (this._value < -LONG_SWIPE) {
-				toValue = 0;
-				this.setState({ rowState: 0 });
-				this.hideChannel();
-			} else {
-				toValue = -2 * ACTION_WIDTH;
-			}
-		}
-		this._animateRow(toValue);
-	}
-
-	_animateRow = (toValue) => {
-		this.rowOffSet.setValue(this._value);
-		this._value = toValue;
-		this.dragX.setValue(0);
-		Animated.spring(this.rowOffSet, {
-			toValue,
-			bounciness: 0,
-			useNativeDriver: true
-		}).start();
-	}
-
-	close = () => {
-		this.setState({ rowState: 0 });
-		this._animateRow(0);
-	}
-
-	toggleFav = () => {
-		const { toggleFav, rid, favorite } = this.props;
-		if (toggleFav) {
-			toggleFav(rid, favorite);
-		}
-		this.close();
-	};
-
-	toggleRead = () => {
-		const { toggleRead, rid, isRead } = this.props;
-		if (toggleRead) {
-			toggleRead(rid, isRead);
-		}
-	};
-
-	hideChannel = () => {
-		const { hideChannel, rid, type } = this.props;
-		if (hideChannel) {
-			hideChannel(rid, type);
-		}
-	};
-
-	onToggleReadPress = () => {
-		this.toggleRead();
-		this.close();
-	};
-
-	onHidePress = () => {
-		this.hideChannel();
-		this.close();
-	};
-
-	onPress = () => {
-		const { rowState } = this.state;
-		if (rowState !== 0) {
-			this.close();
-			return;
-		}
-		const { onPress } = this.props;
-		if (onPress) {
-			onPress();
-		}
-	};
-
 	render() {
 		const {
-			unread, userMentions, name, _updatedAt, alert, testID, type, avatarSize, baseUrl, userId, username, token, id, prid, showLastMessage, lastMessage, isRead, width, favorite, status, avatar
+			unread, userMentions, name, _updatedAt, alert, type, avatarSize, baseUrl, userId, username, token, id, prid, showLastMessage, lastMessage, status, avatar
 		} = this.props;
 
 		const date = formatDate(_updatedAt);
@@ -239,101 +87,68 @@ class RoomItem extends React.Component {
 		}
 
 		return (
-			<PanGestureHandler
-				minDeltaX={20}
-				onGestureEvent={this._onGestureEvent}
-				onHandlerStateChange={this._onHandlerStateChange}
+
+			<View
+				style={styles.container}
+				accessibilityLabel={accessibilityLabel}
 			>
-				<Animated.View>
-					<LeftActions
-						transX={this.transX}
-						isRead={isRead}
-						width={width}
-						onToggleReadPress={this.onToggleReadPress}
-					/>
-					<RightActions
-						transX={this.transX}
-						favorite={favorite}
-						width={width}
-						toggleFav={this.toggleFav}
-						onHidePress={this.onHidePress}
-					/>
-					<Animated.View
-						style={{
-							transform: [{ translateX: this.transX }]
-						}}
-					>
-						<RectButton
-							onPress={this.onPress}
-							activeOpacity={0.8}
-							underlayColor='#e1e5e8'
-							testID={testID}
-							style={styles.button}
+				<Avatar
+					text={avatar}
+					size={avatarSize}
+					type={type}
+					baseUrl={baseUrl}
+					style={styles.avatar}
+					userId={userId}
+					token={token}
+				/>
+				<View style={styles.centerContainer}>
+					<View style={styles.titleContainer}>
+						<TypeIcon
+							type={type}
+							id={id}
+							prid={prid}
+							status={status}
+						/>
+						<Text
+							style={[
+								styles.title,
+								alert && styles.alert
+							]}
+							ellipsizeMode='tail'
+							numberOfLines={1}
 						>
-							<View
-								style={styles.container}
-								accessibilityLabel={accessibilityLabel}
+							{name}
+						</Text>
+						{_updatedAt ? (
+							<Text
+								style={[
+									styles.date,
+									alert && styles.updateAlert
+								]}
+								ellipsizeMode='tail'
+								numberOfLines={1}
 							>
-								<Avatar
-									text={avatar}
-									size={avatarSize}
-									type={type}
-									baseUrl={baseUrl}
-									style={styles.avatar}
-									userId={userId}
-									token={token}
-								/>
-								<View style={styles.centerContainer}>
-									<View style={styles.titleContainer}>
-										<TypeIcon
-											type={type}
-											id={id}
-											prid={prid}
-											status={status}
-										/>
-										<Text
-											style={[
-												styles.title,
-												alert && styles.alert
-											]}
-											ellipsizeMode='tail'
-											numberOfLines={1}
-										>
-											{name}
-										</Text>
-										{_updatedAt ? (
-											<Text
-												style={[
-													styles.date,
-													alert && styles.updateAlert
-												]}
-												ellipsizeMode='tail'
-												numberOfLines={1}
-											>
-												{capitalize(date)}
-											</Text>
-										) : null}
-									</View>
-									<View style={styles.row}>
-										<LastMessage
-											lastMessage={lastMessage}
-											type={type}
-											showLastMessage={showLastMessage}
-											username={username}
-											alert={alert}
-										/>
-										<UnreadBadge
-											unread={unread}
-											userMentions={userMentions}
-											type={type}
-										/>
-									</View>
-								</View>
-							</View>
-						</RectButton>
-					</Animated.View>
-				</Animated.View>
-			</PanGestureHandler>
+								{capitalize(date)}
+							</Text>
+						) : null}
+					</View>
+					<View style={styles.row}>
+						<LastMessage
+							lastMessage={lastMessage}
+							type={type}
+							showLastMessage={showLastMessage}
+							username={username}
+							alert={alert}
+						/>
+						<UnreadBadge
+							unread={unread}
+							userMentions={userMentions}
+							type={type}
+						/>
+					</View>
+				</View>
+			</View>
+
 		);
 	}
 }
@@ -342,4 +157,4 @@ const mapStateToProps = (state, ownProps) => ({
 	status: state.meteor.connected && ownProps.type === 'd' ? state.activeUsers[ownProps.id] : 'offline'
 });
 
-export default connect(mapStateToProps)(RoomItem);
+export default compose(connect(mapStateToProps), withTouchableComponent)(RoomItem);
---
+++ app/presentation/RoomItem/withTouchableComponent.js
@@ -0,0 +1,258 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
+
+// /* eslint-disable react/jsx-tag-spacing */
+// /* eslint-disable no-mixed-spaces-and-tabs */
+// /* eslint-disable react/jsx-closing-tag-location */
+// /* eslint-disable react/prop-types */
+// /* eslint-disable react/jsx-closing-bracket-location */
+// /* eslint-disable react/jsx-indent-props */
+// /* eslint-disable padded-blocks */
+// /* eslint-disable react/prefer-stateless-function */
+// /* eslint-disable no-undef */
+
+// /* eslint-disable react/self-closing-comp */
+// /* eslint-disable react/jsx-indent */
+// /* eslint-disable indent */
+// /* eslint-disable semi */
+// /* eslint-disable arrow-body-style */
+// /* eslint-disable no-trailing-spaces */
+// /* eslint-disable eol-last */
+
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Animated } from 'react-native';
+import {
+	RectButton,
+	PanGestureHandler,
+	State
+} from 'react-native-gesture-handler';
+import styles, {
+	ACTION_WIDTH,
+	SMALL_SWIPE,
+	LONG_SWIPE
+} from './styles';
+import { LeftActions, RightActions } from './Actions';
+
+const withTouchableComponent = (WrappedComponent) => {
+	class Test extends React.Component {
+        static propTypes = {
+        	type: PropTypes.string.isRequired,
+        	name: PropTypes.string.isRequired,
+        	baseUrl: PropTypes.string.isRequired,
+        	showLastMessage: PropTypes.bool,
+        	_updatedAt: PropTypes.string,
+        	lastMessage: PropTypes.object,
+        	alert: PropTypes.bool,
+        	unread: PropTypes.number,
+        	userMentions: PropTypes.number,
+        	id: PropTypes.string,
+        	prid: PropTypes.string,
+        	onPress: PropTypes.func,
+        	userId: PropTypes.string,
+        	username: PropTypes.string,
+        	token: PropTypes.string,
+        	avatarSize: PropTypes.number,
+        	withTouchableID: PropTypes.string,
+        	width: PropTypes.number,
+        	favorite: PropTypes.bool,
+        	isRead: PropTypes.bool,
+        	rid: PropTypes.string,
+        	status: PropTypes.string,
+        	toggleFav: PropTypes.func,
+        	toggleRead: PropTypes.func,
+        	hideChannel: PropTypes.func,
+        	avatar: PropTypes.bool
+        }
+
+        constructor(props) {
+        	super(props);
+        	this.dragX = new Animated.Value(0);
+        	this.rowOffSet = new Animated.Value(0);
+        	this.transX = Animated.add(
+        		this.rowOffSet,
+        		this.dragX
+        	);
+        	this.state = {
+        		rowState: 0 // 0: closed, 1: right opened, -1: left opened
+        	};
+        	this._onGestureEvent = Animated.event(
+        		[{ nativeEvent: { translationX: this.dragX } }]
+        	);
+        	this._value = 0;
+        }
+
+        _onHandlerStateChange = ({ nativeEvent }) => {
+ 	if (nativeEvent.oldState === State.ACTIVE) {
+ 	this._handleRelease(nativeEvent);
+        	}
+        };
+
+_handleRelease = (nativeEvent) => {
+	const { translationX } = nativeEvent;
+	const { rowState } = this.state;
+	this._value = this._value + translationX;
+
+	let toValue = 0;
+	if (rowState === 0) { // if no option is opened
+		if (translationX > 0 && translationX < LONG_SWIPE) {
+			toValue = ACTION_WIDTH; // open left option if he swipe right but not enough to trigger action
+			this.setState({ rowState: -1 });
+		} else if (translationX >= LONG_SWIPE) {
+			toValue = 0;
+			this.toggleRead();
+		} else if (translationX < 0 && translationX > -LONG_SWIPE) {
+			toValue = -2 * ACTION_WIDTH; // open right option if he swipe left
+			this.setState({ rowState: 1 });
+		} else if (translationX <= -LONG_SWIPE) {
+			toValue = 0;
+			this.setState({ rowState: 0 });
+			this.hideChannel();
+		} else {
+			toValue = 0;
+		}
+	}
+
+	if (rowState === -1) { // if left option is opened
+		if (this._value < SMALL_SWIPE) {
+			toValue = 0;
+			this.setState({ rowState: 0 });
+		} else if (this._value > LONG_SWIPE) {
+			toValue = 0;
+			this.setState({ rowState: 0 });
+			this.toggleRead();
+		} else {
+			toValue = ACTION_WIDTH;
+		}
+	}
+
+	if (rowState === 1) { // if right option is opened
+		if (this._value > -2 * SMALL_SWIPE) {
+			toValue = 0;
+			this.setState({ rowState: 0 });
+		} else if (this._value < -LONG_SWIPE) {
+			toValue = 0;
+			this.setState({ rowState: 0 });
+			this.hideChannel();
+		} else {
+			toValue = -2 * ACTION_WIDTH;
+		}
+	}
+	this._animateRow(toValue);
+}
+
+_animateRow = (toValue) => {
+	this.rowOffSet.setValue(this._value);
+	this._value = toValue;
+	this.dragX.setValue(0);
+	Animated.spring(this.rowOffSet, {
+		toValue,
+		bounciness: 0,
+		useNativeDriver: true
+	}).start();
+}
+
+close = () => {
+	this.setState({ rowState: 0 });
+	this._animateRow(0);
+}
+
+toggleFav = () => {
+	const { toggleFav, rid, favorite } = this.props;
+	if (toggleFav) {
+		toggleFav(rid, favorite);
+	}
+	this.close();
+};
+
+toggleRead = () => {
+	const { toggleRead, rid, isRead } = this.props;
+	if (toggleRead) {
+		toggleRead(rid, isRead);
+	}
+};
+
+hideChannel = () => {
+	const { hideChannel, rid, type } = this.props;
+	if (hideChannel) {
+		hideChannel(rid, type);
+	}
+};
+
+onToggleReadPress = () => {
+	this.toggleRead();
+	this.close();
+};
+
+onHidePress = () => {
+	this.hideChannel();
+	this.close();
+};
+
+onPress = () => {
+	const { rowState } = this.state;
+	if (rowState !== 0) {
+		this.close();
+		return;
+	}
+	const { onPress } = this.props;
+	if (onPress) {
+		onPress();
+	}
+};
+
+render() {
+	const {
+		// eslint-disable-next-line no-unused-vars
+		withTouchableID, isRead, width, favorite
+	} = this.props;
+
+	return (
+
+		<PanGestureHandler
+			minDeltaX={20}
+			onGestureEvent={this._onGestureEvent}
+			onHandlerStateChange={this._onHandlerStateChange}
+		>
+			<Animated.View>
+				<LeftActions
+					transX={this.transX}
+					isRead={isRead}
+					width={width}
+					onToggleReadPress={this.onToggleReadPress}
+				/>
+				<RightActions
+					transX={this.transX}
+					favorite={favorite}
+					width={width}
+					toggleFav={this.toggleFav}
+					onHidePress={this.onHidePress}
+				/>
+				<Animated.View
+					style={{
+						transform: [{ translateX: this.transX }]
+					}}
+				>
+					<RectButton
+						onPress={this.onPress}
+						activeOpacity={0.8}
+						underlayColor='#e1e5e8'
+						withTouchableID
+						style={styles.button}
+					>
+						<WrappedComponent {...this.props} />
+					</RectButton>
+				</Animated.View>
+			</Animated.View>
+
+		</PanGestureHandler>
+	);
+}
+	}
+
+	return (
+		Test
+	);
+};
+
+export default withTouchableComponent;
