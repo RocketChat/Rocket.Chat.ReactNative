b3090f3be70cfcbbc883630cea619cc5231c1d73
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2019-10-18 08:15:45 -0300

    Custom User Agent on fetch/websocket

--- app/utils/fetch.js
+++ app/utils/fetch.js
@@ -1,3 +1,12 @@
-export const headers = { 'User-Agent': 'RC-RN Mobile' };
+import { Platform } from 'react-native';
+import VersionNumber from 'react-native-version-number';
 
-export default url => fetch(url, { headers });
+export const headers = { 'User-Agent': `RC-RN Mobile/${ VersionNumber.appVersion } (build: ${ VersionNumber.buildVersion }; os: ${ Platform.OS } ${ Platform.Version })` };
+
+export default (url, options = {}) => {
+	let customOptions = { ...options, headers };
+	if (options && options.headers) {
+		customOptions = { ...customOptions, headers: { ...options.headers, ...headers } };
+	}
+	return fetch(url, customOptions);
+};
