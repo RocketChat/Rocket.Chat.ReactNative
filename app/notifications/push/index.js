7a36accb8862c22152c74047ca45e65636635316
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2019-10-25 10:42:11 -0300

    Reply iOS Notifications

--- app/notifications/push/index.js
+++ app/notifications/push/index.js
@@ -1,55 +1,86 @@
+import RNUserDefaults from 'rn-user-defaults';
 import EJSON from 'ejson';
 
 import PushNotification from './push';
+import random from '../../utils/random';
 import store from '../../lib/createStore';
 import { deepLinkingOpen } from '../../actions/deepLinking';
+import { SERVERS, TOKEN, SERVER_URL } from '../../constants/userDefaults';
 import RocketChat from '../../lib/rocketchat';
+import database from '../../lib/database';
+
+const handleReply = async(action, host, rid) => {
+	if (action) {
+		const { text } = action;
+		let user = {};
+		const userId = await RNUserDefaults.get(`${ RocketChat.TOKEN_KEY }-${ host }`);
+		if (userId) {
+			try {
+				const serversDB = database.servers;
+				const userCollections = serversDB.collections.get('users');
+				const userRecord = await userCollections.find(userId);
+				user = {
+					id: userRecord.id,
+					token: userRecord.token
+				};
+			} catch (e) {
+				// We only run it if not has user on DB
+				const servers = await RNUserDefaults.objectForKey(SERVERS);
+				const userCredentials = servers && servers.find(srv => srv[SERVER_URL] === host);
+				user = userCredentials && {
+					id: userId,
+					token: userCredentials[TOKEN]
+				};
+			}
+		}
+		fetch(`${ host }/api/v1/chat.sendMessage`, {
+			method: 'POST',
+			headers: {
+				'content-type': 'application/json',
+				'x-auth-token': user.token,
+				'x-user-id': user.id
+			},
+			body: JSON.stringify({
+				message: {
+					_id: random(17),
+					rid,
+					msg: text,
+					tmid: null
+				}
+			})
+		});
+	}
+};
 
 export const onNotification = (notification, action) => {
 	if (notification) {
 		const data = notification.getData();
-		const { rid } = data;
-		if (action) {
-			const { text } = action;
-			RocketChat.sendMessage(rid, text, null, {
-				id
-				username
-				token
-			});
+
+		if (data) {
+			try {
+				const {
+					rid, name, sender, type, host
+				} = EJSON.parse(data.ejson);
+
+				if (action) {
+					handleReply(action, host, rid);
+				} else {
+					const types = {
+						c: 'channel', d: 'direct', p: 'group'
+					};
+					const roomName = type === 'd' ? sender.username : name;
+					const params = {
+						host,
+						rid,
+						path: `${ types[type] }/${ roomName }`
+					};
+					store.dispatch(deepLinkingOpen(params));
+				}
+			} catch (e) {
+				console.warn(e);
+			}
 		}
 	}
-
-	// 	if (data) {
-	// 		try {
-	// 			const {
-	// 				rid, name, sender, type, host
-	// 			} = EJSON.parse(data.ejson);
-
-	// 			if (action) {
-	// 				const { text } = action;
-	// 				RocketChat.sendMessage(rid, text, null, {
-	// 					id: 'phq3Dxnf9mNXSBcj4',
-	// 					username: 'djorkaeff.alexandre',
-	// 					token
-	// 				});
-	// 			}
-
-	// 			const types = {
-	// 				c: 'channel', d: 'direct', p: 'group'
-	// 			};
-	// 			const roomName = type === 'd' ? sender.username : name;
-
-	// 			const params = {
-	// 				host,
-	// 				rid,
-	// 				path: `${ types[type] }/${ roomName }`
-	// 			};
-	// 			store.dispatch(deepLinkingOpen(params));
-	// 		} catch (e) {
-	// 			console.warn(e);
-	// 		}
-	// 	}
-	// }
 };
 
 export const getDeviceToken = () => PushNotification.getDeviceToken();
