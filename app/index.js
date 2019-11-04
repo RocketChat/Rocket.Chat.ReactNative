682ac6b4a5fe692b3161fa80ab63ebfd85645615
Diego Mello <diegolmello@gmail.com>
2019-11-04 12:19:27 -0300

    [FIX] Paste (#1350)

--- app/index.js
+++ app/index.js
@@ -3,7 +3,6 @@ import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator } from 'react-navigation-drawer';
 import { Provider } from 'react-redux';
-import { useScreens } from 'react-native-screens'; // eslint-disable-line import/no-unresolved
 import { Linking } from 'react-native';
 import PropTypes from 'prop-types';
 
@@ -19,8 +18,12 @@ import { defaultHeader, onNavigationStateChange } from './utils/navigation';
 import { loggerConfig, analytics } from './utils/log';
 import Toast from './containers/Toast';
 import RocketChat from './lib/rocketchat';
+import { isIOS } from './utils/deviceInfo';
 
-useScreens();
+if (isIOS) {
+	const RNScreens = require('react-native-screens');
+	RNScreens.useScreens();
+}
 
 const parseDeepLinking = (url) => {
 	if (url) {
