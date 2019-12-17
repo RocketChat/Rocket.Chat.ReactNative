f1fcdf73791895a2b3d0acd1bd7881ced66f6b33
Guilherme Siqueira <guilhersiqueira@gmail.com>
2019-12-17 11:11:24 -0300

    [FIX] Crash while displaying the attached image with http on file name (#1401)

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -1,3 +1,3 @@
 export const formatAttachmentUrl = (attachmentUrl, userId, token, server) => (
-	encodeURI(attachmentUrl.includes('http') ? attachmentUrl : `${ server }${ attachmentUrl }?rc_uid=${ userId }&rc_token=${ token }`)
+	encodeURI(attachmentUrl.startsWith('http') ? attachmentUrl : `${ server }${ attachmentUrl }?rc_uid=${ userId }&rc_token=${ token }`)
 );
