02c91d765adc8a062ed8cd521ab3298bdf7f4582
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2019-12-23 15:44:54 -0300

    :clown_face: Mock UIKitMessage

--- app/containers/message/Blocks.js
+++ app/containers/message/Blocks.js
@@ -1,24 +1,46 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
+import { UiKitMessage } from '../UIKit';
 
-const Blocks = React.memo(({ blocks, id, rid }) => {
-	if (blocks && blocks.lenght > 0) {
-		return (
-			<View>
-				<Text>{id}</Text>
-				<Text>{rid}</Text>
-				{blocks.map(block => <Text>{JSON.stringify(block)}</Text>)}
-			</View>
-		);
+const mockBlocks = [
+	{
+		type: 'section',
+		text: {
+			type: 'plain_text',
+			text: 'Choose an action'
+		},
+		blockId: '6186484c-e359-411a-9175-5c3ebf0017cd',
+		appId: 'c33fa1a6-68a7-491e-bf49-9d7b99671c48'
+	},
+	{
+		type: 'actions',
+		elements: [
+			{
+				type: 'button',
+				text: {
+					type: 'plain_text',
+					text: 'Create poll'
+				},
+				actionId: 'create',
+				value: 'GENERAL'
+			}
+		],
+		blockId: '74fa4c2a-1ee9-4669-86bb-6d882a51af29',
+		appId: 'c33fa1a6-68a7-491e-bf49-9d7b99671c48'
+	}
+];
+
+const Blocks = React.memo((/* { blocks, id, rid } */) => {
+	if (mockBlocks) {
+		return <UiKitMessage blocks={mockBlocks} />;
 	}
 	return null;
 });
 
 Blocks.propTypes = {
-	blocks: PropTypes.array,
-	id: PropTypes.string,
-	rid: PropTypes.string
+	// blocks: PropTypes.array,
+	// id: PropTypes.string,
+	// rid: PropTypes.string
 };
 Blocks.displayName = 'MessageBlocks';
 
