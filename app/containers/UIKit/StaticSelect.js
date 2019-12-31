6837a7abe5d0314c82ac55b43475ff8eb02b88aa
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2019-12-31 12:50:50 -0300

    :lipstick: StaticSelect

--- app/containers/UIKit/StaticSelect.js
+++ app/containers/UIKit/StaticSelect.js
@@ -7,10 +7,14 @@ import sharedStyles from '../../views/Styles';
 import { themes } from '../../constants/colors';
 import { CustomIcon } from '../../lib/Icons';
 import { extractText } from './utils';
+import { isAndroid, isIOS } from '../../utils/deviceInfo';
 
 const styles = StyleSheet.create({
+	iosPadding: {
+		paddingVertical: 16
+	},
 	viewContainer: {
-		padding: 16,
+		paddingHorizontal: 16,
 		borderWidth: StyleSheet.hairlineWidth,
 		borderRadius: 2,
 		justifyContent: 'center'
@@ -18,6 +22,9 @@ const styles = StyleSheet.create({
 	pickerText: {
 		...sharedStyles.textRegular,
 		fontSize: 16
+	},
+	icon: {
+		right: 16
 	}
 });
 
@@ -27,14 +34,22 @@ export const StaticSelect = ({
 	theme = 'light'
 }) => {
 	const items = options.map(option => ({ label: extractText(option.text), value: option.value }));
+	const pickerStyle = {
+		...styles.viewContainer,
+		...(isIOS ? styles.iosPadding : {}),
+		borderColor: themes[theme].auxiliaryTintColor
+	};
 	return (
 		<RNPickerSelect
 			items={items}
 			placeholder={{}}
 			useNativeAndroidPickerStyle={false}
 			onValueChange={value => onChange({ value })}
-			style={{ viewContainer: { ...styles.viewContainer, borderColor: themes[theme].auxiliaryTintColor } }}
-			Icon={() => <CustomIcon size={22} name='arrow-down' color={themes[theme].auxiliaryText} />}
+			style={{
+				viewContainer: pickerStyle,
+				inputAndroidContainer: pickerStyle
+			}}
+			Icon={() => <CustomIcon size={22} name='arrow-down' style={isAndroid && styles.icon} color={themes[theme].auxiliaryText} />}
 			textInputProps={{ style: { ...styles.pickerText, color: themes[theme].auxiliaryText } }}
 		/>
 	);
