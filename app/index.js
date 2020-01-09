4d06b6886bdb9837be26f743f5a8028e949ee1be
Diego Mello <diegolmello@gmail.com>
2020-01-09 16:48:10 -0300

    Fix invite regex

--- app/index.js
+++ app/index.js
@@ -49,8 +49,7 @@ if (isIOS) {
 const parseDeepLinking = (url) => {
 	if (url) {
 		url = url.replace(/rocketchat:\/\/|https:\/\/go.rocket.chat\//, '');
-		// FIXME: fix after invite link PR is fixed
-		const regex = /^((room|auth)\?|[?])/;
+		const regex = /^(room|auth|invite)\?/;
 		if (url.match(regex)) {
 			url = url.replace(regex, '').trim();
 			if (url) {
