3569ee01066ad614c42d91f7782a76e0991ba86d
Mithilesh Tiwari <shubhamtiwari.tiwari84@gmail.com>
2020-01-13 02:57:49 +0530

    added a message which says to verify email and added navigation to login portal

--- app/views/RegisterView.js
+++ app/views/RegisterView.js
@@ -130,6 +130,15 @@ class RegisterView extends React.Component {
 			Alert.alert(I18n.t('Oops'), e.data.error);
 		}
 		this.setState({ saving: false });
+		const { navigation } = this.props;
+		Alert.alert(
+			I18n.t('Congrats'),
+			I18n.t('Verify_your_email'),
+			[{
+				text: I18n.t('Close'),
+				onPress: (() => { navigation.navigate('LoginView'); })
+			}]
+		);
 	}
 
 	renderCustomFields = () => {
