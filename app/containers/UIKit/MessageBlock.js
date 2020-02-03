7ed883adbf2142e31c9db33e3a8d1d4b5f990ea6
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2020-02-03 13:20:23 -0300

    :fire: Remove unused code

--- app/containers/UIKit/MessageBlock.js
+++ app/containers/UIKit/MessageBlock.js
@@ -4,11 +4,6 @@ import React from 'react';
 import { UiKitMessage, UiKitModal } from './index';
 import { KitContext } from './utils';
 
-const contextDefault = {
-	action: console.log,
-	state: console.log
-};
-
 export const messageBlockWithContext = context => ({ blocks }) => (
 	<KitContext.Provider value={context}>
 		{UiKitMessage(blocks)}
@@ -18,11 +13,5 @@ export const messageBlockWithContext = context => ({ blocks }) => (
 export const modalBlockWithContext = context => ({ blocks }) => (
 	<KitContext.Provider value={context}>
 		{UiKitModal(blocks)}
-	</KitContext.Provider>
-);
-
-export const MessageBlock = (props, context = contextDefault) => (
-	<KitContext.Provider value={context}>
-		{UiKitMessage(props)}
 	</KitContext.Provider>
 );
