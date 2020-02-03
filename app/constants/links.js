a5e20e116cf5fb74394b534191b8a3e61868a493
Diego Mello <diegolmello@gmail.com>
2020-02-03 13:34:55 -0300

    Refactor App Store ID

--- app/constants/links.js
+++ app/constants/links.js
@@ -1,8 +1,8 @@
 import { getBundleId, isIOS } from '../utils/deviceInfo';
 
-export const PLAY_MARKET_LINK = 'https://play.google.com/store/apps/details?id=chat.rocket.reactnative';
-export const APP_STORE_LINK = 'https://itunes.apple.com/app/rocket-chat-experimental/id1272915472?ls=1&mt=8';
-export const LICENSE_LINK = 'https://github.com/RocketChat/Rocket.Chat.ReactNative/blob/develop/LICENSE';
+const APP_STORE_ID = '1272915472';
 
-const id = getBundleId.includes('reactnative') ? '1272915472' : '1148741252';
-export const STORE_REVIEW_LINK = isIOS ? `itms-apps://itunes.apple.com/app/id${ id }?action=write-review` : `market://details?id=${ getBundleId }`;
+export const PLAY_MARKET_LINK = 'https://play.google.com/store/apps/details?id=chat.rocket.reactnative';
+export const APP_STORE_LINK = `https://itunes.apple.com/app/rocket-chat-experimental/${ APP_STORE_ID }?ls=1&mt=8`;
+export const LICENSE_LINK = 'https://github.com/RocketChat/Rocket.Chat.ReactNative/blob/develop/LICENSE';
+export const STORE_REVIEW_LINK = isIOS ? `itms-apps://itunes.apple.com/app/id${ APP_STORE_ID }?action=write-review` : `market://details?id=${ getBundleId }`;
