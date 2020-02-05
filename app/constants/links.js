98f12bc058c8adcbfc4223af6d5a156ad52b4974
Diego Mello <diegolmello@gmail.com>
2020-02-05 13:41:25 -0300

    [REGRESSION] Use HTTPS links for sharing and markets protocol for review (#1663)

--- app/constants/links.js
+++ app/constants/links.js
@@ -2,7 +2,7 @@ import { getBundleId, isIOS } from '../utils/deviceInfo';
 
 const APP_STORE_ID = '1272915472';
 
-export const PLAY_MARKET_LINK = `market://details?id=${ getBundleId }`;
-export const APP_STORE_LINK = `itms-apps://itunes.apple.com/app/id${ APP_STORE_ID }`;
+export const PLAY_MARKET_LINK = `https://play.google.com/store/apps/details?id=${ getBundleId }`;
+export const APP_STORE_LINK = `https://itunes.apple.com/app/id${ APP_STORE_ID }`;
 export const LICENSE_LINK = 'https://github.com/RocketChat/Rocket.Chat.ReactNative/blob/develop/LICENSE';
-export const STORE_REVIEW_LINK = isIOS ? `${ APP_STORE_LINK }?action=write-review` : PLAY_MARKET_LINK;
+export const STORE_REVIEW_LINK = isIOS ? `itms-apps://itunes.apple.com/app/id${ APP_STORE_ID }?action=write-review` : `market://details?id=${ getBundleId }`;
