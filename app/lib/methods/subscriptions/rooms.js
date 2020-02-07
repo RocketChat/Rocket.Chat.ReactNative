e8d610dd8ef2062aadd128f53aeca721b7e5d225
Diego Mello <diegolmello@gmail.com>
2020-02-07 09:46:39 -0300

    Run after interactions and tweak window time to 500ms

--- app/lib/methods/subscriptions/rooms.js
+++ app/lib/methods/subscriptions/rooms.js
@@ -1,4 +1,5 @@
 import { sanitizedRaw } from '@nozbe/watermelondb/RawRecord';
+import { InteractionManager } from 'react-native';
 
 import database from '../../database';
 import { merge } from '../helpers/mergeSubscriptionsRooms';
@@ -21,7 +22,7 @@ let subQueue = {};
 let subTimer = null;
 let roomQueue = {};
 let roomTimer = null;
-const WINDOW_TIME = 1000;
+const WINDOW_TIME = 500;
 
 const createOrUpdateSubscription = async(subscription, room) => {
 	try {
@@ -174,7 +175,9 @@ const debouncedUpdateSub = (subscription) => {
 			subQueue = {};
 			subTimer = null;
 			Object.keys(subBatch).forEach((key) => {
-				createOrUpdateSubscription(subBatch[key]);
+				InteractionManager.runAfterInteractions(() => {
+					createOrUpdateSubscription(subBatch[key]);
+				});
 			});
 		}, WINDOW_TIME);
 	}
@@ -188,7 +191,9 @@ const debouncedUpdateRoom = (room) => {
 			roomQueue = {};
 			roomTimer = null;
 			Object.keys(roomBatch).forEach((key) => {
-				createOrUpdateSubscription(null, roomBatch[key]);
+				InteractionManager.runAfterInteractions(() => {
+					createOrUpdateSubscription(null, roomBatch[key]);
+				});
 			});
 		}, WINDOW_TIME);
 	}
