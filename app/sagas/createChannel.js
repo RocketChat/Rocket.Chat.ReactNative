5c974990bac46e3217a33286f18a57cfa4316860
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2020-02-21 12:59:13 -0300

    [FIX] Join Room (#1769)
    Co-authored-by: Diego Mello <diegolmello@gmail.com>

--- app/sagas/createChannel.js
+++ app/sagas/createChannel.js
@@ -1,10 +1,12 @@
 import {
 	select, put, call, take, takeLatest
 } from 'redux-saga/effects';
+import { sanitizedRaw } from '@nozbe/watermelondb/RawRecord';
 
 import { CREATE_CHANNEL, LOGIN } from '../actions/actionsTypes';
 import { createChannelSuccess, createChannelFailure } from '../actions/createChannel';
 import RocketChat from '../lib/rocketchat';
+import database from '../lib/database';
 
 const create = function* create(data) {
 	return yield RocketChat.createChannel(data);
@@ -16,8 +18,22 @@ const handleRequest = function* handleRequest({ data }) {
 		if (!auth) {
 			yield take(LOGIN.SUCCESS);
 		}
-		const result = yield call(create, data);
-		yield put(createChannelSuccess(result));
+		const sub = yield call(create, data);
+
+		try {
+			const db = database.active;
+			const subCollection = db.collections.get('subscriptions');
+			yield db.action(async() => {
+				await subCollection.create((s) => {
+					s._raw = sanitizedRaw({ id: sub.rid }, subCollection.schema);
+					Object.assign(s, sub);
+				});
+			});
+		} catch {
+			// do nothing
+		}
+
+		yield put(createChannelSuccess(sub));
 	} catch (err) {
 		yield put(createChannelFailure(err));
 	}
