2951869ea5ae6a42520079989069ac54ad6ea5e0
TheTobin1 <tobinvandenhurk@gmail.com>
2020-03-03 14:56:36 +0100

    Update config.ts

--- modules/social-football/src/config/config.ts
+++ modules/social-football/src/config/config.ts
@@ -1,3 +1,4 @@
 export default {
-    api: 'https://sep-rewards-develop.herokuapp.com/graphql',
+    //api: 'https://sep-rewards-develop.herokuapp.com/graphql',
+    api: 'http://localhost:3000',
 };
