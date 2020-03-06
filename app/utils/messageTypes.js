e28c452f42316cc76620846af64d923c0c505417
Youssef Muhamad <yiweigao0226@gmail.com>
2020-03-06 11:19:03 -0300

    [NEW] Filter system messages per room (#1815)
    Co-authored-by: Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
    Co-authored-by: Diego Mello <diegolmello@gmail.com>

---
+++ app/utils/messageTypes.js
@@ -0,0 +1,42 @@
+export const MessageTypeValues = [
+	{
+		value: 'uj',
+		text: 'Message_HideType_uj'
+	}, {
+		value: 'ul',
+		text: 'Message_HideType_ul'
+	}, {
+		value: 'ru',
+		text: 'Message_HideType_ru'
+	}, {
+		value: 'au',
+		text: 'Message_HideType_au'
+	}, {
+		value: 'mute_unmute',
+		text: 'Message_HideType_mute_unmute'
+	}, {
+		value: 'r',
+		text: 'Message_HideType_r'
+	}, {
+		value: 'ut',
+		text: 'Message_HideType_ut'
+	}, {
+		value: 'wm',
+		text: 'Message_HideType_wm'
+	}, {
+		value: 'rm',
+		text: 'Message_HideType_rm'
+	}, {
+		value: 'subscription_role_added',
+		text: 'Message_HideType_subscription_role_added'
+	}, {
+		value: 'subscription_role_removed',
+		text: 'Message_HideType_subscription_role_removed'
+	}, {
+		value: 'room_archived',
+		text: 'Message_HideType_room_archived'
+	}, {
+		value: 'room_unarchived',
+		text: 'Message_HideType_room_unarchived'
+	}
+];
