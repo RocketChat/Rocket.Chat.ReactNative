ea2bbefd58e278265b3c747dde0b79512fffc1f1
devyaniChoubey <devyanichoubey16@gmail.com>
2020-03-09 11:52:22 +0530

    fixed

--- app/views/NotificationPreferencesView/index.js
+++ app/views/NotificationPreferencesView/index.js
@@ -252,7 +252,7 @@ class NotificationPreferencesView extends React.Component {
 						theme={theme}
 					/>
 					<Separator theme={theme} />
-					<Info info={I18n.t('Receive_notifications_from', { name: room.fname })} theme={theme} />
+					<Info info={I18n.t('Receive_notifications_from', { name: room.prid ? room.fname : room.name })} theme={theme} />
 					<SectionSeparator theme={theme} />
 
 					<Separator theme={theme} />
