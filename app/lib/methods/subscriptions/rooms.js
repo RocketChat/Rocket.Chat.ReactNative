411bc3a2add926b559e43586869189bc5f97779e
Ezequiel De Oliveira <ezequiel1de1oliveira@gmail.com>
2020-03-28 03:00:44 -0300

    [FIX] removed lastMessage.url from rooms to stop flash in the url preview for the last message  when reactions changed
    Signed-off-by: Ezequiel De Oliveira <ezequiel1de1oliveira@gmail.com>

--- app/lib/methods/subscriptions/rooms.js
+++ app/lib/methods/subscriptions/rooms.js
@@ -140,6 +140,8 @@ const createOrUpdateSubscription = async(subscription, room) => {
 
 			if (tmp.lastMessage) {
 				const lastMessage = buildMessage(tmp.lastMessage);
+				// This last Message have a different format when merge two streams results in a error caused by the urls object
+				delete lastMessage.urls;
 				const messagesCollection = db.collections.get('messages');
 				let messageRecord;
 				try {
