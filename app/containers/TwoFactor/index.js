38152b90c4cd35546423b279ddd148249e438df8
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2020-03-31 18:45:30 -0300

    [NEW] Password 2fa

--- app/containers/TwoFactor/index.js
+++ app/containers/TwoFactor/index.js
@@ -1,67 +1,41 @@
 import React, { useEffect, useState } from 'react';
-import {
-	Modal,
-	View,
-	Text,
-	StyleSheet
-} from 'react-native';
+import { Modal, View, Text } from 'react-native';
 import _ from 'lodash';
 import PropTypes from 'prop-types';
 
-import TextInput from './TextInput';
-import I18n from '../i18n';
-import EventEmitter from '../utils/events';
-import { withTheme } from '../theme';
-import { withSplit } from '../split';
-import { themes } from '../constants/colors';
-import Button from './Button';
-import sharedStyles from '../views/Styles';
-import RocketChat from '../lib/rocketchat';
+import TextInput from '../TextInput';
+import I18n from '../../i18n';
+import EventEmitter from '../../utils/events';
+import { withTheme } from '../../theme';
+import { withSplit } from '../../split';
+import { themes } from '../../constants/colors';
+import Button from '../Button';
+import sharedStyles from '../../views/Styles';
+import RocketChat from '../../lib/rocketchat';
+import styles from './styles';
 
 export const TWO_FACTOR = 'TWO_FACTOR';
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center'
+const methods = {
+	totp: {
+		text: 'Open_your_authentication_app_and_enter_the_code'
 	},
-	content: {
-		padding: 16,
-		width: '90%',
-		borderRadius: 4
+	email: {
+		text: 'Verify_your_email_for_the_code_we_sent'
 	},
-	title: {
-		fontSize: 14,
-		...sharedStyles.textBold
-	},
-	subtitle: {
-		fontSize: 14,
-		paddingVertical: 8,
-		...sharedStyles.textRegular,
-		...sharedStyles.textAlignCenter
-	},
-	sendEmail: {
-		fontSize: 14,
-		paddingBottom: 24,
-		paddingTop: 8,
-		alignSelf: 'center',
-		...sharedStyles.textRegular
-	},
-	button: {
-		marginBottom: 0
-	},
-	buttonContainer: {
-		flexDirection: 'row',
-		justifyContent: 'space-between'
+	password: {
+		title: 'Please_enter_your_password',
+		text: 'For_your_security_you_must_enter_your_current_password_to_continue',
+		secureTextEntry: true
 	}
-});
+};
 
 const TwoFactor = React.memo(({ theme, split }) => {
 	const [visible, setVisible] = useState(false);
 	const [data, setData] = useState({});
 	const [code, setCode] = useState('');
 
+	const method = methods[data.method];
 	const isEmail = data.method === 'email';
 
 	const sendEmail = () => RocketChat.sendEmailCode();
@@ -106,15 +80,9 @@ const TwoFactor = React.memo(({ theme, split }) => {
 		<Modal visible={visible} transparent>
 			<View style={[styles.container, { backgroundColor: `${ themes[theme].backdropColor }30` }]}>
 				<View style={[styles.content, split && [sharedStyles.modal, sharedStyles.modalFormSheet], { backgroundColor: themes[theme].backgroundColor }]}>
-					<Text style={[styles.title, { color }]}>{I18n.t('Two_Factor_Authentication')}</Text>
-					<Text style={[styles.subtitle, { color }]}>
-						{I18n.t(
-							isEmail
-								? 'Verify_your_email_for_the_code_we_sent'
-								: 'Open_your_authentication_app_and_enter_the_code'
-						)}
-					</Text>
-					<TextInput value={code} onChangeText={setCode} />
+					<Text style={[styles.title, { color }]}>{I18n.t(method?.title || 'Two_Factor_Authentication')}</Text>
+					<Text style={[styles.subtitle, { color }]}>{I18n.t(method?.text)}</Text>
+					<TextInput value={code} onChangeText={setCode} secureTextEntry={method?.secureTextEntry} />
 					{isEmail && <Text style={[styles.sendEmail, { color }]} onPress={sendEmail}>{I18n.t('Send_me_the_code_again')}</Text>}
 					<View style={styles.buttonContainer}>
 						<Button
