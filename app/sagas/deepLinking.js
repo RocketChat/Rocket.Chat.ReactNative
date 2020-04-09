1427e530bff6594f67af291c91919cda88d25f32
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2020-04-09 02:27:00 -0300

    [REGRESSION] Invite Links (#2007)
    Co-authored-by: Diego Mello <diegolmello@gmail.com>

--- app/sagas/deepLinking.js
+++ app/sagas/deepLinking.js
@@ -30,19 +30,21 @@ const handleInviteLink = function* handleInviteLink({ params, requireLogin = fal
 const navigate = function* navigate({ params }) {
 	yield put(appStart('inside'));
 	if (params.path) {
-		const room = yield RocketChat.canOpenRoom(params);
 		const [type, name] = params.path.split('/');
-		if (room) {
-			yield Navigation.navigate('RoomsListView');
-			Navigation.navigate('RoomView', {
-				name,
-				t: roomTypes[type],
-				roomUserId: RocketChat.getUidDirectMessage(room),
-				...room
-			});
+		if (type !== 'invite') {
+			const room = yield RocketChat.canOpenRoom(params);
+			if (room) {
+				yield Navigation.navigate('RoomsListView');
+				Navigation.navigate('RoomView', {
+					name,
+					t: roomTypes[type],
+					roomUserId: RocketChat.getUidDirectMessage(room),
+					...room
+				});
+			}
+		} else {
+			yield handleInviteLink({ params });
 		}
-	} else {
-		yield handleInviteLink({ params });
 	}
 };
 
