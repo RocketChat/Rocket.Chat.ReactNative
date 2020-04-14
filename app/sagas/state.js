9d2929836a148260af0a35240cb7191971bdc93a
Diego Mello <diegolmello@gmail.com>
2020-04-14 09:25:17 -0300

    Auth on app resume

--- app/sagas/state.js
+++ app/sagas/state.js
@@ -1,9 +1,11 @@
-import { takeLatest, select } from 'redux-saga/effects';
+import { takeLatest, select, put } from 'redux-saga/effects';
 import { FOREGROUND, BACKGROUND } from 'redux-enhancer-react-native-appstate';
+import * as LocalAuthentication from 'expo-local-authentication';
 
 import RocketChat from '../lib/rocketchat';
 import { setBadgeCount } from '../notifications/push';
 import log from '../utils/log';
+import * as actions from '../actions';
 
 const appHasComeBackToForeground = function* appHasComeBackToForeground() {
 	const appRoot = yield select(state => state.app.root);
@@ -15,6 +17,10 @@ const appHasComeBackToForeground = function* appHasComeBackToForeground() {
 		return;
 	}
 	try {
+		const authResult = yield LocalAuthentication.authenticateAsync();
+		if (!authResult?.success) {
+			yield put(actions.appStart('locked'));
+		}
 		setBadgeCount();
 		return yield RocketChat.setUserPresenceOnline();
 	} catch (e) {
