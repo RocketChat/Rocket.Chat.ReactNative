b8cfebf40736d9a661f60d410ebbe496812bd7cb
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2020-04-30 14:55:35 -0300

    [FIX] Some alignments

--- app/views/RoomActionsView/index.js
+++ app/views/RoomActionsView/index.js
@@ -478,7 +478,7 @@ class RoomActionsView extends React.Component {
 					>
 						{t === 'd' && member._id ? <Status style={sharedStyles.status} id={member._id} /> : null }
 					</Avatar>
-					<View style={styles.roomTitleContainer}>
+					<View style={[styles.roomTitleContainer, item.disabled && styles.roomTitlePadding]}>
 						{room.t === 'd'
 							? <Text style={[styles.roomTitle, { color: themes[theme].titleText }]} numberOfLines={1}>{room.fname}</Text>
 							: (
