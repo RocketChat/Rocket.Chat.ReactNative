94c115af3976d356ac01091dacb795769e094779
Ezequiel De Oliveira <ezequiel1de1oliveira@gmail.com>
2020-05-27 12:18:54 -0300

    Removing setTimeout in the Dimensions middleware
    Signed-off-by: Ezequiel De Oliveira <ezequiel1de1oliveira@gmail.com>

--- app/lib/dimensionsMiddleware.js
+++ app/lib/dimensionsMiddleware.js
@@ -22,6 +22,5 @@ export default () => createStore => (...args) => {
 
 	Dimensions.addEventListener('change', handleDimensionsChange);
 
-	setTimeout(() => handleDimensionsChange(Dimensions.get('window')));
 	return store;
 };
