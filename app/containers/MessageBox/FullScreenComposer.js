b457cd1feed428f44e08bd3703a1bcd16bcaebf2
Ezequiel De Oliveira <ezequiel1de1oliveira@gmail.com>
2020-05-30 15:39:34 -0300

    Beginning animation
    Signed-off-by: Ezequiel De Oliveira <ezequiel1de1oliveira@gmail.com>

--- app/containers/MessageBox/FullScreenComposer.js
+++ app/containers/MessageBox/FullScreenComposer.js
@@ -1,20 +1,29 @@
 import React, { useState } from 'react';
 import { TextInput, StyleSheet, View } from 'react-native';
 import PropTypes from 'prop-types';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
+import Animated from 'react-native-reanimated';
+import { onGestureEvent, withSpring } from 'react-native-redash';
 
 import sharedStyles from '../../views/Styles';
 import LeftButtons from './LeftButtons';
 import RightButtons from './RightButtons';
 
+const { Value } = Animated;
+const { UNDETERMINED } = State;
 const TOP = 0;
 const BOTTOM = 450;
 const styles = StyleSheet.create({
-	input: {
+	container: {
 		position: 'absolute',
 		top: 0,
 		right: 0,
 		left: 0,
-		bottom: 0,
+		height: '100%',
+		paddingTop: 20,
+		backgroundColor: 'red'
+	},
+	input: {
 		textAlignVertical: 'top',
 		// paddingVertical: 12, needs to be paddingTop/paddingBottom because of iOS/Android's TextInput differences on rendering
 		padding: 15,
@@ -34,34 +43,52 @@ const styles = StyleSheet.create({
 });
 
 
-const MessageBox = ({ theme }) => {
+const MessageBox = React.memo(({ theme }) => {
 	const [text, setText] = useState();
-	const [up, setUp] = useState(true);
-	const translateY = up ? TOP : BOTTOM;
-
+	const translationY = new Value(0);
+	const velocityY = new Value(0);
+	const offset = new Value(0)
+	const state = new Value(UNDETERMINED);
+	const gestureHandler = onGestureEvent({
+		translationY,
+		state,
+		velocityY
+	});
+	const translateY = withSpring({
+		state,
+		offset,
+		value: translationY,
+		velocity: velocityY,
+		snapPoints: [TOP, BOTTOM]
+	});
+	/* const translateY = translationY; */
 	return (
 		<>
-			<TextInput
-				style={[styles.input, { transform: [{ translateY }] }]}
-				multiline
-				placeholder='New message'
-				value={text}
-				onChangeText={setText}
-				returnKeyType='default'
-				keyboardType='twitter'
-				blurOnSubmit={false}
-				underlineColorAndroid='transparent'
-				defaultValue=''
-			/>
+			<PanGestureHandler {...gestureHandler}>
+				<Animated.View style={[styles.container, { transform: [{ translateY }] }]}>
+					<TextInput
+						style={styles.input}
+						multiline
+						placeholder='New message'
+						value={text}
+						onChangeText={setText}
+						returnKeyType='default'
+						keyboardType='twitter'
+						blurOnSubmit={false}
+						underlineColorAndroid='transparent'
+						defaultValue=''
+					/>
+				</Animated.View>
+			</PanGestureHandler>
 			<View style={styles.buttons}>
 				<LeftButtons
 					theme={theme}
 					showEmojiKeyboard={false}
 					editing={false}
-					showMessageBoxActions={() => {}}
-					editCancel={() => {}}
-					openEmoji={() => { setUp(!up); }}
-					closeEmoji={() => {}}
+					showMessageBoxActions={() => { }}
+					editCancel={() => { }}
+					openEmoji={() => { }}
+					closeEmoji={() => { }}
 				/>
 				<View style={styles.rightButtons}>
 					<RightButtons
@@ -76,7 +103,7 @@ const MessageBox = ({ theme }) => {
 			</View>
 		</>
 	);
-};
+});
 
 MessageBox.propTypes = {
 	theme: PropTypes.string
