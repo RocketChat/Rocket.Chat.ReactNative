41ef5a9fb4edea7a598cf0ae851f05974409b43e
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2020-06-02 17:29:11 -0300

    [WIP] Add error styles

--- app/views/ShareView/styles.js
+++ app/views/ShareView/styles.js
@@ -3,6 +3,19 @@ import { StyleSheet } from 'react-native';
 import sharedStyles from '../Styles';
 
 export default StyleSheet.create({
+	// Error
+	container: {
+		flex: 1
+	},
+	centered: {
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	title: {
+		fontSize: 18,
+		...sharedStyles.textBold,
+		...sharedStyles.textAlignCenter
+	},
 	// Header
 	header: {
 		width: '70%',
