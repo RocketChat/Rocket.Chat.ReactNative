3dc3178e9296704b8a22b8e700bfe225eb753cf2
Diego Mello <diegolmello@gmail.com>
2020-06-23 10:27:26 -0300

    Thumb button

--- app/views/ShareView/Thumbs.js
+++ app/views/ShareView/Thumbs.js
@@ -1,18 +1,64 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FlatList, Image, View } from 'react-native';
-import { BorderlessButton, RectButton } from 'react-native-gesture-handler';
+import {
+	FlatList, Image, View, StyleSheet
+} from 'react-native';
+import { RectButton, TouchableOpacity, TouchableNativeFeedback } from 'react-native-gesture-handler';
 
 import { BUTTON_HIT_SLOP } from '../../containers/message/utils';
 import { themes } from '../../constants/colors';
 import { CustomIcon } from '../../lib/Icons';
-import styles from './styles';
+import { isIOS } from '../../utils/deviceInfo';
+
+const THUMB_SIZE = 64;
+
+const styles = StyleSheet.create({
+	list: {
+		height: 86,
+		paddingVertical: 8,
+		paddingHorizontal: 8
+	},
+	play: {
+		position: 'absolute',
+		left: 8,
+		top: 16
+	},
+	removeButton: {
+		position: 'absolute',
+		right: 6,
+		width: 28,
+		height: 28,
+		borderWidth: 2,
+		borderRadius: 14,
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	removeView: {
+		width: 28,
+		height: 28,
+		borderWidth: 2,
+		borderRadius: 14,
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	item: {
+		paddingTop: 8
+	},
+	thumb: {
+		width: THUMB_SIZE,
+		height: THUMB_SIZE,
+		borderRadius: 2,
+		marginRight: 16,
+		overflow: 'hidden'
+	}
+});
+
+const ThumbButton = isIOS ? TouchableOpacity : TouchableNativeFeedback;
 
 const Thumb = React.memo(({ item, theme }) => {
 	const type = item?.mime;
 
 	if (type?.match(/video/)) {
-		console.log(item);
 		const { uri } = item;
 		return (
 			<>
@@ -46,33 +92,28 @@ const Thumbs = React.memo(({ attachments, onPress, theme }) => {
 				horizontal
 				data={attachments}
 				renderItem={({ item, index }) => (
-					<RectButton style={styles.item} onPress={() => onPress(index)}>
-						<Thumb
-							item={item}
-							theme={theme}
-						/>
-						<RectButton
-							hitSlop={BUTTON_HIT_SLOP}
-							style={[styles.remove, { backgroundColor: themes[theme].bodyText, borderColor: themes[theme].auxiliaryBackground }]}
-						>
-							<View style={[
-								{ 
-									width: 28,
-									height: 28,
-									borderWidth: 2,
-									borderRadius: 14,
-									alignItems: 'center',
-									justifyContent: 'center',
-									borderColor: themes[theme].auxiliaryBackground
-								}]}>
-								<CustomIcon
-									name='Cross'
-									color={themes[theme].backgroundColor}
-									size={14}
-								/>
-							</View>
-						</RectButton>
-					</RectButton>
+					<ThumbButton style={styles.item} onPress={() => onPress(index)} activeOpacity={0.7}>
+						<>
+							<Thumb
+								item={item}
+								theme={theme}
+							/>
+							<RectButton
+								hitSlop={BUTTON_HIT_SLOP}
+								style={[styles.removeButton, { backgroundColor: themes[theme].bodyText, borderColor: themes[theme].auxiliaryBackground }]}
+								activeOpacity={1}
+								rippleColor={themes[theme].bannerBackground}
+							>
+								<View style={[styles.removeView, { borderColor: themes[theme].auxiliaryBackground }]}>
+									<CustomIcon
+										name='Cross'
+										color={themes[theme].backgroundColor}
+										size={14}
+									/>
+								</View>
+							</RectButton>
+						</>
+					</ThumbButton>
 				)}
 				style={[styles.list, { backgroundColor: themes[theme].auxiliaryBackground }]}
 			/>
--- app/views/ShareView/styles.js
+++ app/views/ShareView/styles.js
@@ -2,8 +2,6 @@ import { StyleSheet } from 'react-native';
 
 import sharedStyles from '../Styles';
 
-const THUMB_SIZE = 64;
-
 export default StyleSheet.create({
 	container: {
 		flex: 1
@@ -27,38 +25,6 @@ export default StyleSheet.create({
 	},
 	textInput: {
 		height: '100%'
-	},
-	// Media
-	list: {
-		height: 86,
-		paddingVertical: 8,
-		paddingHorizontal: 8
-	},
-	play: {
-		position: 'absolute',
-		left: 8,
-		top: 16
-	},
-	remove: {
-		position: 'absolute',
-		right: 6,
-		// padding: 4,
-		width: 28,
-		height: 28,
-		borderWidth: 2,
-		borderRadius: 14,
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	item: {
-		paddingTop: 8
-	},
-	thumb: {
-		width: THUMB_SIZE,
-		height: THUMB_SIZE,
-		borderRadius: 2,
-		marginRight: 16,
-		overflow: 'hidden'
 	},
 	// Error
 	centered: {
