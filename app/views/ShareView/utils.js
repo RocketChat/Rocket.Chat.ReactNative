b0def4c6321e2c74e1447cd7959b7ca5eb85201a
Diego Mello <diegolmello@gmail.com>
2020-06-26 15:36:21 -0300

    Preview 3MB

--- app/views/ShareView/utils.js
+++ app/views/ShareView/utils.js
@@ -1,4 +1,4 @@
 import { isAndroid } from '../../utils/deviceInfo';
 
-// Limit preview to 5MB on iOS share extension
-export const allowPreview = (isShareExtension, size) => isAndroid || !isShareExtension || size < 5000000;
+// Limit preview to 3MB on iOS share extension
+export const allowPreview = (isShareExtension, size) => isAndroid || !isShareExtension || size < 3000000;
