9db86d9235cfa2eaeb20acf46abc88e10f0f2aa2
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2020-07-07 12:35:09 -0300

    reply from notification android

--- android/app/src/main/java/chat/rocket/reactnative/Ejson.java
+++ android/app/src/main/java/chat/rocket/reactnative/Ejson.java
@@ -1,18 +1,36 @@
 package chat.rocket.reactnative;
 
+import com.tencent.mmkv.MMKV;
+
 public class Ejson {
     String host;
     String rid;
     String type;
     Sender sender;
 
+    private final MMKV mmkv;
+
     private String TOKEN_KEY = "reactnativemeteor_usertoken-";
+
+    public Ejson() {
+        // Start MMKV container
+        MMKV.initialize(CustomPushNotification.reactApplicationContext);
+        mmkv = MMKV.mmkvWithID(CustomPushNotification.reactApplicationContext.getPackageName());
+    }
 
     public String getAvatarUri() {
         if (type == null) {
             return null;
         }
-        return serverURL() + "/avatar/" + this.sender.username;
+        return serverURL() + "/avatar/" + this.sender.username + "?rc_token=" + token() + "&rc_uid=" + userId();
+    }
+
+    public String token() {
+        return mmkv.decodeString(TOKEN_KEY.concat(userId()), "");
+    }
+
+    public String userId() {
+        return mmkv.decodeString(TOKEN_KEY.concat(serverURL()), "");
     }
 
     public String serverURL() {
--- android/app/src/main/java/chat/rocket/reactnative/ReplyBroadcast.java
+++ android/app/src/main/java/chat/rocket/reactnative/ReplyBroadcast.java
@@ -25,7 +25,6 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
-import com.tencent.mmkv.MMKV;
 import com.wix.reactnativenotifications.core.NotificationIntentAdapter;
 
 public class ReplyBroadcast extends BroadcastReceiver {
@@ -69,20 +68,10 @@ public class ReplyBroadcast extends BroadcastReceiver {
 
         CustomPushNotification.clearMessages(notId);
 
-        // Start MMKV container
-        MMKV.initialize(mContext);
-        MMKV mmkv = MMKV.mmkvWithID(mContext.getPackageName());
-
-        String TOKEN_KEY = "reactnativemeteor_usertoken-";
-
-        // Get user credentials
-        String userId = mmkv.decodeString(TOKEN_KEY.concat(serverURL));
-        String userToken = mmkv.decodeString(TOKEN_KEY.concat(userId));
-
         RequestBody body = RequestBody.create(JSON, json);
         Request request = new Request.Builder()
-                .header("x-auth-token", userToken)
-                .header("x-user-id", userId)
+                .header("x-auth-token", ejson.token())
+                .header("x-user-id", ejson.userId())
                 .url(String.format("%s/api/v1/chat.sendMessage", serverURL))
                 .post(body)
                 .build();
