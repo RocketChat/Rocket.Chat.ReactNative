1f57d86d2073a19fbb24e4496c362ec4d433fbb8
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2020-07-09 17:45:05 -0300

    [CHORE] Improve reply notification code (iOS)

---
+++ ios/RNNotificationEventHandler+ReplyNotification.h
@@ -0,0 +1,13 @@
+//
+//  RNNotificationEventHandler+ReplyNotification.h
+//  RocketChatRN
+//
+//  Created by Djorkaeff Alexandre Vilela Pereira on 7/9/20.
+//  Copyright © 2020 Rocket.Chat. All rights reserved.
+//
+
+#import <react-native-notifications/RNNotificationEventHandler.h>
+#import <react-native-notifications/RNNotificationCenter.h>
+
+@interface RNNotificationEventHandler (ReplyNotification)
+@end
