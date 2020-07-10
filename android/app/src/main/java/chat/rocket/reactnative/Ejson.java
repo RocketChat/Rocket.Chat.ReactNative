bad050e827287acfa5ed93845a3509c045caad06
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2020-07-10 12:06:14 -0300

    android ejson retrieve encrypted data

--- android/app/src/main/java/chat/rocket/reactnative/Ejson.java
+++ android/app/src/main/java/chat/rocket/reactnative/Ejson.java
@@ -1,6 +1,28 @@
 package chat.rocket.reactnative;
 
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.Callback;
+
+import com.ammarahmed.mmkv.SecureKeystore;
 import com.tencent.mmkv.MMKV;
+
+import java.math.BigInteger;
+
+class RNCallback implements Callback {
+    public void invoke(Object... args) {
+
+    }
+}
+
+class Utils {
+    static public String toHex(String arg) {
+        try {
+            return String.format("%x", new BigInteger(1, arg.getBytes("UTF-8")));
+        } catch (Exception e) {
+            return "";
+        }
+    }
+}
 
 public class Ejson {
     String host;
@@ -8,14 +30,28 @@ public class Ejson {
     String type;
     Sender sender;
 
-    private final MMKV mmkv;
+    private MMKV mmkv;
 
     private String TOKEN_KEY = "reactnativemeteor_usertoken-";
 
     public Ejson() {
+        ReactApplicationContext reactApplicationContext = CustomPushNotification.reactApplicationContext;
+
         // Start MMKV container
-        MMKV.initialize(CustomPushNotification.reactApplicationContext);
-        mmkv = MMKV.mmkvWithID(CustomPushNotification.reactApplicationContext.getPackageName(), MMKV.SINGLE_PROCESS_MODE, "rocketchat");
+        MMKV.initialize(reactApplicationContext);
+        SecureKeystore secureKeystore = new SecureKeystore(reactApplicationContext);
+
+        // https://github.com/ammarahm-ed/react-native-mmkv-storage/blob/master/src/loader.js#L31
+        String alias = Utils.toHex("com.MMKV." + "chat.rocket.reactnative");
+
+        // Retrieve container password
+        secureKeystore.getSecureKey(alias, new RNCallback() {
+            @Override
+            public void invoke(Object... args) {
+                String password = (String) args[1];
+                mmkv = MMKV.mmkvWithID(reactApplicationContext.getPackageName(), MMKV.SINGLE_PROCESS_MODE, password);
+            }
+        });
     }
 
     public String getAvatarUri() {
@@ -26,11 +62,11 @@ public class Ejson {
     }
 
     public String token() {
-        return mmkv.decodeString(TOKEN_KEY.concat(userId()), "");
+        return mmkv.decodeString(TOKEN_KEY.concat(userId()));
     }
 
     public String userId() {
-        return mmkv.decodeString(TOKEN_KEY.concat(serverURL()), "");
+        return mmkv.decodeString(TOKEN_KEY.concat(serverURL()));
     }
 
     public String serverURL() {
