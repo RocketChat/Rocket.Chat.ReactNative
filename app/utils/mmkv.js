d8245f12bc0ae3579ba24d093da41f19679a4c7e
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2020-07-10 11:21:38 -0300

    use fix instanceID

--- app/utils/mmkv.js
+++ app/utils/mmkv.js
@@ -1,10 +1,14 @@
 import MMKVStorage from 'react-native-mmkv-storage';
 
-import { getBundleId } from './deviceInfo';
+/*
+ *	We can't use getBundleId here because
+ *	it's different when using iOS Share Extension.
+ */
+const instanceID = 'chat.rocket.reactnative';
 
 export default new MMKVStorage.Loader()
 	// MODES.MULTI_PROCESS = ACCESSIBLE BY APP GROUP (iOS)
 	.setProcessingMode(MMKVStorage.MODES.MULTI_PROCESS)
-	.withInstanceID(getBundleId)
+	.withInstanceID(instanceID)
 	.withEncryption()
 	.initialize();
