9e2573998a0adbcd9973b45f6ffb27aa3d4a9166
Ezequiel De Oliveira <ezequiel1de1oliveira@gmail.com>
2020-07-11 02:15:33 -0300

    Change to class to work with performance
    Signed-off-by: Ezequiel De Oliveira <ezequiel1de1oliveira@gmail.com>

--- app/containers/MessageBox/FullScreenComposer.js
+++ app/containers/MessageBox/FullScreenComposer.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {
 	View, TouchableOpacity, Keyboard
 } from 'react-native';
 import { KeyboardAccessoryView } from 'react-native-keyboard-input';
 import Modal from 'react-native-modal';
+import equal from 'deep-equal';
 
 import TextInput from '../../presentation/TextInput';
 import styles from './styles';
@@ -19,81 +20,138 @@ import Mentions from './Mentions';
 import CommandsPreview from './CommandsPreview';
 import { CustomIcon } from '../../lib/Icons';
 
-const FullScreenComposer = React.forwardRef(({
-	closeEmoji,
-	toggleFullScreen,
-	commandPreview,
-	editCancel,
-	editing,
-	finishAudioMessage,
-	getCustomEmoji,
-	iOSScrollBehavior,
-	isActionsEnabled,
-	isFullScreen,
-	mentions,
-	message,
-	Message_AudioRecorderEnabled,
-	onChangeText,
-	onKeyboardResigned,
-	onEmojiSelected,
-	openEmoji,
-	recording,
-	recordingCallback,
-	replyCancel,
-	replying,
-	showCommandPreview,
-	showEmojiKeyboard,
-	showMessageBoxActions,
-	showSend,
-	submit,
-	text,
-	theme,
-	toggleRecordAudioWithState,
-	trackingType,
-	user
-}, ref) => {
-	const animationTime = 300;
-	const backgroundColor = editing ? themes[theme].chatComponentBackground : themes[theme].messageboxBackground;
-	const [modalState, setModalState] = useState(isFullScreen);
-	const { component, tracking } = ref;
+const ANIMATIONTIME = 300;
 
-	const isAndroidTablet = isTablet && isAndroid ? {
-		multiline: false,
-		onSubmitEditing: submit,
-		returnKeyType: 'send'
-	} : {};
+class FullScreenComposer extends Component {
+	static PropTypes = {
+		closeEmoji: PropTypes.func,
+		commandPreview: PropTypes.array,
+		editing: PropTypes.bool,
+		editCancel: PropTypes.func,
+		finishAudioMessage: PropTypes.func,
+		getCustomEmoji: PropTypes.func,
+		iOSScrollBehavior: PropTypes.number,
+		isActionsEnabled: PropTypes.bool,
+		isFullScreen: PropTypes.bool,
+		mentions: PropTypes.array,
+		message: PropTypes.object,
+		Message_AudioRecorderEnabled: PropTypes.bool,
+		onChangeText: PropTypes.func,
+		onEmojiSelected: PropTypes.func,
+		onKeyboardResigned: PropTypes.func,
+		openEmoji: PropTypes.func,
+		recording: PropTypes.bool,
+		recordingCallback: PropTypes.func,
+		replying: PropTypes.bool,
+		replyCancel: PropTypes.func,
+		showSend: PropTypes.bool,
+		showEmojiKeyboard: PropTypes.bool,
+		showCommandPreview: PropTypes.bool,
+		showMessageBoxActions: PropTypes.func,
+		submit: PropTypes.func,
+		text: PropTypes.string,
+		toggleRecordAudioWithState: PropTypes.func,
+		theme: PropTypes.string,
+		toggleFullScreen: PropTypes.func,
+		trackingType: PropTypes.array,
+		user: PropTypes.shape({
+			id: PropTypes.string,
+			username: PropTypes.string,
+			token: PropTypes.string
+		}),
+		innerRef: PropTypes.object
+	};
 
-	function closeModal() {
+	constructor(props){
+		super(props);
+		this.state = {};
+	}
+
+	shouldComponentUpdate(nextProps) {
+		const {
+			theme,
+			replying, 
+			editing, 
+			showEmojiKeyboard,
+			showSend, 
+			recording, 
+			mentions, 
+			commandPreview,
+			message, 
+		} = this.props;
+
+		if (nextProps.theme !== theme) {
+			return true;
+		}
+		if (nextProps.replying !== replying) {
+			return true;
+		}
+		if (nextProps.editing !== editing) {
+			return true;
+		}
+		if (nextProps.showEmojiKeyboard !== showEmojiKeyboard) {
+			return true;
+		}
+		if (nextProps.showSend !== showSend) {
+			return true;
+		}
+		if (nextProps.recording !== recording) {
+			return true;
+		}
+		if (!equal(nextProps.mentions, mentions)) {
+			return true;
+		}
+		if (!equal(nextProps.commandPreview, commandPreview)) {
+			return true;
+		}
+		if (!equal(nextProps.message, message)) {
+			return true;
+		}
+		if (!equal(nextProps.message, message)) {
+			return true;
+		}
+		return false;
+	}
+
+	closeModal = () => {
+		const { toggleFullScreen } = this.props;
 		Keyboard.dismiss();
-		setTimeout(toggleFullScreen, animationTime * 0.09);
-		setModalState(!modalState);
+		toggleFullScreen();
 	}
 
-	function renderCloseButton() {
-		const buttonStyle = {
-			...styles.fullScreenComposerCloseButton,
-			backgroundColor: editing ? themes[theme].chatComponentBackground
-				: themes[theme].messageboxBackground
-		};
-		return (
-			<TouchableOpacity onPress={() => closeModal()} style={buttonStyle}>
-				<CustomIcon name='Cross' size={30} color={themes[theme].tintColor} />
-			</TouchableOpacity>
-		);
-	}
-
-	function startRecordingAudio() {
+	startRecordingAudio = () => {
+		const { toggleRecordAudioWithState } = this.props;
 		toggleRecordAudioWithState();
-		closeModal();
+		this.closeModal();
 	}
 
-	function renderFullScreenBottomBar() {
+	renderFullScreenBottomBar = ()  => {
+		const {
+			theme,
+			recordingCallback,
+			finishAudioMessage,
+			commandPreview,
+			showCommandPreview,
+			mentions,
+			trackingType,
+			showEmojiKeyboard,
+			editCancel,
+			openEmoji,
+			closeEmoji,
+			showSend,
+			submit,
+			showMessageBoxActions,
+			isActionsEnabled,
+			editing,
+			Message_AudioRecorderEnabled,
+		} = this.props;
+		
 		const recordAudio = showSend || !Message_AudioRecorderEnabled ? null : (
 			<RecordAudio
 				theme={theme}
 				recordingCallback={recordingCallback}
 				onFinish={finishAudioMessage}
-				onPress={() => startRecordingAudio()}
+				onPress={() => this.startRecordingAudio()}
 			/>
 		);
 
@@ -127,97 +185,97 @@ const FullScreenComposer = React.forwardRef(({
 		);
 	}
 
-	return (
-		<Modal
-			style={{ margin: 0 }}
-			isVisible={modalState}
-			useNativeDriver
-			hideModalContentWhileAnimating
-			animationInTiming={animationTime}
-			animationOutTiming={animationTime}
-		>
-			<View style={{ backgroundColor, flex: 1 }}>
-				{renderCloseButton()}
-				<TextInput
-					ref={component}
-					style={styles.fullScreenComposerInput}
-					returnKeyType='default'
-					keyboardType='twitter'
-					blurOnSubmit={false}
-					placeholder={I18n.t('New_Message')}
-					onChangeText={onChangeText}
-					underlineColorAndroid='transparent'
-					defaultValue={text}
-					multiline
-					autoFocus
-					editable={!recording}
-					testID='full-screen-messagebox-input'
-					theme={theme}
-					{...isAndroidTablet}
-				/>
-				<ReplyPreview
-					message={message}
-					close={replyCancel}
-					username={user.username}
-					replying={replying}
-					getCustomEmoji={getCustomEmoji}
-					theme={theme}
-				/>
-				<KeyboardAccessoryView
-					ref={tracking}
-					renderContent={renderFullScreenBottomBar}
-					kbInputRef={ref}
-					kbComponent={showEmojiKeyboard ? 'EmojiKeyboard' : null}
-					onKeyboardResigned={onKeyboardResigned}
-					onItemSelected={onEmojiSelected}
-					trackInteractive
-					// revealKeyboardInteractive
-					requiresSameParentToManageScrollView
-					addBottomView
-					bottomViewColor={themes[theme].messageboxBackground}
-					iOSScrollBehavior={iOSScrollBehavior}
-				/>
-			</View>
-		</Modal>
-	);
-});
+	render() {
+		const {
+			onChangeText,
+			text,
+			recording,
+			theme,
+			message,
+			replyCancel,
+			user,
+			replying,
+			getCustomEmoji,
+			showEmojiKeyboard,
+			onKeyboardResigned,
+			onEmojiSelected,
+			iOSScrollBehavior,
+			innerRef,
+			editing
+		} = this.props;
+		const { component, tracking } = innerRef;
+		const buttonStyle = {
+			...styles.fullScreenComposerCloseButton,
+			backgroundColor: editing ? themes[theme].chatComponentBackground
+				: themes[theme].messageboxBackground
+		};
+		const isAndroidTablet = isTablet && isAndroid ? {
+			multiline: false,
+			onSubmitEditing: submit,
+			returnKeyType: 'send'
+		} : {};
+		const backgroundColor = editing ? themes[theme].chatComponentBackground : themes[theme].messageboxBackground;
 
-FullScreenComposer.propTypes = {
-	closeEmoji: PropTypes.func,
-	commandPreview: PropTypes.array,
-	editing: PropTypes.bool,
-	editCancel: PropTypes.func,
-	finishAudioMessage: PropTypes.func,
-	getCustomEmoji: PropTypes.func,
-	iOSScrollBehavior: PropTypes.number,
-	isActionsEnabled: PropTypes.bool,
-	isFullScreen: PropTypes.bool,
-	mentions: PropTypes.array,
-	message: PropTypes.object,
-	Message_AudioRecorderEnabled: PropTypes.bool,
-	onChangeText: PropTypes.func,
-	onEmojiSelected: PropTypes.func,
-	onKeyboardResigned: PropTypes.func,
-	openEmoji: PropTypes.func,
-	recording: PropTypes.bool,
-	recordingCallback: PropTypes.func,
-	replying: PropTypes.bool,
-	replyCancel: PropTypes.func,
-	showSend: PropTypes.bool,
-	showEmojiKeyboard: PropTypes.bool,
-	showCommandPreview: PropTypes.bool,
-	showMessageBoxActions: PropTypes.func,
-	submit: PropTypes.func,
-	text: PropTypes.string,
-	toggleRecordAudioWithState: PropTypes.func,
-	theme: PropTypes.string,
-	toggleFullScreen: PropTypes.func,
-	trackingType: PropTypes.array,
-	user: PropTypes.shape({
-		id: PropTypes.string,
-		username: PropTypes.string,
-		token: PropTypes.string
-	})
-};
+		return (
 
-export default FullScreenComposer;
+			<Modal
+				style={{ margin: 0 }}
+				isVisible={true}
+				useNativeDriver
+				hideModalContentWhileAnimating
+				animationInTiming={ANIMATIONTIME}
+				animationOutTiming={ANIMATIONTIME}
+			>
+				<View style={{ backgroundColor, flex: 1 }}>
+					<TouchableOpacity onPress={() => this.closeModal()} style={buttonStyle}>
+						<CustomIcon name='Cross' size={30} color={themes[theme].tintColor} />
+					</TouchableOpacity>
+					<TextInput
+						ref={component}
+						style={styles.fullScreenComposerInput}
+						returnKeyType='default'
+						keyboardType='twitter'
+						blurOnSubmit={false}
+						placeholder={I18n.t('New_Message')}
+						onChangeText={onChangeText}
+						underlineColorAndroid='transparent'
+						defaultValue={text}
+						multiline
+						autoFocus
+						editable={!recording}
+						testID='full-screen-messagebox-input'
+						theme={theme}
+						{...isAndroidTablet}
+					/>
+					<ReplyPreview
+						message={message}
+						close={replyCancel}
+						username={user.username}
+						replying={replying}
+						getCustomEmoji={getCustomEmoji}
+						theme={theme}
+					/>
+					<KeyboardAccessoryView
+						ref={tracking}
+						renderContent={this.renderFullScreenBottomBar}
+						kbInputRef={component}
+						kbComponent={showEmojiKeyboard ? 'EmojiKeyboard' : null}
+						onKeyboardResigned={onKeyboardResigned}
+						onItemSelected={onEmojiSelected}
+						trackInteractive
+						// revealKeyboardInteractive
+						requiresSameParentToManageScrollView
+						addBottomView
+						bottomViewColor={themes[theme].messageboxBackground}
+						iOSScrollBehavior={iOSScrollBehavior}
+					/>
+				</View>
+			</Modal>
+		);
+	}
+
+}
+
+export default React.forwardRef((props, ref) => <FullScreenComposer 
+innerRef={ref} {...props}
+/>);
