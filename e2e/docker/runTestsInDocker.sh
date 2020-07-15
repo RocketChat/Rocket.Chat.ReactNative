d0181f2a7a78fd43fc8f519321ac999a82e3fd93
Dan Caseley <dan@caseley.me.uk>
2020-07-15 10:44:58 +0100

    Add some optional forcing of data.js version for Docker

--- e2e/docker/runTestsInDocker.sh
+++ e2e/docker/runTestsInDocker.sh
@@ -2,6 +2,7 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 ROOT_FOLDER=${SCRIPTPATH%%/e2e*} #Gets path up to (but excluding) "e2e" - this assumes this script is always held within the e2e folder
 PAUSE_ON_FAIL_FOR_DEBUG=0
+FORCE_DEFAULT_DOCKER_DATA=0 # Set this to 1 to automatically overwrite data.js with the working Docker version
 
 TEST_SUBSET="${1:-}"
 
@@ -17,6 +18,9 @@ function cleanup_and_exit () {
 echo "Running tests"
 
 cd "$ROOT_FOLDER"
+if [ $FORCE_DEFAULT_DOCKER_DATA == 1 ]; then
+    cp "./e2e/data/data.docker.js" "./e2e/data.js"
+fi
 npx detox test "$ROOT_FOLDER/e2e/tests/$TEST_SUBSET" -c ios.sim.release
 TEST_SUCCESS=$?
 if [ $TEST_SUCCESS != 0 ] && [ $PAUSE_ON_FAIL_FOR_DEBUG == 1 ]; then
