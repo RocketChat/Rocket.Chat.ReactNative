365752d5e34cee6f226494b7b275b85f1c999e83
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2020-07-21 18:09:27 -0300

    wrap mmkv api class

--- app/utils/userPreferences.js
+++ app/utils/userPreferences.js
@@ -1,7 +1,60 @@
 import MMKVStorage from 'react-native-mmkv-storage';
 
-export default new MMKVStorage.Loader()
+const MMKV = new MMKVStorage.Loader()
 	// MODES.MULTI_PROCESS = ACCESSIBLE BY APP GROUP (iOS)
 	.setProcessingMode(MMKVStorage.MODES.MULTI_PROCESS)
 	.withEncryption()
 	.initialize();
+
+class UserPreferences {
+	constructor() {
+		this.mmkv = MMKV;
+		this.encryption = this.mmkv.encryption;
+	}
+
+	getStringAsync(key) {
+		try {
+			const value = this.mmkv.getStringAsync(key);
+			return value;
+		} catch {
+			return null;
+		}
+	}
+
+	setStringAsync(key, value) {
+		return this.mmkv.setStringAsync(key, value);
+	}
+
+	getBoolAsync(key) {
+		try {
+			const value = this.mmkv.getBoolAsync(key);
+			return value;
+		} catch {
+			return null;
+		}
+	}
+
+	setBoolAsync(key, value) {
+		return this.mmkv.setBoolAsync(key, value);
+	}
+
+	getMapAsync(key) {
+		try {
+			const value = this.mmkv.getMapAsync(key);
+			return value;
+		} catch {
+			return null;
+		}
+	}
+
+	setMapAsync(key, value) {
+		return this.mmkv.setMapAsync(key, value);
+	}
+
+	removeItem(key) {
+		return this.mmkv.removeItem(key);
+	}
+}
+
+const userPreferences = new UserPreferences();
+export default userPreferences;
