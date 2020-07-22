0b69382e23ead7ac37c119172d7a0e31acc19a0a
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2020-07-22 17:14:23 -0300

    notification privacy working on ios

--- ios/NotificationService/NotificationService.swift
+++ ios/NotificationService/NotificationService.swift
@@ -1,6 +1,38 @@
 import CoreLocation
 import UserNotifications
 
+struct PushResponse: Decodable {
+    let success: Bool
+    let data: Data
+
+    struct Data: Decodable {
+        let notification: Notification
+
+        struct Notification: Decodable {
+            let notId: Int
+            let title: String
+            let text: String
+            let payload: Payload
+
+            struct Payload: Decodable, Encodable {
+                let host: String
+                let rid: String?
+                let type: String?
+                let sender: Sender?
+                let messageId: String
+                let notificationType: String?
+                let name: String?
+
+                struct Sender: Decodable, Encodable {
+                    let _id: String
+                    let username: String
+                    let name: String
+                }
+            }
+        }
+    }
+}
+
 class NotificationService: UNNotificationServiceExtension {
 
     var contentHandler: ((UNNotificationContent) -> Void)?
@@ -12,22 +44,30 @@ class NotificationService: UNNotificationServiceExtension {
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
         self.contentHandler = contentHandler
         bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
-        
+
         if let bestAttemptContent = bestAttemptContent {
-            let notificationType = bestAttemptContent.userInfo["notificationType"] as? String ?? ""
+            let ejson = (bestAttemptContent.userInfo["ejson"] as? String ?? "").data(using: .utf8)!
+            guard let data = try? (JSONDecoder().decode(PushResponse.Data.Notification.Payload.self, from: ejson)) else {
+              return
+            }
+
+            let notificationType = data.notificationType ?? ""
           
             // If the notification have the content at her payload, show it
             if notificationType != "message-id-only" {
-                contentHandler(bestAttemptContent);
-                return;
+                contentHandler(bestAttemptContent)
+                return
             }
           
             let suiteName = Bundle.main.object(forInfoDictionaryKey: "AppGroup") as! String
             let userDefaults = UserDefaults(suiteName: suiteName)
 
-            let server = bestAttemptContent.userInfo["server"] as! String
-            let msgId = bestAttemptContent.userInfo["msgId"] as! String
-          
+            var server = data.host
+            if (server.last == "/") {
+                server.removeLast()
+            }
+            let msgId = data.messageId
+
             let userId = userDefaults?.string(forKey: "reactnativemeteor_usertoken-\(server)")! ?? ""
             let token = userDefaults?.string(forKey: "reactnativemeteor_usertoken-\(userId)")! ?? ""
 
@@ -53,7 +93,7 @@ class NotificationService: UNNotificationServiceExtension {
             // Try again after X seconds
             DispatchQueue.main.asyncAfter(deadline: .now() + self.retryTimeout[self.retryCount], execute: {
               self.runRequest(request: request, bestAttemptContent: bestAttemptContent, contentHandler: contentHandler)
-              self.retryCount += 1;
+              self.retryCount += 1
             })
           }
         }
@@ -73,24 +113,24 @@ class NotificationService: UNNotificationServiceExtension {
                 // Process data
                 if let data = data {
                     // Parse data of response
-                    let json = try? (JSONSerialization.jsonObject(with: data) as! [String: Any])
-                    if let json = json {
-                      // Title
-                      if let title = json["title"] as? String {
-                        bestAttemptContent.title = title
-                      }
-                      // Body
-                      if let body = json["message"] as? String {
-                        bestAttemptContent.body = body
-                      }
-                      // Ejson
-                      if let ejson = json["ejson"] as? String {
-                        bestAttemptContent.userInfo["ejson"] = ejson;
+                    let push = try? (JSONDecoder().decode(PushResponse.self, from: data))
+                    if let push = push {
+                      if push.success {
+                        bestAttemptContent.title = push.data.notification.title
+                        bestAttemptContent.body = push.data.notification.text
+
+                        let payload = try? (JSONEncoder().encode(push.data.notification.payload))
+                        if let payload = payload {
+                            bestAttemptContent.userInfo["ejson"] = String(data: payload, encoding: .utf8) ?? "{}"
+                        }
+
+                        // Show notification with the content modified
+                        contentHandler(bestAttemptContent)
+                        return
                       }
                     }
-                    // Show notification with the content modified
-                    contentHandler(bestAttemptContent)
                 }
+                retryRequest()
             }
         }
       }
