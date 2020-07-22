6e5379152f1df1060c309ad8cac7ab1eefebacfe
Dan Caseley <yiweigao0226@gmail.com>
2020-07-22 17:32:21 +0100

    [TESTS] Idempotence for e2e tests (#2308)
    * Idempotence for Assorted
    
    * Idempotence for Onboarding
    
    * Idempotence for Rooms
    
    * Remove redundant expects
    
    * Fixes and improvements
    
    * Remove unneeded sleeps
    
    * Make stable following merge
    
    * Try solving early taps without long sleep
    
    Try solving early taps without long sleep (cont)
    
    Temporary CircleCI hack for quicker testing
    
    Add screenshots to CircleCI for failed tests
    
    Try solving early taps without long sleep (cont. 2)
    
    Revert "Temporary CircleCI hack for quicker testing"
    
    This reverts commit 4abef3a5827910c05b12ac8b8380275b60e8af4f.
    
    * Fix flaky test with a fluent wait on the tap
    
    * Add some new sleeps to workaround #2324
    
    * Add test artifacts to gitignore
    
    * More longpress for dodgy taps, wait for pin response
    
    Co-authored-by: Diego Mello <diegolmello@gmail.com>

--- e2e/tests/onboarding/04-createuser.spec.js
+++ e2e/tests/onboarding/04-createuser.spec.js
@@ -53,10 +53,8 @@ describe('Create user screen', () => {
 			await element(by.id('register-view-username')).replaceText(data.registeringUser.username);
 			await element(by.id('register-view-email')).replaceText(data.users.existing.email);
 			await element(by.id('register-view-password')).replaceText(data.registeringUser.password);
-			await sleep(300);
 			await element(by.id('register-view-submit')).tap();
 			await waitFor(element(by.text('Email already exists. [403]')).atIndex(0)).toExist().withTimeout(10000);
-			await expect(element(by.text('Email already exists. [403]')).atIndex(0)).toExist();
 			await element(by.text('OK')).tap();
 		});
 
@@ -65,10 +63,8 @@ describe('Create user screen', () => {
 			await element(by.id('register-view-username')).replaceText(data.users.existing.username);
 			await element(by.id('register-view-email')).replaceText(data.registeringUser.email);
 			await element(by.id('register-view-password')).replaceText(data.registeringUser.password);
-			await sleep(300);
 			await element(by.id('register-view-submit')).tap();
 			await waitFor(element(by.text('Username is already in use')).atIndex(0)).toExist().withTimeout(10000);
-			await expect(element(by.text('Username is already in use')).atIndex(0)).toExist();
 			await element(by.text('OK')).tap();
 		});
 
@@ -77,10 +73,8 @@ describe('Create user screen', () => {
 			await element(by.id('register-view-username')).replaceText(data.registeringUser.username);
 			await element(by.id('register-view-email')).replaceText(data.registeringUser.email);
 			await element(by.id('register-view-password')).replaceText(data.registeringUser.password);
-			await sleep(300);
 			await element(by.id('register-view-submit')).tap();
 			await waitFor(element(by.id('rooms-list-view'))).toBeVisible().withTimeout(60000);
-			await expect(element(by.id('rooms-list-view'))).toBeVisible();
 		});
 	});
 });
