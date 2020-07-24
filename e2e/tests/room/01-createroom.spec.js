d4e8154f5db5e6edfe30688e745a56a0f23f718e
Dan Caseley <dan@caseley.me.uk>
2020-07-24 13:19:14 +0100

    Be more tolerant of slow running apps in CI

--- e2e/tests/room/01-createroom.spec.js
+++ e2e/tests/room/01-createroom.spec.js
@@ -31,14 +31,14 @@ describe('Create room screen', () => {
 
 		describe('Usage', async() => {
 			it('should back to rooms list', async() => {
-				await waitFor(element(by.id('new-message-view-close'))).toBeVisible().withTimeout(2000);
+				await waitFor(element(by.id('new-message-view-close'))).toBeVisible().withTimeout(5000);
 				await element(by.id('new-message-view-close')).tap();
 
-				await waitFor(element(by.id('rooms-list-view'))).toBeVisible().withTimeout(2000);
+				await waitFor(element(by.id('rooms-list-view'))).toBeVisible().withTimeout(5000);
 				
 				await tryTapping(element(by.id('rooms-list-view-create-channel')), 3000);
 				//await element(by.id('rooms-list-view-create-channel')).tap();
-				await waitFor(element(by.id('new-message-view'))).toExist().withTimeout(2000);
+				await waitFor(element(by.id('new-message-view'))).toExist().withTimeout(5000);
 			});
 
 			it('should search user and navigate', async() => {
@@ -48,14 +48,14 @@ describe('Create room screen', () => {
 				await waitFor(element(by.id('room-view'))).toExist().withTimeout(10000);
 				await waitFor(element(by.id('room-view-title-rocket.cat'))).toExist().withTimeout(60000);
 				await tapBack();
-				await waitFor(element(by.id('rooms-list-view'))).toExist().withTimeout(2000);
+				await waitFor(element(by.id('rooms-list-view'))).toExist().withTimeout(5000);
 			});
 
 			it('should navigate to select users', async() => {
 				await element(by.id('rooms-list-view-create-channel')).tap();
-				await waitFor(element(by.id('new-message-view'))).toExist().withTimeout(2000);
+				await waitFor(element(by.id('new-message-view'))).toExist().withTimeout(5000);
 				await element(by.id('new-message-view-create-channel')).tap();
-				await waitFor(element(by.id('select-users-view'))).toExist().withTimeout(2000);
+				await waitFor(element(by.id('select-users-view'))).toExist().withTimeout(5000);
 			});
 		})
 	});
@@ -70,14 +70,14 @@ describe('Create room screen', () => {
 			await element(by.id('select-users-view-item-rocket.cat')).tap();
 			await waitFor(element(by.id('selected-user-rocket.cat'))).toBeVisible().withTimeout(10000);
 			await element(by.id('selected-user-rocket.cat')).tap();
-			await waitFor(element(by.id('selected-user-rocket.cat'))).toBeNotVisible().withTimeout(5000);
+			await waitFor(element(by.id('selected-user-rocket.cat'))).toBeNotVisible().withTimeout(10000);
 			await element(by.id('select-users-view-item-rocket.cat')).tap();
-			await waitFor(element(by.id('selected-user-rocket.cat'))).toBeVisible().withTimeout(5000);
+			await waitFor(element(by.id('selected-user-rocket.cat'))).toBeVisible().withTimeout(10000);
 		});
 
 		it('should navigate to create channel view', async() => {
 			await element(by.id('selected-users-view-submit')).tap();
-			await waitFor(element(by.id('create-channel-view'))).toBeVisible().withTimeout(5000);
+			await waitFor(element(by.id('create-channel-view'))).toBeVisible().withTimeout(10000);
 		});
 	})
 
@@ -110,18 +110,18 @@ describe('Create room screen', () => {
 				await waitFor(element(by.id(`room-view-title-${ room }`))).toExist().withTimeout(60000);
 				await expect(element(by.id(`room-view-title-${ room }`))).toExist();
 				await tapBack();
-				await waitFor(element(by.id('rooms-list-view'))).toExist().withTimeout(2000);
+				await waitFor(element(by.id('rooms-list-view'))).toExist().withTimeout(10000);
 				await waitFor(element(by.id(`rooms-list-view-item-${ room }`))).toExist().withTimeout(60000);
 				await expect(element(by.id(`rooms-list-view-item-${ room }`))).toExist();
 			});
 	
 			it('should create private room', async() => {
 				const room = `private${ data.random }`;
-				await waitFor(element(by.id('rooms-list-view'))).toExist().withTimeout(2000);
+				await waitFor(element(by.id('rooms-list-view'))).toExist().withTimeout(5000);
 				await element(by.id('rooms-list-view-create-channel')).tap();
-				await waitFor(element(by.id('new-message-view'))).toExist().withTimeout(2000);
+				await waitFor(element(by.id('new-message-view'))).toExist().withTimeout(5000);
 				await element(by.id('new-message-view-create-channel')).tap();
-				await waitFor(element(by.id('select-users-view'))).toExist().withTimeout(2000);
+				await waitFor(element(by.id('select-users-view'))).toExist().withTimeout(5000);
 				await element(by.id('select-users-view-item-rocket.cat')).tap();
 				await waitFor(element(by.id('selected-user-rocket.cat'))).toExist().withTimeout(5000);
 				await element(by.id('selected-users-view-submit')).tap();
@@ -133,21 +133,21 @@ describe('Create room screen', () => {
 				await waitFor(element(by.id(`room-view-title-${ room }`))).toExist().withTimeout(60000);
 				await expect(element(by.id(`room-view-title-${ room }`))).toExist();
 				await tapBack();
-				await waitFor(element(by.id('rooms-list-view'))).toExist().withTimeout(2000);
+				await waitFor(element(by.id('rooms-list-view'))).toExist().withTimeout(5000);
 				await waitFor(element(by.id(`rooms-list-view-item-${ room }`))).toExist().withTimeout(60000);
 				await expect(element(by.id(`rooms-list-view-item-${ room }`))).toExist();
 			});
 
 			it('should create empty room', async() => {
 				const room = `empty${ data.random }`;
-				await waitFor(element(by.id('rooms-list-view'))).toExist().withTimeout(2000);
+				await waitFor(element(by.id('rooms-list-view'))).toExist().withTimeout(10000);
 				// await device.launchApp({ newInstance: true });
 				await element(by.id('rooms-list-view-create-channel')).tap();
-				await waitFor(element(by.id('new-message-view'))).toExist().withTimeout(2000);
+				await waitFor(element(by.id('new-message-view'))).toExist().withTimeout(5000);
 				await element(by.id('new-message-view-create-channel')).tap();
-				await waitFor(element(by.id('select-users-view'))).toExist().withTimeout(2000);
+				await waitFor(element(by.id('select-users-view'))).toExist().withTimeout(5000);
 				await element(by.id('selected-users-view-submit')).tap();
-				await waitFor(element(by.id('create-channel-view'))).toExist().withTimeout(5000);
+				await waitFor(element(by.id('create-channel-view'))).toExist().withTimeout(10000);
 				await element(by.id('create-channel-name')).replaceText(room);
 				await element(by.id('create-channel-submit')).tap();
 				await waitFor(element(by.id('room-view'))).toExist().withTimeout(60000);
