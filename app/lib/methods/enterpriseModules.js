a562706cd47b2a11d5babb6d28c710f18fceae15
Diego Mello <diegolmello@gmail.com>
2020-08-13 10:39:18 -0300

    Minor refactor

--- app/lib/methods/enterpriseModules.js
+++ app/lib/methods/enterpriseModules.js
@@ -9,15 +9,13 @@ export const LICENSE_OMNICHANNEL_MOBILE_ENTERPRISE = 'omnichannel-mobile-enterpr
 
 export async function setEnterpriseModules() {
 	try {
-		const { version: serverVersion, server: serverId } = reduxStore.getState().server;
-		if (serverVersion && semver.gte(semver.coerce(serverVersion), '3.1.0')) {
-			const serversDB = database.servers;
-			const serversCollection = serversDB.collections.get('servers');
-			const server = await serversCollection.find(serverId);
-			if (server.enterpriseModules) {
-				reduxStore.dispatch(setEnterpriseModulesAction(server.enterpriseModules.split(',')));
-				return;
-			}
+		const { server: serverId } = reduxStore.getState().server;
+		const serversDB = database.servers;
+		const serversCollection = serversDB.collections.get('servers');
+		const server = await serversCollection.find(serverId);
+		if (server.enterpriseModules) {
+			reduxStore.dispatch(setEnterpriseModulesAction(server.enterpriseModules.split(',')));
+			return;
 		}
 		reduxStore.dispatch(clearEnterpriseModules());
 	} catch (e) {
@@ -30,6 +28,7 @@ export function getEnterpriseModules() {
 		try {
 			const { version: serverVersion, server: serverId } = reduxStore.getState().server;
 			if (serverVersion && semver.gte(semver.coerce(serverVersion), '3.1.0')) {
+				// RC 3.1.0
 				const enterpriseModules = await this.methodCallWrapper('license:getModules');
 				if (enterpriseModules) {
 					const serversDB = database.servers;
