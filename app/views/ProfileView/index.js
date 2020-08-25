50a5b093ccd54bbe49f9d441abb2952c32175abb
Diego Mello <diegolmello@gmail.com>
2020-08-25 13:41:12 -0300

    Change backgroundColor

--- app/views/ProfileView/index.js
+++ app/views/ProfileView/index.js
@@ -572,6 +572,7 @@ class ProfileView extends React.Component {
 						<Button
 							title={I18n.t('Logout_from_other_logged_in_locations')}
 							type='secondary'
+							backgroundColor={themes[theme].chatComponentBackground}
 							onPress={this.logoutOtherLocations}
 							testID='profile-view-logout-other-locations'
 							theme={theme}
