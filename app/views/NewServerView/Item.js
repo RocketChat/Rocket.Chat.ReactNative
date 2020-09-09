120c461d2eb636a9306494aa966075bac715ba67
Diego Mello <diegolmello@gmail.com>
2020-09-09 16:41:30 -0300

    Item

---
+++ app/views/NewServerView/Item.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {
+	View, StyleSheet, TouchableOpacity, Text
+} from 'react-native';
+import PropTypes from 'prop-types';
+
+import { themes } from '../../constants/colors';
+import { CustomIcon } from '../../lib/Icons';
+import sharedStyles from '../Styles';
+
+const styles = StyleSheet.create({
+	item: {
+		padding: 15,
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between'
+	},
+	itemText: {
+		...sharedStyles.textRegular,
+		fontSize: 16
+	}
+});
+
+const Item = ({
+	item, onPress, theme, deleteServerLink
+}) => (
+	<View style={styles.item}>
+		<TouchableOpacity onPress={() => onPress(item.link)}>
+			<Text style={[styles.itemText, { color: themes[theme].titleText }]}>{item.link}</Text>
+		</TouchableOpacity>
+		<TouchableOpacity onPress={() => deleteServerLink(item)}>
+			<CustomIcon name='close' size={16} color={themes[theme].auxiliaryText} />
+		</TouchableOpacity>
+	</View>
+);
+
+Item.propTypes = {
+	item: PropTypes.object,
+	theme: PropTypes.string,
+	onPress: PropTypes.func,
+	deleteServerLink: PropTypes.func
+};
+
+export default Item;
--- app/views/NewServerView/ServerInput.js
+++ app/views/NewServerView/ServerInput.js
@@ -1,26 +1,16 @@
 import React, { useState } from 'react';
-import { View, FlatList, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import { View, FlatList, StyleSheet } from 'react-native';
 import PropTypes from 'prop-types';
 
 import TextInput from '../../containers/TextInput';
 import { themes } from '../../constants/colors';
-import { CustomIcon } from '../../lib/Icons';
 import sharedStyles from '../Styles';
+import Item from './Item';
 
 const styles = StyleSheet.create({
 	inputContainer: {
 		marginTop: 24,
 		marginBottom: 32
-	},
-	item: {
-		padding: 15,
-		flexDirection: 'row',
-		alignItems: 'center',
-		justifyContent: 'space-between'
-	},
-	itemText: {
-		...sharedStyles.textRegular,
-		fontSize: 16
 	},
 	serverHistory: {
 		maxHeight: 180,
@@ -31,26 +21,6 @@ const styles = StyleSheet.create({
 		borderWidth: 0.5
 	}
 });
-
-const Item = ({
-	item, onPress, theme, deleteServerLink
-}) => (
-	<View style={styles.item}>
-		<TouchableOpacity onPress={() => onPress(item.link)}>
-			<Text style={[styles.itemText, { color: themes[theme].titleText }]}>{item.link}</Text>
-		</TouchableOpacity>
-		<TouchableOpacity onPress={() => deleteServerLink(item)}>
-			<CustomIcon name='close' size={16} color={themes[theme].auxiliaryText} />
-		</TouchableOpacity>
-	</View>
-);
-
-Item.propTypes = {
-	item: PropTypes.object,
-	theme: PropTypes.string,
-	onPress: PropTypes.func,
-	deleteServerLink: PropTypes.func
-};
 
 const ServerInput = ({
 	text,
