4b4944921ba05ab4712460d4a278402daaa8ca6c
Diego Mello <yiweigao0226@gmail.com>
2020-09-15 10:01:43 -0300

    [FIX] Local database searches using non-latin characters (#2462)
    * [FIX] Local database searches using non-latin characters
    
    * Add tests

---
+++ app/lib/database/utils.test.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-undef */
+import * as utils from './utils';
+
+describe('sanitizeLikeStringTester', () => {
+	// example chars that shouldn't return
+	const disallowedChars = ',./;[]!@#$%^&*()_-=+~';
+
+	const sanitizeLikeStringTester = str => expect(utils.sanitizeLikeString(`${ str }${ disallowedChars }`)).toBe(`${ str }${ '_'.repeat(disallowedChars.length) }`);
+
+	// Testing a couple of different alphabets
+	test('render test (latin)', () => {
+		sanitizeLikeStringTester('test123');
+	});
+
+	test('render test (arabic)', () => {
+		sanitizeLikeStringTester('اختبار123');
+	});
+
+	test('render test (russian)', () => {
+		sanitizeLikeStringTester('тест123');
+	});
+
+	test('render test (chinese trad)', () => {
+		sanitizeLikeStringTester('測試123');
+	});
+
+	test('render test (japanese)', () => {
+		sanitizeLikeStringTester('テスト123');
+	});
+});
+
+describe('sanitizer', () => {
+	test('render the same result', () => {
+		const content = { a: true };
+		expect(utils.sanitizer(content)).toBe(content);
+	});
+});
