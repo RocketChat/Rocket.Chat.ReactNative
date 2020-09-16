87902d0d3e3c07de12ba29d5c917351e0cba6a7b
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2020-09-16 11:50:54 -0300

    database fix: bypass watermelon cache

--- ios/NotificationService/Database.swift
+++ ios/NotificationService/Database.swift
@@ -10,10 +10,8 @@ import Foundation
 import WatermelonDB
 
 final class Database {
-  private var tag: Int = 0
-  private var databases: [String: Int] = [:]
-  private var cacheSet: [String: String] = [:]
-  private final let bridge = DatabaseBridge()
+  private var databases: [String: WatermelonDB.Database] = [:]
+  private var cache: [String: String] = [:]
   
   static let shared = Database()
   
@@ -23,42 +21,43 @@ final class Database {
         return directory.path
       }
     }
-
+    
     return nil
   }
   
-  func setup(server: String) -> Int? {
-    if let tag = databases[server] {
-      return tag
+  func setup(server: String) -> WatermelonDB.Database? {
+    if let database = databases[server] {
+      return database
     }
-
+    
     if let url = URL(string: server) {
       if let domain = url.domain, let directory = self.directory {
-        self.tag += 1
-        let tag = DatabaseBridge.ConnectionTag(value: self.tag)
-        let _ = bridge.initializeSynchronous(tag: tag, databaseName: "\(directory)/\(domain).db", schemaVersion: 10)
-        databases[server] = self.tag
-        return self.tag
+        databases[server] = WatermelonDB.Database(path: "\(directory)/\(domain).db")
+        return databases[server]
       }
     }
-
+    
     return nil
   }
   
   func readRoomEncryptionKey(rid: String, server: String) -> String? {
-    if let e2e = cacheSet[rid] {
-      return e2e
+    if let e2eKey = cache[rid] {
+      return e2eKey
     }
 
-    if let tag = setup(server: server) {
-      if let room = bridge.findSynchronous(tag: DatabaseBridge.ConnectionTag(value: tag), table: "subscriptions", id: rid) as? [String: Any] {
-        if let result = room["result"] as? [String: Any] {
-          if let e2e = result["e2e_key"] as? String {
-            cacheSet[rid] = e2e
-            return cacheSet[rid]
+    if let database = setup(server: server) {
+      let results = try! database.queryRaw("select * from subscriptions where id == ? limit 1", [rid])
+
+        guard let record = results.next() else {
+            return nil
+        }
+
+        if let room = record.resultDictionary! as? [String: Any] {
+          if let e2eKey = room["e2e_key"] as? String {
+            cache[rid] = e2eKey
+            return cache[rid]
           }
         }
-      }
     }
     
     return nil
