e7d452f2c8accb5a3aaccb7e189e9241a8b4aa63
Vitor.Leal <vitor_leal2201@hotmail.com>
2020-10-06 14:01:51 -0300

    Add insert join code text
    Signed-off-by: Vitor.Leal <vitor_leal2201@hotmail.com>

--- app/i18n/locales/pt-BR.js
+++ app/i18n/locales/pt-BR.js
@@ -256,6 +256,7 @@ export default {
 	Invite_users: 'Convidar usuários',
 	Join: 'Entrar',
 	Join_Code: 'Insira o Código da sala',
+	Insert_Join_Code: 'Insira o código para entrar na sala',
 	Join_our_open_workspace: 'Entra na nossa workspace pública',
 	Join_your_workspace: 'Entre na sua workspace',
 	Just_invited_people_can_access_this_channel: 'Apenas as pessoas convidadas podem acessar este canal',
