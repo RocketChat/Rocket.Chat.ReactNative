609e2cf2a4ddc5fe4a3ea8b797ff6ae5c62dbd03
Diego Mello <diegolmello@gmail.com>
2020-10-21 11:16:02 -0300

    Thread badge working

--- app/views/RoomView/Header/RightButtons.js
+++ app/views/RoomView/Header/RightButtons.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import isEqual from 'react-fast-compare';
 
 import * as HeaderButton from '../../../containers/HeaderButton';
 import database from '../../../lib/database';
 import { getUserSelector } from '../../../selectors/login';
 import { logEvent, events } from '../../../utils/log';
 
-
-class RightButtonsContainer extends React.PureComponent {
+class RightButtonsContainer extends Component {
 	static propTypes = {
 		userId: PropTypes.string,
 		threadsEnabled: PropTypes.bool,
@@ -23,20 +23,51 @@ class RightButtonsContainer extends React.PureComponent {
 	constructor(props) {
 		super(props);
 		this.state = {
-			isFollowingThread: true
+			isFollowingThread: true,
+			tunread: [],
+			tunreadUser: [],
+			tunreadGroup: []
 		};
 	}
 
 	async componentDidMount() {
-		const { tmid } = this.props;
+		const { tmid, rid } = this.props;
+		const db = database.active;
 		if (tmid) {
-			const db = database.active;
 			try {
 				const threadRecord = await db.collections.get('messages').find(tmid);
 				this.observeThead(threadRecord);
 			} catch (e) {
 				console.log('Can\'t find message to observe.');
 			}
+		}
+		if (rid) {
+			try {
+				const subCollection = db.collections.get('subscriptions');
+				const subRecord = await subCollection.find(rid);
+				this.updateSubscription(subRecord);
+				this.observeSubscription(subRecord);
+			} catch (e) {
+				console.log('Can\'t find subscription to observe.');
+			}
+		}
+	}
+
+	shouldComponentUpdate(nextProps, nextState) {
+		const {
+			isFollowingThread, tunread, tunreadUser, tunreadGroup
+		} = this.state;
+		if (nextState.isFollowingThread !== isFollowingThread) {
+			return true;
+		}
+		if (!isEqual(nextState.tunread, tunread)) {
+			return true;
+		}
+		if (!isEqual(nextState.tunreadUser, tunreadUser)) {
+			return true;
+		}
+		if (!isEqual(nextState.tunreadGroup, tunreadGroup)) {
+			return true;
 		}
 	}
 
@@ -56,6 +87,22 @@ class RightButtonsContainer extends React.PureComponent {
 		const { userId } = this.props;
 		this.setState({
 			isFollowingThread: thread.replies && !!thread.replies.find(t => t === userId)
+		});
+	}
+
+	observeSubscription = (subRecord) => {
+		const threadObservable = subRecord.observe();
+		this.threadSubscription = threadObservable
+			.subscribe((sub) => {
+				this.updateSubscription(sub);
+			});
+	}
+
+	updateSubscription = (sub) => {
+		this.setState({
+			tunread: sub?.tunread,
+			tunreadUser: sub?.tunreadUser,
+			tunreadGroup: sub?.tunreadGroup
 		});
 	}
 
@@ -93,7 +140,9 @@ class RightButtonsContainer extends React.PureComponent {
 	}
 
 	render() {
-		const { isFollowingThread } = this.state;
+		const {
+			isFollowingThread, tunread, tunreadUser, tunreadGroup
+		} = this.state;
 		const { t, tmid, threadsEnabled } = this.props;
 		if (t === 'l') {
 			return null;
@@ -116,6 +165,13 @@ class RightButtonsContainer extends React.PureComponent {
 						iconName='threads'
 						onPress={this.goThreadsView}
 						testID='room-view-header-threads'
+						badge={() => (
+							<HeaderButton.Badge
+								tunread={tunread}
+								tunreadUser={tunreadUser}
+								tunreadGroup={tunreadGroup}
+							/>
+						)}
 					/>
 				) : null}
 				<HeaderButton.Item
