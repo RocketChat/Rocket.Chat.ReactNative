997814d94a5923ab476f218fbf55459b145df525
Diego Mello <diegolmello@gmail.com>
2020-10-29 15:12:41 -0300

    Minor i18n fix

--- app/views/ThreadMessagesView/Dropdown/DropdownItemFilter.js
+++ app/views/ThreadMessagesView/Dropdown/DropdownItemFilter.js
@@ -2,10 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import DropdownItem from './DropdownItem';
+import I18n from '../../../i18n';
 
 const DropdownItemFilter = ({ currentFilter, value, onPress }) => (
 	<DropdownItem
-		text={value}
+		text={I18n.t(value)}
 		iconName={currentFilter === value ? 'check' : null}
 		onPress={() => onPress(value)}
 	/>
--- app/views/ThreadMessagesView/Dropdown/index.js
+++ app/views/ThreadMessagesView/Dropdown/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
-	Animated, Easing, StyleSheet, TouchableWithoutFeedback
+	Animated, Easing, TouchableWithoutFeedback
 } from 'react-native';
 import { withSafeAreaInsets } from 'react-native-safe-area-context';
 
