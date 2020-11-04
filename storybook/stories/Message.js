48cdc1e2e031c25b724d22fc86de5a41f078648f
Saket Mahajan <yiweigao0226@gmail.com>
2020-11-04 22:55:06 +0530

    [BUG] App isn't showing message for PDF/file uploads (#2584)
    * Fixed the issue #2531
    
    In app/containers/message/Reply.js added a View Contaier around the
    Attachment Touchable and Added a Markdown attribute with msg set to
    description of attachment to display the message if any.
    
    * Added the condition to check if File Description Exists
    
    Added an if statement to check if file description exists and if yes
    then add a markdown with value msg equal to the description.
    
    Also tested using 'yarn test -u' to add/update the tests.
    
    * Made the requested Changes
    
    Removed the condition to check for attachment description.
    Added the `markdown` inside the touchable and wrapped `attachmentContainer` and the `markdown` inside a `<>` component
    
    * Added file not showing message issue code in this branch
    
    * Fixed the mistake in return
    
    * fix
    
    * Add tests
    
    Co-authored-by: Diego Mello <diegolmello@gmail.com>

--- storybook/stories/Message.js
+++ storybook/stories/Message.js
@@ -346,14 +346,14 @@ export default ({ theme }) => {
 				attachments={[{
 					title: 'This is a title',
 					description: 'This is a description',
-					image_url: '/file-upload/2ZrxuwcGeTrsoh376/Clipboard%20-%20September%205,%202018%204:10%20PM'
+					image_url: '/dummypath'
 				}]}
 			/>
 			<Message
 				attachments={[{
 					title: 'This is a title',
 					description: 'This is a description :nyan_rocket:',
-					image_url: '/file-upload/sxLXBzjwuqxMnebyP/Clipboard%20-%2029%20de%20Agosto%20de%202018%20%C3%A0s%2018:10'
+					image_url: '/dummypath'
 				}]}
 			/>
 
@@ -362,13 +362,13 @@ export default ({ theme }) => {
 				attachments={[{
 					title: 'This is a title',
 					description: 'This is a description :nyan_rocket:',
-					video_url: '/file-upload/cqnKqb6kdajky5Rxj/WhatsApp%20Video%202018-08-22%20at%2019.09.55.mp4'
+					video_url: '/dummypath'
 				}]}
 			/>
 			<Message
 				attachments={[{
 					title: 'This is a title',
-					video_url: '/file-upload/cqnKqb6kdajky5Rxj/WhatsApp%20Video%202018-08-22%20at%2019.09.55.mp4'
+					video_url: '/dummypath'
 				}]}
 			/>
 
@@ -377,7 +377,7 @@ export default ({ theme }) => {
 				attachments={[{
 					title: 'This is a title',
 					description: 'This is a description :nyan_rocket:',
-					audio_url: '/file-upload/c4wcNhrbXJLBvAJtN/1535569819516.aac'
+					audio_url: '/dummypath'
 				}]}
 			/>
 			<Message msg='First message' isHeader={false} />
@@ -385,21 +385,36 @@ export default ({ theme }) => {
 				attachments={[{
 					title: 'This is a title',
 					description: 'This is a description',
-					audio_url: '/file-upload/c4wcNhrbXJLBvAJtN/1535569819516.aac'
+					audio_url: '/dummypath'
 				}]}
 				isHeader={false}
 			/>
 			<Message
 				attachments={[{
 					title: 'This is a title',
-					audio_url: '/file-upload/c4wcNhrbXJLBvAJtN/1535569819516.aac'
+					audio_url: '/dummypath'
 				}]}
 				isHeader={false}
 			/>
 			<Message
 				attachments={[{
 					title: 'This is a title',
-					audio_url: '/file-upload/c4wcNhrbXJLBvAJtN/1535569819516.aac'
+					audio_url: '/dummypath'
+				}]}
+				isHeader={false}
+			/>
+
+			<Separator title='With file' theme={theme} />
+			<Message
+				attachments={[{
+					text: 'File.pdf',
+					description: 'This is a description :nyan_rocket:'
+				}]}
+			/>
+			<Message
+				attachments={[{
+					text: 'File.pdf',
+					description: 'This is a description :nyan_rocket:'
 				}]}
 				isHeader={false}
 			/>
