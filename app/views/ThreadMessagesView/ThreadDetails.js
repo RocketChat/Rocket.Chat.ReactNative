7f6362dcef4b53db66a9cace9837a14c6da556fa
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2020-11-25 15:39:49 -0300

    improve: thread details component

--- app/views/ThreadMessagesView/ThreadDetails.js
+++ app/views/ThreadMessagesView/ThreadDetails.js
@@ -11,9 +11,9 @@ import sharedStyles from '../Styles';
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
+		width: '100%',
 		flexDirection: 'row',
-		alignItems: 'center',
-		justifyContent: 'space-between'
+		alignItems: 'center'
 	},
 	detailsContainer: {
 		flexDirection: 'row',
@@ -31,6 +31,8 @@ const styles = StyleSheet.create({
 		...sharedStyles.textSemibold
 	},
 	badgeContainer: {
+		right: 0,
+		position: 'absolute',
 		flexDirection: 'row',
 		alignItems: 'center'
 	},
@@ -55,6 +57,13 @@ const ThreadDetails = ({
 		tlm = formatDateThreads(item.tlm);
 	}
 
+	let { tcount } = item;
+	if (tcount >= 1000) {
+		tcount = '+999';
+	} else if (tcount >= 100) {
+		tcount = '+99';
+	}
+
 	const isFollowing = item.replies?.find(u => u === user?.id);
 
 	return (
@@ -62,17 +71,17 @@ const ThreadDetails = ({
 			<View style={styles.detailsContainer}>
 				<View style={styles.detailContainer}>
 					<CustomIcon name='threads' size={20} color={themes[theme].auxiliaryText} />
-					<Text style={[styles.detailText, { color: themes[theme].auxiliaryText }]}>{item?.tcount}</Text>
+					<Text style={[styles.detailText, { color: themes[theme].auxiliaryText }]} numberOfLines={1}>{tcount}</Text>
 				</View>
 
 				<View style={styles.detailContainer}>
 					<CustomIcon name='user' size={20} color={themes[theme].auxiliaryText} />
-					<Text style={[styles.detailText, { color: themes[theme].auxiliaryText }]}>{item?.replies?.length}</Text>
+					<Text style={[styles.detailText, { color: themes[theme].auxiliaryText }]} numberOfLines={1}>{item?.replies?.length}</Text>
 				</View>
 
 				<View style={styles.detailContainer}>
 					<CustomIcon name='clock' size={20} color={themes[theme].auxiliaryText} />
-					<Text style={[styles.detailText, { color: themes[theme].auxiliaryText }]}>{tlm}</Text>
+					<Text style={[styles.detailText, { color: themes[theme].auxiliaryText }]} numberOfLines={1}>{tlm}</Text>
 				</View>
 			</View>
 
