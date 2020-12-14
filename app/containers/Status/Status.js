0ffbf4121f2d3029e5b268cb4bb31dcdaab12d40
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2020-12-14 18:06:16 -0300

    storybook tests

--- app/containers/Status/Status.js
+++ app/containers/Status/Status.js
@@ -5,15 +5,20 @@ import { CustomIcon } from '../../lib/Icons';
 
 const Status = React.memo(({
 	status, size, style, ...props
-}) => (
-	<CustomIcon
-		style={style}
-		name={`status-${ status }`}
-		size={size}
-		color={STATUS_COLORS[status] ?? STATUS_COLORS.offline}
-		{...props}
-	/>
-));
+}) => {
+	const name = `status-${ status }`;
+	const isNameValid = CustomIcon.hasIcon(name);
+	const iconName = isNameValid ? name : 'status-offline';
+	return (
+		<CustomIcon
+			style={style}
+			size={size}
+			name={iconName}
+			color={STATUS_COLORS[status] ?? STATUS_COLORS.offline}
+			{...props}
+		/>
+	);
+});
 Status.propTypes = {
 	status: PropTypes.string,
 	size: PropTypes.number,
