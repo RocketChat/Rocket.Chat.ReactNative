df701a52a472e269bd78fc69f89aa51ca524102f
Diego Mello <diegolmello@gmail.com>
2020-12-22 15:50:23 -0300

    [CHORE] Update React Native to 0.63.4 (#2737)

--- ios/Pods/FlipperKit/iOS/Plugins/FlipperKitLayoutPlugin/FlipperKitLayoutPlugin/UIView+SKInvalidation.mm
+++ ios/Pods/FlipperKit/iOS/Plugins/FlipperKitLayoutPlugin/FlipperKitLayoutPlugin/UIView+SKInvalidation.mm
@@ -25,17 +25,33 @@ FB_LINKABLE(UIView_SKInvalidation)
         [self class], @selector(addSubview:), @selector(swizzle_addSubview:));
     swizzleMethods(
         [self class],
+        @selector(bringSubviewToFront:),
+        @selector(swizzle_bringSubviewToFront:));
+    swizzleMethods(
+        [self class],
+        @selector(sendSubviewToBack:),
+        @selector(swizzle_sendSubviewToBack:));
+    swizzleMethods(
+        [self class],
+        @selector(insertSubview:atIndex:),
+        @selector(swizzle_insertSubview:atIndex:));
+    swizzleMethods(
+        [self class],
+        @selector(insertSubview:aboveSubview:),
+        @selector(swizzle_insertSubview:aboveSubview:));
+    swizzleMethods(
+        [self class],
+        @selector(insertSubview:belowSubview:),
+        @selector(swizzle_insertSubview:belowSubview:));
+    swizzleMethods(
+        [self class],
+        @selector(exchangeSubviewAtIndex:withSubviewAtIndex:),
+        @selector(swizzle_exchangeSubviewAtIndex:withSubviewAtIndex:));
+    swizzleMethods(
+        [self class],
         @selector(removeFromSuperview),
         @selector(swizzle_removeFromSuperview));
   });
-}
-
-/**
-This function takes in a view and returns true if the view is a UIWindow and its
-windowLevel is an alert one otherwise it returns false.
-*/
-static auto shouldInvalidateRootNode(UIView* view) -> bool {
-  return [view isKindOfClass:[UIWindow class]];
 }
 
 - (void)swizzle_setHidden:(BOOL)hidden {
@@ -50,30 +66,51 @@ static auto shouldInvalidateRootNode(UIView* view) -> bool {
 
 - (void)swizzle_addSubview:(UIView*)view {
   [self swizzle_addSubview:view];
+  [[SKInvalidation sharedInstance].delegate invalidateNode:self];
+}
 
-  id<SKInvalidationDelegate> delegate =
-      [SKInvalidation sharedInstance].delegate;
-  if (delegate != nil) {
-    if (shouldInvalidateRootNode(view.superview)) {
-      [delegate invalidateRootNode];
-      return;
-    }
-    [delegate invalidateNode:view.superview];
-  }
+- (void)swizzle_bringSubviewToFront:(UIView*)subview {
+  [self swizzle_bringSubviewToFront:subview];
+  [[SKInvalidation sharedInstance].delegate invalidateNode:self];
+}
+
+- (void)swizzle_sendSubviewToBack:(UIView*)subview {
+  [self swizzle_sendSubviewToBack:subview];
+  [[SKInvalidation sharedInstance].delegate invalidateNode:self];
+}
+
+- (void)swizzle_insertSubview:(UIView*)subview atIndex:(NSInteger)index {
+  [self swizzle_insertSubview:subview atIndex:index];
+  [[SKInvalidation sharedInstance].delegate invalidateNode:self];
+}
+
+- (void)swizzle_insertSubview:(UIView*)subview
+                 aboveSubview:(UIView*)siblingSubview {
+  [self swizzle_insertSubview:subview aboveSubview:siblingSubview];
+  [[SKInvalidation sharedInstance].delegate invalidateNode:self];
+}
+
+- (void)swizzle_insertSubview:(UIView*)subview
+                 belowSubview:(UIView*)siblingSubview {
+  [self swizzle_insertSubview:subview belowSubview:siblingSubview];
+  [[SKInvalidation sharedInstance].delegate invalidateNode:self];
+}
+
+- (void)swizzle_exchangeSubviewAtIndex:(NSInteger)index1
+                    withSubviewAtIndex:(NSInteger)index2 {
+  [self swizzle_exchangeSubviewAtIndex:index1 withSubviewAtIndex:index2];
+  [[SKInvalidation sharedInstance].delegate invalidateNode:self];
 }
 
 - (void)swizzle_removeFromSuperview {
-  id<SKInvalidationDelegate> delegate =
-      [SKInvalidation sharedInstance].delegate;
-  if (delegate != nil && self.superview != nil) {
-    if (shouldInvalidateRootNode(self.superview)) {
-      [delegate invalidateRootNode];
-      return;
-    }
-    [delegate invalidateNode:self.superview];
-  }
-
+  UIView* oldSuperview = self.superview;
+  // Be careful that we always call the swizzled implementation
+  // before any early returns or mischief below!
   [self swizzle_removeFromSuperview];
+
+  if (oldSuperview) {
+    [[SKInvalidation sharedInstance].delegate invalidateNode:oldSuperview];
+  }
 }
 
 @end
