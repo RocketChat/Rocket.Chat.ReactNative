995d069fc0c93941ba5b884de2f70a9fc2d1056f
Diego Mello <diegolmello@gmail.com>
2021-01-21 13:28:28 -0300

    Fix indentation

--- app/views/RoomsListView/ListHeader/index.js
+++ app/views/RoomsListView/ListHeader/index.js
@@ -10,71 +10,69 @@ import { themes } from '../../../constants/colors';
 
 import OmnichannelStatus from '../../../ee/omnichannel/containers/OmnichannelStatus';
 
-const ListHeader = React.memo(
-	({
-		searching,
-		sortBy,
-		toggleSort,
-		goEncryption,
-		goQueue,
-		queueSize,
-		inquiryEnabled,
-		encryptionBanner,
-		user,
-		theme
-	}) => {
-		const sortTitle = I18n.t('Sorting_by', { key: I18n.t(sortBy === 'alphabetical' ? 'name' : 'activity') });
+const ListHeader = React.memo(({
+	searching,
+	sortBy,
+	toggleSort,
+	goEncryption,
+	goQueue,
+	queueSize,
+	inquiryEnabled,
+	encryptionBanner,
+	user,
+	theme
+}) => {
+	const sortTitle = I18n.t('Sorting_by', { key: I18n.t(sortBy === 'alphabetical' ? 'name' : 'activity') });
 
-		if (searching) { return null; }
+	if (searching) { return null; }
 
-		return (
-			<>
-				{encryptionBanner && (
-					<List.Item
-						title={
-							encryptionBanner === E2E_BANNER_TYPE.REQUEST_PASSWORD
-								? 'Enter_Your_E2E_Password'
-								: 'Save_Your_Encryption_Password'
-						}
-						left={() => (
-							<CustomIcon
-								name='encrypted'
-								color={themes[theme].buttonText}
-								size={22}
-							/>
-						)}
-						underlayColor={themes[theme].tintActive}
-						backgroundColor={themes[theme].actionTintColor}
-						color={themes[theme].buttonText}
-						onPress={goEncryption}
-						testId='listheader-encryption'
-					/>
-				)}
+	return (
+		<>
+			{encryptionBanner && (
 				<List.Item
-					title={sortTitle}
+					title={
+						encryptionBanner === E2E_BANNER_TYPE.REQUEST_PASSWORD
+							? 'Enter_Your_E2E_Password'
+							: 'Save_Your_Encryption_Password'
+					}
 					left={() => (
 						<CustomIcon
-							name='sort'
-							color={themes[theme].auxiliaryText}
+							name='encrypted'
+							color={themes[theme].buttonText}
 							size={22}
 						/>
 					)}
-					color={themes[theme].auxiliaryText}
-					onPress={toggleSort}
-					translateTitle={false}
+					underlayColor={themes[theme].tintActive}
+					backgroundColor={themes[theme].actionTintColor}
+					color={themes[theme].buttonText}
+					onPress={goEncryption}
+					testId='listheader-encryption'
 				/>
-				<List.Separator />
-				<OmnichannelStatus
-					searching={searching}
-					goQueue={goQueue}
-					inquiryEnabled={inquiryEnabled}
-					queueSize={queueSize}
-					user={user}
-				/>
-			</>
-		);
-	}
-);
+			)}
+			<List.Item
+				title={sortTitle}
+				left={() => (
+					<CustomIcon
+						name='sort'
+						color={themes[theme].auxiliaryText}
+						size={22}
+					/>
+				)}
+				color={themes[theme].auxiliaryText}
+				onPress={toggleSort}
+				translateTitle={false}
+			/>
+			<List.Separator />
+			<OmnichannelStatus
+				searching={searching}
+				goQueue={goQueue}
+				inquiryEnabled={inquiryEnabled}
+				queueSize={queueSize}
+				user={user}
+			/>
+		</>
+	);
+});
 
 ListHeader.propTypes = {
 	searching: PropTypes.bool,
