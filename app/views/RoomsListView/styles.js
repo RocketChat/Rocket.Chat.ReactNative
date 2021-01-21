bd09cd32a0dd2896accb12403abe5f92f01b71c8
Gerzon Z <gerzonc@icloud.com>
2021-01-21 15:52:26 -0400

    [FIX] Wrong styling on E2E encryption banner (#2767)
    * [FIX] Wrong styling on E2E encryption banner
    
    * [FIX] Wrong styling on E2E encryption banner
    
    * [FIX] Wrong styling on E2E encryption banner
    
    * [FIX] Wrong styling on E2E encryption banner (#2767)
    
    * Updated SortDropdown, ListHeader, ListItem and added stories for List.Item
    
    * Updated SortDropdown
    
    * Removed unused component
    
    * Updated List.Item and stories
    
    * Reverted unnecessary changes and updated ListItem stories
    
    * Fix minor indentation
    
    * Stop breaking Touch's default underlay color
    
    * Fix indentation
    
    * Remove falsy comparison from render
    
    * Fix left icon
    
    * Use List.Item on OmnichannelStatus
    
    * Add missing separator
    
    * Lint
    
    * Fix sort dropdown
    
    * Remove unnecessary styles
    
    * Fix detox
    
    Co-authored-by: Diego Mello <diegolmello@gmail.com>

--- app/views/RoomsListView/styles.js
+++ app/views/RoomsListView/styles.js
@@ -15,55 +15,21 @@ export default StyleSheet.create({
 		alignItems: 'center',
 		flexDirection: 'row'
 	},
-	sortToggleContainerClose: {
-		position: 'absolute',
-		top: 0,
-		width: '100%'
-	},
-	sortToggleText: {
-		fontSize: 16,
-		flex: 1,
-		...sharedStyles.textRegular
-	},
-	queueToggleText: {
-		fontSize: 16,
-		flex: 1,
-		...sharedStyles.textRegular
-	},
 	dropdownContainer: {
 		width: '100%',
 		position: 'absolute',
 		top: 0,
 		borderBottomWidth: StyleSheet.hairlineWidth
 	},
-	sortItemButton: {
-		height: 57,
-		justifyContent: 'center'
-	},
-	sortItemContainer: {
-		flexDirection: 'row',
-		alignItems: 'center'
-	},
-	sortItemText: {
-		fontSize: 18,
-		flex: 1,
-		...sharedStyles.textRegular
-	},
 	backdrop: {
 		...StyleSheet.absoluteFill
 	},
-	sortSeparator: {
-		height: StyleSheet.hairlineWidth,
-		marginHorizontal: 12,
-		flex: 1
-	},
-	sortIcon: {
-		width: 22,
-		height: 22,
-		marginHorizontal: 12
-	},
 	queueIcon: {
 		marginHorizontal: 12
+	},
+	omnichannelRightContainer: {
+		flexDirection: 'row',
+		alignItems: 'center'
 	},
 	groupTitleContainer: {
 		paddingHorizontal: 12,
@@ -90,56 +56,5 @@ export default StyleSheet.create({
 		marginRight: 12,
 		paddingVertical: 10,
 		...sharedStyles.textRegular
-	},
-	serverItem: {
-		height: 68
-	},
-	serverItemContainer: {
-		flexDirection: 'row',
-		alignItems: 'center',
-		height: 68
-	},
-	serverIcon: {
-		width: 42,
-		height: 42,
-		marginHorizontal: 12,
-		marginVertical: 13,
-		borderRadius: 4,
-		resizeMode: 'contain'
-	},
-	serverTextContainer: {
-		flex: 1,
-		flexDirection: 'column',
-		justifyContent: 'center'
-	},
-	serverName: {
-		fontSize: 18,
-		...sharedStyles.textSemibold
-	},
-	serverUrl: {
-		fontSize: 16,
-		...sharedStyles.textRegular
-	},
-	serverSeparator: {
-		height: StyleSheet.hairlineWidth,
-		marginLeft: 72
-	},
-	encryptionButton: {
-		width: '100%',
-		flexDirection: 'row',
-		alignItems: 'center',
-		padding: 12
-	},
-	encryptionIcon: {
-		...sharedStyles.textMedium
-	},
-	encryptionText: {
-		flex: 1,
-		fontSize: 16,
-		marginHorizontal: 16,
-		...sharedStyles.textMedium
-	},
-	omnichannelToggle: {
-		marginRight: 12
 	}
 });
