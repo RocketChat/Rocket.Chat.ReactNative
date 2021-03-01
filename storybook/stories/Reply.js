bf0e3d9b8f69f99329b3966208d02064103bb19b
Gerzon <gerzonc@icloud.com>
2021-03-01 14:49:35 -0400

    Update Reply stories

--- storybook/stories/Reply.js
+++ storybook/stories/Reply.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions, react/prop-types */
 import React from 'react';
-import { ScrollView } from "react-native";
+import { ScrollView, View } from "react-native";
 import { storiesOf } from '@storybook/react-native';
 
 import ReplyComponent from '../../app/containers/message/Reply';
@@ -19,7 +19,6 @@ const baseUrl = "https://open.rocket.chat";
 const stories = storiesOf('Reply', module);
 
 const item = {
-    color: 'red',
     ts: "1970-01-01T00:00:00.000Z",
 	author_name: "John Doe",
 	text: "This is a sample message",
@@ -29,15 +28,10 @@ const item = {
         value: "Lorem ipsum dolor sit amet",
         short: true
       },
-	  {
-        title: "Attachment's title:",
-		value: "[Link](https://google.com/) something and this and that.",
-        short: true,
-      }
     ],
   };
 
-const Reply = ({theme, ...props}) => (
+const Reply = ({ theme, ...props }) => (
 	<MessageContext.Provider
 		value={{
 			user,
@@ -45,16 +39,16 @@ const Reply = ({theme, ...props}) => (
 		}}
 	>
 		<ThemeContext.Provider value={theme}>
-			<ReplyComponent theme={theme} attachment={item} timeFormat="LT" {...props} />
+			<ReplyComponent theme={theme || 'light'} attachment={item} timeFormat="LT" {...props} />
 		</ThemeContext.Provider>
 	</MessageContext.Provider>
 );
 
 stories.add('content', () => (
-	<ScrollView style={{margin: 16}}>
-		<Reply theme={'light'} attachment={{ ...item, color: 'green' }}/>
+	<ScrollView style={{ margin: 16 }}>
+		<Reply />
+		<Reply attachment={{ ...item, color: 'green' }} />
 		<Reply 
-			theme={'light'} 
 			attachment={{
 							...item, 
 							color: 'orange',
@@ -62,22 +56,50 @@ stories.add('content', () => (
 								[
 									{
 										title: "Attachment's title:",
-										value: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet aliquet augue. Nulla malesuada enim ac magna pharetra viverra. Cras enim massa, tincidunt ac porta in, pharetra quis metus. Nam at imperdiet lorem. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum ut diam diam. Maecenas elementum tristique tristique. Sed egestas arcu sit amet mattis ultrices. Nam auctor sit amet massa eu volutpat. In at ultrices elit. Mauris venenatis metus arcu.",
+										value: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sit amet aliquet augue. Nulla malesuada enim ac magna pharetra viverra.",
 										short: false
 									}, 
 								]
 						}}
 		/>
-		<Reply theme={'light'} />
-		<Reply theme={'light'} />
-		<Reply theme={'light'} />
+		<Reply attachment={{...item,  
+								title: 'Name',
+								fields: [
+										{
+											title: "Attachment's title:",
+											value: "Lorem ipsum dolor sit amet",
+											short: true
+										},
+										{
+											title: "Attachment's title:",
+											value: "Lorem ipsum dolor sit amet",
+											short: true
+										},
+									],
+							}} 
+		/>
+		<Reply />
 	</ScrollView>
 ));
 
+const backgroundColor = (theme) => (
+	{
+		backgroundColor: themes[theme].backgroundColor, 
+		flex: 1,
+		padding: 16
+	}
+);
+
 stories.add('themes', () => (
-	<>
-		<ThemeStory theme={themes.light} />
-		<ThemeStory theme={themes.dark} />
-		<ThemeStory theme={themes.black} />
-	</>
+	<View style={{ flex: 1 }}>
+		<View style={backgroundColor('light')}>
+			<Reply theme={'light'} />
+		</View>
+		<View style={backgroundColor('dark')}>
+			<Reply theme={'dark'} />
+		</View>
+		<View style={backgroundColor('black')}>
+			<Reply theme={'black'} />
+		</View>
+	</View>
 ));
