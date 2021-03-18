460e82f759a5e8e42b10107508945eeb28b86bc4
Gerzon Z <gerzonc@icloud.com>
2021-03-18 09:33:35 -0400

    [CHORE] Simplify server version comparison (#2922)
    * Simplify server version where needed
    
    * Added lte and gte functions and updated imports
    
    * Updated functions names
    
    * Update util functions
    
    * Update util function and added methods
    
    * Remove lt and coerce from getPermissions and mergeSubscriptionsRooms
    
    * Fix comparison
    
    * Update getPermissions.js
    
    * Remove unused import
    
    * Fix lint
    
    * Fix lint
    
    Co-authored-by: Diego Mello <diegolmello@gmail.com>

--- app/lib/methods/getUsersPresence.js
+++ app/lib/methods/getUsersPresence.js
@@ -1,8 +1,7 @@
 import { InteractionManager } from 'react-native';
-import lt from 'semver/functions/lt';
-import gte from 'semver/functions/gte';
 import { sanitizedRaw } from '@nozbe/watermelondb/RawRecord';
 
+import { compareServerVersion, methods } from '../utils';
 import reduxStore from '../createStore';
 import { setActiveUsers } from '../../actions/activeUsers';
 import { setUser } from '../../actions/login';
@@ -12,7 +11,7 @@ export function subscribeUsersPresence() {
 	const serverVersion = reduxStore.getState().server.version;
 
 	// if server is lower than 1.1.0
-	if (serverVersion && lt(serverVersion, '1.1.0')) {
+	if (compareServerVersion(serverVersion, '1.1.0', methods.lowerThan)) {
 		if (this.activeUsersSubTimeout) {
 			clearTimeout(this.activeUsersSubTimeout);
 			this.activeUsersSubTimeout = false;
@@ -37,11 +36,11 @@ export default async function getUsersPresence() {
 	const { user: loggedUser } = reduxStore.getState().login;
 
 	// if server is greather than or equal 1.1.0
-	if (serverVersion && gte(serverVersion, '1.1.0')) {
+	if (compareServerVersion(serverVersion, '1.1.0', methods.greaterThanOrEqualTo)) {
 		let params = {};
 
 		// if server is greather than or equal 3.0.0
-		if (serverVersion && gte(serverVersion, '3.0.0')) {
+		if (compareServerVersion(serverVersion, '3.0.0', methods.greaterThanOrEqualTo)) {
 			// if not have any id
 			if (!ids.length) {
 				return;
