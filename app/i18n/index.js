809d23faab6a3bcddd71de98b6678437d6e0ab5c
Diego Mello <diegolmello@gmail.com>
2021-03-23 18:02:57 -0300

    Use json

--- app/i18n/index.js
+++ app/i18n/index.js
@@ -8,59 +8,59 @@ export const LANGUAGES = [
 	{
 		label: 'English',
 		value: 'en',
-		file: require('./locales/en').default
+		file: require('./locales/en.json')
 	}, {
 		label: '简体中文',
 		value: 'zh-CN',
-		file: require('./locales/zh-CN').default
+		file: require('./locales/zh-CN.json')
 	}, {
 		label: '繁體中文',
 		value: 'zh-TW',
-		file: require('./locales/zh-TW').default
+		file: require('./locales/zh-TW.json')
 	}, {
 		label: 'Deutsch',
 		value: 'de',
-		file: require('./locales/de').default
+		file: require('./locales/de.json')
 	}, {
 		label: 'Español (ES)',
 		value: 'es-ES',
-		file: require('./locales/es-ES').default
+		file: require('./locales/es-ES.json')
 	}, {
 		label: 'Français',
 		value: 'fr',
-		file: require('./locales/fr').default
+		file: require('./locales/fr.json')
 	}, {
 		label: 'Português (BR)',
 		value: 'pt-BR',
-		file: require('./locales/pt-BR').default
+		file: require('./locales/pt-BR.json')
 	}, {
 		label: 'Português (PT)',
 		value: 'pt-PT',
-		file: require('./locales/pt-PT').default
+		file: require('./locales/pt-PT.json')
 	}, {
 		label: 'Russian',
 		value: 'ru',
-		file: require('./locales/ru').default
+		file: require('./locales/ru.json')
 	}, {
 		label: 'Nederlands',
 		value: 'nl',
-		file: require('./locales/nl').default
+		file: require('./locales/nl.json')
 	}, {
 		label: 'Italiano',
 		value: 'it',
-		file: require('./locales/it').default
+		file: require('./locales/it.json')
 	}, {
 		label: '日本語',
 		value: 'ja',
-		file: require('./locales/ja').default
+		file: require('./locales/ja.json')
 	}, {
 		label: 'العربية',
 		value: 'ar',
-		file: require('./locales/ar').default
+		file: require('./locales/ar.json')
 	}, {
 		label: 'Türkçe',
 		value: 'tr',
-		file: require('./locales/tr').default
+		file: require('./locales/tr.json')
 	}
 ];
 
