185b2c0a9767dae056b6aaf3d9673d89d904dcf9
Gerzon Z <gerzonc@icloud.com>
2021-03-25 15:52:21 -0400

    Update deeplinking e2e

--- e2e/tests/assorted/11-deeplinking.spec.js
+++ e2e/tests/assorted/11-deeplinking.spec.js
@@ -7,6 +7,7 @@ const { navigateToLogin, login, mockMessage, tapBack, searchRoom } = require('..
 const testuser = data.users.regular
 const room = data.channels.detoxpublic.name;
 const mainRoom = data.groups.private.name;
+
 async function navigateToRoom(roomName) {
 	await searchRoom(`${ roomName }`);
 	await waitFor(element(by.id(`rooms-list-view-item-${ roomName }`))).toExist().withTimeout(60000);
@@ -14,25 +15,31 @@ async function navigateToRoom(roomName) {
 	await waitFor(element(by.id('room-view'))).toBeVisible().withTimeout(5000);
 }
 
-describe('Deep linking authentication', () => {
-	it('should be logged in server from URL', async() => {
-        await device.launchApp({ permissions: { notifications: 'YES' }, newInstance: true, delete: true });
-        await device.launchApp({
-            newInstance: true,
-            url: 'https://go.rocket.chat/auth',
-            sourceApp: 'com.apple.mobilesafari',
-            permissions: { notifications: 'YES' }
-          });
-		await waitFor(element(by.id('rooms-list-view'))).toBeVisible().withTimeout(60000);
-	});
+describe('Deep linking', () => {
+    // it('should handle push notification to room', async() => {
+    //     await device.launchApp({ permissions: { notifications: 'YES' }, newInstance: true, delete: true });
+    //     await device.sendUserNotification(localNotification);
+    //     await expect(element(by.text('from local notification'))).toBeVisible();
+    // })
 
-    it('should be logged in from URL sent to room', async() => {
-        await device.launchApp({ permissions: { notifications: 'YES' }, newInstance: true, delete: true });
-        await navigateToLogin();
-		await login(testuser.username, testuser.password);
-		await element(by.id('login-view-submit')).tap();
-		await waitFor(element(by.id('rooms-list-view'))).toBeVisible().withTimeout(60000);
-        await navigateToRoom();
-        await mockMessage('https://go.rocket.chat/auth');
-	});
+    describe('Deep linking authentication', () => {
+        it('should be logged in to server directly from URL', async() => {
+            await device.launchApp({
+                newInstance: false,
+                url: 'https://go.rocket.chat/auth', 
+                sourceApp: 'com.apple.mobilesafari'
+              });
+            await expect(element(by.text('Open'))).toBeVisible();
+        });
+    
+        it('should be logged in from URL sent to room', async() => {
+            await device.launchApp({ permissions: { notifications: 'YES' }, newInstance: true, delete: true });
+            await navigateToLogin();
+            await login(testuser.username, testuser.password);
+            await element(by.id('login-view-submit')).tap();
+            await waitFor(element(by.id('rooms-list-view'))).toBeVisible().withTimeout(60000);
+            await navigateToRoom();
+            await mockMessage('https://go.rocket.chat/auth');
+        });
+    });
 });
