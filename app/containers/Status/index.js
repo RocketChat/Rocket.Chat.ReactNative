5e734a67f0a7fb27b0db9e4576aee78c744c163d
Diego Mello <diegolmello@gmail.com>
2021-03-30 17:54:56 -0300

    Small refactor

--- app/containers/Status/index.js
+++ app/containers/Status/index.js
@@ -1,27 +1,16 @@
-import React from 'react';
+import React, { memo } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import Status from './Status';
 
-class StatusContainer extends React.PureComponent {
-	static propTypes = {
-		style: PropTypes.any,
-		size: PropTypes.number,
-		status: PropTypes.string
-	};
+const StatusContainer = memo(({ style, size = 32, status }) => <Status size={size} style={style} status={status} />);
 
-	static defaultProps = {
-		size: 32
-	}
-
-	render() {
-		const {
-			style, size, status
-		} = this.props;
-		return <Status size={size} style={style} status={status} />;
-	}
-}
+StatusContainer.propTypes = {
+	style: PropTypes.any,
+	size: PropTypes.number,
+	status: PropTypes.string
+};
 
 const mapStateToProps = (state, ownProps) => ({
 	status: state.meteor.connected ? (state.activeUsers[ownProps.id] && state.activeUsers[ownProps.id].status) : 'loading'
