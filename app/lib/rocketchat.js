fc52286bfa9e256e97941a06b7df0d5c7adac9af
Noach Magedman <nmagedman@gmail.com>
2021-05-05 19:02:26 +0300

    [FIX] Connecting stream listener not being cleared (#3008)
    Co-authored-by: Diego Mello <diegolmello@gmail.com>

--- app/lib/rocketchat.js
+++ app/lib/rocketchat.js
@@ -196,6 +196,10 @@ const RocketChat = {
 				clearTimeout(this.connectTimeout);
 			}
 
+			if (this.connectingListener) {
+				this.connectingListener.then(this.stopListener);
+			}
+
 			if (this.connectedListener) {
 				this.connectedListener.then(this.stopListener);
 			}
@@ -243,7 +247,7 @@ const RocketChat = {
 
 			sdkConnect();
 
-			this.connectedListener = this.sdk.onStreamData('connecting', () => {
+			this.connectingListener = this.sdk.onStreamData('connecting', () => {
 				reduxStore.dispatch(connectRequest());
 			});
 
