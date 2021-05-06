8f571fd0292dedbadbd0934626b763ab625bcf51
Diego Mello <diegolmello@gmail.com>
2021-05-06 11:06:52 -0300

    [FIX] App making calls to DDP after socket was killed by OS (#3062)
    Co-authored-by: Gerzon Z <gerzonc@icloud.com>

--- app/lib/rocketchat.js
+++ app/lib/rocketchat.js
@@ -1140,7 +1140,7 @@ const RocketChat = {
 	methodCall(...args) {
 		return new Promise(async(resolve, reject) => {
 			try {
-				const result = await this.sdk.methodCall(...args, this.code || '');
+				const result = await this.sdk?.methodCall(...args, this.code || '');
 				return resolve(result);
 			} catch (e) {
 				if (e.error && (e.error === 'totp-required' || e.error === 'totp-invalid')) {
