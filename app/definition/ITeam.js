a30b0000c7ca210c530cb8b0e12a49e4869cb671
Gerzon Z <gerzonzcanario@gmail.com>
2021-05-19 20:43:15 -0400

    Update ITeam.js

--- app/definition/ITeam.js
+++ app/definition/ITeam.js
@@ -1,5 +1,7 @@
 // https://github.com/RocketChat/Rocket.Chat/blob/develop/definition/ITeam.ts
-export const TEAM_TYPE = {
+const TEAM_TYPE = {
 	PUBLIC: 0,
 	PRIVATE: 1
 };
+
+module.exports = { TEAM_TYPE };
