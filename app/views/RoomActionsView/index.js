1fdd2da3fcc24166a369d7dc2c42edbb4cd4e3f8
Diego Mello <diegolmello@gmail.com>
2021-05-21 16:40:50 -0300

    Remove search from RoomActionsView

--- app/views/RoomActionsView/index.js
+++ app/views/RoomActionsView/index.js
@@ -588,7 +588,7 @@ class RoomActionsView extends React.Component {
 			room, membersCount, canViewMembers, canAddUser, canInviteUser, joined, canAutoTranslate, canForwardGuest, canReturnQueue
 		} = this.state;
 		const {
-			rid, t, encrypted
+			rid, t
 		} = room;
 		const isGroupChat = RocketChat.isGroupChat(room);
 
@@ -706,24 +706,6 @@ class RoomActionsView extends React.Component {
 										})}
 										testID='room-actions-starred'
 										left={() => <List.Icon name='star' />}
-										showActionIndicator
-									/>
-									<List.Separator />
-								</>
-							)
-							: null}
-
-						{['c', 'p', 'd'].includes(t)
-							? (
-								<>
-									<List.Item
-										title='Search'
-										onPress={() => this.onPressTouchable({
-											route: 'SearchMessagesView',
-											params: { rid, encrypted }
-										})}
-										testID='room-actions-search'
-										left={() => <List.Icon name='search' />}
 										showActionIndicator
 									/>
 									<List.Separator />
