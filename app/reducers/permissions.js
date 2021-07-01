30487c392fbd6c90703bcc78ad7a359252af3fef
Gung Wah <41157464+kresnaputra@users.noreply.github.com>
2021-07-01 21:57:23 +0800

    [IMPROVE] Subscribe to permissions (#2993)
    * [CHORE] Subscribe to permissions
    
    * add redux action for update
    
    * Minor tweaks
    
    Co-authored-by: Gerzon Z <gerzonc@icloud.com>
    Co-authored-by: Gerzon Z <gerzonzcanario@gmail.com>
    Co-authored-by: Diego Mello <diegolmello@gmail.com>

--- app/reducers/permissions.js
+++ app/reducers/permissions.js
@@ -8,6 +8,11 @@ export default function permissions(state = initialState, action) {
 	switch (action.type) {
 		case PERMISSIONS.SET:
 			return action.permissions;
+		case PERMISSIONS.UPDATE:
+			return {
+				...state,
+				[action.payload.id]: action.payload.roles
+			};
 		default:
 			return state;
 	}
