6855dbff7c88a8c1168d592a0465a1c1155b76ab
Reinaldo Neto <reinaldonetof@hotmail.com>
2021-07-13 16:24:26 -0300

    Fixed to view files inside the file list

--- app/lib/methods/helpers/getFileUrlFromMessage.js
+++ app/lib/methods/helpers/getFileUrlFromMessage.js
@@ -6,7 +6,13 @@ export default function(message) {
 		return { audio_url: message.url };
 	}
 	if (/video/.test(message.type)) {
-		return { video_url: message.url };
+		let video_url = message.url;
+		const regUfsUpload = /ufs\/.*Uploads/;
+		if (regUfsUpload.test(video_url)) {
+			video_url = video_url.replace(regUfsUpload, 'file-upload');
+		}
+
+		return { video_url, video_type: message.type };
 	}
 	return {
 		title_link: message.url,
