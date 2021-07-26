08daa01b57d636e12439b935887d05a154f096e2
Diego Mello <diegolmello@gmail.com>
2021-07-26 18:22:11 -0300

    Cleanup

--- app/utils/log/index.js
+++ app/utils/log/index.js
@@ -1,26 +1,19 @@
 import firebaseAnalytics from '@react-native-firebase/analytics';
 import { isFDroidBuild } from '../../constants/environment';
-import config from '../../../config';
 import events from './events';
 
 const analytics = firebaseAnalytics || '';
 let bugsnag = '';
 let crashlytics;
-import Bugsnag from '@bugsnag/react-native'
-Bugsnag.start()
-Bugsnag.notify(new Error('Bad, but not fatal'));
 
 if (!isFDroidBuild) {
-	// const { Client } = require('bugsnag-react-native');
+	bugsnag = require('@bugsnag/react-native').default;
+	bugsnag.start();
 	crashlytics = require('@react-native-firebase/crashlytics').default;
-	// bugsnag = new Client(config.BUGSNAG_API_KEY);
-	
 }
 
 export { analytics };
 export const loggerConfig = bugsnag.config;
-export const leaveBreadcrumb = () => {}
-// export const { leaveBreadcrumb } = bugsnag;
 export { events };
 
 let metadata = {};
@@ -35,7 +28,7 @@ export const logEvent = (eventName, payload) => {
 	try {
 		if (!isFDroidBuild) {
 			analytics().logEvent(eventName, payload);
-			leaveBreadcrumb(eventName, payload);
+			bugsnag.leaveBreadcrumb(eventName, payload);
 		}
 	} catch {
 		// Do nothing
@@ -45,19 +38,15 @@ export const logEvent = (eventName, payload) => {
 export const setCurrentScreen = (currentScreen) => {
 	if (!isFDroidBuild) {
 		analytics().setCurrentScreen(currentScreen);
-		leaveBreadcrumb(currentScreen, { type: 'navigation' });
+		bugsnag.leaveBreadcrumb(currentScreen, { type: 'navigation' });
 	}
 };
 
 export default (e) => {
 	if (e instanceof Error && bugsnag && e.message !== 'Aborted' && !__DEV__) {
-		// bugsnag.notify(e, (report) => {
-		// 	report.metadata = {
-		// 		details: {
-		// 			...metadata
-		// 		}
-		// 	};
-		// });
+		bugsnag.notify(e, (event) => {
+			event.addMetadata('details', { ...metadata });
+		});
 		if (!isFDroidBuild) {
 			crashlytics().recordError(e);
 		}
