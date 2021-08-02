00165dfa57f100cd48ea4032045f37ed5193a151
Reinaldo Neto <reinaldonetof@hotmail.com>
2021-08-02 16:55:25 -0300

    Layout for mobile complete, missing tablets

--- app/views/DisplayPrefsView.js
+++ app/views/DisplayPrefsView.js
@@ -1,44 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { FlatList, StyleSheet, Switch } from 'react-native';
-import { connect } from 'react-redux';
+import { Switch } from 'react-native';
 
+import { RadioButton } from 'react-native-ui-lib';
 import StatusBar from '../containers/StatusBar';
 import I18n from '../i18n';
 import * as List from '../containers/List';
-import Status from '../containers/Status/Status';
-import TextInput from '../containers/TextInput';
-import EventEmitter from '../utils/events';
-import { showErrorAlert } from '../utils/info';
 import Loading from '../containers/Loading';
-import RocketChat from '../lib/rocketchat';
-import log, { logEvent, events } from '../utils/log';
 
-import { LISTENER } from '../containers/Toast';
 import { withTheme } from '../theme';
-import { getUserSelector } from '../selectors/login';
+import { themes } from '../constants/colors';
+
 import * as HeaderButton from '../containers/HeaderButton';
-import { setUser as setUserAction } from '../actions/login';
 import SafeAreaView from '../containers/SafeAreaView';
-
-
-const styles = StyleSheet.create({
-	inputContainer: {
-		marginTop: 32,
-		marginBottom: 32
-	},
-	inputLeft: {
-		position: 'absolute',
-		top: 12,
-		left: 12
-	},
-	inputStyle: {
-		paddingLeft: 48
-	}
-});
+import { ICON_SIZE } from '../containers/List/constants';
 
 const DisplayPrefsView = (props) => {
-	const [loading, setLoading] = useState(false);
+	const { theme } = props;
+	const [loading] = useState(false);
 
 
 
@@ -62,12 +41,55 @@ const DisplayPrefsView = (props) => {
 			))
 		});
 	};
+
 	useEffect(() => {
 		setHeader();
 	}, []);
 
+	const sortByName = () => {
+		// logEvent(events.RL_SORT_CHANNELS_BY_NAME);
+		// this.setSortPreference({ sortBy: 'alphabetical' });
+		// this.close();
+	};
+
+	const sortByActivity = () => {
+		// logEvent(events.RL_SORT_CHANNELS_BY_ACTIVITY);
+		// this.setSortPreference({ sortBy: 'activity' });
+		// this.close();
+	};
+
+	const toggleGroupByType = () => {
+		// logEvent(events.RL_GROUP_CHANNELS_BY_TYPE);
+		// const { groupByType } = this.props;
+		// this.setSortPreference({ groupByType: !groupByType });
+	};
+
+	const toggleGroupByFavorites = () => {
+		// logEvent(events.RL_GROUP_CHANNELS_BY_FAVORITE);
+		// const { showFavorites } = this.props;
+		// this.setSortPreference({ showFavorites: !showFavorites });
+	};
+
+	const toggleUnread = () => {
+		// logEvent(events.RL_GROUP_CHANNELS_BY_UNREAD);
+		// const { showUnread } = this.props;
+		// this.setSortPreference({ showUnread: !showUnread });
+	};
+
+	const renderCheckBox = value => (
+		value ? <List.Icon name='checkbox-checked' color={themes[theme].actionTintColor} /> : <List.Icon name='checkbox-unchecked' />
+	);
+
 	const renderAvatarSwitch = () => (
 		<Switch />
+	);
+
+	const renderRadio = value => (
+		<RadioButton
+			selected={!!value}
+			color={value ? themes[theme].actionTintColor : themes[theme].auxiliaryText}
+			size={ICON_SIZE}
+		/>
 	);
 
 
@@ -81,11 +103,53 @@ const DisplayPrefsView = (props) => {
 						title='Display'
 					/>
 					<List.Item
+						left={() => <List.Icon name='avatar' />}
 						title='Avatars'
 						right={() => renderAvatarSwitch()}
 					/>
 				</List.Section>
 
+				<List.Section>
+					<List.Item
+						title='Sort_by'
+					/>
+					<List.Item
+						title='Activity'
+						left={() => <List.Icon name='clock' />}
+						onPress={sortByActivity}
+						right={() => (renderRadio(true))}
+					/>
+					<List.Item
+						title='Name'
+						left={() => <List.Icon name='sort-az' />}
+						onPress={sortByName}
+						right={() => (renderRadio())}
+					/>
+				</List.Section>
+
+				<List.Section>
+					<List.Item
+						title='Group_by'
+					/>
+					<List.Item
+						title='Unread_on_top'
+						left={() => <List.Icon name='unread-on-top-disabled' />}
+						onPress={toggleUnread}
+						right={() => (renderCheckBox(true))}
+					/>
+					<List.Item
+						title='Favorites'
+						left={() => <List.Icon name='star' />}
+						onPress={toggleGroupByFavorites}
+						right={() => (renderCheckBox(true))}
+					/>
+					<List.Item
+						title='Types'
+						left={() => <List.Icon name='group-by-type' />}
+						onPress={toggleGroupByType}
+						right={() => (renderCheckBox())}
+					/>
+				</List.Section>
 
 
 			</List.Container>
@@ -103,9 +167,7 @@ DisplayPrefsView.propTypes = {
 	}),
 	theme: PropTypes.string,
 	navigation: PropTypes.object,
-	isMasterDetail: PropTypes.bool,
-	setUser: PropTypes.func,
-	Accounts_AllowInvisibleStatusOption: PropTypes.bool
+	isMasterDetail: PropTypes.bool
 };
 
 export default (withTheme(DisplayPrefsView));
