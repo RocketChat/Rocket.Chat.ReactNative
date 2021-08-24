34b766db838dbe8599287d29aa5df38972a019cd
Gerzon Z <gerzonzcanario@gmail.com>
2021-08-24 09:51:13 -0400

    Update reanimated to 2.2.0

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,8 @@
 module.exports = {
 	presets: ['module:metro-react-native-babel-preset'],
 	plugins: [
-		['@babel/plugin-proposal-decorators', { legacy: true }]
+		['@babel/plugin-proposal-decorators', { legacy: true }],
+		'react-native-reanimated/plugin'
 	],
 	env: {
 		production: {
