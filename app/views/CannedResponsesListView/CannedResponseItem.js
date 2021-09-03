8ea823494351aab18d569bf493f1fde4382e9312
Reinaldo Neto <reinaldonetof@hotmail.com>
2021-09-03 20:07:24 -0300

    Pagination with debounce and usecallback

--- app/views/CannedResponsesListView/CannedResponseItem.js
+++ app/views/CannedResponsesListView/CannedResponseItem.js
@@ -4,6 +4,7 @@ import { View, Text } from 'react-native';
 
 import Touchable from 'react-native-platform-touchable';
 import { themes } from '../../constants/colors';
+import * as List from '../../containers/List';
 import styles from './styles';
 import Button from '../../containers/Button';
 import { CustomIcon } from '../../lib/Icons';
@@ -12,47 +13,51 @@ import I18n from '../../i18n';
 const CannedResponseItem = ({
 	theme, onPressDetail, shortcut, scope, onPressUse, text, tags
 }) => (
-	<Touchable onPress={onPressDetail} style={[styles.wrapCannedItem, { backgroundColor: themes[theme].messageboxBackground }]}>
-		<>
-			<View style={styles.cannedRow}>
-				<View style={styles.cannedWrapShortcutScope}>
-					<Text style={[styles.cannedShortcut, { color: themes[theme].titleText }]}>!{shortcut}</Text>
-					<Text style={[styles.cannedScope, { color: themes[theme].auxiliaryTintColor }]}>{scope}</Text>
+	<>
+		<Touchable onPress={onPressDetail} style={[styles.wrapCannedItem, { backgroundColor: themes[theme].messageboxBackground }]}>
+			<>
+				<View style={styles.cannedRow}>
+					<View style={styles.cannedWrapShortcutScope}>
+						<Text style={[styles.cannedShortcut, { color: themes[theme].titleText }]}>!{shortcut}</Text>
+						<Text style={[styles.cannedScope, { color: themes[theme].auxiliaryTintColor }]}>{scope}</Text>
+					</View>
+
+					<Button
+						title={I18n.t('Use')}
+						fontSize={12}
+						color={themes[theme].titleText}
+						style={[styles.cannedUseButton, { backgroundColor: themes[theme].headerBackground }]}
+						theme={theme}
+						onPress={onPressUse}
+					/>
+
+					<CustomIcon
+						name='chevron-right'
+						color={themes[theme].auxiliaryText}
+						size={20}
+					/>
+
 				</View>
 
-				<Button
-					title={I18n.t('Use')}
-					fontSize={12}
-					color={themes[theme].titleText}
-					style={[styles.cannedUseButton, { backgroundColor: themes[theme].headerBackground }]}
-					theme={theme}
-					onPress={onPressUse}
-				/>
+				<Text style={[styles.cannedText, { color: themes[theme].auxiliaryTintColor }]}>“{text}”</Text>
 
-				<CustomIcon
-					name='chevron-right'
-					color={themes[theme].auxiliaryText}
-					size={20}
-				/>
+				<View style={styles.cannedTagContainer}>
+					{
+						tags?.length > 0
+							? (
+								<View style={[styles.cannedTagWrap, { backgroundColor: themes[theme].searchboxBackground }]}>
+									<Text style={[styles.cannedTag, { color: themes[theme].auxiliaryTintColor }]}>sales</Text>
+								</View>
+							)
+							: null
+					}
 
-			</View>
+				</View>
+			</>
+		</Touchable>
+		<List.Separator />
+	</>
 
-			<Text style={[styles.cannedText, { color: themes[theme].auxiliaryTintColor }]}>“{text}”</Text>
-
-			<View style={styles.cannedTagContainer}>
-				{
-					tags?.length > 0
-						? (
-							<View style={[styles.cannedTagWrap, { backgroundColor: themes[theme].searchboxBackground }]}>
-								<Text style={[styles.cannedTag, { color: themes[theme].auxiliaryTintColor }]}>sales</Text>
-							</View>
-						)
-						: null
-				}
-
-			</View>
-		</>
-	</Touchable>
 );
 
 CannedResponseItem.propTypes = {
--- app/views/CannedResponsesListView/index.js
+++ app/views/CannedResponsesListView/index.js
@@ -1,33 +1,34 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { FlatList } from 'react-native';
 import I18n from '../../i18n';
 
-import * as List from '../../containers/List';
 import SafeAreaView from '../../containers/SafeAreaView';
 import StatusBar from '../../containers/StatusBar';
+import ActivityIndicator from '../../containers/ActivityIndicator';
 import { useTheme } from '../../theme';
 import HeaderCanned from './HeaderCanned';
 import CannedResponseItem from './CannedResponseItem';
 import RocketChat from '../../lib/rocketchat';
+import debounce from '../../utils/debounce';
 
 const COUNT = 25;
 
 const CannedResponsesListView = (props) => {
 	const [cannedResponses, setCannedResponses] = useState([]);
+	const [searchText, setSearchText] = useState('');
+	const [loading, setLoading] = useState(false);
+	const [offset, setOffset] = useState(0);
 	const { theme } = useTheme();
-
-	const offset = 0;
 
 	useEffect(() => {
 		const { navigation } = props;
 		navigation.setOptions({
 			title: I18n.t('Canned_Responses')
-
 		});
 	}, []);
 
-	const getListCannedResponse = async(text) => {
+	const getListCannedResponse = async(text = '', debounced) => {
 		try {
 			const res = await RocketChat.getListCannedResponse({
 				text,
@@ -35,38 +36,65 @@ const CannedResponsesListView = (props) => {
 				count: COUNT
 			});
 			if (res.success) {
-				setCannedResponses(res.cannedResponses);
+				setCannedResponses(prevCanned => (
+					debounced
+						? res.cannedResponses
+						: [...prevCanned, ...res.cannedResponses]
+				));
+				setLoading(false);
+				setOffset(prevOffset => prevOffset + COUNT);
 			}
 		} catch (e) {
 			// do nothing
 		}
 	};
 
+	const searchCallback = useCallback(debounce(async(text) => {
+		await getListCannedResponse(text, true);
+	}, 1000), []); // use debounce with useCallback https://stackoverflow.com/a/58594890
+
 	useEffect(() => {
 		getListCannedResponse();
 	}, []);
+
+	const search = (text) => {
+		setCannedResponses([]);
+		setLoading(true);
+		setOffset(0);
+		setSearchText(text);
+		searchCallback(text);
+	};
+
+	const onEndReached = async() => {
+		if (cannedResponses.length < offset || loading) {
+			return;
+		}
+		setLoading(true);
+		await getListCannedResponse(searchText);
+	};
 
 	return (
 		<SafeAreaView>
 			<StatusBar />
 
-			<HeaderCanned theme={theme} />
+			<HeaderCanned theme={theme} onChangeText={search} />
 			<FlatList
 				data={cannedResponses}
 				extraData={cannedResponses}
 				renderItem={({ item }) => (
-					<>
-						<CannedResponseItem
-							theme={theme}
-							scope={item.scope}
-							shortcut={item.shortcut}
-							tags={item?.tags}
-							text={item.text}
-						/>
-						<List.Separator />
-					</>
+					<CannedResponseItem
+						theme={theme}
+						scope={item.scope}
+						shortcut={item.shortcut}
+						tags={item?.tags}
+						text={item.text}
+						onPressDetail={() => console.log('🏎️', cannedResponses, offset)}
+					/>
 				)}
 				keyExtractor={item => item._id || item.shortcut}
+				onEndReached={onEndReached}
+				onEndReachedThreshold={0.5}
+				ListFooterComponent={loading ? <ActivityIndicator theme={theme} /> : null}
 			/>
 
 		</SafeAreaView>
