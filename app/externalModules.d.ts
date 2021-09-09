d824d86b3cf06e65ff761554a664e4cd0dc3e262
Diego Mello <diegolmello@gmail.com>
2021-09-09 16:43:27 -0300

    Fix sdk

--- app/externalModules.d.ts
+++ app/externalModules.d.ts
@@ -6,5 +6,6 @@ declare module 'react-native-image-progress';
 declare module 'react-native-platform-touchable';
 declare module 'react-native-ui-lib/keyboard';
 declare module '@rocket.chat/ui-kit';
+declare module '@rocket.chat/sdk';
 declare module 'react-native-config-reader';
 declare module 'react-native-keycommands';
