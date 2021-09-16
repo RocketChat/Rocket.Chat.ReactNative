4a073057ab966996422731b67f1a16e15610d2af
Diego Mello <diegolmello@gmail.com>
2021-09-16 15:26:22 -0300

    Other fixes

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,6 @@
 module.exports = {
 	presets: ['module:metro-react-native-babel-preset'],
-	plugins: [
-		['@babel/plugin-proposal-decorators', { legacy: true }],
-		'react-native-reanimated/plugin'
-	],
+	plugins: [['@babel/plugin-proposal-decorators', { legacy: true }], 'react-native-reanimated/plugin'],
 	env: {
 		production: {
 			plugins: ['transform-remove-console']
