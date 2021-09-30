76e2bfca6085f81ef030b1e2c0fb6d4a77142faa
Diego Mello <diegolmello@gmail.com>
2021-09-30 14:10:22 -0300

    Apply changes

--- app/stacks/OutsideStack.js
+++ app/stacks/OutsideStack.js
@@ -21,7 +21,7 @@ const _OutsideStack = () => {
 
 	return (
 		<Outside.Navigator screenOptions={{ ...defaultHeader, ...themedHeader(theme), ...StackAnimation }}>
-			<Outside.Screen name='NewServerView' component={NewServerView} options={NewServerView.navigationOptions} />
+			{/* <Outside.Screen name='NewServerView' component={NewServerView} options={NewServerView.navigationOptions} /> */}
 			<Outside.Screen name='WorkspaceView' component={WorkspaceView} options={WorkspaceView.navigationOptions} />
 			<Outside.Screen name='LoginView' component={LoginView} options={LoginView.navigationOptions} />
 			<Outside.Screen name='ForgotPasswordView' component={ForgotPasswordView} options={ForgotPasswordView.navigationOptions} />
