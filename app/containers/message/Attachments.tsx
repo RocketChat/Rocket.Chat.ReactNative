89a2381a1e45ebf44b344130f0e2d7fca19c7075
Gerzon Z <gerzonzcanario@gmail.com>
2021-10-04 18:03:28 -0400

    test commit

--- app/containers/message/Attachments.tsx
+++ app/containers/message/Attachments.tsx
@@ -36,20 +36,16 @@ const Attachments = React.memo(
 
 		return attachments.map((file: any, index: number) => {
 			if (file.image_url) {
-				return (
-					<Image key={file.image_url} file={file} showAttachment={showAttachment} getCustomEmoji={getCustomEmoji} theme={theme} />
-				);
+				return <Image key={index} file={file} showAttachment={showAttachment} getCustomEmoji={getCustomEmoji} theme={theme} />;
 			}
 			if (file.audio_url) {
-				return <Audio key={file.audio_url} file={file} getCustomEmoji={getCustomEmoji} theme={theme} />;
+				return <Audio key={index} file={file} getCustomEmoji={getCustomEmoji} theme={theme} />;
 			}
 			if (file.video_url) {
-				return (
-					<Video key={file.video_url} file={file} showAttachment={showAttachment} getCustomEmoji={getCustomEmoji} theme={theme} />
-				);
+				return <Video key={index} file={file} showAttachment={showAttachment} getCustomEmoji={getCustomEmoji} theme={theme} />;
 			}
 			if (file.actions && file.actions.length > 0) {
-				return <AttachedActions attachment={file} theme={theme} />;
+				return <AttachedActions key={index} attachment={file} theme={theme} />;
 			}
 
 			return (
