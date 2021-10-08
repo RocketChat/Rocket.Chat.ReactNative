c8b7f043f516e3f1bc08816f61f29a954bcac76c
Reinaldo Neto <reinaldonetof@hotmail.com>
2021-10-08 12:12:11 -0300

    minor tweak on logic

--- app/sagas/deepLinking.js
+++ app/sagas/deepLinking.js
@@ -51,8 +51,7 @@ const navigate = function* navigate({ params }) {
 			type = pathSplitted[0];
 			name = pathSplitted[1];
 			// Check if has thread at path from params: channel/general/thread/threadId and expect that threadId is the next param
-			const threadIdIndexOf = pathSplitted.lastIndexOf('thread') + 1;
-			jumpToThreadId = threadIdIndexOf > 0 && threadIdIndexOf < pathSplitted.length ? pathSplitted[threadIdIndexOf] : null;
+			jumpToThreadId = pathSplitted[3] && pathSplitted[2] === 'thread' ? pathSplitted[3] : null;
 		}
 		if (type !== 'invite' || params.rid) {
 			const room = yield RocketChat.canOpenRoom(params);
