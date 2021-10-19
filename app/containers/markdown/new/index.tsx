0c7dda59d17d8bae59274bff9300601a1c4564e9
Diego Mello <diegolmello@gmail.com>
2021-10-19 13:21:34 -0300

    Use big emoji as a normal token

--- app/containers/markdown/new/index.tsx
+++ app/containers/markdown/new/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MarkdownAST, BigEmoji as BigEmojiProps } from '@rocket.chat/message-parser';
+import { MarkdownAST } from '@rocket.chat/message-parser';
 
 import Quote from './Quote';
 import Paragraph from './Paragraph';
@@ -25,8 +25,6 @@ interface IBodyProps {
 	baseUrl: string;
 }
 
-const isBigEmoji = (tokens: MarkdownAST): tokens is [BigEmojiProps] => tokens.length === 1 && tokens[0].type === 'BIG_EMOJI';
-
 const Body = ({
 	tokens,
 	mentions,
@@ -36,46 +34,42 @@ const Body = ({
 	navToRoomInfo,
 	getCustomEmoji,
 	baseUrl
-}: IBodyProps): JSX.Element => {
-	if (isBigEmoji(tokens)) {
-		return <BigEmoji value={tokens[0].value} getCustomEmoji={getCustomEmoji!} baseUrl={baseUrl} />;
-	}
-
-	return (
-		<>
-			{tokens.map(block => {
-				switch (block.type) {
-					case 'UNORDERED_LIST':
-						return <UnorderedList value={block.value} />;
-					case 'ORDERED_LIST':
-						return <OrderedList value={block.value} />;
-					case 'TASKS':
-						return <TaskList value={block.value} />;
-					case 'QUOTE':
-						return <Quote value={block.value} />;
-					case 'PARAGRAPH':
-						return (
-							<Paragraph
-								value={block.value}
-								navToRoomInfo={navToRoomInfo}
-								baseUrl={baseUrl}
-								getCustomEmoji={getCustomEmoji!}
-								channels={channels}
-								mentions={mentions}
-								useRealName={useRealName}
-								username={username}
-							/>
-						);
-					case 'CODE':
-						return <Code value={block.value} />;
-					case 'HEADING':
-						return <Heading value={block.value} level={block.level} />;
-					default:
-						return null;
-				}
-			})}
-		</>
-	);
-};
+}: IBodyProps): JSX.Element => (
+	<>
+		{tokens.map(block => {
+			switch (block.type) {
+				case 'BIG_EMOJI':
+					return <BigEmoji value={block.value} getCustomEmoji={getCustomEmoji!} baseUrl={baseUrl} />;
+				case 'UNORDERED_LIST':
+					return <UnorderedList value={block.value} />;
+				case 'ORDERED_LIST':
+					return <OrderedList value={block.value} />;
+				case 'TASKS':
+					return <TaskList value={block.value} />;
+				case 'QUOTE':
+					return <Quote value={block.value} />;
+				case 'PARAGRAPH':
+					return (
+						<Paragraph
+							value={block.value}
+							navToRoomInfo={navToRoomInfo}
+							baseUrl={baseUrl}
+							getCustomEmoji={getCustomEmoji!}
+							channels={channels}
+							mentions={mentions}
+							useRealName={useRealName}
+							username={username}
+						/>
+					);
+				case 'CODE':
+					return <Code value={block.value} />;
+				case 'HEADING':
+					return <Heading value={block.value} level={block.level} />;
+				default:
+					return null;
+			}
+		})}
+	</>
+);
 
 export default Body;
