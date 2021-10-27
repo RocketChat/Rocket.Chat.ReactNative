27e4ab1d621800c661ef6865183bc6d74a0f351c
AlexAlexandre <alexalexandrejr@gmail.com>
2021-10-27 17:57:56 -0300

    feat: create a util file with download functions and file views

---
+++ app/utils/fileDownload/index.ts
@@ -0,0 +1,45 @@
+import RNFetchBlob from 'rn-fetch-blob';
+import FileViewer from 'react-native-file-viewer';
+
+interface IAttachment {
+	title: string;
+	title_link: string;
+	title_link_download: boolean;
+	type: string;
+	description: string;
+}
+
+const DOCUMENT_PATH = `${RNFetchBlob.fs.dirs.DocumentDir}/`;
+
+export const getExtensionType = (text: string) => text.split('.').pop();
+
+export const fileDownload = async (url: string, attachment: IAttachment) => {
+	const fileName = attachment.title.split('.')[0];
+
+	const options = {
+		path: `${DOCUMENT_PATH}${fileName}.${getExtensionType(attachment.title_link)}`,
+		fileCache: true,
+		timeout: 10000,
+		indicator: true,
+		overwrite: true
+	};
+
+	const { data } = await RNFetchBlob.config(options)
+		.fetch('GET', url)
+		.then(res => res)
+		.catch(err => err);
+	return data;
+};
+
+export const filePreview = async (url: string, attachment: IAttachment) => {
+	const file = await fileDownload(url, attachment);
+	FileViewer.open(file)
+		.then(res => {
+			console.log('entrou no then *************');
+			console.log(res);
+		})
+		.catch(err => {
+			console.log('entrou no catch *************');
+			console.error(err);
+		});
+};
