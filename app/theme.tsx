0cf961fdbea20ce64501c44cca38edad850d281b
Gerzon Z <gerzonzcanario@gmail.com>
2021-12-01 17:21:17 -0400

    Undo withTheme changes to open a new PR around it

--- app/theme.tsx
+++ app/theme.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import hoistNonReactStatics from 'hoist-non-react-statics';
-import { StackNavigationOptions } from '@react-navigation/stack';
 
 interface IThemeContextProps {
 	theme: string;
@@ -11,17 +10,12 @@ interface IThemeContextProps {
 	setTheme?: (newTheme?: {}) => void;
 }
 
-type TOptions = {
-	navigationOptions?: StackNavigationOptions;
-};
-
 export const ThemeContext = React.createContext<IThemeContextProps>({ theme: 'light' });
 
-export function withTheme<T extends object>(Component: React.ComponentType<T> & TOptions): typeof Component {
+export function withTheme(Component: any) {
 	const ThemedComponent = (props: any) => (
 		<ThemeContext.Consumer>{contexts => <Component {...props} {...contexts} />}</ThemeContext.Consumer>
 	);
-	ThemedComponent.navigationOptions = Component.navigationOptions;
 	hoistNonReactStatics(ThemedComponent, Component);
 	return ThemedComponent;
 }
