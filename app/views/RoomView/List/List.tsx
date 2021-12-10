13ee494d8ac7a25799bf77cb57a8638feff15049
AlexAlexandre <alexalexandrejr@gmail.com>
2021-12-09 21:10:35 -0300

    minor tweak

--- app/views/RoomView/List/List.tsx
+++ app/views/RoomView/List/List.tsx
@@ -19,7 +19,6 @@ const styles = StyleSheet.create({
 
 interface IRoomListProps extends FlatListProps<IRoomItem> {
 	listRef: React.Ref<FlatList>;
-	// listRef: React.MutableRefObject<AnimatedComponent<typeof FlatList>>;
 }
 
 const List = ({ listRef, ...props }: IRoomListProps): JSX.Element => (
