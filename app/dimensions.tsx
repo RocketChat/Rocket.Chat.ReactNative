6387dfcebb3c1d75f89824b0dfbf895c8cdc0597
AlexAlexandre <alexalexandrejr@gmail.com>
2021-12-13 23:18:28 -0300

    minor tweak

--- app/dimensions.tsx
+++ app/dimensions.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Dimensions } from 'react-native';
 import hoistNonReactStatics from 'hoist-non-react-statics';
 
-import { TOptions } from './definitions/navigationTypes';
+import { TNavigationOptions } from './definitions/navigationTypes';
 
 export interface IDimensionsContextProps {
 	width: number;
@@ -24,8 +24,8 @@ export interface IDimensionsContextProps {
 
 export const DimensionsContext = React.createContext<Partial<IDimensionsContextProps>>(Dimensions.get('window'));
 
-export function withDimensions<T extends object>(Component: React.ComponentType<T> & TOptions): typeof Component {
-	const DimensionsComponent = (props: any) => (
+export function withDimensions<T extends object>(Component: React.ComponentType<T> & TNavigationOptions): typeof Component {
+	const DimensionsComponent = (props: T) => (
 		<DimensionsContext.Consumer>{contexts => <Component {...props} {...contexts} />}</DimensionsContext.Consumer>
 	);
 	DimensionsComponent.navigationOptions = Component.navigationOptions;
--- app/theme.tsx
+++ app/theme.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import hoistNonReactStatics from 'hoist-non-react-statics';
 
-import { TOptions } from './definitions/navigationTypes';
+import { TNavigationOptions } from './definitions/navigationTypes';
 
 interface IThemeContextProps {
 	theme: string;
@@ -14,8 +14,8 @@ interface IThemeContextProps {
 
 export const ThemeContext = React.createContext<IThemeContextProps>({ theme: 'light' });
 
-export function withTheme<T extends object>(Component: React.ComponentType<T> & TOptions): typeof Component {
-	const ThemedComponent = (props: any) => (
+export function withTheme<T extends object>(Component: React.ComponentType<T> & TNavigationOptions): typeof Component {
+	const ThemedComponent = (props: T) => (
 		<ThemeContext.Consumer>{contexts => <Component {...props} {...contexts} />}</ThemeContext.Consumer>
 	);
 	ThemedComponent.navigationOptions = Component.navigationOptions;
