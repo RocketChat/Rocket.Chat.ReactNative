1b586c3115accbadc55ed7c52be23dee4669b37f
Reinaldo Neto <reinaldonetof@hotmail.com>
2021-12-15 23:15:44 -0300

    minor tweak

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-// import 'react-native-gesture-handler';
-// import 'react-native-console-time-polyfill';
-// import { AppRegistry } from 'react-native';
+import 'react-native-gesture-handler';
+import 'react-native-console-time-polyfill';
+import { AppRegistry } from 'react-native';
 
-// import { name as appName, share as shareName } from './app.json';
+import { name as appName, share as shareName } from './app.json';
 
-// if (__DEV__) {
-// 	require('./app/ReactotronConfig');
-// } else {
-// 	console.log = () => {};
-// 	console.time = () => {};
-// 	console.timeLog = () => {};
-// 	console.timeEnd = () => {};
-// 	console.warn = () => {};
-// 	console.count = () => {};
-// 	console.countReset = () => {};
-// 	console.error = () => {};
-// 	console.info = () => {};
-// }
+if (__DEV__) {
+	require('./app/ReactotronConfig');
+} else {
+	console.log = () => {};
+	console.time = () => {};
+	console.timeLog = () => {};
+	console.timeEnd = () => {};
+	console.warn = () => {};
+	console.count = () => {};
+	console.countReset = () => {};
+	console.error = () => {};
+	console.info = () => {};
+}
 
-// AppRegistry.registerComponent(appName, () => require('./app/index').default);
-// AppRegistry.registerComponent(shareName, () => require('./app/share').default);
+AppRegistry.registerComponent(appName, () => require('./app/index').default);
+AppRegistry.registerComponent(shareName, () => require('./app/share').default);
 
 // For storybook, comment everything above and uncomment below
-import './storybook';
+// import './storybook';
