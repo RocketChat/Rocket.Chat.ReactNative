0663b4408d21e26bbb8098f597e39aa22ce770ee
Reinaldo Neto <reinaldonetof@hotmail.com>
2021-12-21 00:27:21 -0300

    minor tweak

--- storybook/stories/Markdown.tsx
+++ storybook/stories/Markdown.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { ScrollView, StyleSheet, View } from 'react-native';
 import { storiesOf } from '@storybook/react-native';
