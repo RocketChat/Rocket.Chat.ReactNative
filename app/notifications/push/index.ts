b104e7a8a7b79f307159fcd339f2d26b2a3b80d4
GleidsonDaniel <gleidson10daniel@hotmail.com>
2022-01-05 18:19:21 -0300

    add types for all push functions and interfaces

--- app/notifications/push/index.ts
+++ app/notifications/push/index.ts
@@ -5,46 +5,72 @@ import { deepLinkingOpen } from '../../actions/deepLinking';
 import { isFDroidBuild } from '../../constants/environment';
 import PushNotification from './push';
 
-export const onNotification = notification => {
+export interface INotification {
+	message: string;
+	style: string;
+	ejson: string;
+	collapse_key: string;
+	notId: string;
+	msgcnt: string;
+	title: string;
+	from: string;
+	image: string;
+	soundname: string;
+	// only for info
+	'google.delivered_priority': string;
+	'google.original_priority': string;
+	'google.message_id': string;
+	'google.sent_time': number;
+	'google.c.sender.id': string;
+	'google.ttl': number;
+}
+
+interface IEjson {
+	rid: string;
+	name: string;
+	sender: { username: string; name: string };
+	type: string;
+	host: string;
+	messageType: string;
+	messageId: string;
+}
+
+export const onNotification = (notification: INotification): void => {
 	if (notification) {
-		const data = notification.getData();
-		if (data) {
-			try {
-				const { rid, name, sender, type, host, messageType, messageId } = EJSON.parse(data.ejson);
+		try {
+			const { rid, name, sender, type, host, messageType, messageId }: IEjson = EJSON.parse(notification.ejson);
 
-				const types = {
-					c: 'channel',
-					d: 'direct',
-					p: 'group',
-					l: 'channels'
-				};
-				let roomName = type === 'd' ? sender.username : name;
-				if (type === 'l') {
-					roomName = sender.name;
-				}
-
-				const params = {
-					host,
-					rid,
-					messageId,
-					path: `${types[type]}/${roomName}`,
-					isCall: messageType === 'jitsi_call_started'
-				};
-				store.dispatch(deepLinkingOpen(params));
-			} catch (e) {
-				console.warn(e);
+			const types: Record<string, string> = {
+				c: 'channel',
+				d: 'direct',
+				p: 'group',
+				l: 'channels'
+			};
+			let roomName = type === 'd' ? sender.username : name;
+			if (type === 'l') {
+				roomName = sender.name;
 			}
+
+			const params = {
+				host,
+				rid,
+				messageId,
+				path: `${types[type]}/${roomName}`,
+				isCall: messageType === 'jitsi_call_started'
+			};
+			// TODO REDUX MIGRATION TO TS
+			store.dispatch(deepLinkingOpen(params));
+		} catch (e) {
+			console.warn(e);
 		}
 	}
 };
 
-export const getDeviceToken = () => PushNotification.getDeviceToken();
-export const setBadgeCount = count => PushNotification.setBadgeCount(count);
-export const initializePushNotifications = () => {
+export const getDeviceToken = (): string => PushNotification.getDeviceToken();
+export const setBadgeCount = (count?: number): void => PushNotification.setBadgeCount(count);
+export const initializePushNotifications = (): void => {
 	if (!isFDroidBuild) {
 		setBadgeCount();
-		return PushNotification.configure({
-			onNotification
-		});
+		return PushNotification.configure(onNotification);
 	}
 };
