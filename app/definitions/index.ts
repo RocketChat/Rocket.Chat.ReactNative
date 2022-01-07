4cece39ed64ab5f5220ba55a8c40caf67639c9aa
GleidsonDaniel <gleidson10daniel@hotmail.com>
2022-01-07 11:31:05 -0300

    chore: migrate INotification to definitions folder and create index

---
+++ app/definitions/index.ts
@@ -0,0 +1,7 @@
+export * from './IAttachment';
+export * from './IMessage';
+export * from './INotification';
+export * from './IRocketChatRecord';
+export * from './IRoom';
+export * from './IServer';
+// export * from './ITeam';
