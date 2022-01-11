328733f3e86012b75681050a191baabd60dd1005
GleidsonDaniel <gleidson10daniel@hotmail.com>
2022-01-11 14:03:37 -0300

    chore: fix import

--- app/views/RoomsListView/index.js
+++ app/views/RoomsListView/index.js
@@ -49,7 +49,7 @@ import { showConfirmationAlert, showErrorAlert } from '../../utils/info';
 import { E2E_BANNER_TYPE } from '../../lib/encryption/constants';
 import { getInquiryQueueSelector } from '../../ee/omnichannel/selectors/inquiry';
 import { changeLivechatStatus, isOmnichannelStatusAvailable } from '../../ee/omnichannel/lib';
-import { DISPLAY_MODE_CONDENSED } from '../../constants/constantDisplayMode';
+import { DisplayMode, SortBy } from '../../constants/constantDisplayMode';
 import { RootEnum } from '../../definitions';
 import styles from './styles';
 import ServerDropdown from './ServerDropdown';
