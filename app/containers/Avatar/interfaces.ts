ad2aaa62aa6d3d375dee4d77a61fb5d00aaf2f7c
Reinaldo Neto <yiweigao0226@gmail.com>
2022-01-12 09:54:04 -0300

    Chore: Migrate Utils Folder to Typescript (#3544)
    Co-authored-by: AlexAlexandre <alexalexandrejr@gmail.com>

--- app/containers/Avatar/interfaces.ts
+++ app/containers/Avatar/interfaces.ts
@@ -1,23 +1,23 @@
 export interface IAvatar {
-	server: string;
-	style: any;
+	server?: string;
+	style?: any;
 	text: string;
-	avatar: string;
-	emoji: string;
-	size: number;
-	borderRadius: number;
-	type: string;
-	children: JSX.Element;
-	user: {
-		id: string;
-		token: string;
+	avatar?: string;
+	emoji?: string;
+	size?: number;
+	borderRadius?: number;
+	type?: string;
+	children?: JSX.Element;
+	user?: {
+		id?: string;
+		token?: string;
 	};
-	theme: string;
-	onPress(): void;
-	getCustomEmoji(): any;
-	avatarETag: string;
-	isStatic: boolean | string;
-	rid: string;
-	blockUnauthenticatedAccess: boolean;
-	serverVersion: string;
+	theme?: string;
+	onPress?: () => void;
+	getCustomEmoji?: () => any;
+	avatarETag?: string;
+	isStatic?: boolean | string;
+	rid?: string;
+	blockUnauthenticatedAccess?: boolean;
+	serverVersion?: string;
 }
