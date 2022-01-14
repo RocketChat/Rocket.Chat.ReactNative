3458f022c6226f06f0370be0ea688df3ad257e9f
GleidsonDaniel <yiweigao0226@gmail.com>
2022-01-14 14:19:08 -0300

    chore: migrate usersTyping to typescript

--- app/definitions/redux/index.ts
+++ app/definitions/redux/index.ts
@@ -3,6 +3,7 @@ import { TActionActiveUsers } from '../../actions/activeUsers';
 // REDUCERS
 import { IActiveUsers } from '../../reducers/activeUsers';
 import { ISelectedUsers } from '../../reducers/selectedUsers';
+import { TActionUserTyping } from '../../actions/usersTyping';
 
 export interface IApplicationState {
 	settings: any;
@@ -28,4 +29,4 @@ export interface IApplicationState {
 	roles: any;
 }
 
-export type TApplicationActions = TActionActiveUsers & TActionSelectedUsers;
+export type TApplicationActions = TActionActiveUsers & TActionSelectedUsers & TActionUserTyping;
