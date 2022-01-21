732cdaa6ad96e67988de4a63b12328847626e039
Danish Ahmed Mirza <danishmirza30602@gmail.com>
2022-01-21 19:01:09 +0530

    [IMPROVE] Display user status in the members list

--- app/presentation/UserItem.tsx
+++ app/presentation/UserItem.tsx
@@ -4,8 +4,9 @@ import { Pressable, StyleProp, StyleSheet, Text, View, ViewStyle } from 'react-n
 
 import Avatar from '../containers/Avatar';
 import { CustomIcon } from '../lib/Icons';
+import { STATUS_COLORS, themes } from '../constants/colors';
+import Status from '../containers/Status/Status';
 import sharedStyles from '../views/Styles';
-import { themes } from '../constants/colors';
 import { isIOS } from '../utils/deviceInfo';
 
 const styles = StyleSheet.create({
@@ -25,6 +26,11 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 		marginRight: 15
 	},
+	nameContainer: {		
+		width: '100%',
+		flexDirection: 'row',
+		alignItems: 'center'
+	},
 	name: {
 		fontSize: 17,
 		...sharedStyles.textMedium
@@ -36,6 +42,9 @@ const styles = StyleSheet.create({
 	icon: {
 		marginHorizontal: 15,
 		alignSelf: 'center'
+	},
+	statusIcon: {
+		marginRight:2
 	}
 });
 
@@ -46,34 +55,46 @@ interface IUserItem {
 	testID: string;
 	onLongPress?: () => void;
 	style?: StyleProp<ViewStyle>;
+	status?: string;
+	size?: number;
 	icon?: string | null;
 	theme: string;
 }
 
-const UserItem = ({ name, username, onPress, testID, onLongPress, style, icon, theme }: IUserItem) => (
-	<Pressable
-		onPress={onPress}
-		onLongPress={onLongPress}
-		testID={testID}
-		android_ripple={{
-			color: themes[theme].bannerBackground
-		}}
-		style={({ pressed }: any) => ({
-			backgroundColor: isIOS && pressed ? themes[theme].bannerBackground : 'transparent'
-		})}>
-		<View style={[styles.container, styles.button, style]}>
-			<Avatar text={username} size={30} style={styles.avatar} />
-			<View style={styles.textContainer}>
-				<Text style={[styles.name, { color: themes[theme].titleText }]} numberOfLines={1}>
-					{name}
-				</Text>
-				<Text style={[styles.username, { color: themes[theme].auxiliaryText }]} numberOfLines={1}>
-					@{username}
-				</Text>
+
+const UserItem = ({ name, username, onPress, testID, onLongPress, style, status, size = 16, icon, theme }: IUserItem) => {
+	const color = themes[theme!].titleText;
+	const iconStyle = [styles.statusIcon, { color }, style];
+
+	return (
+		<Pressable
+			onPress={onPress}
+			onLongPress={onLongPress}
+			testID={testID}
+			android_ripple={{
+				color: themes[theme].bannerBackground
+			}}
+			style={({ pressed }: any) => ({
+				backgroundColor: isIOS && pressed ? themes[theme].bannerBackground : 'transparent'
+			})}>
+			<View style={[styles.container, styles.button, style]}>
+				<Avatar text={username} size={30} style={styles.avatar} />
+				<View style={styles.textContainer}>
+					<View style={styles.nameContainer}>
+						{status?<Status style={[iconStyle, { color: STATUS_COLORS[status!] ?? STATUS_COLORS.offline }]} size={size} status={status!} />:null}
+						<Text style={[styles.name, { color: themes[theme].titleText }]} numberOfLines={1}>
+							{name}
+						</Text>
+					</View>
+					
+					<Text style={[styles.username, { color: themes[theme].auxiliaryText }]} numberOfLines={1}>
+						@{username}
+					</Text>
+				</View>
+				{icon ? <CustomIcon name={icon} size={22} style={[styles.icon, { color: themes[theme].actionTintColor }]} /> : null}
 			</View>
-			{icon ? <CustomIcon name={icon} size={22} style={[styles.icon, { color: themes[theme].actionTintColor }]} /> : null}
-		</View>
-	</Pressable>
-);
+		</Pressable>);
+}
+
 
 export default UserItem;
