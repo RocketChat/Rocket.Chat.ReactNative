a2c6d2c901fd736bb3eb6384d32273fbb31366d7
GleidsonDaniel <gleidson10daniel@hotmail.com>
2022-01-25 09:38:56 -0300

    chore: migrate canOpenRoom to ts

--- app/lib/methods/canOpenRoom.ts
+++ app/lib/methods/canOpenRoom.ts
@@ -7,13 +7,19 @@ const restTypes = {
 	group: 'groups'
 };
 
-async function open({ type, rid, name }) {
+enum ETypes {
+	DIRECT = 'direct',
+	GROUP = 'group',
+	CHANNEL = 'channel'
+}
+
+async function open(this: any, { type, rid, name }: { type: ETypes; rid: string; name: string }) {
 	try {
 		const params = rid ? { roomId: rid } : { roomName: name };
 
 		// if it's a direct link without rid we'll create a new dm
 		// if the dm already exists it'll return the existent
-		if (type === 'direct' && !rid) {
+		if (type === ETypes.DIRECT && !rid) {
 			const result = await this.createDirectMessage(name);
 			if (result.success) {
 				const { room } = result;
@@ -23,11 +29,11 @@ async function open({ type, rid, name }) {
 		}
 
 		// if it's a group we need to check if you can open
-		if (type === 'group') {
+		if (type === ETypes.GROUP) {
 			try {
 				// RC 0.61.0
 				await this.sdk.post(`${restTypes[type]}.open`, params);
-			} catch (e) {
+			} catch (e: any) {
 				if (!(e.data && /is already open/.test(e.data.error))) {
 					return false;
 				}
@@ -36,7 +42,7 @@ async function open({ type, rid, name }) {
 
 		// if it's a channel or group and the link don't have rid
 		// we'll get info from the room
-		if ((type === 'channel' || type === 'group') && !rid) {
+		if ((type === ETypes.CHANNEL || type === ETypes.GROUP) && !rid) {
 			// RC 0.72.0
 			const result = await this.sdk.get(`${restTypes[type]}.info`, params);
 			if (result.success) {
@@ -56,7 +62,10 @@ async function open({ type, rid, name }) {
 	}
 }
 
-export default async function canOpenRoom({ rid, path, isCall }) {
+export default async function canOpenRoom(
+	this: any,
+	{ rid, path, isCall }: { rid: string; isCall: boolean; path: string }
+): Promise<any> {
 	try {
 		const db = database.active;
 		const subsCollection = db.get('subscriptions');
@@ -86,8 +95,9 @@ export default async function canOpenRoom({ rid, path, isCall }) {
 		}
 
 		const [type, name] = path.split('/');
+		const t = type as ETypes;
 		try {
-			const result = await open.call(this, { type, rid, name });
+			const result = await open.call(this, { type: t, rid, name });
 			return result;
 		} catch (e) {
 			return false;
