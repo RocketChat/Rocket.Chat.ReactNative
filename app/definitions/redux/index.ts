c03551b19d40863e9344fcce546e2eb959badfea
Gleidson Daniel Silva <gleidson10daniel@hotmail.com>
2022-01-26 09:52:12 -0300

    Merge branch 'develop' into chore/migration-ts-redux-sortPreferences

--- app/definitions/redux/index.ts
+++ app/definitions/redux/index.ts
@@ -3,6 +3,7 @@ import { IActionSettings } from '../../actions/settings';
 import { TActionEncryption } from '../../actions/encryption';
 import { TActionCustomEmojis } from '../../actions/customEmojis';
 import { TActionSelectedUsers } from '../../actions/selectedUsers';
+import { TActionSortPreferences } from '../../actions/sortPreferences';
 // REDUCERS
 import { IActiveUsers } from '../../reducers/activeUsers';
 import { IEncryption } from '../../reducers/encryption';
@@ -37,4 +38,5 @@ export type TApplicationActions = TActionActiveUsers &
 	TActionSelectedUsers &
 	TActionEncryption &
 	IActionSettings &
-	TActionCustomEmojis;
+	TActionCustomEmojis &
+	TActionSortPreferences;
