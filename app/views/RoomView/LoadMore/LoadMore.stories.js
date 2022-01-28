5288e18f96ec35d3a85f949d5f2d2f9e37acbdd1
GleidsonDaniel <gleidson10daniel@hotmail.com>
2022-01-28 18:28:50 -0300

    test disable

--- app/views/RoomView/LoadMore/LoadMore.stories.js
+++ app/views/RoomView/LoadMore/LoadMore.stories.js
@@ -14,48 +14,47 @@ import {
 } from '../../../constants/messageTypeLoad';
 import LoadMore from './index';
 
-const stories = storiesOf('LoadMore', module);
+// const stories = storiesOf('LoadMore', module);
 
-// FIXME: for some reason, this promise never resolves on Storybook (it works on the app, so maybe the issue isn't on the component)
-const load = () => new Promise(res => setTimeout(res, 0));
+// const load = () => new Promise(res => setTimeout(res, 0));
 
-stories.add('basic', () => (
-	<>
-		<LoadMore load={load} />
-		<LoadMore load={load} runOnRender />
-		<LoadMore load={load} type={MESSAGE_TYPE_LOAD_PREVIOUS_CHUNK} />
-		<LoadMore load={load} type={MESSAGE_TYPE_LOAD_NEXT_CHUNK} />
-	</>
-));
+// stories.add('basic', () => (
+// 	<>
+// 		<LoadMore load={load} />
+// 		<LoadMore load={load} runOnRender />
+// 		<LoadMore load={load} type={MESSAGE_TYPE_LOAD_PREVIOUS_CHUNK} />
+// 		<LoadMore load={load} type={MESSAGE_TYPE_LOAD_NEXT_CHUNK} />
+// 	</>
+// ));
 
-const ThemeStory = ({ theme }) => (
-	<ThemeContext.Provider value={{ theme }}>
-		<ScrollView style={{ backgroundColor: themes[theme].backgroundColor }}>
-			<LoadMore load={load} type={MESSAGE_TYPE_LOAD_PREVIOUS_CHUNK} />
-			<Message msg='Hey!' theme={theme} />
-			<Message msg={longText} theme={theme} isHeader={false} />
-			<Message msg='Older message' theme={theme} isHeader={false} />
-			<LoadMore load={load} type={MESSAGE_TYPE_LOAD_NEXT_CHUNK} />
-			<LoadMore load={load} type={MESSAGE_TYPE_LOAD_MORE} />
-			<Message msg={longText} theme={theme} />
-			<Message msg='This is the third message' isHeader={false} theme={theme} />
-			<Message msg='This is the second message' isHeader={false} theme={theme} />
-			<Message msg='This is the first message' theme={theme} />
-		</ScrollView>
-	</ThemeContext.Provider>
-);
+// const ThemeStory = ({ theme }) => (
+// 	<ThemeContext.Provider value={{ theme }}>
+// 		<ScrollView style={{ backgroundColor: themes[theme].backgroundColor }}>
+// 			<LoadMore load={load} type={MESSAGE_TYPE_LOAD_PREVIOUS_CHUNK} />
+// 			<Message msg='Hey!' theme={theme} />
+// 			<Message msg={longText} theme={theme} isHeader={false} />
+// 			<Message msg='Older message' theme={theme} isHeader={false} />
+// 			<LoadMore load={load} type={MESSAGE_TYPE_LOAD_NEXT_CHUNK} />
+// 			<LoadMore load={load} type={MESSAGE_TYPE_LOAD_MORE} />
+// 			<Message msg={longText} theme={theme} />
+// 			<Message msg='This is the third message' isHeader={false} theme={theme} />
+// 			<Message msg='This is the second message' isHeader={false} theme={theme} />
+// 			<Message msg='This is the first message' theme={theme} />
+// 		</ScrollView>
+// 	</ThemeContext.Provider>
+// );
 
-stories
-	.addDecorator(StoryProvider)
-	.addDecorator(MessageDecorator)
-	.add('light theme', () => <ThemeStory theme='light' />);
+// stories
+// 	.addDecorator(StoryProvider)
+// 	.addDecorator(MessageDecorator)
+// 	.add('light theme', () => <ThemeStory theme='light' />);
 
-stories
-	.addDecorator(StoryProvider)
-	.addDecorator(MessageDecorator)
-	.add('dark theme', () => <ThemeStory theme='dark' />);
+// stories
+// 	.addDecorator(StoryProvider)
+// 	.addDecorator(MessageDecorator)
+// 	.add('dark theme', () => <ThemeStory theme='dark' />);
 
-stories
-	.addDecorator(StoryProvider)
-	.addDecorator(MessageDecorator)
-	.add('black theme', () => <ThemeStory theme='black' />);
+// stories
+// 	.addDecorator(StoryProvider)
+// 	.addDecorator(MessageDecorator)
+// 	.add('black theme', () => <ThemeStory theme='black' />);
