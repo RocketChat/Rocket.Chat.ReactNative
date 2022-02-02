09ca71ab9a284e81714bcaa151137721a7911071
Gleidson Daniel Silva <yiweigao0226@gmail.com>
2022-02-02 14:46:05 -0300

    Chore: Migrate redux module share to typescript (#3612)
    * chore: migrate redux module share to typescript
    
    * chore: fix types
    
    * chore: update types
    
    * chore: migrate redux module share to typescript
    
    * remove double import
    
    * chore: fix import

--- app/definitions/redux/index.ts
+++ app/definitions/redux/index.ts
@@ -1,23 +1,25 @@
 // ACTIONS
-import { TActionServer } from '../../actions/server';
 import { TActionActiveUsers } from '../../actions/activeUsers';
 import { TActionCustomEmojis } from '../../actions/customEmojis';
 import { TActionEncryption } from '../../actions/encryption';
 import { TActionInviteLinks } from '../../actions/inviteLinks';
 import { IActionRoles } from '../../actions/roles';
 import { TActionSelectedUsers } from '../../actions/selectedUsers';
+import { TActionServer } from '../../actions/server';
 import { IActionSettings } from '../../actions/settings';
+import { TActionsShare } from '../../actions/share';
 import { TActionSortPreferences } from '../../actions/sortPreferences';
 import { TActionUserTyping } from '../../actions/usersTyping';
 // REDUCERS
 import { IActiveUsers } from '../../reducers/activeUsers';
+import { IConnect } from '../../reducers/connect';
 import { IEncryption } from '../../reducers/encryption';
 import { IInviteLinks } from '../../reducers/inviteLinks';
 import { IRoles } from '../../reducers/roles';
 import { ISelectedUsers } from '../../reducers/selectedUsers';
 import { IServer } from '../../reducers/server';
-import { IConnect } from '../../reducers/connect';
 import { ISettings } from '../../reducers/settings';
+import { IShare } from '../../reducers/share';
 
 export interface IApplicationState {
 	settings: ISettings;
@@ -30,7 +32,7 @@ export interface IApplicationState {
 	room: any;
 	rooms: any;
 	sortPreferences: any;
-	share: any;
+	share: IShare;
 	customEmojis: any;
 	activeUsers: IActiveUsers;
 	usersTyping: any;
@@ -52,4 +54,5 @@ export type TApplicationActions = TActionActiveUsers &
 	TActionEncryption &
 	TActionSortPreferences &
 	TActionUserTyping &
+	TActionsShare &
 	TActionServer;
