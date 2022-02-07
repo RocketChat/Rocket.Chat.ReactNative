ab53974f1600bfe2005f57e0d6b9d38d6d7d2b0d
Reinaldo Neto <yiweigao0226@gmail.com>
2022-02-07 12:18:37 -0300

    Chore: Migrate LivechatEditView to Typescript (#3499)
    * Chore: Migrate LivechatEditView to Typescript
    
    * refactor: minor tweak
    
    * refactor: fix the interfaces for input
    
    * refactor: fix lint erros
    
    * minor tweak with new navigation types
    
    * function
    
    * iroom tweak
    
    * livechateditview tweak
    
    * TextInput tweak
    
    * refactor: update new types and interfaces for use ISubscription
    
    * refactor to default useState type
    
    * change the component name in SearchBox
    
    * changed state type
    
    Co-authored-by: AlexAlexandre <alexalexandrejr@gmail.com>
    Co-authored-by: Gerzon Z <gerzonc@icloud.com>
    Co-authored-by: Diego Mello <diegolmello@gmail.com>

--- app/containers/UIKit/MultiSelect/index.tsx
+++ app/containers/UIKit/MultiSelect/index.tsx
@@ -22,8 +22,8 @@ interface IMultiSelect {
 	context?: number;
 	loading?: boolean;
 	multiselect?: boolean;
-	onSearch: Function;
-	onClose: Function;
+	onSearch?: () => void;
+	onClose?: () => void;
 	inputStyle: object;
 	value?: any[];
 	disabled?: boolean | object;
