56869bc251aef445fb833ad1aa319be69544f6da
Gerzon Z <gerzonzcanario@gmail.com>
2022-02-07 14:31:55 -0400

    Fix minor error

--- app/lib/methods/getCustomEmojis.js
+++ app/lib/methods/getCustomEmojis.js
@@ -92,7 +92,7 @@ export function getCustomEmojis() {
 			const updatedSince = await getUpdatedSince(allRecords);
 
 			// if server version is lower than 0.75.0, fetches from old api
-			if (compareServerVersion(serverVersion, 'greaterThanOrEqualTo', '0.75.0')) {
+			if (compareServerVersion(serverVersion, 'lowerThan', '0.75.0')) {
 				// RC 0.61.0
 				const result = await this.sdk.get('emoji-custom');
 
