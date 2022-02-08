38ec7fd4dc601b6b1d2955ea0841ee1b702042a2
AlexAlexandre <alexalexandrejr@gmail.com>
2022-02-08 11:21:41 -0300

    chore: removing custom MMKV JSI module, since is no more necessary after 0.6.11 version

--- android/app/src/main/java/chat/rocket/reactnative/MainApplication.java
+++ android/app/src/main/java/chat/rocket/reactnative/MainApplication.java
@@ -11,7 +11,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.reactnativecommunity.viewpager.RNCViewPagerPackage;
 import com.facebook.react.bridge.JSIModulePackage;
-
+import com.swmansion.reanimated.ReanimatedJSIModulePackage;
 import org.unimodules.adapters.react.ModuleRegistryAdapter;
 import org.unimodules.adapters.react.ReactModuleRegistryProvider;
 
@@ -20,7 +20,6 @@ import java.util.List;
 
 import chat.rocket.reactnative.generated.BasePackageList;
 import chat.rocket.reactnative.networking.SSLPinningPackage;
-import chat.rocket.reactnative.mmkv.CustomMMKVJSIModulePackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -54,7 +53,7 @@ public class MainApplication extends Application implements ReactApplication {
 
     @Override
     protected JSIModulePackage getJSIModulePackage() {
-      return new CustomMMKVJSIModulePackage();
+      return new ReanimatedJSIModulePackage();
     }
 
     @Override
