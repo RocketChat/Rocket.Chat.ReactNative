7d157470994fe8252dadd55e72c9e712135ca2e9
Gerzon Z <gerzonzcanario@gmail.com>
2022-02-08 11:16:26 -0400

    Fix sdk

--- app/lib/rocketchat/services/sdk.ts
+++ app/lib/rocketchat/services/sdk.ts
@@ -1,27 +1,25 @@
-import Rocketchat from '@rocket.chat/sdk';
+import { Rocketchat } from '@rocket.chat/sdk';
 
-import reduxStore from '../../createStore';
+import store from '../../createStore';
 import { useSsl } from '../../../utils/url';
 
 class Sdk {
-	private sdk: typeof Rocketchat;
-
-	constructor() {
-		const { server } = reduxStore.getState();
-		this.sdk = new Rocketchat({ host: server, protocol: 'ddp', useSsl: useSsl(server) });
-		return this.sdk;
-	}
-
 	public get(...args: any[]): Promise<unknown> {
-		return this.sdk.get(...args);
+		const { server } = store.getState().server;
+		const sdk = new Rocketchat({ host: server, protocol: 'ddp', useSsl: useSsl(server) });
+		return sdk.get(...args);
 	}
 
 	public post(...args: any[]): Promise<unknown> {
-		return this.sdk.post(...args);
+		const { server } = store.getState().server;
+		const sdk = new Rocketchat({ host: server, protocol: 'ddp', useSsl: useSsl(server) });
+		return sdk.post(...args);
 	}
 
 	public methodCallWrapper(...args: any[]): Promise<unknown> {
-		return this.sdk.methodCall(...args);
+		const { server } = store.getState().server;
+		const sdk = new Rocketchat({ host: server, protocol: 'ddp', useSsl: useSsl(server) });
+		return sdk.methodCall(...args);
 	}
 }
 
