aece08ac3379959afced6dec6b5a21472f9c2a9d
GleidsonDaniel <gleidson10daniel@hotmail.com>
2022-02-09 13:43:05 -0300

    back to old times :)

--- app/lib/methods/callJitsi.js
+++ app/lib/methods/callJitsi.js
@@ -1,10 +1,8 @@
 import reduxStore from '../createStore';
 import Navigation from '../Navigation';
 import { events, logEvent } from '../../utils/log';
-import { ISubscription } from '../../definitions';
-import { IRocketChat } from '../../definitions/IRocketChat';
 
-async function jitsiURL(this: IRocketChat, { room }: { room: ISubscription }) {
+async function jitsiURL({ room }) {
 	const { settings } = reduxStore.getState();
 	const { Jitsi_Enabled } = settings;
 
@@ -32,15 +30,13 @@ async function jitsiURL(this: IRocketChat, { room }: { room: ISubscription }) {
 	if (Jitsi_URL_Room_Hash) {
 		rname = uniqueID + room?.rid;
 	} else {
-		// @ts-ignore
 		rname = encodeURIComponent(room.t === 'd' ? room?.usernames?.join?.(' x ') : room?.name);
-		// rname = encodeURIComponent(room.t === SubscriptionType.DIRECT ? room?.usernames?.join?.(' x ') || '' : room?.name);
 	}
 
 	return `${protocol}${domain}${prefix}${rname}${queryString}`;
 }
 
-export function callJitsiWithoutServer(path: string): void {
+export function callJitsiWithoutServer(path) {
 	logEvent(events.RA_JITSI_VIDEO);
 	const { Jitsi_SSL } = reduxStore.getState().settings;
 	const protocol = Jitsi_SSL ? 'https://' : 'http://';
@@ -48,7 +44,7 @@ export function callJitsiWithoutServer(path: string): void {
 	Navigation.navigate('JitsiMeetView', { url, onlyAudio: false });
 }
 
-async function callJitsi(this: IRocketChat, room: ISubscription, onlyAudio = false): Promise<void> {
+async function callJitsi(room, onlyAudio = false) {
 	logEvent(onlyAudio ? events.RA_JITSI_AUDIO : events.RA_JITSI_VIDEO);
 	const url = await jitsiURL.call(this, { room });
 	Navigation.navigate('JitsiMeetView', { url, onlyAudio, rid: room?.rid });
