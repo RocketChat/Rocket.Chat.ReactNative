f93caa6942318b9e397141f559974427d2c933b2
Gerzon Z <gerzonzcanario@gmail.com>
2022-02-09 10:29:27 -0400

    update test file

--- app/reducers/enterpriseModules.test.ts
+++ app/reducers/enterpriseModules.test.ts
@@ -1,6 +1,6 @@
 import { clearEnterpriseModules, setEnterpriseModules } from '../actions/enterpriseModules';
 import { mockedStore } from './mockedStore';
-import { initialState } from './enterpriseModules';
+import { IEnterpriseModules, initialState } from './enterpriseModules';
 
 describe('test enterpriseModules reducer', () => {
 	it('should return initial state', () => {
@@ -9,13 +9,13 @@ describe('test enterpriseModules reducer', () => {
 	});
 
 	it('should return modified store after setEnterpriseModules', () => {
-		const modules = ['omnichannel-mobile-enterprise', 'livechat-enterprise'];
+		const modules = ['omnichannel-mobile-enterprise', 'livechat-enterprise'] as IEnterpriseModules[];
 		mockedStore.dispatch(setEnterpriseModules(modules));
 		const state = mockedStore.getState().enterpriseModules;
 		expect(state).toEqual(modules);
 	});
 
-	it('should return empty store after setEnterpriseModules', () => {
+	it('should return empty store after clearEnterpriseModules', () => {
 		mockedStore.dispatch(clearEnterpriseModules());
 		const state = mockedStore.getState().enterpriseModules;
 		expect(state).toEqual([]);
