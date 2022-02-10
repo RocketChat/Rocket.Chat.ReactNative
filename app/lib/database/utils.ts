f0537c8b7628304b770fec596ec1061dd0bc5b3a
Diego Mello <diegolmello@gmail.com>
2022-02-10 15:08:44 -0300

    Migrate tests

--- app/lib/database/utils.ts
+++ app/lib/database/utils.ts
@@ -2,6 +2,6 @@ import XRegExp from 'xregexp';
 
 // Matches letters from any alphabet and numbers
 const likeStringRegex = XRegExp('[^\\p{L}\\p{Nd}]', 'g');
-export const sanitizeLikeString = (str: string) => str?.replace(likeStringRegex, '_');
+export const sanitizeLikeString = (str?: string): string | undefined => str?.replace(likeStringRegex, '_');
 
-export const sanitizer = (r: object) => r;
+export const sanitizer = (r: object): object => r;
