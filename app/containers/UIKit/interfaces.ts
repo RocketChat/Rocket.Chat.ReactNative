843a805ae8df48257d67ad9db63aade444915d5e
Reinaldo Neto <reinaldonetof@hotmail.com>
2022-02-10 19:52:28 -0300

    interface and uikit done

--- app/containers/UIKit/interfaces.ts
+++ app/containers/UIKit/interfaces.ts
@@ -1,10 +1,12 @@
+import { TThemeMode } from '../../definitions/ITheme';
+
 export type TTextBlock = 'plain_text' | 'plain_text_input';
 
 export type TButton = 'button';
 
 export type TInput = 'input';
 
-type TTypes = TTextBlock | TButton | TInput | 'divider' | 'static_select' | 'actions';
+type TTypes = TTextBlock | TButton | TInput | 'divider' | 'static_select' | 'actions' | 'image' | 'overflow';
 
 export enum ActionTypes {
 	ACTION = 'blockAction',
@@ -59,6 +61,9 @@ export interface IElement {
 	options?: Option[];
 	text?: IText;
 	value?: string;
+	initial_date?: any;
+	imageUrl?: string;
+	blockId?: string;
 }
 
 export interface IText {
@@ -83,7 +88,7 @@ export interface IContainer {
 	id: string;
 }
 
-// METHODS/ACTIONS
+// methods/actions
 export interface IUserInteraction {
 	triggerId: string;
 	appId?: string;
@@ -134,4 +139,100 @@ export interface ITriggerCancel {
 	appId: string;
 	viewId: string;
 	isCleared: boolean;
+}
+
+// UiKit components
+interface IParser {
+	renderAccessories: (data: TElementAccessory, context: string, parser: IParser) => JSX.Element;
+	renderActions: (data: Block, context: string, parser: IParser) => JSX.Element;
+	renderContext: (data: IElement, context: string, parser: IParser) => JSX.Element;
+	renderInputs: (data: Partial<IElement>, context: string, parser: IParser) => JSX.Element;
+	text: (data: IText) => JSX.Element;
+}
+export interface IActions extends Block {
+	parser?: IParser;
+	theme: TThemeMode;
+}
+
+export interface IContext extends Block {
+	parser?: IParser;
+}
+
+export interface IDatePicker extends Block {
+	language: string;
+	action: Function;
+	context: number;
+	loading: boolean;
+	value: string;
+	error: string;
+	theme: TThemeMode;
+}
+
+export interface IInput extends Block {
+	parser: IParser;
+	description: string;
+	error: string;
+	hint: string;
+	theme: TThemeMode;
+}
+
+export interface IThumb {
+	element: IElement;
+	size?: number;
+}
+export interface IImage {
+	element: IElement;
+	theme: TThemeMode;
+	context?: number;
+}
+
+// UiKit/Overflow
+export interface IOverflow extends Block {
+	action: Function;
+	loading: boolean;
+	parser: IParser;
+	theme: TThemeMode;
+	context: number;
+}
+
+interface PropsOption {
+	onOptionPress: Function;
+	parser: IParser;
+	theme: TThemeMode;
+}
+export interface IOptions extends PropsOption {
+	options: Option[];
+}
+
+export interface IOption extends PropsOption {
+	option: Option;
+}
+
+// UiKit/Section
+interface IAccessory {
+	type: TTypes;
+	actionId: string;
+	value: number;
+	text: IText;
+}
+
+type TElementAccessory = IAccessory & { blockId: string; appId: string };
+export interface IAccessoryComponent {
+	element: TElementAccessory;
+	parser: IParser;
+}
+export interface ISection {
+	blockId: string;
+	appId: string;
+	text?: IText;
+	accessory?: IAccessory;
+	parser: IParser;
+	theme: TThemeMode;
+	fields?: any[];
+}
+
+export interface IFields {
+	parser: IParser;
+	theme: TThemeMode;
+	fields: any[];
 }
