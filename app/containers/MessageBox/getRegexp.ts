fe831501a1569c95b01e491c0d492f813548c50c
Danish Ahmed Mirza <77742477+try-catch-stack@users.noreply.github.com>
2022-02-11 00:10:58 +0530

    Update getRegexp.ts

--- app/containers/MessageBox/getRegexp.ts
+++ app/containers/MessageBox/getRegexp.ts
@@ -1,9 +1,9 @@
 import { MENTIONS_TRACKING_TYPE_USERS } from './constants';
 
-// Regexp to select query string from the message to replace it with the suggestion
+// Match query string from the message to replace it with the suggestion
 const getRegexp = (trackingType: String): any => {
 	if (trackingType === MENTIONS_TRACKING_TYPE_USERS) {
-		// Selects query text after '@' for all languages while mentioning a user
+		// Match any character in query text after '@' while mentioning a user
 		return /([^@p{L}]+)$/im;
 	}
 	return /([a-z0-9._-]+)$/im;
