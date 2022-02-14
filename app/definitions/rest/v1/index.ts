a830867ac2a73f148e4fbe8ea5b9b6b520bbabef
Diego Mello <diegolmello@gmail.com>
2022-02-14 12:01:58 -0300

    Remove unused endpoints

--- app/definitions/rest/v1/index.ts
+++ app/definitions/rest/v1/index.ts
@@ -1,45 +1,33 @@
-import { BannersEndpoints } from './banners';
 import { ChannelsEndpoints } from './channels';
 import { ChatEndpoints } from './chat';
-import { CloudEndpoints } from './cloud';
 import { CustomUserStatusEndpoints } from './customUserStatus';
 import { DmEndpoints } from './dm';
-import { DnsEndpoints } from './dns';
 import { EmojiCustomEndpoints } from './emojiCustom';
 import { GroupsEndpoints } from './groups';
 import { ImEndpoints } from './im';
-import { InstancesEndpoints } from './instances';
 import { InvitesEndpoints } from './invites';
-import { MiscEndpoints } from './misc';
 import { OmnichannelEndpoints } from './omnichannel';
 import { PermissionsEndpoints } from './permissions';
 import { RolesEndpoints } from './roles';
 import { RoomsEndpoints } from './rooms';
 import { OauthCustomConfiguration } from './settings';
-import { StatisticsEndpoints } from './statistics';
 import { UserEndpoints } from './user';
 import { UsersEndpoints } from './users';
 import { TeamsEndpoints } from './teams';
 
-export type Endpoints = BannersEndpoints &
-	ChannelsEndpoints &
+export type Endpoints = ChannelsEndpoints &
 	ChatEndpoints &
-	CloudEndpoints &
 	CustomUserStatusEndpoints &
 	DmEndpoints &
-	DnsEndpoints &
 	EmojiCustomEndpoints &
 	GroupsEndpoints &
 	ImEndpoints &
-	InstancesEndpoints &
 	InvitesEndpoints &
-	MiscEndpoints &
 	OmnichannelEndpoints &
 	PermissionsEndpoints &
 	RolesEndpoints &
 	RoomsEndpoints &
 	OauthCustomConfiguration &
-	StatisticsEndpoints &
 	UserEndpoints &
 	UsersEndpoints &
 	TeamsEndpoints;
