0dddef12edc9d716e92d5eca6731f896ab49f001
Gerzon Z <gerzonzcanario@gmail.com>
2022-02-15 12:46:47 -0400

    set aliases for returns

--- app/selectors/login.ts
+++ app/selectors/login.ts
@@ -3,15 +3,25 @@ import isEmpty from 'lodash/isEmpty';
 
 import { IApplicationState } from '../definitions';
 
+interface IServices {
+	facebook: { clientId: string };
+	github: { clientId: string };
+	gitlab: { clientId: string };
+	google: { clientId: string };
+	linkedin: { clientId: string };
+	'meteor-developer': { clientId: string };
+	wordpress: { clientId: string; serverURL: string };
+}
+
 const getUser = (state: IApplicationState) => {
 	if (!isEmpty(state.share?.user)) {
 		return state.share.user;
 	}
 	return state.login?.user;
 };
-const getLoginServices = (state: IApplicationState) => state.login.services || {};
-const getShowFormLoginSetting = (state: IApplicationState) => state.settings.Accounts_ShowFormLogin || false;
-const getIframeEnabledSetting = (state: IApplicationState) => state.settings.Accounts_iframe_enabled || false;
+const getLoginServices = (state: IApplicationState) => (state.login.services as IServices) || {};
+const getShowFormLoginSetting = (state: IApplicationState) => (state.settings.Accounts_ShowFormLogin as boolean) || false;
+const getIframeEnabledSetting = (state: IApplicationState) => (state.settings.Accounts_iframe_enabled as boolean) || false;
 
 export const getUserSelector = createSelector([getUser], user => user);
 
