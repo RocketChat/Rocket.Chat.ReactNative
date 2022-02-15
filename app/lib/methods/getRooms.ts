ddcecea8b7232afbeef2ec0badd611d324305aab
Gerzon Z <gerzonzcanario@gmail.com>
2022-02-15 16:21:52 -0400

    add sdk and set any types

--- app/lib/methods/getRooms.ts
+++ app/lib/methods/getRooms.ts
@@ -1,12 +1,15 @@
-import { IRocketChat } from '../../definitions/IRocketChat';
+import sdk from '../rocketchat/services/sdk';
 
-export default function (this: IRocketChat, updatedSince: Date) {
+export default function (updatedSince: Date) {
 	let updatedDate = '';
 	// subscriptions.get: Since RC 0.60.0
 	// rooms.get: Since RC 0.62.0
 	if (updatedSince) {
 		updatedDate = updatedSince.toISOString();
-		return Promise.all([this.sdk.get('subscriptions.get', { updatedDate }), this.sdk.get('rooms.get', { updatedDate })]);
+		return Promise.all([
+			sdk.get('subscriptions.get' as any, { updatedDate } as any),
+			sdk.get('rooms.get' as any, { updatedDate } as any)
+		]);
 	}
-	return Promise.all([this.sdk.get('subscriptions.get'), this.sdk.get('rooms.get')]);
+	return Promise.all([sdk.get('subscriptions.get' as any), sdk.get('rooms.get' as any)]);
 }
