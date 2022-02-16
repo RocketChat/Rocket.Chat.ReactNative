784311e53e219b663804f4603801899d9bcd10b8
Diego Mello <diegolmello@gmail.com>
2022-02-16 11:38:30 -0300

    Moved the new variable around and added ts-ignore to follow the pattern from /services/restApi.ts

--- app/lib/methods/getRooms.ts
+++ app/lib/methods/getRooms.ts
@@ -1,15 +1,15 @@
 import sdk from '../rocketchat/services/sdk';
 
 export default function (updatedSince: Date) {
-	let updatedDate = '';
 	// subscriptions.get: Since RC 0.60.0
 	// rooms.get: Since RC 0.62.0
 	if (updatedSince) {
-		updatedDate = updatedSince.toISOString();
-		return Promise.all([
-			sdk.get('subscriptions.get' as any, { updatedDate } as any),
-			sdk.get('rooms.get' as any, { updatedDate } as any)
-		]);
+		const updatedDate = updatedSince.toISOString();
+		// TODO: missing definitions from server
+		// @ts-ignore
+		return Promise.all([sdk.get('subscriptions.get', { updatedDate }), sdk.get('rooms.get', { updatedDate })]);
 	}
-	return Promise.all([sdk.get('subscriptions.get' as any), sdk.get('rooms.get' as any)]);
+	// TODO: missing definitions from server
+	// @ts-ignore
+	return Promise.all([sdk.get('subscriptions.get'), sdk.get('rooms.get')]);
 }
