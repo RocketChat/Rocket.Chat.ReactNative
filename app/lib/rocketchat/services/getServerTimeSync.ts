330f8f5335bdd5162d1b7b7c17a16967ef227fff
Reinaldo Neto <reinaldonetof@hotmail.com>
2022-02-17 22:41:36 -0300

    tweak

--- app/lib/rocketchat/services/getServerTimeSync.ts
+++ app/lib/rocketchat/services/getServerTimeSync.ts
@@ -1,12 +1,8 @@
 import RNFetchBlob from 'rn-fetch-blob';
 
-interface IResponse {
-	data?: string;
-}
-
 export const getServerTimeSync = async (server: string) => {
 	try {
-		const response: IResponse | undefined = await Promise.race([
+		const response = await Promise.race([
 			RNFetchBlob.fetch('GET', `${server}/_timesync`),
 			new Promise<undefined>(res => setTimeout(res, 2000))
 		]);
