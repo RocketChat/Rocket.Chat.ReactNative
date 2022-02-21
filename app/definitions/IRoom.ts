dbe84e47ffa931cdc0e07081c2a6ce249facfc21
Gleidson Daniel Silva <yiweigao0226@gmail.com>
2022-02-21 16:41:49 -0300

    Chore: Migrate to Typescript mergeSubscriptionRooms and findSubscriptionsRooms (#3747)
    * fix fromJSONValue type
    
    * migrate findSubscription and mergeSubscription to typescript
    
    * chore: fix subscription param returning null

--- app/definitions/IRoom.ts
+++ app/definitions/IRoom.ts
@@ -1,5 +1,7 @@
 import Model from '@nozbe/watermelondb/Model';
+import { MarkdownAST } from '@rocket.chat/message-parser';
 
+import { IAttachment } from './IAttachment';
 import { IMessage } from './IMessage';
 import { IServedBy } from './IServedBy';
 import { SubscriptionType } from './ISubscription';
@@ -101,3 +103,52 @@ export interface IOmnichannelRoom extends Omit<IRoom, 'default' | 'featured' | '
 }
 
 export type TRoomModel = IRoom & Model;
+
+export interface IServerRoomItem {
+	_id: string;
+	name: string;
+	fname: string;
+	t: SubscriptionType;
+	u: {
+		_id: string;
+		username: string;
+	};
+	customFields: {};
+	ts: string;
+	ro: boolean;
+	_updatedAt: string;
+	lm: string;
+	lastMessage: {
+		alias: string;
+		msg: string;
+		attachments: IAttachment[];
+		parseUrls: boolean;
+		bot: {
+			i: string;
+		};
+		groupable: boolean;
+		avatar: string;
+		ts: string;
+		u: IUser;
+		rid: string;
+		_id: string;
+		_updatedAt: string;
+		mentions: [];
+		channels: [];
+		md: MarkdownAST;
+	};
+	topic: string;
+	joinCodeRequired: boolean;
+	description: string;
+	jitsiTimeout: string;
+	usersCount: number;
+	e2eKeyId: string;
+	avatarETag: string;
+	encrypted: boolean;
+}
+
+export interface IServerRoom {
+	update: IServerRoomItem[];
+	remove: IServerRoomItem[];
+	success: boolean;
+}
