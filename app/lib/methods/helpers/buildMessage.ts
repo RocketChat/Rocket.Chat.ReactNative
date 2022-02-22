b7645961649e03ce141f2a7e2f3fb6941f71c925
GleidsonDaniel <gleidson10daniel@hotmail.com>
2022-02-22 11:25:12 -0300

    fix type

--- app/lib/methods/helpers/buildMessage.ts
+++ app/lib/methods/helpers/buildMessage.ts
@@ -2,7 +2,7 @@ import { IMessage, IThreadResult } from '../../../definitions';
 import messagesStatus from '../../../constants/messagesStatus';
 import normalizeMessage from './normalizeMessage';
 
-export default (message: IMessage | IThreadResult): IMessage | IThreadResult => {
+export default (message: Partial<IMessage> | IThreadResult): Partial<IMessage> | IThreadResult => {
 	message.status = messagesStatus.SENT;
 	return normalizeMessage(message);
 };
