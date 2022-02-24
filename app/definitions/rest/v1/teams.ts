0ae6ed7f6bb90383ad4a6c6a1b34062b3641eb03
AlexAlexandre <alexalexandrejr@gmail.com>
2022-02-24 11:05:39 -0300

    chore: type the API call `teams.updateRoom`

--- app/definitions/rest/v1/teams.ts
+++ app/definitions/rest/v1/teams.ts
@@ -4,4 +4,7 @@ export type TeamsEndpoints = {
 	'teams.removeRoom': {
 		POST: (params: { roomId: string; teamId: string }) => { room: IRoom };
 	};
+	'teams.updateRoom': {
+		POST: (params: { roomId: string; isDefault: boolean }) => { room: IRoom };
+	};
 };
