12e08f54989993034c96b0f6c565e88e18c1a697
AlexAlexandre <alexalexandrejr@gmail.com>
2022-02-24 11:16:07 -0300

    chore: creating the interface `IServerTeamUpdateRoom`

--- app/definitions/rest/v1/teams.ts
+++ app/definitions/rest/v1/teams.ts
@@ -1,10 +1,11 @@
 import { IRoom } from '../../IRoom';
+import { IServerTeamUpdateRoom } from '../../ITeam';
 
 export type TeamsEndpoints = {
 	'teams.removeRoom': {
 		POST: (params: { roomId: string; teamId: string }) => { room: IRoom };
 	};
 	'teams.updateRoom': {
-		POST: (params: { roomId: string; isDefault: boolean }) => { room: IRoom };
+		POST: (params: { roomId: string; isDefault: boolean }) => { room: IServerTeamUpdateRoom };
 	};
 };
