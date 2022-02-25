d5f8721274f62380643b7e262321ec91e9589c40
Reinaldo Neto <reinaldonetof@hotmail.com>
2022-02-25 16:04:33 -0300

    react.reactelement | null

--- app/containers/Avatar/interfaces.ts
+++ app/containers/Avatar/interfaces.ts
@@ -11,7 +11,7 @@ export interface IAvatar {
 	size?: number;
 	borderRadius?: number;
 	type?: string;
-	children?: React.ReactNode;
+	children?: React.ReactElement | null;
 	user?: {
 		id?: string;
 		token?: string;
