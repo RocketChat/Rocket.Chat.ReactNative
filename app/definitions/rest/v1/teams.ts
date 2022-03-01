fd36d3fa7bb0f61cae69310e114695941728e1da
Gerzon Z <gerzonzcanario@gmail.com>
2022-03-01 17:17:17 -0400

    migrate convertTeamToChannel to ts

--- app/definitions/rest/v1/teams.ts
+++ app/definitions/rest/v1/teams.ts
@@ -4,4 +4,7 @@ export type TeamsEndpoints = {
 	'teams.removeRoom': {
 		POST: (params: { roomId: string; teamId: string }) => { room: IRoom };
 	};
+	'teams.convertToChannel': {
+		POST: (params: { teamId: string; roomsToRemove?: string[] }) => { success: boolean };
+	};
 };
