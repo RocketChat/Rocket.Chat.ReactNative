5f579289152f4a787e48d95b2ba237ac1a5ef9ab
Gerzon Z <gerzonzcanario@gmail.com>
2022-03-01 17:58:51 -0400

    migrate subscriptions.unread to ts

--- app/lib/rocketchat/services/restApi.ts
+++ app/lib/rocketchat/services/restApi.ts
@@ -264,10 +264,8 @@ export const deleteMessage = (messageId: string, rid: string): any =>
 	// @ts-ignore
 	sdk.post('chat.delete', { msgId: messageId, roomId: rid });
 
-export const markAsUnread = ({ messageId }: { messageId: string }): any =>
+export const markAsUnread = ({ messageId }: { messageId: string }) =>
 	// RC 0.65.0
-	// TODO: missing definitions from server
-	// @ts-ignore
 	sdk.post('subscriptions.unread', { firstUnreadMessage: { _id: messageId } });
 
 export const toggleStarMessage = (messageId: string, starred: boolean): any => {
