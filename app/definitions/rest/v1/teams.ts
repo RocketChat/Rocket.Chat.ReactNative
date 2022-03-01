3a720e8f4c52f84ffd09da70fd402d98db67fd45
Gerzon Z <gerzonzcanario@gmail.com>
2022-03-01 16:56:55 -0400

    migrate removeMember rest api to ts

--- app/definitions/rest/v1/teams.ts
+++ app/definitions/rest/v1/teams.ts
@@ -4,4 +4,7 @@ export type TeamsEndpoints = {
 	'teams.removeRoom': {
 		POST: (params: { roomId: string; teamId: string }) => { room: IRoom };
 	};
+	'teams.removeMember': {
+		POST: (params: { teamId: string; userId: string; rooms?: string[] }) => { success: boolean };
+	};
 };
