3b9fbb4f7a363ea9fac5150d27117d005e0cd135
Gerzon Z <gerzonzcanario@gmail.com>
2022-03-02 13:48:33 -0400

    Update teams.ts

--- app/definitions/rest/v1/teams.ts
+++ app/definitions/rest/v1/teams.ts
@@ -5,6 +5,6 @@ export type TeamsEndpoints = {
 		POST: (params: { roomId: string; teamId: string }) => { room: IRoom };
 	};
 	'teams.removeMember': {
-		POST: (params: { teamId: string; userId: string; rooms?: string[] }) => { success: boolean };
+		POST: (params: { teamId: string; userId: string; rooms?: string[] }) => {};
 	};
 };
