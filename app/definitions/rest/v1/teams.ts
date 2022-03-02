44879526a2acc3d98ba0029c3b8d54507b8c9299
Gerzon Z <yiweigao0226@gmail.com>
2022-03-02 16:17:42 -0400

    Chore: Migrate REST API - createTeam to TS (#3788)
    * Migrate `teams.create` REST API to TypeScript

--- app/definitions/rest/v1/teams.ts
+++ app/definitions/rest/v1/teams.ts
@@ -1,7 +1,16 @@
 import { IRoom } from '../../IRoom';
+import { ITeam, TEAM_TYPE } from '../../ITeam';
 
 export type TeamsEndpoints = {
 	'teams.removeRoom': {
 		POST: (params: { roomId: string; teamId: string }) => { room: IRoom };
+	};
+	'teams.create': {
+		POST: (params: {
+			name: string;
+			users: string[];
+			type: TEAM_TYPE;
+			room: { readOnly: boolean; extraData: { broadcast: boolean; encrypted: boolean } };
+		}) => { team: ITeam };
 	};
 };
