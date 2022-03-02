c5023eaeea284b07b61abd418e0d198f6a0c055e
Gerzon Z <gerzonzcanario@gmail.com>
2022-03-02 13:44:42 -0400

    Chore: Migrate REST API - convertChannelToTeam to TS (#3792)
    * migrate channels.convertToTeam and groups.convertToTeam to ts
    
    Co-authored-by: Gleidson Daniel Silva <gleidson10daniel@hotmail.com>

--- app/definitions/rest/v1/channels.ts
+++ app/definitions/rest/v1/channels.ts
@@ -1,3 +1,4 @@
+import { ITeam } from '../../ITeam';
 import type { IMessage, IMessageFromServer } from '../../IMessage';
 import type { IRoom } from '../../IRoom';
 import type { IUser } from '../../IUser';
@@ -27,5 +28,8 @@ export type ChannelsEndpoints = {
 		GET: (params: { roomId: string; count: number; latest?: string }) => {
 			messages: IMessageFromServer[];
 		};
+	};
+	'channels.convertToTeam': {
+		POST: (params: { channelId: string; channelName: string }) => { team: ITeam };
 	};
 };
