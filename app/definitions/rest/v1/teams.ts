9138f14261ea52bb2bf6f87589f1c215a1ff0211
Gerzon Z <gerzonzcanario@gmail.com>
2022-03-02 10:42:09 -0400

    migrate teams.listRoomsOfUser to ts

--- app/definitions/rest/v1/teams.ts
+++ app/definitions/rest/v1/teams.ts
@@ -4,4 +4,13 @@ export type TeamsEndpoints = {
 	'teams.removeRoom': {
 		POST: (params: { roomId: string; teamId: string }) => { room: IRoom };
 	};
+	'teams.listRoomsOfUser': {
+		GET: (params: { teamId: string; userId: string }) => {
+			rooms: IRoom[];
+			total: number;
+			count: number;
+			offset: number;
+			success: boolean;
+		};
+	};
 };
