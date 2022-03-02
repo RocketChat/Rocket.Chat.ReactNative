79a034e2b0dc8bca0152a2d4b7d749edee7230fe
Reinaldo Neto <reinaldonetof@hotmail.com>
2022-03-02 16:31:31 -0300

    Chore: Migrate REST API - toggleBlockUser to Typescript

--- app/lib/rocketchat/services/restApi.ts
+++ app/lib/rocketchat/services/restApi.ts
@@ -461,7 +461,7 @@ export const getListCannedResponse = ({ scope = '', departmentId = '', offset = 
 	return sdk.get('canned-responses', params);
 };
 
-export const toggleBlockUser = (rid: string, blocked: string, block: boolean) => {
+export const toggleBlockUser = (rid: string, blocked: string, block: boolean): Promise<boolean> => {
 	if (block) {
 		// RC 0.49.0
 		return sdk.methodCallWrapper('blockUser', { rid, blocked });
