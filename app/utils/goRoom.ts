6b41444467dcb06440083d96719558a76ab4f457
Diego Mello <diegolmello@gmail.com>
2022-03-02 11:46:03 -0300

    Fix ts lint

--- app/utils/goRoom.ts
+++ app/utils/goRoom.ts
@@ -1,9 +1,9 @@
 import { ChatsStackParamList } from '../stacks/types';
 import Navigation from '../lib/Navigation';
 import RocketChat from '../lib/rocketchat';
-import { IVisitor, SubscriptionType, TSubscriptionModel } from '../definitions/ISubscription';
+import { ISubscription, IVisitor, SubscriptionType, TSubscriptionModel } from '../definitions/ISubscription';
 
-export interface IGoRoomItem {
+interface IGoRoomItem {
 	search?: boolean; // comes from spotlight
 	username?: string;
 	t?: SubscriptionType;
@@ -13,12 +13,14 @@ export interface IGoRoomItem {
 	visitor?: IVisitor;
 }
 
+export type TGoRoomItem = IGoRoomItem | TSubscriptionModel | ISubscription;
+
 const navigate = ({
 	item,
 	isMasterDetail,
 	...props
 }: {
-	item: IGoRoomItem | TSubscriptionModel;
+	item: TGoRoomItem;
 	isMasterDetail: boolean;
 	navigationMethod?: () => ChatsStackParamList;
 }) => {
@@ -45,7 +47,7 @@ export const goRoom = async ({
 	isMasterDetail = false,
 	...props
 }: {
-	item: IGoRoomItem | TSubscriptionModel;
+	item: TGoRoomItem;
 	isMasterDetail: boolean;
 	navigationMethod?: any;
 	jumpToMessageId?: string;
