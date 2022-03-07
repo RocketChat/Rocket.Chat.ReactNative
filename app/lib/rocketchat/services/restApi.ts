b21d044f138498759b9e8ef129f1683030e16378
Reinaldo Neto <47038980+reinaldonetof@users.noreply.github.com>
2022-03-07 12:16:20 -0300

    Chore: Migrate REST API - spotlight to Typescript (#3821)
    * Chore: Migrate REST API - spotlight to Typescript
    
    * minor tweak

--- app/lib/rocketchat/services/restApi.ts
+++ app/lib/rocketchat/services/restApi.ts
@@ -2,6 +2,7 @@ import sdk from './sdk';
 import { TEAM_TYPE } from '../../../definitions/ITeam';
 import roomTypeToApiType, { RoomTypes } from '../methods/roomTypeToApiType';
 import { SubscriptionType, INotificationPreferences } from '../../../definitions';
+import { ISpotlight } from '../../../definitions/ISpotlight';
 
 export const createChannel = ({
 	name,
@@ -72,7 +73,7 @@ export const forgotPassword = (email: string) =>
 export const sendConfirmationEmail = (email: string): Promise<{ message: string; success: boolean }> =>
 	sdk.methodCallWrapper('sendConfirmationEmail', email);
 
-export const spotlight = (search: string, usernames: string, type: { users: boolean; rooms: boolean }) =>
+export const spotlight = (search: string, usernames: string[], type: { users: boolean; rooms: boolean }): Promise<ISpotlight> =>
 	// RC 0.51.0
 	sdk.methodCallWrapper('spotlight', search, usernames, type);
 
