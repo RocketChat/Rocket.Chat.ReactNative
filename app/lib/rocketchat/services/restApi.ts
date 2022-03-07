5afa268527730b89398a3f7f86abda07a8ee0eea
Gleidson Daniel Silva <yiweigao0226@gmail.com>
2022-03-07 09:33:58 -0300

    Chore: Migrate REST API - deleteRoom to Typescript (#3834)

--- app/lib/rocketchat/services/restApi.ts
+++ app/lib/rocketchat/services/restApi.ts
@@ -432,10 +432,8 @@ export const leaveRoom = (roomId: string, t: RoomTypes) =>
 	// RC 0.48.0
 	sdk.post(`${roomTypeToApiType(t)}.leave`, { roomId });
 
-export const deleteRoom = (roomId: string, t: RoomTypes): any =>
+export const deleteRoom = (roomId: string, t: RoomTypes) =>
 	// RC 0.49.0
-	// TODO: missing definitions from server
-	// @ts-ignore
 	sdk.post(`${roomTypeToApiType(t)}.delete`, { roomId });
 
 export const toggleMuteUserInRoom = (rid: string, username: string, mute: boolean) => {
