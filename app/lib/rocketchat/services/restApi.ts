0ad761e395e5b2e1902be2f51756d2595f4816a2
Alex Junior <yiweigao0226@gmail.com>
2022-03-07 12:23:28 -0300

    chore: add rest api return (#3844)

--- app/lib/rocketchat/services/restApi.ts
+++ app/lib/rocketchat/services/restApi.ts
@@ -435,7 +435,11 @@ export const deleteRoom = (roomId: string, t: RoomTypes) =>
 	// RC 0.49.0
 	sdk.post(`${roomTypeToApiType(t)}.delete`, { roomId });
 
-export const toggleMuteUserInRoom = (rid: string, username: string, mute: boolean) => {
+export const toggleMuteUserInRoom = (
+	rid: string,
+	username: string,
+	mute: boolean
+): Promise<{ message: { msg: string; result: boolean }; success: boolean }> => {
 	if (mute) {
 		// RC 0.51.0
 		return sdk.methodCallWrapper('muteUserInRoom', { rid, username });
