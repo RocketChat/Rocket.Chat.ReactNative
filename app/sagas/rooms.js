8c410d0b7f2b3a9969d35b1131e9a78563570762
Diego Mello <diegolmello@gmail.com>
2022-03-08 10:09:10 -0300

    Remove migration

--- app/sagas/rooms.js
+++ app/sagas/rooms.js
@@ -47,15 +47,6 @@ const handleRoomsRequest = function* handleRoomsRequest({ params }) {
 			}
 		}
 
-		// Force fetch all subscriptions to update columns related to Teams feature
-		// TODO: remove it a couple of releases
-		const teamsMigrationKey = `${server}_TEAMS_MIGRATION`;
-		const teamsMigration = yield UserPreferences.getBoolAsync(teamsMigrationKey);
-		if (!teamsMigration) {
-			roomsUpdatedAt = null;
-			UserPreferences.setBoolAsync(teamsMigrationKey, true);
-		}
-
 		const [subscriptionsResult, roomsResult] = yield RocketChat.getRooms(roomsUpdatedAt);
 		const subscriptions = yield mergeSubscriptionsRooms(subscriptionsResult, roomsResult);
 		const db = database.active;
