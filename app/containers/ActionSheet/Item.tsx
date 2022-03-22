45e5a1e93de3607e3a1100865524e5cc536a119b
Gleidson Daniel Silva <yiweigao0226@gmail.com>
2022-03-22 17:44:27 -0300

    Chore: Evaluate ActionSheet - TypeScript (#3927)

--- app/containers/ActionSheet/Item.tsx
+++ app/containers/ActionSheet/Item.tsx
@@ -3,23 +3,24 @@ import { Text, View } from 'react-native';
 
 import { themes } from '../../constants/colors';
 import { CustomIcon } from '../../lib/Icons';
+import { useTheme } from '../../theme';
 import { Button } from './Button';
 import styles from './styles';
 
-interface IActionSheetItem {
+export interface IActionSheetItem {
 	item: {
 		title: string;
 		icon: string;
-		danger: boolean;
-		testID: string;
-		onPress(): void;
-		right: Function;
+		danger?: boolean;
+		testID?: string;
+		onPress: () => void;
+		right?: Function;
 	};
-	theme: string;
 	hide(): void;
 }
 
-export const Item = React.memo(({ item, hide, theme }: IActionSheetItem) => {
+export const Item = React.memo(({ item, hide }: IActionSheetItem) => {
+	const { theme } = useTheme();
 	const onPress = () => {
 		hide();
 		item?.onPress();
