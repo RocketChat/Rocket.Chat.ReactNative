1e4d971dd4553acaec74c78d37fce92a77b62546
GleidsonDaniel <gleidson10daniel@hotmail.com>
2022-03-23 16:15:10 -0300

    upgrade pod versions

--- ios/RocketChatRN/AppDelegate.m
+++ ios/RocketChatRN/AppDelegate.m
@@ -102,6 +102,10 @@ static void InitializeFlipper(UIApplication *application) {
   [RNNotifications didFailToRegisterForRemoteNotificationsWithError:error];
 }
 
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult result))completionHandler {
+  [RNNotifications didReceiveBackgroundNotification:userInfo withCompletionHandler:completionHandler];
+}
+
 - (BOOL)application:(UIApplication *)application openURL:(NSURL *)url
   sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
 {
