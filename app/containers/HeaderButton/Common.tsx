748ff95f0db34e5f2d26009e32380781a8523e23
Gerzon Z <yiweigao0226@gmail.com>
2022-03-25 10:35:25 -0400

    [FIX] Drawer failing to open/close on ProfileView (#3963)

--- app/containers/HeaderButton/Common.tsx
+++ app/containers/HeaderButton/Common.tsx
@@ -13,7 +13,7 @@ interface IHeaderButtonCommon {
 
 // Left
 export const Drawer = React.memo(
-	({ navigation, testID, onPress = navigation?.toggleDrawer(), ...props }: IHeaderButtonCommon) => (
+	({ navigation, testID, onPress = () => navigation?.toggleDrawer(), ...props }: IHeaderButtonCommon) => (
 		<Container left>
 			<Item iconName='hamburguer' onPress={onPress} testID={testID} {...props} />
 		</Container>
@@ -57,6 +57,8 @@ export const Preferences = React.memo(({ onPress, testID, ...props }: IHeaderBut
 	</Container>
 ));
 
-export const Legal = React.memo(({ navigation, testID, onPress = navigation?.navigate('LegalView') }: IHeaderButtonCommon) => (
-	<More onPress={onPress} testID={testID} />
-));
+export const Legal = React.memo(
+	({ navigation, testID, onPress = () => navigation?.navigate('LegalView') }: IHeaderButtonCommon) => (
+		<More onPress={onPress} testID={testID} />
+	)
+);
