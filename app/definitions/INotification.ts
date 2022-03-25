569f19aed4273344f1780f537566da00260ad177
GleidsonDaniel <gleidson10daniel@hotmail.com>
2022-03-25 12:16:45 -0300

    update INotification interface

--- app/definitions/INotification.ts
+++ app/definitions/INotification.ts
@@ -1,13 +1,21 @@
 export interface INotification {
-	message: string;
-	style: string;
-	ejson: string;
-	collapse_key: string;
-	notId: string;
-	msgcnt: string;
-	title: string;
-	from: string;
-	image: string;
-	soundname: string;
-	getData: () => INotification;
+	payload: {
+		message: string;
+		'google.message_id': string;
+		style: string;
+		ejson: string;
+		collapse_key: string;
+		'google.delivered_priority': string;
+		'google.original_priority': string;
+		notId: string;
+		msgcnt: string;
+		title: string;
+		'google.sent_time': number;
+		from: string;
+		'google.c.sender.id': string;
+		'google.ttl': number;
+		image: string;
+		soundname: string;
+	};
+	identifier: string;
 }
