15d09c58fff1d1ea4220f3c0d1e78e6392f85bf4
Reinaldo Neto <reinaldonetof@hotmail.com>
2022-03-29 16:13:04 -0300

    remove fix to roomItem

--- app/i18n/index.ts
+++ app/i18n/index.ts
@@ -114,7 +114,6 @@ export const setLanguage = (l: string) => {
 	i18n.translations = { ...i18n.translations, [locale]: translations[locale]?.() };
 	I18nManager.forceRTL(isRTL(locale));
 	I18nManager.swapLeftAndRightInRTL(isRTL(locale));
-	// TODO: Evaluate why this is here
 	// @ts-ignore
 	i18n.isRTL = I18nManager.isRTL;
 	moment.locale(toMomentLocale(locale));
--- app/i18n/isRTL.ts
+++ app/i18n/isRTL.ts
@@ -1,6 +1,4 @@
-import i18n from './index';
-
 // https://github.com/zoontek/react-native-localize/blob/master/src/constants.ts#L5
 const USES_RTL_LAYOUT = ['ar', 'ckb', 'fa', 'he', 'ks', 'lrc', 'mzn', 'ps', 'ug', 'ur', 'yi'];
 
-export const isRTL = (locale?: string) => (locale ? USES_RTL_LAYOUT.includes(locale) : i18n.isRTL);
+export const isRTL = (locale?: string) => (locale ? USES_RTL_LAYOUT.includes(locale) : false);
