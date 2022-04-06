a684ee8ed2de3e3a76757e374551e8bf8b34ee8e
GleidsonDaniel <gleidson10daniel@hotmail.com>
2022-04-06 09:26:21 -0300

    fix import E2E_MESSAGE_TYPE

--- app/views/RoomView/index.tsx
+++ app/views/RoomView/index.tsx
@@ -48,7 +48,6 @@ import Navigation from '../../lib/Navigation';
 import SafeAreaView from '../../containers/SafeAreaView';
 import { withDimensions } from '../../dimensions';
 import { getHeaderTitlePosition } from '../../containers/Header';
-import { E2E_MESSAGE_TYPE, E2E_STATUS } from '../../lib/constants';
 import { takeInquiry } from '../../ee/omnichannel/lib';
 import Loading from '../../containers/Loading';
 import { goRoom, TGoRoomItem } from '../../utils/goRoom';
@@ -82,6 +81,7 @@ import {
 	TThreadModel
 } from '../../definitions';
 import { ICustomEmojis } from '../../reducers/customEmojis';
+import { E2E_MESSAGE_TYPE, E2E_STATUS } from '../../constants';
 
 const stateAttrsUpdate = [
 	'joined',
