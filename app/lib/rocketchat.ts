42500661de42894fa675c27d5fed4845da33007b
GleidsonDaniel <gleidson10daniel@hotmail.com>
2022-04-07 12:03:41 -0300

    move methods to lib

--- app/lib/rocketchat.ts
+++ app/lib/rocketchat.ts
@@ -1,18 +1,18 @@
 // Methods
-import canOpenRoom from '../methods/canOpenRoom';
-import clearCache from '../methods/clearCache';
-import getRoom from '../methods/getRoom';
-import getRooms from '../methods/getRooms';
-import getSlashCommands from '../methods/getSlashCommands';
-import loadMessagesForRoom from '../methods/loadMessagesForRoom';
-import loadMissedMessages from '../methods/loadMissedMessages';
-import loadNextMessages from '../methods/loadNextMessages';
-import loadSurroundingMessages from '../methods/loadSurroundingMessages';
-import loadThreadMessages from '../methods/loadThreadMessages';
-import readMessages from '../methods/readMessages';
-import roomTypeToApiType from '../methods/roomTypeToApiType';
+import canOpenRoom from './methods/canOpenRoom';
+import clearCache from './methods/clearCache';
+import getRoom from './methods/getRoom';
+import getRooms from './methods/getRooms';
+import getSlashCommands from './methods/getSlashCommands';
+import loadMessagesForRoom from './methods/loadMessagesForRoom';
+import loadMissedMessages from './methods/loadMissedMessages';
+import loadNextMessages from './methods/loadNextMessages';
+import loadSurroundingMessages from './methods/loadSurroundingMessages';
+import loadThreadMessages from './methods/loadThreadMessages';
+import readMessages from './methods/readMessages';
+import roomTypeToApiType from './methods/roomTypeToApiType';
 // Spread Methods
-import * as sendMessage from '../methods/sendMessage';
+import * as sendMessage from './methods/sendMessage';
 import * as callJitsi from './methods/callJitsi';
 import * as enterpriseModules from './methods/enterpriseModules';
 import * as getCustomEmojis from './methods/getCustomEmojis';
@@ -28,9 +28,9 @@ import * as sendFileMessage from './methods/sendFileMessage';
 import * as setUser from './methods/setUser';
 import * as triggerActions from './methods/triggerActions';
 import * as userPreferencesMethods from './methods/userPreferencesMethods';
-import * as connect from '../services/connect';
-import * as restApis from '../services/restApi';
-import * as shareExtension from '../services/shareExtension';
+import * as connect from './services/connect';
+import * as restApis from './services/restApi';
+import * as shareExtension from './services/shareExtension';
 
 const TOKEN_KEY = 'reactnativemeteor_usertoken';
 const CURRENT_SERVER = 'currentServer';
