e1b81651c766c574f4d63b2bbf7fbb8b22493e1a
Reinaldo Neto <reinaldoneto@Reinaldos-MacBook-Pro.local>
2022-05-09 19:37:42 -0300

    improve usePermissions

--- app/hooks/usePermissions.ts
+++ app/hooks/usePermissions.ts
@@ -1,40 +1,45 @@
-/* eslint-disable @typescript-eslint/indent */
-import React, { useState, useEffect } from 'react'
-import { useSelector } from 'react-redux'
+import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { dequal } from 'dequal';
 
 import { TSupportedPermissions } from '../reducers/permissions';
-import { IApplicationState } from '../definitions'
+import { IApplicationState } from '../definitions';
 import { getUserSelector } from '../selectors/login';
 import { hasPermission } from '../lib/methods';
 
 function usePermissions(permissions: TSupportedPermissions[], rid?: string) {
-    const [permissionsState, setPermissionsState] = useState<boolean[]>([])
+	const [permissionsState, setPermissionsState] = useState<boolean[]>([]);
 
-    const permissionsRedux = useSelector((state: IApplicationState) => state.permissions)
-    const userRoles = useSelector((state: IApplicationState) => getUserSelector(state).roles)
+	const permissionsRedux = useSelector(
+		(state: IApplicationState) => state.permissions,
+		(nextState, previousState) => {
+			const someDifferent = permissions.some(key => !dequal(nextState?.[key], previousState?.[key]));
+			// The equality function is expecting return false when we want to re-render and true when we don't want to re-render
+			if (someDifferent) {
+				return false;
+			}
+			return true;
+		}
+	);
 
-    console.log(permissions, permissionsState, permissionsRedux, userRoles)
-    console.count('usePermissions')
+	const userRoles = useSelector((state: IApplicationState) => getUserSelector(state).roles);
 
-    const haspermissions = async (perms: (string[] | undefined)[]) => {
-        console.log("🚀 ~ file: usePermissions.ts ~ line 19 ~ haspermissions ~ perms", perms)
+	console.count('usePermissions');
 
-        const result = await hasPermission(perms)
-        console.log("🚀 ~ file: usePermissions.ts ~ line 20 ~ haspermissions ~ result", result)
-        setPermissionsState(result)
-    }
+	const _hasPermissions = async (perms: (string[] | undefined)[], _rid?: string) => {
+		const result = await hasPermission(perms, _rid);
+		setPermissionsState(result);
+	};
 
-    useEffect(() => {
-        if (permissionsRedux) {
-            const array: (string[] | undefined)[] = []
-            permissions.forEach(p => array.push(permissionsRedux[p]))
-            haspermissions(array)
-        }
-    }, [userRoles, permissionsRedux])
+	useEffect(() => {
+		if (permissionsRedux) {
+			const array: (string[] | undefined)[] = [];
+			permissions.forEach(p => array.push(permissionsRedux[p]));
+			_hasPermissions(array, rid);
+		}
+	}, [userRoles, permissionsRedux]);
 
-
-    return permissionsState
-
+	return permissionsState;
 }
 
-export default usePermissions
+export default usePermissions;
