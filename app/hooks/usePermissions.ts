d368c74466a6c6f1676491ab5b1a38fb4ccdf3e0
Reinaldo Neto <reinaldoneto@Reinaldos-MacBook-Pro.local>
2022-05-10 00:20:11 -0300

    add rule to usePermission

--- app/hooks/usePermissions.ts
+++ app/hooks/usePermissions.ts
@@ -5,10 +5,11 @@ import { dequal } from 'dequal';
 import { TSupportedPermissions } from '../reducers/permissions';
 import { IApplicationState } from '../definitions';
 import { getUserSelector } from '../selectors/login';
-import { hasPermission } from '../lib/methods';
+import database from '../lib/database';
+import log from '../utils/log';
 
 function usePermissions(permissions: TSupportedPermissions[], rid?: string) {
-	const [permissionsState, setPermissionsState] = useState<boolean[]>([]);
+	const [permissionsState, setPermissionsState] = useState<(boolean | undefined)[]>([]);
 
 	const permissionsRedux = useSelector(
 		(state: IApplicationState) => state.permissions,
@@ -27,8 +28,30 @@ function usePermissions(permissions: TSupportedPermissions[], rid?: string) {
 	console.count('usePermissions');
 
 	const _hasPermissions = async (perms: (string[] | undefined)[], _rid?: string) => {
-		const result = await hasPermission(perms, _rid);
-		setPermissionsState(result);
+		let roomRoles: string[] = [];
+		if (rid) {
+			const db = database.active;
+			const subsCollection = db.get('subscriptions');
+			try {
+				// get the room from database
+				const room = await subsCollection.find(rid);
+				// get room roles
+				roomRoles = room.roles || [];
+			} catch (error) {
+				console.log('hasPermission -> Room not found');
+				return perms.map(() => false);
+			}
+		}
+
+		try {
+			// get user roles on the server from redux
+			const userRolesTmp = userRoles || [];
+			const mergedRoles = [...new Set([...roomRoles, ...userRolesTmp])];
+			const result = perms.map(permission => permission?.some(r => mergedRoles.includes(r) ?? false));
+			setPermissionsState(result);
+		} catch (e) {
+			log(e);
+		}
 	};
 
 	useEffect(() => {
