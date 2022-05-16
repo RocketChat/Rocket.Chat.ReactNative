2e04330a6dff140742d1ce713584843dd23f0acb
Reinaldo Neto <reinaldonetof@hotmail.com>
2022-05-16 11:18:51 -0300

    Chore: Update UiKit

--- app/externalModules.d.ts
+++ app/externalModules.d.ts
@@ -4,7 +4,6 @@ declare module 'commonmark-react-renderer';
 declare module 'remove-markdown';
 declare module 'react-native-image-progress';
 declare module 'react-native-ui-lib/keyboard';
-declare module '@rocket.chat/ui-kit';
 declare module '@rocket.chat/sdk';
 declare module 'react-native-config-reader';
 declare module 'react-native-keycommands';
