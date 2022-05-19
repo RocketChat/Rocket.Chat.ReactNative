4e09009e251620a0df278c9e56a09c6121d6cae9
Alex Junior <alexalexandrejr@gmail.com>
2022-05-18 22:34:33 -0300

    minor tweak

--- app/containers/SearchBox/index.tsx
+++ app/containers/SearchBox/index.tsx
@@ -1,23 +1,15 @@
 import React from 'react';
-import { StyleSheet, TextInputProps, View } from 'react-native';
+import { TextInputProps, View } from 'react-native';
 
 import { useTheme } from '../../theme';
 import I18n from '../../i18n';
 import FormTextInput from '../TextInput/FormTextInput';
 
-const styles = StyleSheet.create({
-	input: {
-		height: 44,
-		paddingHorizontal: 16,
-		borderWidth: 2
-	}
-});
-
 const SearchBox = ({ onChangeText, onSubmitEditing, value, testID }: TextInputProps): JSX.Element => {
 	const { colors, theme } = useTheme();
 	const background = theme === 'light' ? colors.backgroundColor : colors.searchboxBackground;
 	const inputStyle = {
-		...styles.input,
+		borderWidth: 2,
 		borderColor: colors.searchboxBackground,
 		backgroundColor: background,
 		color: colors.auxiliaryTintColor
