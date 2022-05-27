94e27546389ff4226ee42d7f8ea0810917bebcb0
Reinaldo Neto <yiweigao0226@gmail.com>
2022-05-27 14:18:51 -0300

    [FIX] Navigate to MarkdownTableView using tablets (#4222)
    * [FIX] Navigate to MarkdownTableView using tablets
    
    * [FIX] Navigate to MarkdownTableView using tablets
    
    * fix lint
    
    * update yarn.lock
    
    Co-authored-by: Alex Junior <alexalexandrejr@gmail.com>

--- app/containers/markdown/Table.tsx
+++ app/containers/markdown/Table.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { ScrollView, Text, TouchableOpacity, View, ViewStyle } from 'react-native';
 
 import { CELL_WIDTH } from './TableCell';
-import styles from './styles';
 import Navigation from '../../lib/navigation/appNavigation';
+import styles from './styles';
 import I18n from '../../i18n';
 import { TSupportedThemes } from '../../theme';
 import { themes } from '../../lib/constants';
+import { useAppSelector } from '../../lib/hooks';
 
 interface ITable {
 	children: React.ReactElement | null;
@@ -18,6 +19,7 @@ const MAX_HEIGHT = 300;
 
 const Table = React.memo(({ children, numColumns, theme }: ITable) => {
 	const getTableWidth = () => numColumns * CELL_WIDTH;
+	const isMasterDetail = useAppSelector(state => state.app.isMasterDetail);
 
 	const renderRows = (drawExtraBorders = true) => {
 		const tableStyle: ViewStyle[] = [styles.table, { borderColor: themes[theme].borderColor }];
@@ -33,7 +35,16 @@ const Table = React.memo(({ children, numColumns, theme }: ITable) => {
 		return <View style={tableStyle}>{rows}</View>;
 	};
 
-	const onPress = () => Navigation.navigate('MarkdownTableView', { renderRows, tableWidth: getTableWidth() });
+	const onPress = () => {
+		if (isMasterDetail) {
+			Navigation.navigate('ModalStackNavigator', {
+				screen: 'MarkdownTableView',
+				params: { renderRows, tableWidth: getTableWidth() }
+			});
+		} else {
+			Navigation.navigate('MarkdownTableView', { renderRows, tableWidth: getTableWidth() });
+		}
+	};
 
 	return (
 		<TouchableOpacity onPress={onPress}>
