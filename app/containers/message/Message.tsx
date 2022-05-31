b2862be1f2906662f347f4d6cdde96b7b21ea21a
GleidsonDaniel <gleidson10daniel@hotmail.com>
2022-05-31 13:21:24 -0300

    fix verification adding fields logic

--- app/containers/message/Message.tsx
+++ app/containers/message/Message.tsx
@@ -23,7 +23,7 @@ import RightIcons from './Components/RightIcons';
 
 const MessageInner = React.memo((props: IMessageInner) => {
 	const { attachments } = props;
-	const isCollapsible = attachments ? attachments[0] && attachments[0].collapsed : false;
+	const isCollapsible = attachments ? (attachments[0] && attachments[0].collapsed) || !!attachments[0]?.fields?.length : false;
 
 	if (props.type === 'discussion-created') {
 		return (
