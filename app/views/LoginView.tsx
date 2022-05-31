1b826e45658a592abca4f8bf0831c1f9a7b7c760
Reinaldo Neto <reinaldonetof@hotmail.com>
2022-05-30 22:30:06 -0300

    back the separator as props

--- app/views/LoginView.tsx
+++ app/views/LoginView.tsx
@@ -230,10 +230,12 @@ class LoginView extends React.Component<ILoginViewProps, ILoginViewState> {
 	};
 
 	render() {
+		const { Accounts_ShowFormLogin } = this.props;
+
 		return (
 			<FormContainer testID='login-view'>
 				<FormContainerInner>
-					<LoginServices />
+					<LoginServices separator={Accounts_ShowFormLogin} />
 					{this.renderUserForm()}
 				</FormContainerInner>
 			</FormContainer>
