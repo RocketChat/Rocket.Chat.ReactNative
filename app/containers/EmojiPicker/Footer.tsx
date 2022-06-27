8ea57553455a3177f51f625e045ca9cf6730d408
Danish Ahmed Mirza <danishmirza30602@gmail.com>
2022-06-28 01:02:34 +0530

    Add searchbar for Emojis

--- app/containers/EmojiPicker/Footer.tsx
+++ app/containers/EmojiPicker/Footer.tsx
@@ -7,11 +7,11 @@ import { CustomIcon } from '../CustomIcon';
 import styles from './styles';
 import { IFooterProps } from './interfaces';
 
-const Footer = React.memo(({ onBackspacePressed }: IFooterProps) => {
+const Footer = React.memo(({ onSearchPressed, onBackspacePressed }: IFooterProps) => {
 	const { colors } = useTheme();
 	return (
 		<View style={[styles.footerContainer, { backgroundColor: colors.bannerBackground }]}>
-			<BorderlessButton activeOpacity={0.7} onPress={() => console.log('Search!')} style={styles.footerButtonsContainer}>
+			<BorderlessButton activeOpacity={0.7} onPress={onSearchPressed} style={styles.footerButtonsContainer}>
 				<CustomIcon color={colors.auxiliaryTintColor} size={24} name='search' />
 			</BorderlessButton>
 
