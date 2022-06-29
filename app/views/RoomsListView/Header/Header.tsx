aacd4b0cba537b2794064ad970d532c3740ffdff
Diego Mello <diegolmello@gmail.com>
2022-06-29 16:15:57 -0300

    Cleanup

--- app/views/RoomsListView/Header/Header.tsx
+++ app/views/RoomsListView/Header/Header.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, TextInputProps, TouchableOpacity, TouchableOpacityProps, View } from 'react-native';
 
-// import { TextInput } from '../../../containers/TextInput';
 import I18n from '../../../i18n';
 import sharedStyles from '../../Styles';
 import { CustomIcon } from '../../../containers/CustomIcon';
