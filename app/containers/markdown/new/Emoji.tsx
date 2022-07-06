91eb6e1d9b9bcf36065623d0368d3af046fbb159
Diego Mello <yiweigao0226@gmail.com>
2022-07-06 17:01:33 -0300

    [FIX] Markdown not rendering emojis correctly on server version 5.0.0 (#4343)

--- app/containers/markdown/new/Emoji.tsx
+++ app/containers/markdown/new/Emoji.tsx
@@ -10,20 +10,29 @@ import CustomEmoji from '../../EmojiPicker/CustomEmoji';
 import MarkdownContext from './MarkdownContext';
 
 interface IEmojiProps {
-	value: EmojiProps['value'];
+	block: EmojiProps;
 	isBigEmoji?: boolean;
 }
 
-const Emoji = ({ value, isBigEmoji }: IEmojiProps) => {
+const Emoji = ({ block, isBigEmoji }: IEmojiProps) => {
 	const { theme } = useTheme();
 	const { baseUrl, getCustomEmoji } = useContext(MarkdownContext);
-	const emojiUnicode = shortnameToUnicode(`:${value.value}:`);
-	const emoji = getCustomEmoji?.(value.value);
+
+	if ('unicode' in block) {
+		return <Text style={[{ color: themes[theme].bodyText }, isBigEmoji ? styles.textBig : styles.text]}>{block.unicode}</Text>;
+	}
+	const emojiToken = block?.shortCode ? `:${block.shortCode}:` : `:${block.value?.value}:`;
+	const emojiUnicode = shortnameToUnicode(emojiToken);
+	const emoji = getCustomEmoji?.(block.value?.value);
 
 	if (emoji) {
 		return <CustomEmoji baseUrl={baseUrl} style={[isBigEmoji ? styles.customEmojiBig : styles.customEmoji]} emoji={emoji} />;
 	}
-	return <Text style={[{ color: themes[theme].bodyText }, isBigEmoji ? styles.textBig : styles.text]}>{emojiUnicode}</Text>;
+	return (
+		<Text style={[{ color: themes[theme].bodyText }, isBigEmoji && emojiToken !== emojiUnicode ? styles.textBig : styles.text]}>
+			{emojiUnicode}
+		</Text>
+	);
 };
 
 export default Emoji;
