41dbbf4d4b88b6152595bcbd91847142ca4cb785
Gleidson Daniel Silva <gleidson10daniel@hotmail.com>
2022-08-01 13:35:05 -0300

    Chore: Migrate NotificationPreferencesView to hooks (#4327)
    * add none option
    
    * create showErrorAlertWithEMessage function
    
    * migrate NotificationPreferencesView to hooks and improves overall
    
    * change icon to right
    
    * fix navigation options
    
    * remove none
    
    * fix types
    
    * remove memo
    
    * cleaning
    
    * switching to destructuring
    
    * add observe for hideUnreadStatus
    
    * fix desktop options
    
    * remove unused options
    
    Co-authored-by: Reinaldo Neto <47038980+reinaldonetof@users.noreply.github.com>

--- app/lib/methods/helpers/info.ts
+++ app/lib/methods/helpers/info.ts
@@ -5,6 +5,14 @@ import I18n from '../../../i18n';
 export const showErrorAlert = (message: string, title?: string, onPress = () => {}): void =>
 	Alert.alert(title || '', message, [{ text: 'OK', onPress }], { cancelable: true });
 
+export const showErrorAlertWithEMessage = (e: any): void => {
+	const messageError =
+		e.data && e.data.error.includes('[error-too-many-requests]')
+			? I18n.t('error-too-many-requests', { seconds: e.data.error.replace(/\D/g, '') })
+			: e.data.errorType;
+	showErrorAlert(messageError);
+};
+
 interface IShowConfirmationAlert {
 	title?: string;
 	message: string;
