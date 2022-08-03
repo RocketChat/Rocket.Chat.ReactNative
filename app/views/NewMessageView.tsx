a72b9f7a26c7ed5aeb52f721e58a3e52922a5235
Reinaldo Neto <reinaldonetof@hotmail.com>
2022-08-03 19:26:45 -0300

    migration to hooks new message view

--- app/views/NewMessageView.tsx
+++ app/views/NewMessageView.tsx
@@ -1,9 +1,9 @@
 import { Q } from '@nozbe/watermelondb';
-import { StackNavigationOptions } from '@react-navigation/stack';
-import { dequal } from 'dequal';
-import React from 'react';
+import { StackNavigationProp } from '@react-navigation/stack';
+import React, { useCallback, useEffect, useLayoutEffect, useState } from 'react';
 import { FlatList, StyleSheet, Text, View } from 'react-native';
-import { connect } from 'react-redux';
+import { shallowEqual, useDispatch } from 'react-redux';
+import { useNavigation } from '@react-navigation/native';
 
 import { createChannelRequest } from '../actions/createChannel';
 import { themes } from '../lib/constants';
@@ -12,21 +12,22 @@ import * as List from '../containers/List';
 import SafeAreaView from '../containers/SafeAreaView';
 import SearchBox from '../containers/SearchBox';
 import StatusBar from '../containers/StatusBar';
-import { IApplicationState, IBaseScreen, ISearch, TSubscriptionModel } from '../definitions';
+import { ISearch, TSubscriptionModel } from '../definitions';
 import I18n from '../i18n';
 import database from '../lib/database';
 import { CustomIcon, TIconsName } from '../containers/CustomIcon';
 import Navigation from '../lib/navigation/appNavigation';
 import UserItem from '../containers/UserItem';
-import { withTheme } from '../theme';
-import { goRoom, TGoRoomItem } from '../lib/methods/helpers/goRoom';
+import { TSupportedThemes, useTheme } from '../theme';
+import { goRoom as goRoomMethod, TGoRoomItem } from '../lib/methods/helpers/goRoom';
 import log, { events, logEvent } from '../lib/methods/helpers/log';
 import Touch from '../lib/methods/helpers/touch';
 import sharedStyles from './Styles';
 import { NewMessageStackParamList } from '../stacks/types';
-import { search } from '../lib/methods';
+import { search as searchMethod } from '../lib/methods';
 import { hasPermission, compareServerVersion } from '../lib/methods/helpers';
 import { PADDING_HORIZONTAL } from '../containers/List/constants';
+import { useAppSelector } from '../lib/hooks';
 
 const QUERY_SIZE = 50;
 
@@ -62,112 +63,134 @@ interface IButton {
 	first?: boolean;
 }
 
-interface INewMessageViewState {
-	search: (ISearch | TSubscriptionModel)[];
+type TItem = ISearch | TSubscriptionModel;
+
+interface IRenderItem {
+	item: TItem;
+	index: number;
+	search: TItem[];
 	chats: TSubscriptionModel[];
-	permissions: boolean[];
-}
-
-interface INewMessageViewProps extends IBaseScreen<NewMessageStackParamList, 'NewMessageView'> {
-	maxUsers: number;
-	isMasterDetail: boolean;
-	serverVersion: string;
-	createTeamPermission?: string[];
-	createDirectMessagePermission?: string[];
-	createPublicChannelPermission?: string[];
-	createPrivateChannelPermission?: string[];
-	createDiscussionPermission?: string[];
 	useRealName: boolean;
+	goRoom: (item: TGoRoomItem) => void;
 }
 
-class NewMessageView extends React.Component<INewMessageViewProps, INewMessageViewState> {
-	static navigationOptions = ({ navigation }: INewMessageViewProps): StackNavigationOptions => ({
-		headerLeft: () => <HeaderButton.CloseModal navigation={navigation} testID='new-message-view-close' />,
-		title: I18n.t('New_Message')
-	});
+const RenderItem = ({ item, index, chats, search, useRealName, goRoom }: IRenderItem) => {
+	const { theme } = useTheme();
 
-	constructor(props: INewMessageViewProps) {
-		super(props);
-		this.init();
-		this.state = {
-			search: [],
-			chats: [],
-			permissions: []
+	let style = { borderColor: themes[theme].separatorColor };
+	if (index === 0) {
+		style = { ...style };
+	}
+	if (search.length > 0 && index === search.length - 1) {
+		style = { ...style, ...sharedStyles.separatorBottom };
+	}
+	if (search.length === 0 && index === chats.length - 1) {
+		style = { ...style, ...sharedStyles.separatorBottom };
+	}
+
+	const itemSearch = item as ISearch;
+	const itemModel = item as TSubscriptionModel;
+
+	return (
+		<UserItem
+			name={useRealName && itemSearch.fname ? itemSearch.fname : itemModel.name}
+			username={itemSearch.search ? itemSearch.username : itemModel.name}
+			onPress={() => goRoom(itemModel)}
+			testID={`new-message-view-item-${item.name}`}
+			style={style}
+			theme={theme}
+		/>
+	);
+};
+
+const RenderButton = ({ onPress, testID, title, icon, first }: IButton) => {
+	const { theme } = useTheme();
+
+	return (
+		<Touch onPress={onPress} style={{ backgroundColor: themes[theme].backgroundColor }} testID={testID} theme={theme}>
+			<View
+				style={[
+					first ? sharedStyles.separatorVertical : sharedStyles.separatorBottom,
+					styles.button,
+					{ borderColor: themes[theme].separatorColor }
+				]}>
+				<CustomIcon name={icon} size={24} color={themes[theme].bodyText} style={styles.buttonIcon} />
+				<Text style={[styles.buttonText, { color: themes[theme].bodyText }]}>{title}</Text>
+				<View style={styles.rightContainer}>
+					<CustomIcon name={'chevron-right'} size={24} color={themes[theme].bodyText} style={styles.buttonIcon} />
+				</View>
+			</View>
+		</Touch>
+	);
+};
+
+const Header = ({
+	maxUsers,
+	theme,
+	onChangeText
+}: {
+	maxUsers: number;
+	theme: TSupportedThemes;
+	onChangeText: (text: string) => void;
+}) => {
+	const [permissions, setPermissions] = useState<boolean[]>([]);
+	const navigation = useNavigation<StackNavigationProp<NewMessageStackParamList, 'NewMessageView'>>();
+	const dispatch = useDispatch();
+
+	const {
+		serverVersion,
+		createTeamPermission,
+		createDirectMessagePermission,
+		createPublicChannelPermission,
+		createPrivateChannelPermission,
+		createDiscussionPermission
+	} = useAppSelector(
+		state => ({
+			serverVersion: state.server.version as string,
+			createTeamPermission: state.permissions['create-team'],
+			createDirectMessagePermission: state.permissions['create-d'],
+			createPublicChannelPermission: state.permissions['create-c'],
+			createPrivateChannelPermission: state.permissions['create-p'],
+			createDiscussionPermission: state.permissions['start-discussion']
+		}),
+		shallowEqual
+	);
+
+	useEffect(() => {
+		const getPermissions = async () => {
+			const permissionsToCreate = await hasPermission([
+				createPublicChannelPermission,
+				createPrivateChannelPermission,
+				createTeamPermission,
+				createDirectMessagePermission,
+				createDiscussionPermission
+			]);
+			setPermissions(permissionsToCreate);
 		};
-	}
 
-	// eslint-disable-next-line react/sort-comp
-	init = async () => {
-		try {
-			const db = database.active;
-			const chats = await db
-				.get('subscriptions')
-				.query(Q.where('t', 'd'), Q.experimentalTake(QUERY_SIZE), Q.experimentalSortBy('room_updated_at', Q.desc))
-				.fetch();
+		getPermissions();
+	}, [
+		createDirectMessagePermission,
+		createDiscussionPermission,
+		createPrivateChannelPermission,
+		createPublicChannelPermission,
+		createTeamPermission
+	]);
 
-			this.setState({ chats });
-		} catch (e) {
-			log(e);
-		}
-	};
-
-	componentDidMount() {
-		this.handleHasPermission();
-	}
-
-	componentDidUpdate(prevProps: INewMessageViewProps) {
-		const {
-			createTeamPermission,
-			createPublicChannelPermission,
-			createPrivateChannelPermission,
-			createDirectMessagePermission,
-			createDiscussionPermission
-		} = this.props;
-
-		if (
-			!dequal(createTeamPermission, prevProps.createTeamPermission) ||
-			!dequal(createPublicChannelPermission, prevProps.createPublicChannelPermission) ||
-			!dequal(createPrivateChannelPermission, prevProps.createPrivateChannelPermission) ||
-			!dequal(createDirectMessagePermission, prevProps.createDirectMessagePermission) ||
-			!dequal(createDiscussionPermission, prevProps.createDiscussionPermission)
-		) {
-			this.handleHasPermission();
-		}
-	}
-
-	handleSearch = async (text: string) => {
-		const result = (await search({ text, filterRooms: false })) as ISearch[];
-		this.setState({
-			search: result
-		});
-	};
-
-	onSearchChangeText(text: string) {
-		this.handleSearch(text);
-	}
-
-	dismiss = () => {
-		const { navigation } = this.props;
-		return navigation.pop();
-	};
-
-	createChannel = () => {
+	const createChannel = () => {
 		logEvent(events.NEW_MSG_CREATE_CHANNEL);
-		const { navigation } = this.props;
 		navigation.navigate('SelectedUsersViewCreateChannel', { nextAction: () => navigation.navigate('CreateChannelView') });
 	};
 
-	createTeam = () => {
+	const createTeam = () => {
 		logEvent(events.NEW_MSG_CREATE_TEAM);
-		const { navigation } = this.props;
 		navigation.navigate('SelectedUsersViewCreateChannel', {
 			nextAction: () => navigation.navigate('CreateChannelView', { isTeam: true })
 		});
 	};
 
-	createGroupChat = () => {
+	const createGroupChat = () => {
 		logEvent(events.NEW_MSG_CREATE_GROUP_CHAT);
-		const { dispatch, maxUsers, navigation } = this.props;
 		navigation.navigate('SelectedUsersViewCreateChannel', {
 			nextAction: () => dispatch(createChannelRequest({ group: true })),
 			buttonText: I18n.t('Create'),
@@ -175,174 +198,130 @@ class NewMessageView extends React.Component<INewMessageViewProps, INewMessageVi
 		});
 	};
 
-	goRoom = (item: TGoRoomItem) => {
-		logEvent(events.NEW_MSG_CHAT_WITH_USER);
-		const { isMasterDetail, navigation } = this.props;
-		if (isMasterDetail) {
-			navigation.pop();
-		}
-		goRoom({ item, isMasterDetail });
-	};
-
-	renderButton = ({ onPress, testID, title, icon, first }: IButton) => {
-		const { theme } = this.props;
-		return (
-			<Touch onPress={onPress} style={{ backgroundColor: themes[theme].backgroundColor }} testID={testID} theme={theme}>
-				<View
-					style={[
-						first ? sharedStyles.separatorVertical : sharedStyles.separatorBottom,
-						styles.button,
-						{ borderColor: themes[theme].separatorColor }
-					]}>
-					<CustomIcon name={icon} size={24} color={themes[theme].bodyText} style={styles.buttonIcon} />
-					<Text style={[styles.buttonText, { color: themes[theme].bodyText }]}>{title}</Text>
-					<View style={styles.rightContainer}>
-						<CustomIcon name={'chevron-right'} size={24} color={themes[theme].bodyText} style={styles.buttonIcon} />
-					</View>
-				</View>
-			</Touch>
-		);
-	};
-
-	createDiscussion = () => {
+	const createDiscussion = () => {
 		logEvent(events.NEW_MSG_CREATE_DISCUSSION);
 		Navigation.navigate('CreateDiscussionView');
 	};
 
-	handleHasPermission = async () => {
-		const {
-			createTeamPermission,
-			createDirectMessagePermission,
-			createPublicChannelPermission,
-			createPrivateChannelPermission,
-			createDiscussionPermission
-		} = this.props;
-		const permissions = [
-			createPublicChannelPermission,
-			createPrivateChannelPermission,
-			createTeamPermission,
-			createDirectMessagePermission,
-			createDiscussionPermission
-		];
-		const permissionsToCreate = await hasPermission(permissions);
-		this.setState({ permissions: permissionsToCreate });
-	};
-
-	renderHeader = () => {
-		const { maxUsers, theme, serverVersion } = this.props;
-		const { permissions } = this.state;
-
-		return (
-			<>
-				<View style={{ backgroundColor: themes[theme].auxiliaryBackground, paddingTop: 16 }}>
-					<View style={styles.buttonContainer}>
-						{permissions[0] || permissions[1]
-							? this.renderButton({
-									onPress: this.createChannel,
-									title: I18n.t('Create_Channel'),
-									icon: 'channel-public',
-									testID: 'new-message-view-create-channel',
-									first: true
-							  })
-							: null}
-						{compareServerVersion(serverVersion, 'greaterThanOrEqualTo', '3.13.0') && permissions[2]
-							? this.renderButton({
-									onPress: this.createTeam,
-									title: I18n.t('Create_Team'),
-									icon: 'teams',
-									testID: 'new-message-view-create-team'
-							  })
-							: null}
-						{maxUsers > 2 && permissions[3]
-							? this.renderButton({
-									onPress: this.createGroupChat,
-									title: I18n.t('Create_Direct_Messages'),
-									icon: 'message',
-									testID: 'new-message-view-create-direct-message'
-							  })
-							: null}
-						{permissions[4]
-							? this.renderButton({
-									onPress: this.createDiscussion,
-									title: I18n.t('Create_Discussion'),
-									icon: 'discussions',
-									testID: 'new-message-view-create-discussion'
-							  })
-							: null}
-					</View>
+	return (
+		<>
+			<View style={{ backgroundColor: themes[theme].auxiliaryBackground, paddingTop: 16 }}>
+				<View style={styles.buttonContainer}>
+					{permissions[0] || permissions[1] ? (
+						<RenderButton
+							onPress={createChannel}
+							title={I18n.t('Create_Channel')}
+							icon={'channel-public'}
+							testID={'new-message-view-create-channel'}
+							first={true}
+						/>
+					) : null}
+					{compareServerVersion(serverVersion, 'greaterThanOrEqualTo', '3.13.0') && permissions[2] ? (
+						<RenderButton
+							onPress={createTeam}
+							title={I18n.t('Create_Team')}
+							icon={'teams'}
+							testID={'new-message-view-create-team'}
+						/>
+					) : null}
+					{maxUsers > 2 && permissions[3] ? (
+						<RenderButton
+							onPress={createGroupChat}
+							title={I18n.t('Create_Direct_Messages')}
+							icon={'message'}
+							testID={'new-message-view-create-direct-message'}
+						/>
+					) : null}
+					{permissions[4] ? (
+						<RenderButton
+							onPress={createDiscussion}
+							title={I18n.t('Create_Discussion')}
+							icon={'discussions'}
+							testID={'new-message-view-create-discussion'}
+						/>
+					) : null}
 				</View>
-				<SearchBox onChangeText={(text: string) => this.onSearchChangeText(text)} testID='new-message-view-search' />
-			</>
-		);
-	};
+			</View>
+			<SearchBox onChangeText={(text: string) => onChangeText(text)} testID='new-message-view-search' />
+		</>
+	);
+};
 
-	renderItem = ({ item, index }: { item: ISearch | TSubscriptionModel; index: number }) => {
-		const { search, chats } = this.state;
-		const { theme, useRealName } = this.props;
+const NewMessageView = () => {
+	const [chats, setChats] = useState<TSubscriptionModel[]>([]);
+	// eslint-disable-next-line @typescript-eslint/no-extra-parens
+	const [search, setSearch] = useState<TItem[]>([]);
 
-		let style = { borderColor: themes[theme].separatorColor };
-		if (index === 0) {
-			style = { ...style };
-		}
-		if (search.length > 0 && index === search.length - 1) {
-			style = { ...style, ...sharedStyles.separatorBottom };
-		}
-		if (search.length === 0 && index === chats.length - 1) {
-			style = { ...style, ...sharedStyles.separatorBottom };
-		}
+	const { colors, theme } = useTheme();
 
-		const itemSearch = item as ISearch;
-		const itemModel = item as TSubscriptionModel;
+	const navigation = useNavigation<StackNavigationProp<NewMessageStackParamList, 'NewMessageView'>>();
 
-		return (
-			<UserItem
-				name={useRealName && itemSearch.fname ? itemSearch.fname : itemModel.name}
-				username={itemSearch.search ? itemSearch.username : itemModel.name}
-				onPress={() => this.goRoom(itemModel)}
-				testID={`new-message-view-item-${item.name}`}
-				style={style}
-				theme={theme}
-			/>
-		);
-	};
+	const { isMasterDetail, maxUsers, useRealName } = useAppSelector(
+		state => ({
+			isMasterDetail: state.app.isMasterDetail,
+			maxUsers: (state.settings.DirectMesssage_maxUsers as number) || 1,
+			useRealName: state.settings.UI_Use_Real_Name as boolean
+		}),
+		shallowEqual
+	);
 
-	renderList = () => {
-		const { search, chats } = this.state;
-		const { theme } = this.props;
-		return (
+	useLayoutEffect(() => {
+		navigation.setOptions({
+			headerLeft: () => <HeaderButton.CloseModal navigation={navigation} testID='new-message-view-close' />,
+			title: I18n.t('New_Message')
+		});
+	}, [navigation]);
+
+	useEffect(() => {
+		const init = async () => {
+			try {
+				const db = database.active;
+				const c = await db
+					.get('subscriptions')
+					.query(Q.where('t', 'd'), Q.experimentalTake(QUERY_SIZE), Q.experimentalSortBy('room_updated_at', Q.desc))
+					.fetch();
+				setChats(c);
+			} catch (e) {
+				log(e);
+			}
+		};
+
+		init();
+	}, []);
+
+	const handleSearch = useCallback(async (text: string) => {
+		const result = (await searchMethod({ text, filterRooms: false })) as ISearch[];
+		setSearch(result);
+	}, []);
+
+	const goRoom = useCallback(
+		(item: TGoRoomItem) => {
+			logEvent(events.NEW_MSG_CHAT_WITH_USER);
+
+			if (isMasterDetail) {
+				navigation.pop();
+			}
+			goRoomMethod({ item, isMasterDetail });
+		},
+		[isMasterDetail, navigation]
+	);
+
+	return (
+		<SafeAreaView testID='new-message-view'>
+			<StatusBar />
 			<FlatList
 				data={search.length > 0 ? search : chats}
-				extraData={this.state}
 				keyExtractor={item => item._id || item.rid}
-				ListHeaderComponent={this.renderHeader}
-				renderItem={this.renderItem}
+				ListHeaderComponent={<Header maxUsers={maxUsers} theme={theme} onChangeText={handleSearch} />}
+				renderItem={({ item, index }) => (
+					<RenderItem goRoom={goRoom} chats={chats} index={index} item={item} search={search} useRealName={useRealName} />
+				)}
 				ItemSeparatorComponent={List.Separator}
-				contentContainerStyle={{ backgroundColor: themes[theme].backgroundColor }}
+				contentContainerStyle={{ backgroundColor: colors.backgroundColor }}
 				keyboardShouldPersistTaps='always'
 			/>
-		);
-	};
+		</SafeAreaView>
+	);
+};
 
-	render() {
-		return (
-			<SafeAreaView testID='new-message-view'>
-				<StatusBar />
-				{this.renderList()}
-			</SafeAreaView>
-		);
-	}
-}
-
-const mapStateToProps = (state: IApplicationState) => ({
-	serverVersion: state.server.version as string,
-	isMasterDetail: state.app.isMasterDetail,
-	maxUsers: (state.settings.DirectMesssage_maxUsers as number) || 1,
-	createTeamPermission: state.permissions['create-team'],
-	createDirectMessagePermission: state.permissions['create-d'],
-	createPublicChannelPermission: state.permissions['create-c'],
-	createPrivateChannelPermission: state.permissions['create-p'],
-	createDiscussionPermission: state.permissions['start-discussion'],
-	useRealName: state.settings.UI_Use_Real_Name as boolean
-});
-
-export default connect(mapStateToProps)(withTheme(NewMessageView));
+export default NewMessageView;
