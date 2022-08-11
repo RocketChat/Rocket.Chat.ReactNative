76efadac0e3100db16c91c6f68099155bb680d09
Gleidson Daniel Silva <gleidson10daniel@hotmail.com>
2022-08-11 11:59:40 -0300

    Chore: Migrate E2EEnterYourPasswordView to hooks (#4423)
    * migrate E2EEnterYourPasswordView to hooks
    
    * remove navigation options
    
    * minor tweak
    
    Co-authored-by: Reinaldo Neto <reinaldonetof@hotmail.com>

--- app/stacks/MasterDetailStack/index.tsx
+++ app/stacks/MasterDetailStack/index.tsx
@@ -201,11 +201,7 @@ const ModalStackNavigator = React.memo(({ navigation }: INavigation) => {
 					options={E2ESaveYourPasswordView.navigationOptions}
 				/>
 				<ModalStack.Screen name='E2EHowItWorksView' component={E2EHowItWorksView} />
-				<ModalStack.Screen
-					name='E2EEnterYourPasswordView'
-					component={E2EEnterYourPasswordView}
-					options={E2EEnterYourPasswordView.navigationOptions}
-				/>
+				<ModalStack.Screen name='E2EEnterYourPasswordView' component={E2EEnterYourPasswordView} />
 				<ModalStack.Screen name='UserPreferencesView' component={UserPreferencesView} />
 				<ModalStack.Screen
 					name='UserNotificationPrefView'
