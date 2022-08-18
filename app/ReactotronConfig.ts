3d5ceac4325964ae631dcbd14319eafe08502231
Diego Mello <diegolmello@gmail.com>
2022-08-18 15:35:09 -0300

    ReactotronConfig.ts

--- app/ReactotronConfig.ts
+++ app/ReactotronConfig.ts
@@ -1,23 +1,24 @@
-/* eslint-disable */
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { NativeModules } from 'react-native';
+import { NativeModules, LogBox } from 'react-native';
 import Reactotron from 'reactotron-react-native';
 import { reactotronRedux } from 'reactotron-redux';
 import sagaPlugin from 'reactotron-redux-saga';
 
 if (__DEV__) {
-	const scriptURL = NativeModules.SourceCode.scriptURL;
+	const { scriptURL } = NativeModules.SourceCode;
 	const scriptHostname = scriptURL.split('://')[1].split(':')[0];
-	Reactotron.setAsyncStorageHandler(AsyncStorage)
+	Reactotron.setAsyncStorageHandler?.(AsyncStorage)
 		.configure({ host: scriptHostname })
 		.useReactNative()
 		.use(reactotronRedux())
-		.use(sagaPlugin())
+		.use(sagaPlugin({}))
 		.connect();
 	// Running on android device
 	// $ adb reverse tcp:9090 tcp:9090
-	Reactotron.clear();
+	Reactotron.clear?.();
+	// @ts-ignore
 	console.warn = Reactotron.log;
+	// @ts-ignore
 	console.log = Reactotron.log;
-	console.disableYellowBox = true;
+	LogBox.ignoreAllLogs(true);
 }
