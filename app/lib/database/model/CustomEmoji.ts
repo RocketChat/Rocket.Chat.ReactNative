1c47bedefe0efd2d2b299842398908cb3af7fac8
liversonp <liverson.p@gmail.com>
2022-08-18 01:20:51 -0300

    Chore: Refactor CustomEmoji to Typescript
    Co-authored-by: guilhermedfs <guilhermedfs11@gmail.com>

--- app/lib/database/model/CustomEmoji.ts
+++ app/lib/database/model/CustomEmoji.ts
@@ -8,11 +8,11 @@ export const CUSTOM_EMOJIS_TABLE = 'custom_emojis';
 export default class CustomEmoji extends Model {
 	static table = CUSTOM_EMOJIS_TABLE;
 
-	@field('name') name;
+	@field('name') name: string | undefined;
 
-	@json('aliases', sanitizer) aliases;
+	@json('aliases', sanitizer) aliases: string | undefined;
 
-	@field('extension') extension;
+	@field('extension') extension!: string;
 
-	@date('_updated_at') _updatedAt;
+	@date('_updated_at') _updatedAt!: number;
 }
