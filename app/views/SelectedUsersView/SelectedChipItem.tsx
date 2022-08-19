52d6ed8a934dc75a3b8f1a2bd359d909ecc6138c
Reinaldo Neto <reinaldonetof@hotmail.com>
2022-08-19 16:11:45 -0300

    fix memo chipitem

--- app/views/SelectedUsersView/SelectedChipItem.tsx
+++ app/views/SelectedUsersView/SelectedChipItem.tsx
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { memo } from 'react';
 
 import { ISelectedUser } from '../../reducers/selectedUsers';
 import Chip from '../../containers/Chip';
 
-const SelectedChipItem = ({
-	item,
-	useRealName,
-	onPressItem
-}: {
-	item: ISelectedUser;
-	useRealName: boolean;
-	onPressItem: (userItem: ISelectedUser) => void;
-}) => {
-	console.count(`🧯 RenderSelectedItem ${item.name}`);
-	const name = useRealName && item.fname ? item.fname : item.name;
-	const username = item.search ? (item.username as string) : item.name;
+const SelectedChipItem = memo(
+	({
+		item,
+		useRealName,
+		onPressItem
+	}: {
+		item: ISelectedUser;
+		useRealName: boolean;
+		onPressItem: (userItem: ISelectedUser) => void;
+	}) => {
+		console.count(`🧯 RenderSelectedItem ${item.name}`);
+		const name = useRealName && item.fname ? item.fname : item.name;
+		const username = item.search ? (item.username as string) : item.name;
 
-	return <Chip text={name} avatar={username} onPress={() => onPressItem(item)} testID={`selected-user-${item.name}`} />;
-};
+		return <Chip text={name} avatar={username} onPress={() => onPressItem(item)} testID={`selected-user-${item.name}`} />;
+	},
+	(prevProps, nextProps) => {
+		if (prevProps.item?.name !== nextProps.item?.name) {
+			return false;
+		}
+		if (prevProps.item?.fname !== nextProps.item?.fname) {
+			return false;
+		}
+		if (prevProps.useRealName !== nextProps.useRealName) {
+			return false;
+		}
+		return true;
+	}
+);
 
 export default SelectedChipItem;
