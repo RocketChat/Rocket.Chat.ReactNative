631d90259c5ed03d3fce7042b6ac5e70d538e194
Gleidson Daniel <gleidson10daniel@hotmail.com>
2022-08-25 11:49:31 -0300

    remove unused styles

--- app/views/RoomMembersView/styles.ts
+++ app/views/RoomMembersView/styles.ts
@@ -6,19 +6,6 @@ export default StyleSheet.create({
 	list: {
 		flex: 1
 	},
-	item: {
-		flexDirection: 'row',
-		paddingVertical: 10,
-		paddingHorizontal: 16,
-		alignItems: 'center'
-	},
-	avatar: {
-		marginRight: 16
-	},
-	separator: {
-		height: StyleSheet.hairlineWidth,
-		marginLeft: 60
-	},
 	noResult: {
 		fontSize: 16,
 		paddingVertical: 56,
