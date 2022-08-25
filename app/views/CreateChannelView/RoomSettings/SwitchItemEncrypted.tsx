e0cf604591047b35fdb8f25a14413f6500418a27
Reinaldo Neto <reinaldonetof@hotmail.com>
2022-08-25 15:41:39 -0300

    remove memo from switchs

--- app/views/CreateChannelView/RoomSettings/SwitchItemEncrypted.tsx
+++ app/views/CreateChannelView/RoomSettings/SwitchItemEncrypted.tsx
@@ -1,5 +1,4 @@
-import React, { memo } from 'react';
-import { shallowEqual } from 'react-redux';
+import React from 'react';
 
 import { SwitchItem } from './SwitchItem';
 
@@ -11,36 +10,39 @@ export interface ISwitchItemEncrypted {
 	onValueChangeEncrypted: (value: boolean) => void;
 }
 
-export const SwitchItemEncrypted = memo(
-	({ encryptionEnabled, isTeam, type, encrypted, onValueChangeEncrypted }: ISwitchItemEncrypted) => {
-		if (!encryptionEnabled) {
-			return null;
-		}
+export const SwitchItemEncrypted = ({
+	encryptionEnabled,
+	isTeam,
+	type,
+	encrypted,
+	onValueChangeEncrypted
+}: ISwitchItemEncrypted) => {
+	if (!encryptionEnabled) {
+		return null;
+	}
 
-		let hint = '';
-		if (isTeam && type) {
-			hint = 'Team_hint_encrypted';
-		}
-		if (isTeam && !type) {
-			hint = 'Team_hint_encrypted_not_available';
-		}
-		if (!isTeam && type) {
-			hint = 'Channel_hint_encrypted';
-		}
-		if (!isTeam && !type) {
-			hint = 'Channel_hint_encrypted_not_available';
-		}
+	let hint = '';
+	if (isTeam && type) {
+		hint = 'Team_hint_encrypted';
+	}
+	if (isTeam && !type) {
+		hint = 'Team_hint_encrypted_not_available';
+	}
+	if (!isTeam && type) {
+		hint = 'Channel_hint_encrypted';
+	}
+	if (!isTeam && !type) {
+		hint = 'Channel_hint_encrypted_not_available';
+	}
 
-		return (
-			<SwitchItem
-				id={'encrypted'}
-				value={encrypted}
-				label={'Encrypted'}
-				hint={hint}
-				onValueChange={onValueChangeEncrypted}
-				disabled={!type}
-			/>
-		);
-	},
-	shallowEqual
-);
+	return (
+		<SwitchItem
+			id={'encrypted'}
+			value={encrypted}
+			label={'Encrypted'}
+			hint={hint}
+			onValueChange={onValueChangeEncrypted}
+			disabled={!type}
+		/>
+	);
+};
--- app/views/CreateChannelView/RoomSettings/SwitchItemType.tsx
+++ app/views/CreateChannelView/RoomSettings/SwitchItemType.tsx
@@ -1,39 +1,43 @@
-import React, { memo } from 'react';
-import { shallowEqual } from 'react-redux';
+import React from 'react';
 
 import { usePermissions } from '../../../lib/hooks';
 import { SwitchItem } from './SwitchItem';
 
-export const SwitchItemType = memo(
-	({ isTeam, type, onValueChangeType }: { isTeam: boolean; type: boolean; onValueChangeType: (value: boolean) => void }) => {
-		const [createChannelPermission, createPrivateChannelPermission] = usePermissions(['create-c', 'create-p']);
+export const SwitchItemType = ({
+	isTeam,
+	type,
+	onValueChangeType
+}: {
+	isTeam: boolean;
+	type: boolean;
+	onValueChangeType: (value: boolean) => void;
+}) => {
+	const [createChannelPermission, createPrivateChannelPermission] = usePermissions(['create-c', 'create-p']);
 
-		const isDisabled = [createChannelPermission, createPrivateChannelPermission].filter(r => r === true).length <= 1;
+	const isDisabled = [createChannelPermission, createPrivateChannelPermission].filter(r => r === true).length <= 1;
 
-		let hint = '';
-		if (isTeam && type) {
-			hint = 'Team_hint_private';
-		}
-		if (isTeam && !type) {
-			hint = 'Team_hint_public';
-		}
-		if (!isTeam && type) {
-			hint = 'Channel_hint_private';
-		}
-		if (!isTeam && !type) {
-			hint = 'Channel_hint_public';
-		}
+	let hint = '';
+	if (isTeam && type) {
+		hint = 'Team_hint_private';
+	}
+	if (isTeam && !type) {
+		hint = 'Team_hint_public';
+	}
+	if (!isTeam && type) {
+		hint = 'Channel_hint_private';
+	}
+	if (!isTeam && !type) {
+		hint = 'Channel_hint_public';
+	}
 
-		return (
-			<SwitchItem
-				id={'type'}
-				value={createPrivateChannelPermission ? type : false}
-				disabled={isDisabled}
-				label={'Private'}
-				hint={hint}
-				onValueChange={onValueChangeType}
-			/>
-		);
-	},
-	shallowEqual
-);
+	return (
+		<SwitchItem
+			id={'type'}
+			value={createPrivateChannelPermission ? type : false}
+			disabled={isDisabled}
+			label={'Private'}
+			hint={hint}
+			onValueChange={onValueChangeType}
+		/>
+	);
+};
