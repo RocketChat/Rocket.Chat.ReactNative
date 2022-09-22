71528c89155c632dc506ed7f35ab9699e12deb8a
Gleidson Daniel <gleidson10daniel@hotmail.com>
2022-09-22 09:36:56 -0300

    parse server url

--- app/lib/methods/audioFile.ts
+++ app/lib/methods/audioFile.ts
@@ -1,7 +1,10 @@
 import * as FileSystem from 'expo-file-system';
 
+import { sanitizeLikeString } from '../database/utils';
 import { store } from '../store/auxStore';
 import log from './helpers/log';
+
+const parseServerUrl = (serverUrl: string) => sanitizeLikeString(serverUrl.substring(serverUrl.lastIndexOf('/') + 1));
 
 const ensureDirAsync = async (dir: string, intermediates = true): Promise<void> => {
 	const info = await FileSystem.getInfoAsync(dir);
@@ -16,7 +19,7 @@ export const downloadAudioFile = async (url: string, fileUrl: string): Promise<s
 	let path = '';
 	try {
 		const serverUrl = store.getState().server.server;
-		const serverUrlParsed = serverUrl.substring(serverUrl.lastIndexOf('/') + 1);
+		const serverUrlParsed = parseServerUrl(serverUrl);
 		const folderPath = `${FileSystem.documentDirectory}audios/${serverUrlParsed}`;
 		const filePath = `${folderPath}/${fileUrl.substring(fileUrl.lastIndexOf('/') + 1)}`;
 		await ensureDirAsync(folderPath);
@@ -35,7 +38,7 @@ export const downloadAudioFile = async (url: string, fileUrl: string): Promise<s
 
 export const deleteAllAudioFiles = async (serverUrl: string): Promise<void> => {
 	try {
-		const serverUrlParsed = serverUrl.substring(serverUrl.lastIndexOf('/') + 1);
+		const serverUrlParsed = parseServerUrl(serverUrl);
 		const path = `${FileSystem.documentDirectory}audios/${serverUrlParsed}`;
 		await FileSystem.deleteAsync(path);
 	} catch (error) {
