6b7c1a61ce2e651a99b0630d0b334f1e12aca344
Gleidson Daniel <gleidson10daniel@hotmail.com>
2022-10-14 10:55:06 -0300

    create VideoConferenceBlock

---
+++ app/containers/UIKit/VideoConferenceBlock/CallParticipants.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import i18n from '../../../i18n';
+import useStyle from './styles';
+import AvatarContainer from '../../Avatar';
+
+const MAX_USERS = 3;
+
+export const CallParticipants = ({
+	users
+}: {
+	users: { _id: string; username: string; name: string; avatarETag: string }[];
+}): React.ReactElement => {
+	const style = useStyle();
+	return (
+		<>
+			{users.map(({ username }, index) =>
+				index < MAX_USERS ? <AvatarContainer style={{ marginRight: 4 }} key={index} size={28} text={username} /> : null
+			)}
+			{users.length > MAX_USERS ? (
+				<View style={style.plusUsers}>
+					<Text style={style.plusUsersText}>{users.length > 9 ? '+9' : `+${users.length}`}</Text>
+				</View>
+			) : null}
+			<Text style={style.joined}>{i18n.t('Joined')}</Text>
+		</>
+	);
+};
---
+++ app/containers/UIKit/VideoConferenceBlock/styles.ts
@@ -0,0 +1,92 @@
+import { StyleSheet } from 'react-native';
+
+import { useTheme } from '../../../theme';
+import sharedStyles from '../../../views/Styles';
+
+export default function useStyle() {
+	const { colors } = useTheme();
+	return StyleSheet.create({
+		container: { height: 108, flex: 1, borderWidth: 1, borderRadius: 2, marginTop: 8, borderColor: colors.conferenceCallBorder },
+		callInfoContainer: { flex: 1, alignItems: 'center', paddingLeft: 16, flexDirection: 'row' },
+		infoContainerText: {
+			fontSize: 12,
+			marginLeft: 8,
+			...sharedStyles.textBold,
+			color: colors.auxiliaryTintColor
+		},
+		iconContainer: {
+			width: 28,
+			height: 28,
+			alignItems: 'center',
+			justifyContent: 'center',
+			borderRadius: 4
+		},
+		callToActionContainer: {
+			height: 48,
+			backgroundColor: colors.conferenceCallBackground,
+			flexDirection: 'row',
+			alignItems: 'center',
+			paddingLeft: 16
+		},
+		callToActionButtonText: {
+			fontSize: 12,
+			...sharedStyles.textSemibold,
+			color: colors.buttonText
+		},
+		callToActionCallBackText: {
+			fontSize: 12,
+			...sharedStyles.textSemibold,
+			color: colors.conferenceCallCallBackText
+		},
+		callToActionButton: {
+			backgroundColor: colors.tintColor,
+			minWidth: 50,
+			alignItems: 'center',
+			justifyContent: 'center',
+			height: 32,
+			borderRadius: 4,
+			marginRight: 8,
+			paddingHorizontal: 8
+		},
+		joined: {
+			fontSize: 12,
+			...sharedStyles.textRegular,
+			color: colors.passcodeSecondary,
+			marginLeft: 8
+		},
+		plusUsers: {
+			width: 28,
+			height: 28,
+			backgroundColor: colors.conferenceCallPlusUsersButton,
+			borderRadius: 4,
+			alignItems: 'center',
+			justifyContent: 'center'
+		},
+		plusUsersText: {
+			fontSize: 14,
+			...sharedStyles.textSemibold,
+			color: colors.conferenceCallPlusUsersText,
+			alignSelf: 'center'
+		},
+		callBack: {
+			fontSize: 12,
+			...sharedStyles.textRegular,
+			color: colors.passcodeSecondary
+		},
+		callToActionCallBack: {
+			backgroundColor: colors.conferenceCallPlusUsersButton,
+			minWidth: 50,
+			alignItems: 'center',
+			justifyContent: 'center',
+			height: 32,
+			borderRadius: 4,
+			marginRight: 8,
+			paddingHorizontal: 8
+		},
+		notAnswered: {
+			fontSize: 12,
+			...sharedStyles.textRegular,
+			color: colors.passcodeSecondary
+		}
+	});
+}
