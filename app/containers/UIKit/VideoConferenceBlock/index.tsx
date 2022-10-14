5db77ec81be470836c3bce5bc2761930cd1d0e3c
Gleidson Daniel <gleidson10daniel@hotmail.com>
2022-10-14 15:56:40 -0300

    fix call originator

--- app/containers/UIKit/VideoConferenceBlock/index.tsx
+++ app/containers/UIKit/VideoConferenceBlock/index.tsx
@@ -3,6 +3,7 @@ import { Text } from 'react-native';
 import Touchable from 'react-native-platform-touchable';
 
 import i18n from '../../../i18n';
+import { useAppSelector } from '../../../lib/hooks';
 import { useEndpointData } from '../../../lib/hooks/useEndpointData';
 import { CallParticipants } from './CallParticipants';
 import { useVideoConf } from './hooks';
@@ -12,26 +13,32 @@ import VideoConferenceSkeletonLoading from './VideoConferenceSkeletonLoading';
 
 export default function VideoConferenceBlock({ callId, blockId }: { callId: string; blockId?: string }): React.ReactElement {
 	const style = useStyle();
-	const { loading, result } = useEndpointData('video-conference.info', { callId });
 	const { joinCall } = useVideoConf();
+	const username = useAppSelector(state => state.login.user.username);
+
+	const { loading, result } = useEndpointData('video-conference.info', { callId });
 
 	if (loading) return <VideoConferenceSkeletonLoading />;
 
 	if (result?.success) {
-		const { users, type, status } = result;
+		const { users, type, status, createdBy } = result;
+		const onlyAuthorOnCall = users.length === 1 && users.some(user => user.username === createdBy.username);
+
 		if ('endedAt' in result) {
 			return (
 				<VideoConferenceBaseContainer variant='ended'>
 					{type === 'direct' ? (
 						<>
 							<Touchable style={style.callToActionCallBack}>
-								<Text style={style.callToActionCallBackText}>{i18n.t('Call_back')}</Text>
+								<Text style={style.callToActionCallBackText}>
+									{createdBy.username === username ? i18n.t('Call_back') : i18n.t('Call_again')}
+								</Text>
 							</Touchable>
 							<Text style={style.callBack}>{i18n.t('Call_was_not_answered')}</Text>
 						</>
 					) : (
 						<>
-							{users.length ? (
+							{users.length && !onlyAuthorOnCall ? (
 								<CallParticipants users={users} />
 							) : (
 								<Text style={style.notAnswered}>{i18n.t('Call_was_not_answered')}</Text>
