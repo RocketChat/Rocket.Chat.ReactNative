3e8d34d7aee7e8fba49c8788bef953e7f11af568
Reinaldo Neto <reinaldonetof@hotmail.com>
2022-10-20 16:10:11 -0300

    Regression: Fix styles on FormTextInput

--- app/containers/TextInput/FormTextInput.tsx
+++ app/containers/TextInput/FormTextInput.tsx
@@ -24,9 +24,9 @@ const styles = StyleSheet.create({
 	},
 	input: {
 		...sharedStyles.textRegular,
-		height: 48,
 		fontSize: 16,
-		padding: 14,
+		paddingHorizontal: 16,
+		paddingVertical: 10,
 		borderWidth: 2,
 		borderRadius: 2
 	},
