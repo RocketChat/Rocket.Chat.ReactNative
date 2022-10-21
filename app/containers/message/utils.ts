d99bcf9c922952f32d3a080d5b00093f6a388d2c
Gleidson Daniel Silva <gleidson10daniel@hotmail.com>
2022-10-21 13:35:26 -0300

    [IMPROVE] System messages consistency (#4461)
    * fix sys messages
    
    * update snapshot
    
    * update snapshot
    
    * add username
    
    * wip
    
    * wip
    
    * change to switch and fix types
    
    * wip
    
    * new translations
    
    * update snapshot
    
    * Update app/containers/message/utils.ts
    
    Co-authored-by: Reinaldo Neto <47038980+reinaldonetof@users.noreply.github.com>
    
    * fix types and return
    
    * remove unused translation
    
    * fix types
    
    * remove unused params
    
    * minor tweak
    
    * update story
    
    Co-authored-by: Reinaldo Neto <47038980+reinaldonetof@users.noreply.github.com>
    Co-authored-by: Reinaldo Neto <reinaldonetof@hotmail.com>

--- app/containers/message/utils.ts
+++ app/containers/message/utils.ts
@@ -1,5 +1,5 @@
 /* eslint-disable complexity */
-import { TMessageModel } from '../../definitions/IMessage';
+import { MessageTypesValues, TMessageModel } from '../../definitions/IMessage';
 import I18n from '../../i18n';
 import { DISCUSSION } from './constants';
 
@@ -26,30 +26,18 @@ export const BUTTON_HIT_SLOP = {
 	left: 4
 };
 
-export const SYSTEM_MESSAGES = [
+const messagesWithAuthorName: MessageTypesValues[] = [
 	'r',
-	'au',
 	'ru',
-	'ul',
-	'ult',
+	'au',
+	'rm',
 	'uj',
 	'ujt',
 	'ut',
-	'rm',
-	'user-muted',
-	'user-unmuted',
+	'ul',
+	'ult',
 	'message_pinned',
-	'subscription-role-added',
-	'subscription-role-removed',
-	'room_changed_description',
-	'room_changed_announcement',
-	'room_changed_topic',
-	'room_changed_privacy',
-	'room_changed_avatar',
 	'message_snippeted',
-	'thread-created',
-	'room_e2e_enabled',
-	'room_e2e_disabled',
 	'removed-user-from-team',
 	'added-user-to-team',
 	'user-added-room-to-team',
@@ -57,81 +45,39 @@ export const SYSTEM_MESSAGES = [
 	'user-converted-to-channel',
 	'user-deleted-room-from-team',
 	'user-removed-room-from-team',
-	'room-disallowed-reacting',
-	'room-allowed-reacting',
-	'room-set-read-only',
-	'room-removed-read-only',
 	'omnichannel_placed_chat_on_hold',
-	'omnichannel_on_hold_chat_resumed'
-];
-
-export const IGNORED_LIVECHAT_SYSTEM_MESSAGES = [
+	'omnichannel_on_hold_chat_resumed',
 	'livechat_navigation_history',
 	'livechat_transcript_history',
-	'livechat_transfer_history',
 	'command',
-	'livechat-close',
 	'livechat-started',
+	'livechat-close',
 	'livechat_video_call',
-	'livechat_webrtc_video_call'
+	'livechat_webrtc_video_call',
+	'livechat_transfer_history',
+	'room-archived',
+	'room-unarchived',
+	'user-muted',
+	'room_changed_description',
+	'room_changed_announcement',
+	'room_changed_topic',
+	'room_changed_privacy',
+	'room_changed_avatar',
+	'room_e2e_disabled',
+	'room_e2e_enabled',
+	'room-disallowed-reacting',
+	'room-set-read-only',
+	'room-removed-read-only',
+	'user-unmuted',
+	'room-unarchived',
+	'subscription-role-added',
+	'subscription-role-removed'
 ];
 
-export const SYSTEM_MESSAGE_TYPES = {
-	MESSAGE_REMOVED: 'rm',
-	MESSAGE_PINNED: 'message_pinned',
-	MESSAGE_SNIPPETED: 'message_snippeted',
-	USER_JOINED_CHANNEL: 'uj',
-	USER_JOINED_TEAM: 'ujt',
-	USER_JOINED_DISCUSSION: 'ut',
-	USER_LEFT_CHANNEL: 'ul',
-	USER_LEFT_TEAM: 'ult',
-	REMOVED_USER_FROM_TEAM: 'removed-user-from-team',
-	ADDED_USER_TO_TEAM: 'added-user-to-team',
-	ADDED_ROOM_TO_TEAM: 'user-added-room-to-team',
-	CONVERTED_TO_TEAM: 'user-converted-to-team',
-	CONVERTED_TO_CHANNEL: 'user-converted-to-channel',
-	DELETED_ROOM_FROM_TEAM: 'user-deleted-room-from-team',
-	REMOVED_ROOM_FROM_TEAM: 'user-removed-room-from-team',
-	OMNICHANNEL_PLACED_CHAT_ON_HOLD: 'omnichannel_placed_chat_on_hold',
-	OMNICHANNEL_ON_HOLD_CHAT_RESUMED: 'omnichannel_on_hold_chat_resumed',
-	LIVECHAT_NAVIGATION_HISTORY: 'livechat_navigation_history',
-	LIVECHAT_TRANSCRIPT_HISTORY: 'livechat_transcript_history',
-	COMMAND: 'command',
-	LIVECHAT_STARTED: 'livechat-started',
-	LIVECHAT_CLOSE: 'livechat-close',
-	LIVECHAT_VIDEO_CALL: 'livechat_video_call',
-	LIVECHAT_WEBRTC_VIDEO_CALL: 'livechat_webrtc_video_call',
-	LIVECHAT_TRANSFER_HISTORY: 'livechat_transfer_history'
-};
-
-export const SYSTEM_MESSAGE_TYPES_WITH_AUTHOR_NAME = [
-	SYSTEM_MESSAGE_TYPES.MESSAGE_REMOVED,
-	SYSTEM_MESSAGE_TYPES.MESSAGE_PINNED,
-	SYSTEM_MESSAGE_TYPES.MESSAGE_SNIPPETED,
-	SYSTEM_MESSAGE_TYPES.USER_JOINED_CHANNEL,
-	SYSTEM_MESSAGE_TYPES.USER_JOINED_TEAM,
-	SYSTEM_MESSAGE_TYPES.USER_JOINED_DISCUSSION,
-	SYSTEM_MESSAGE_TYPES.USER_LEFT_CHANNEL,
-	SYSTEM_MESSAGE_TYPES.USER_LEFT_TEAM,
-	SYSTEM_MESSAGE_TYPES.REMOVED_USER_FROM_TEAM,
-	SYSTEM_MESSAGE_TYPES.ADDED_USER_TO_TEAM,
-	SYSTEM_MESSAGE_TYPES.ADDED_ROOM_TO_TEAM,
-	SYSTEM_MESSAGE_TYPES.CONVERTED_TO_TEAM,
-	SYSTEM_MESSAGE_TYPES.CONVERTED_TO_CHANNEL,
-	SYSTEM_MESSAGE_TYPES.DELETED_ROOM_FROM_TEAM,
-	SYSTEM_MESSAGE_TYPES.REMOVED_ROOM_FROM_TEAM,
-	SYSTEM_MESSAGE_TYPES.LIVECHAT_NAVIGATION_HISTORY,
-	SYSTEM_MESSAGE_TYPES.LIVECHAT_TRANSCRIPT_HISTORY,
-	SYSTEM_MESSAGE_TYPES.COMMAND,
-	SYSTEM_MESSAGE_TYPES.LIVECHAT_STARTED,
-	SYSTEM_MESSAGE_TYPES.LIVECHAT_CLOSE,
-	SYSTEM_MESSAGE_TYPES.LIVECHAT_VIDEO_CALL,
-	SYSTEM_MESSAGE_TYPES.LIVECHAT_WEBRTC_VIDEO_CALL,
-	SYSTEM_MESSAGE_TYPES.LIVECHAT_TRANSFER_HISTORY
-];
+export const messageHaveAuthorName = (type: MessageTypesValues): boolean => messagesWithAuthorName.includes(type);
 
 type TInfoMessage = {
-	type: string;
+	type: MessageTypesValues;
 	role: string;
 	msg: string;
 	author: { username: string };
@@ -141,137 +87,109 @@ type TInfoMessage = {
 export const getInfoMessage = ({ type, role, msg, author, comment }: TInfoMessage): string => {
 	const { username } = author;
 
-	if (type === 'rm') {
-		return I18n.t('Message_removed');
+	switch (type) {
+		// with author name
+		case 'rm':
+			return I18n.t('Message_removed');
+		case 'uj':
+			return I18n.t('User_joined_the_channel');
+		case 'ujt':
+			return I18n.t('User_joined_the_team');
+		case 'ut':
+			return I18n.t('User_joined_the_conversation');
+		case 'r':
+			return I18n.t('Room_name_changed_to', { name: msg });
+		case 'ru':
+			return I18n.t('User_has_been_removed', { userRemoved: msg });
+		case 'au':
+			return I18n.t('User_added_to', { userAdded: msg });
+		case 'user-muted':
+			return I18n.t('User_has_been_muted', { userMuted: msg });
+		case 'room_changed_description':
+			return I18n.t('changed_room_description', { description: msg });
+		case 'room_changed_announcement':
+			return I18n.t('changed_room_announcement', { announcement: msg });
+		case 'room_changed_topic':
+			return I18n.t('room_changed_topic_to', { topic: msg });
+		case 'room_changed_privacy':
+			return I18n.t('room_changed_type', { type: msg });
+		case 'room_changed_avatar':
+			return I18n.t('room_avatar_changed');
+		case 'message_snippeted':
+			return I18n.t('Created_snippet');
+		case 'room_e2e_disabled':
+			return I18n.t('Disabled_E2E_Encryption_for_this_room');
+		case 'room_e2e_enabled':
+			return I18n.t('Enabled_E2E_Encryption_for_this_room');
+		case 'removed-user-from-team':
+			return I18n.t('Removed__username__from_the_team', { userRemoved: msg });
+		case 'added-user-to-team':
+			return I18n.t('Added__username__to_this_team', { user_added: msg });
+		case 'user-added-room-to-team':
+			return I18n.t('added__roomName__to_this_team', { roomName: msg });
+		case 'user-converted-to-team':
+			return I18n.t('Converted__roomName__to_a_team', { roomName: msg });
+		case 'user-converted-to-channel':
+			return I18n.t('Converted__roomName__to_a_channel', { roomName: msg });
+		case 'user-deleted-room-from-team':
+			return I18n.t('Deleted__roomName__', { roomName: msg });
+		case 'user-removed-room-from-team':
+			return I18n.t('Removed__roomName__from_the_team', { roomName: msg });
+		case 'room-disallowed-reacting':
+			return I18n.t('room_disallowed_reactions');
+		case 'room-allowed-reacting':
+			return I18n.t('room_allowed_reactions');
+		case 'room-set-read-only':
+			return I18n.t('room_set_read_only_permission');
+		case 'room-removed-read-only':
+			return I18n.t('room_removed_read_only_permission');
+		case 'user-unmuted':
+			return I18n.t('User_has_been_unmuted', { userUnmuted: msg });
+		case 'room-archived':
+			return I18n.t('room_archived');
+		case 'room-unarchived':
+			return I18n.t('room_unarchived');
+		case 'subscription-role-added':
+			return I18n.t('Defined_user_as_role', { user: msg, role });
+		case 'subscription-role-removed':
+			return I18n.t('Removed_user_as_role', { user: msg, role });
+		case 'message_pinned':
+			return I18n.t('Message_pinned');
+
+		// without author name
+		case 'ul':
+			return I18n.t('User_left_this_channel');
+		case 'ult':
+			return I18n.t('Has_left_the_team');
+		case 'jitsi_call_started':
+			return I18n.t('Started_call', { userBy: username });
+		case 'omnichannel_placed_chat_on_hold':
+			return I18n.t('Omnichannel_placed_chat_on_hold', { comment });
+		case 'omnichannel_on_hold_chat_resumed':
+			return I18n.t('Omnichannel_on_hold_chat_resumed', { comment });
+		case 'command':
+			return I18n.t('Livechat_transfer_return_to_the_queue');
+		case 'livechat-started':
+			return I18n.t('Chat_started');
+		case 'livechat-close':
+			return I18n.t('Conversation_closed');
+		case 'livechat_transfer_history':
+			return I18n.t('New_chat_transfer', { agent: username });
+
+		// default value
+		default:
+			return I18n.t('Unsupported_system_message');
 	}
-	if (type === 'uj') {
-		return I18n.t('Has_joined_the_channel');
-	}
-	if (type === 'ujt') {
-		return I18n.t('Has_joined_the_team');
-	}
-	if (type === 'ut') {
-		return I18n.t('Has_joined_the_conversation');
-	}
-	if (type === 'r') {
-		return I18n.t('Room_name_changed', { name: msg, userBy: username });
-	}
-	if (type === 'message_pinned') {
-		return I18n.t('Message_pinned');
-	}
-	if (type === 'jitsi_call_started') {
-		return I18n.t('Started_call', { userBy: username });
-	}
-	if (type === 'ul') {
-		return I18n.t('Has_left_the_channel');
-	}
-	if (type === 'ult') {
-		return I18n.t('Has_left_the_team');
-	}
-	if (type === 'ru') {
-		return I18n.t('User_removed_by', { userRemoved: msg, userBy: username });
-	}
-	if (type === 'au') {
-		return I18n.t('User_added_by', { userAdded: msg, userBy: username });
-	}
-	if (type === 'user-muted') {
-		return I18n.t('User_muted_by', { userMuted: msg, userBy: username });
-	}
-	if (type === 'user-unmuted') {
-		return I18n.t('User_unmuted_by', { userUnmuted: msg, userBy: username });
-	}
-	if (type === 'subscription-role-added') {
-		return `${msg} was set ${role} by ${username}`;
-	}
-	if (type === 'subscription-role-removed') {
-		return `${msg} is no longer ${role} by ${username}`;
-	}
-	if (type === 'room_changed_description') {
-		return I18n.t('Room_changed_description', { description: msg, userBy: username });
-	}
-	if (type === 'room_changed_announcement') {
-		return I18n.t('Room_changed_announcement', { announcement: msg, userBy: username });
-	}
-	if (type === 'room_changed_topic') {
-		return I18n.t('Room_changed_topic', { topic: msg, userBy: username });
-	}
-	if (type === 'room_changed_privacy') {
-		return I18n.t('Room_changed_privacy', { type: msg, userBy: username });
-	}
-	if (type === 'room_changed_avatar') {
-		return I18n.t('Room_changed_avatar', { userBy: username });
-	}
-	if (type === 'message_snippeted') {
-		return I18n.t('Created_snippet');
-	}
-	if (type === 'room_e2e_disabled') {
-		return I18n.t('This_room_encryption_has_been_disabled_by__username_', { username });
-	}
-	if (type === 'room_e2e_enabled') {
-		return I18n.t('This_room_encryption_has_been_enabled_by__username_', { username });
-	}
-	if (type === 'removed-user-from-team') {
-		return I18n.t('Removed__username__from_team', { user_removed: msg });
-	}
-	if (type === 'added-user-to-team') {
-		return I18n.t('Added__username__to_team', { user_added: msg });
-	}
-	if (type === 'user-added-room-to-team') {
-		return I18n.t('added__roomName__to_team', { roomName: msg });
-	}
-	if (type === 'user-converted-to-team') {
-		return I18n.t('Converted__roomName__to_team', { roomName: msg });
-	}
-	if (type === 'user-converted-to-channel') {
-		return I18n.t('Converted__roomName__to_channel', { roomName: msg });
-	}
-	if (type === 'user-deleted-room-from-team') {
-		return I18n.t('Deleted__roomName__', { roomName: msg });
-	}
-	if (type === 'user-removed-room-from-team') {
-		return I18n.t('Removed__roomName__from_this_team', { roomName: msg });
-	}
-	if (type === 'room-disallowed-reacting') {
-		return I18n.t('Room_disallowed_reacting', { userBy: username });
-	}
-	if (type === 'room-allowed-reacting') {
-		return I18n.t('Room_allowed_reacting', { userBy: username });
-	}
-	if (type === 'room-set-read-only') {
-		return I18n.t('Room_set_read_only', { userBy: username });
-	}
-	if (type === 'room-removed-read-only') {
-		return I18n.t('Room_removed_read_only', { userBy: username });
-	}
-	if (type === 'omnichannel_placed_chat_on_hold') {
-		return I18n.t('Omnichannel_placed_chat_on_hold', { comment });
-	}
-	if (type === 'omnichannel_on_hold_chat_resumed') {
-		return I18n.t('Omnichannel_on_hold_chat_resumed', { comment });
-	}
-	if (type === 'command') {
-		return I18n.t('Livechat_transfer_return_to_the_queue');
-	}
-	if (type === 'livechat-started') {
-		return I18n.t('Chat_started');
-	}
-	if (type === 'livechat-close') {
-		return I18n.t('Conversation_closed');
-	}
-	if (type === 'livechat_transfer_history') {
-		return I18n.t('New_chat_transfer', { agent: username });
-	}
-	return I18n.t('Unsupported_system_message');
 };
 
-export const getMessageTranslation = (message: TMessageModel, autoTranslateLanguage: string) => {
+export const getMessageTranslation = (message: TMessageModel, autoTranslateLanguage: string): string | null => {
 	if (!autoTranslateLanguage) {
 		return null;
 	}
 	const { translations } = message;
 	if (translations) {
 		const translation = translations.find((trans: any) => trans.language === autoTranslateLanguage);
-		return translation && translation.value;
+		return translation?.value || null;
 	}
 	return null;
 };
