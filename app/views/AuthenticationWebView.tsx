ac2fea5b6d3a2b460a4888cc3db49d16cfbe71ab
Reinaldo Neto <reinaldonetof@hotmail.com>
2023-01-02 20:52:51 -0300

    minor tweak

--- app/views/AuthenticationWebView.tsx
+++ app/views/AuthenticationWebView.tsx
@@ -17,7 +17,7 @@ import { IApplicationState, ICredentials } from '../definitions';
 
 const userAgent = isIOS
 	? 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.5 Mobile/15E148 Safari/604.1'
-	: 'Mozilla/5.0 (Linux; Android 12; sdk_gphone64_x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Mobile Safari/537.36';
+	: 'Mozilla/5.0 (Linux; Android 12; SM-A315G) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Mobile Safari/537.36';
 
 // iframe uses a postMessage to send the token to the client
 // We'll handle this sending the token to the hash of the window.location
