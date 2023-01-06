ea20167981898bc61ba70dbd05f759c9494183a1
Reinaldo Neto <reinaldonetof@hotmail.com>
2023-01-06 19:34:57 -0300

    minor tweak tests

--- app/lib/methods/helpers/image.test.ts
+++ app/lib/methods/helpers/image.test.ts
@@ -1,26 +1,31 @@
 import { isImage } from './image';
 
-const imageJPG = 'https://i.redd.it/t2mul61342l91.jpg';
-const imagePNG = 'https://user-images.githubusercontent.com/47038980/205175493-fc1f7fdd-d10a-4099-88c4-146bac69e223.png';
-const imageSVG = 'https://i.redd.it/t2mul61342l91.svg';
-const imageUrlWithQueryParams =
+const imageJPG = 't2mul61342l91.jpg';
+const imagePNG = '205175493-fc1f7fdd-d10a-4099-88c4-146bac69e223.png';
+const imageSVG = 't2mul61342l91.svg';
+const linkToImagePNG = 'https://user-images.githubusercontent.com/47038980/205175493-fc1f7fdd-d10a-4099-88c4-146bac69e223.png';
+const linkToImageWithQueryParams =
 	'https://i.ytimg.com/vi/suFuJZCfC7g/hqdefault.jpg?sqp=-oaymwE2CNACELwBSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARhlIGUoZTAP&rs=AOn4CLB_0OCFNuoCRBlaTJEa2PPOOHxkbQ';
 
-describe("Evaluate if the link is returning an image's type", () => {
-	test('return true when the link ends with .jpg', () => {
+describe("Evaluate if the string is returning an image's type", () => {
+	test('return true when the image ends with .jpg', () => {
 		const result = isImage(imageJPG);
 		expect(result).toBe(true);
 	});
-	test('return true when the link ends with .png', () => {
+	test('return true when the image ends with .png', () => {
 		const result = isImage(imagePNG);
 		expect(result).toBe(true);
 	});
-	test('return false when the link ends with .svg', () => {
+	test('return false when the image ends with .svg', () => {
 		const result = isImage(imageSVG);
 		expect(result).toBe(false);
 	});
-	test('return true when the link ends with .jpg and query params', () => {
-		const result = isImage(imageUrlWithQueryParams);
+	test('return true when the image ends with .jpg and query params', () => {
+		const result = isImage(linkToImageWithQueryParams);
+		expect(result).toBe(true);
+	});
+	test('return true when a link ends with .png', () => {
+		const result = isImage(linkToImagePNG);
 		expect(result).toBe(true);
 	});
 });
