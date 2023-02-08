e7792edfcb457e7de3d4dd478da35ec7613c7f51
GleidsonDaniel <gleidson10daniel@hotmail.com>
2023-02-08 12:35:58 -0300

    add error handling to videoconf capabilities

--- app/lib/hooks/useVideoConf.tsx
+++ app/lib/hooks/useVideoConf.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 
 import { callJitsi } from '../methods';
-import { compareServerVersion } from '../methods/helpers';
+import { compareServerVersion, showErrorAlert } from '../methods/helpers';
 import { ISubscription, SubscriptionType } from '../../definitions';
 import { useAppSelector } from './useAppSelector';
 import { videoConfStartAndJoin } from '../methods/videoConf';
@@ -10,6 +10,19 @@ import { useActionSheet } from '../../containers/ActionSheet';
 import { useSnaps } from './useSnaps';
 import StartACallActionSheet from '../../containers/UIKit/VideoConferenceBlock/components/StartACallActionSheet';
 import { getSubscriptionByRoomId } from '../database/services/Subscription';
+import { getUserSelector } from '../../selectors/login';
+import i18n from '../../i18n';
+
+const availabilityErrors = {
+	NOT_CONFIGURED: 'video-conf-provider-not-configured',
+	NOT_ACTIVE: 'no-active-video-conf-provider',
+	NO_APP: 'no-videoconf-provider-app'
+} as const;
+
+const handleErrors = (isAdmin: boolean, error: typeof availabilityErrors[keyof typeof availabilityErrors]) => {
+	if (isAdmin) return showErrorAlert(i18n.t(`admin-${error}-body`), i18n.t(`admin-${error}-header`));
+	return showErrorAlert(i18n.t(`${error}-body`), i18n.t(`${error}-header`));
+};
 
 export const useVideoConf = (rid: string): { showInitCallActionSheet: () => Promise<void>; showCallOption: boolean } => {
 	const [showCallOption, setShowCallOption] = useState(false);
@@ -18,6 +31,7 @@ export const useVideoConf = (rid: string): { showInitCallActionSheet: () => Prom
 	const jitsiEnabled = useAppSelector(state => state.settings.Jitsi_Enabled);
 	const jitsiEnableTeams = useAppSelector(state => state.settings.Jitsi_Enable_Teams);
 	const jitsiEnableChannels = useAppSelector(state => state.settings.Jitsi_Enable_Channels);
+	const user = useAppSelector(state => getUserSelector(state));
 
 	const greaterThanFive = compareServerVersion(serverVersion, 'greaterThanOrEqualTo', '5.0.0');
 
@@ -43,9 +57,18 @@ export const useVideoConf = (rid: string): { showInitCallActionSheet: () => Prom
 			try {
 				await Services.videoConferenceGetCapabilities();
 				return true;
-			} catch (error) {
-				// TODO HANDLE ERROR
-				return false;
+			} catch (error: any) {
+				const isAdmin = !!['admin', 'livechat-manager'].find(role => user.roles?.includes(role));
+				switch (error?.error) {
+					case availabilityErrors.NOT_CONFIGURED:
+						return handleErrors(isAdmin, availabilityErrors.NOT_CONFIGURED);
+					case availabilityErrors.NOT_ACTIVE:
+						return handleErrors(isAdmin, availabilityErrors.NOT_ACTIVE);
+					case availabilityErrors.NO_APP:
+						return handleErrors(isAdmin, availabilityErrors.NO_APP);
+					default:
+						return handleErrors(isAdmin, availabilityErrors.NOT_CONFIGURED);
+				}
 			}
 		}
 		return true;
