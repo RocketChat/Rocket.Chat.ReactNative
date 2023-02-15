03e4b6f852c3be6db23fc3b1e7706dd8c89c8571
Reinaldo Neto <reinaldonetof@hotmail.com>
2023-02-15 13:16:03 -0300

    fix where to add the color

--- app/containers/markdown/new/Plain.tsx
+++ app/containers/markdown/new/Plain.tsx
@@ -3,19 +3,15 @@ import { Text } from 'react-native';
 import { Plain as PlainProps } from '@rocket.chat/message-parser';
 
 import styles from '../styles';
-import { useTheme } from '../../../theme';
 
 interface IPlainProps {
 	value: PlainProps['value'];
 }
 
-const Plain = ({ value }: IPlainProps): React.ReactElement => {
-	const { colors } = useTheme();
-	return (
-		<Text accessibilityLabel={value} style={[styles.plainText, { color: colors.bodyText }]}>
-			{value}
-		</Text>
-	);
-};
+const Plain = ({ value }: IPlainProps): React.ReactElement => (
+	<Text accessibilityLabel={value} style={styles.plainText}>
+		{value}
+	</Text>
+);
 
 export default Plain;
