ee3a42c18f5b4ffe445e4b2f42a144611d39dced
Diego Mello <diegolmello@gmail.com>
2023-02-27 17:21:04 -0300

    Try split by timing

--- e2e/jest.config.js
+++ e2e/jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
 	maxWorkers: process.env.CI ? 1 : 3, // ci already uses parallelism
 	globalSetup: 'detox/runners/jest/globalSetup',
 	globalTeardown: 'detox/runners/jest/globalTeardown',
-	reporters: ['detox/runners/jest/reporter'],
+	reporters: ['detox/runners/jest/reporter', 'jest-junit'],
 	testEnvironment: 'detox/runners/jest/testEnvironment',
 	verbose: true
 };
