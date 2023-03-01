fb47f8221fd7bfb6bd9703873d255c435a52a9d8
Reinaldo Neto <reinaldonetof@hotmail.com>
2023-03-01 12:11:02 -0300

    minor tweak

--- app/lib/methods/helpers/addProtocol.ts
+++ app/lib/methods/helpers/addProtocol.ts
@@ -1,3 +1,5 @@
+// If the link does not have the protocol at the beginning, we are inserting https as the default,
+// since by conversion the most used is the secure protocol, with the same behavior as the web.
 const addProtocol = (url: string): string => {
 	if (!url.toLowerCase().startsWith('http')) {
 		return `https://${url.replace('//', '')}`;
