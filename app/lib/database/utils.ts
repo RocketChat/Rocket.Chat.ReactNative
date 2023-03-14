8e87e53b02b63060bacd78cfc44e39c0b7df2d26
Reinaldo Neto <reinaldonetof@hotmail.com>
2023-03-13 22:56:40 -0300

    using unsafeSql instead of the slug

--- app/lib/database/utils.ts
+++ app/lib/database/utils.ts
@@ -1,14 +1,7 @@
 import XRegExp from 'xregexp';
-import slug from 'slug';
 
 // Matches letters from any alphabet and numbers
 const likeStringRegex = XRegExp('[^\\p{L}\\p{Nd}]', 'g');
 export const sanitizeLikeString = (str?: string): string | undefined => str?.replace(likeStringRegex, '_');
-
-export const slugifyLikeString = (str: string) => {
-	str?.replace(likeStringRegex, '_');
-	const slugified = slug(str);
-	return slugified;
-};
 
 export const sanitizer = (r: object): object => r;
