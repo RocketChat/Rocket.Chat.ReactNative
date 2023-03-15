586e24976ead3907933954c382c4c166cd1ae721
Reinaldo Neto <reinaldonetof@hotmail.com>
2023-03-15 19:51:48 -0300

    fix: Error while editing team description

--- app/views/RoomInfoEditView/index.tsx
+++ app/views/RoomInfoEditView/index.tsx
@@ -281,7 +281,7 @@ class RoomInfoEditView extends React.Component<IRoomInfoEditViewProps, IRoomInfo
 		const params = {} as IRoomSettings;
 
 		// Name
-		if (room.name !== name) {
+		if (getRoomTitle(room) !== name) {
 			params.roomName = name;
 		}
 		// Avatar
@@ -318,7 +318,7 @@ class RoomInfoEditView extends React.Component<IRoomInfoEditViewProps, IRoomInfo
 		}
 
 		// Join Code
-		if (this.randomValue !== joinCode) {
+		if (room.joinCodeRequired && this.randomValue !== joinCode) {
 			params.joinCode = joinCode;
 		}
 
