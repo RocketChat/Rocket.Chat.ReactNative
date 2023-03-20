01f75a6cc953cb570230064682c8acd7348227b2
Reinaldo Neto <reinaldonetof@hotmail.com>
2023-03-20 12:39:00 -0300

    test the reset, discard alert, cancel and discard

--- e2e/tests/assorted/03-profile.spec.ts
+++ e2e/tests/assorted/03-profile.spec.ts
@@ -1,6 +1,6 @@
 import { device, waitFor, element, by, expect } from 'detox';
 
-import { navigateToLogin, login, sleep, platformTypes, TTextMatcher } from '../../helpers/app';
+import { navigateToLogin, login, sleep, platformTypes, TTextMatcher, tapBack } from '../../helpers/app';
 import { createRandomUser, ITestUser } from '../../helpers/data_setup';
 import random from '../../helpers/random';
 
@@ -100,7 +100,7 @@ describe('Profile screen', () => {
 			await waitForToast();
 		});
 
-		it('should reset avatar', async () => {
+		it('should click to reset avatar', async () => {
 			await element(by.type(scrollViewType)).atIndex(1).swipe('down');
 			await element(by.id('avatar-edit-button')).tap();
 			await waitFor(element(by.id('change-avatar-view-avatar')))
@@ -111,12 +111,35 @@ describe('Profile screen', () => {
 				.withTimeout(2000);
 			await element(by.id('reset-avatar-suggestion')).tap();
 			await sleep(300);
-			await waitFor(element(by.id('change-avatar-view-submit')))
+		});
+
+		it('should appear the discard alert when click the back icon ', async () => {
+			await tapBack();
+			await waitFor(element(by[textMatcher]('Discard changes?')).atIndex(0))
 				.toBeVisible()
 				.withTimeout(2000);
-			await element(by.id('change-avatar-view-submit')).tap();
-			await sleep(300);
-			await waitFor(element(by.id('profile-view')))
+			await waitFor(element(by[textMatcher]('Cancel')).atIndex(0))
+				.toBeVisible()
+				.withTimeout(2000);
+			await element(by[textMatcher]('Cancel')).atIndex(0).tap();
+			await sleep(200);
+			await tapBack();
+			await waitFor(element(by[textMatcher]('Discard changes?')).atIndex(0))
+				.toBeVisible()
+				.withTimeout(2000);
+			await waitFor(element(by[textMatcher]('Discard')).atIndex(0))
+				.toBeVisible()
+				.withTimeout(2000);
+			await element(by[textMatcher]('Discard')).atIndex(0).tap();
+		});
+
+		it('should change the avatar through a base64 image mocked', async () => {
+			await element(by.type(scrollViewType)).atIndex(1).swipe('down');
+			await element(by.id('avatar-edit-button')).tap();
+		});
+
+		it('should enable the save button and submit the avatar', async () => {
+			await waitFor(element(by.id('change-avatar-view-submit')))
 				.toBeVisible()
 				.withTimeout(2000);
 		});
