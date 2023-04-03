62957c6b8f1b9a629bba26bd709924b135f11c61
Diego Mello <diegolmello@gmail.com>
2023-04-03 17:34:23 -0300

    Starting android

--- android/app/src/main/java/chat/rocket/reactnative/MainActivity.java
+++ android/app/src/main/java/chat/rocket/reactnative/MainActivity.java
@@ -6,9 +6,10 @@ import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.ReactRootView;
-import com.zoontek.rnbootsplash.RNBootSplash;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
+import com.zoontek.rnbootsplash.RNBootSplash;
 import expo.modules.ReactActivityDelegateWrapper;
 
 public class MainActivity extends ReactActivity {
@@ -43,34 +44,20 @@ public class MainActivity extends ReactActivity {
         this.sendBroadcast(intent);
     }
 
-    /**
-    * Returns the instance of the {@link ReactActivityDelegate}. There the RootView is created and
-    * you can specify the renderer you wish to use - the new renderer (Fabric) or the old renderer
-    * (Paper).
+     /**
+    * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
+    * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
+    * (aka React 18) with two boolean flags.
     */
     @Override
     protected ReactActivityDelegate createReactActivityDelegate() {
-        return new ReactActivityDelegateWrapper(this, new MainActivityDelegate(this, getMainComponentName()));
-    }
-
-    public static class MainActivityDelegate extends ReactActivityDelegate {
-        public MainActivityDelegate(ReactActivity activity, String mainComponentName) {
-            super(activity, mainComponentName);
-        }
-
-        @Override
-        protected ReactRootView createRootView() {
-            ReactRootView reactRootView = new ReactRootView(getContext());
+        return new DefaultReactActivityDelegate(
+            this,
+            getMainComponentName(),
             // If you opted-in for the New Architecture, we enable the Fabric Renderer.
-            reactRootView.setIsFabric(BuildConfig.IS_NEW_ARCHITECTURE_ENABLED);
-            return reactRootView;
-        }
-
-        @Override
-        protected boolean isConcurrentRootEnabled() {
-            // If you opted-in for the New Architecture, we enable Concurrent Root (i.e. React 18).
-            // More on this on https://reactjs.org/blog/2022/03/29/react-v18.html
-            return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
-        }
+            DefaultNewArchitectureEntryPoint.getFabricEnabled(), // fabricEnabled
+            // If you opted-in for the New Architecture, we enable Concurrent React (i.e. React 18).
+            DefaultNewArchitectureEntryPoint.getConcurrentReactEnabled() // concurrentRootEnabled
+        );
     }
 }
