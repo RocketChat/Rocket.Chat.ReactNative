d9230d10edc41aeeac8d493bfb4b2335c45d2415
GleidsonDaniel <gleidson10daniel@hotmail.com>
2023-05-03 16:40:23 -0300

    fix tests

--- app/reducers/videoConf.test.ts
+++ app/reducers/videoConf.test.ts
@@ -11,33 +11,36 @@ describe('test videoConf reducer', () => {
 	const call1: ICallInfo = {
 		callId: '123',
 		rid: '123',
-		type: 'accepted',
+		action: 'accepted',
 		uid: '123'
 	};
 
 	const call2: ICallInfo = {
 		callId: '321',
 		rid: '321',
-		type: 'accepted',
+		action: 'accepted',
 		uid: '321'
 	};
 
 	it('should return call1 after call addSettings action with call1 as parameter', () => {
 		mockedStore.dispatch(setVideoConfCall(call1));
 		const state = mockedStore.getState().videoConf;
-		expect(state.calls[call1.callId]).toEqual(call1);
+		const call = state.calls.find(c => c.callId === call1.callId);
+		expect(call).toEqual(call1);
 	});
 
 	it('should return call2 after call addSettings action with call2 as parameter', () => {
 		mockedStore.dispatch(setVideoConfCall(call2));
 		const state = mockedStore.getState().videoConf;
-		expect(state.calls[call2.callId]).toEqual(call2);
+		const call = state.calls.find(c => c.callId === call2.callId);
+		expect(call).toEqual(call2);
 	});
 
 	it('should remove call1 after call removeVideoConfCall action with call1 as parameter', () => {
 		mockedStore.dispatch(removeVideoConfCall(call1));
 		const state = mockedStore.getState().videoConf;
-		expect(state.calls[call1.callId]).toEqual(undefined);
+		const call = state.calls.find(c => c.callId === call1.callId);
+		expect(call).toEqual(undefined);
 	});
 
 	it('should set calling true after call setCalling action with true as parameter', () => {
