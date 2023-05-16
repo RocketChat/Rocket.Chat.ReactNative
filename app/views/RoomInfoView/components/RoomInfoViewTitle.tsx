fd312f3ebfbe8c9631356e0288ec42ed32ce69c2
GleidsonDaniel <gleidson10daniel@hotmail.com>
2023-05-16 14:28:12 -0300

    create RoomInfoViewTitle

---
+++ app/views/RoomInfoView/components/RoomInfoViewTitle.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import { ISubscription, SubscriptionType } from '../../../definitions';
+import styles from '../styles';
+import { useTheme } from '../../../theme';
+import { MarkdownPreview } from '../../../containers/markdown';
+import RoomTypeIcon from '../../../containers/RoomTypeIcon';
+import { getRoomTitle } from '../../../lib/methods/helpers';
+
+interface IRoomInfoViewTitle {
+	room: ISubscription;
+	type: SubscriptionType;
+	name?: string;
+	username: string;
+	statusText?: string;
+}
+
+const RoomInfoViewTitle = ({ room, type, name, username, statusText }: IRoomInfoViewTitle): React.ReactElement => {
+	const { colors } = useTheme();
+	if (type === SubscriptionType.DIRECT) {
+		return (
+			<>
+				<Text testID='room-info-view-name' style={[styles.roomTitle, { color: colors.titleText }]}>
+					{name}
+				</Text>
+				{username && (
+					<Text
+						testID='room-info-view-username'
+						style={[styles.roomUsername, { color: colors.auxiliaryText }]}
+					>{`@${username}`}</Text>
+				)}
+				{!!statusText && (
+					<View testID='room-info-view-custom-status'>
+						<MarkdownPreview msg={statusText} style={[styles.roomUsername, { color: colors.auxiliaryText }]} />
+					</View>
+				)}
+			</>
+		);
+	}
+	return (
+		<View style={styles.roomTitleRow}>
+			<RoomTypeIcon
+				type={room.prid ? 'discussion' : room.t}
+				teamMain={room.teamMain}
+				key='room-info-type'
+				status={room.visitor?.status}
+				sourceType={room.source}
+			/>
+			<Text testID='room-info-view-name' style={[styles.roomTitle, { color: colors.titleText }]} key='room-info-name'>
+				{getRoomTitle(room)}
+			</Text>
+		</View>
+	);
+};
+
+export default RoomInfoViewTitle;
