f97f29d2c57a1c7e343416677a97f39f3ea47660
Diego Mello <diegolmello@gmail.com>
2023-05-19 17:40:58 -0300

    Hide mic

--- app/containers/MessageComposer/Toolbar/MicOrSendButton.tsx
+++ app/containers/MessageComposer/Toolbar/MicOrSendButton.tsx
@@ -3,9 +3,11 @@ import React, { useContext } from 'react';
 import { BaseButton } from './BaseButton';
 import { MessageComposerContext } from '../context';
 import { useTheme } from '../../../theme';
+import { useAppSelector } from '../../../lib/hooks';
 
 export const MicOrSendButton = () => {
-	const { micOrSend, sendMessage } = useContext(MessageComposerContext);
+	const { micOrSend, permissionToUpload, sendMessage } = useContext(MessageComposerContext);
+	const { Message_AudioRecorderEnabled } = useAppSelector(state => state.settings);
 	const { colors } = useTheme();
 
 	if (micOrSend === 'send') {
@@ -20,12 +22,16 @@ export const MicOrSendButton = () => {
 		);
 	}
 
-	return (
-		<BaseButton
-			onPress={() => alert('tbd')}
-			testID='message-composer-send-audio'
-			accessibilityLabel='Send_audio_message'
-			icon='microphone'
-		/>
-	);
+	if (Message_AudioRecorderEnabled && permissionToUpload) {
+		return (
+			<BaseButton
+				onPress={() => alert('tbd')}
+				testID='message-composer-send-audio'
+				accessibilityLabel='Send_audio_message'
+				icon='microphone'
+			/>
+		);
+	}
+
+	return null;
 };
