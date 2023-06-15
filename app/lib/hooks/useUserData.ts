2d92091277ab1425dc807f017c3431766461c63e
GleidsonDaniel <gleidson10daniel@hotmail.com>
2023-06-15 14:11:58 -0300

    use realName

--- app/lib/hooks/useUserData.ts
+++ app/lib/hooks/useUserData.ts
@@ -4,18 +4,23 @@ import { getSubscriptionByRoomId } from '../database/services/Subscription';
 import { getRoomAvatar, getUidDirectMessage } from '../methods/helpers';
 import { SubscriptionType } from '../../definitions';
 import { Services } from '../services';
+import { useAppSelector } from './useAppSelector';
 
 const useUserData = (rid: string) => {
 	const [user, setUser] = useState({ username: '', avatar: '', uid: '', type: '', direct: false });
+	const { useRealName } = useAppSelector(state => ({
+		useRealName: state.settings.UI_Use_Real_Name as boolean
+	}));
 	useEffect(() => {
 		(async () => {
 			const room = await getSubscriptionByRoomId(rid);
 			if (room) {
 				const uid = (await getUidDirectMessage(room)) as string;
 				const avt = getRoomAvatar(room);
+				const username = useRealName && room.fname ? room.fname : room.name;
 				setUser({
 					uid,
-					username: room?.name || '',
+					username,
 					avatar: avt,
 					type: room?.t || '',
 					direct: room?.t === SubscriptionType.DIRECT
@@ -24,10 +29,12 @@ const useUserData = (rid: string) => {
 				try {
 					const result = await Services.getUserInfo(rid);
 					if (result.success) {
+						const { user } = result;
+						const username = useRealName && user.name ? user.name : user.username;
 						setUser({
-							username: result.user.name || result.user.username,
-							avatar: result.user.username,
-							uid: result.user._id,
+							username,
+							avatar: user.username,
+							uid: user._id,
 							type: SubscriptionType.DIRECT,
 							direct: true
 						});
