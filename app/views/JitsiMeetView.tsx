5cd3648f3e42a0bce49ccf87222d8de33d62d52c
GleidsonDaniel <gleidson10daniel@hotmail.com>
2023-06-22 14:35:17 -0300

    add fix for ios

--- app/views/JitsiMeetView.tsx
+++ app/views/JitsiMeetView.tsx
@@ -94,7 +94,9 @@ class JitsiMeetView extends React.Component<TJitsiMeetViewProps> {
 					userAgent={userAgent}
 					javaScriptEnabled
 					domStorageEnabled
-					mediaPlaybackRequiresUserAction={false}
+					// fix for ios webview
+					allowsInlineMediaPlayback={true}
+					mediaPlaybackRequiresUserAction={true}
 					mediaCapturePermissionGrantType={'grant'}
 				/>
 			</SafeAreaView>
