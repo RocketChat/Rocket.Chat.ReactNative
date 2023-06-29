4aee6d5e2154f12fae27c221b62c830b91e99b42
Diego Mello <diegolmello@gmail.com>
2023-06-29 14:59:37 -0300

    AutocompleteItem base

--- app/containers/MessageComposer/components/Autocomplete/Autocomplete.tsx
+++ app/containers/MessageComposer/components/Autocomplete/Autocomplete.tsx
@@ -4,23 +4,27 @@ import { useContext } from 'react';
 import { MessageComposerContext } from '../../context';
 import { AutocompleteItem } from './AutocompleteItem';
 import { useAutocomplete } from '../../hooks';
+import { useTheme } from '../../../../theme';
 
 export const Autocomplete = () => {
 	const { rid, trackingViewHeight, keyboardHeight, autocompleteType, autocompleteText } = useContext(MessageComposerContext);
 	console.log('🚀 ~ file: Autocomplete.tsx:8 ~ Autocomplete ~ autocompleteType:', autocompleteType, autocompleteText);
 	const items = useAutocomplete({ text: autocompleteText, type: autocompleteType, rid });
 	console.log('🚀 ~ file: Autocomplete.tsx:26 ~ Autocomplete ~ items:', items);
+	const { colors } = useTheme();
 
 	if (autocompleteType && items.length > 0) {
 		return (
 			<View
 				style={{
-					height: 100,
+					maxHeight: 216,
 					left: 8,
 					right: 8,
-					backgroundColor: '#00000080',
+					backgroundColor: colors.surfaceNeutral,
 					position: 'absolute',
-					bottom: trackingViewHeight + keyboardHeight + 50
+					bottom: trackingViewHeight + keyboardHeight + 50,
+					borderRadius: 4,
+					overflow: 'hidden'
 				}}
 			>
 				<FlatList data={items} renderItem={({ item }) => <AutocompleteItem item={item} onPress={() => {}} />} />
--- app/containers/MessageComposer/components/Autocomplete/AutocompleteItem.tsx
+++ app/containers/MessageComposer/components/Autocomplete/AutocompleteItem.tsx
@@ -1,11 +1,38 @@
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 
-export const AutocompleteItem = ({ item, onPress }) => (
-	<View
-		style={{
-			height: 50,
-			backgroundColor: '#FFFFFF80',
-			borderWidth: 1
-		}}
-	/>
-);
+import { useTheme } from '../../../../theme';
+import sharedStyles from '../../../../views/Styles';
+
+export const AutocompleteItem = ({ item, onPress }) => {
+	const { colors } = useTheme();
+	return (
+		<RectButton
+			onPress={() => onPress(item)}
+			underlayColor={colors.buttonBackgroundPrimaryPress}
+			style={{ backgroundColor: colors.surfaceLight }}
+			rippleColor={colors.buttonBackgroundPrimaryPress}
+		>
+			<View
+				style={{
+					minHeight: 48,
+					borderWidth: 1,
+					flexDirection: 'row',
+					paddingHorizontal: 16,
+					paddingVertical: 6,
+					gap: 12
+				}}
+			>
+				<View style={{ width: 36, height: 36, backgroundColor: colors.surfaceNeutral, borderRadius: 4 }} />
+				<View style={{ flex: 1, justifyContent: 'center' }}>
+					<View style={{ flex: 1, flexDirection: 'row' }}>
+						<Text style={[sharedStyles.textBold, { fontSize: 14, color: colors.fontDefault }]}>Title</Text>
+					</View>
+					<View style={{ flex: 1, flexDirection: 'row' }}>
+						<Text style={[sharedStyles.textRegular, { fontSize: 14, color: colors.fontSecondaryInfo }]}>Subtitle</Text>
+					</View>
+				</View>
+			</View>
+		</RectButton>
+	);
+};
