bb3139376ead5e2e4ae97b8b053cdf38bbea0f84
Diego Mello <diegolmello@gmail.com>
2023-06-29 13:36:17 -0300

    Small useAutocomplete hook plan

--- app/containers/MessageComposer/components/Autocomplete.tsx
+++ app/containers/MessageComposer/components/Autocomplete.tsx
@@ -1,11 +1,29 @@
 import { View } from 'react-native';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import { MessageComposerContext } from '../context';
+import { search } from '../../../lib/methods';
+import { TAutocompleteType } from '../interfaces';
+
+const useAutocomplete = ({ text, type, rid }: { text: string; type: TAutocompleteType; rid: string }) => {
+	const [items, setItems] = useState([]);
+	useEffect(() => {
+		const getAutocomplete = async () => {
+			if (type === '@') {
+				const res = await search({ text, filterRooms: false, filterUsers: true, rid });
+				setItems(res);
+			}
+		};
+		getAutocomplete();
+	}, [text, type, rid]);
+	return items;
+};
 
 export const Autocomplete = () => {
-	const { trackingViewHeight, autocompleteType, autocompleteText } = useContext(MessageComposerContext);
+	const { rid, trackingViewHeight, autocompleteType, autocompleteText } = useContext(MessageComposerContext);
 	console.log('🚀 ~ file: Autocomplete.tsx:8 ~ Autocomplete ~ autocompleteType:', autocompleteType, autocompleteText);
+	const items = useAutocomplete({ text: autocompleteText, type: autocompleteType, rid });
+	console.log('🚀 ~ file: Autocomplete.tsx:26 ~ Autocomplete ~ items:', items);
 
 	return autocompleteType ? (
 		<View
