8451cc76ae78dadae0b76b811a1f363d24517ac6
Diego Mello <diegolmello@gmail.com>
2023-07-13 17:46:04 -0300

    Fix e2e tests mock

--- metro.config.js
+++ metro.config.js
@@ -1,31 +1,4 @@
-// /**
-//  * Metro configuration for React Native
-//  * https://github.com/facebook/react-native
-//  *
-//  * @format
-//  */
-
-// // eslint-disable-next-line import/no-extraneous-dependencies
-// const blocklist = require('metro-config/src/defaults/exclusionList');
-
-// const defaultSourceExts = require('metro-config/src/defaults/defaults').sourceExts;
-
-// module.exports = {
-// 	transformer: {
-// 		getTransformOptions: () => ({
-// 			transform: {
-// 				// experimentalImportSupport: true,
-// 				inlineRequires: true
-// 			}
-// 		})
-// 	},
-// 	maxWorkers: 2,
-// 	resolver: {
-// 		resolverMainFields: ['sbmodern', 'react-native', 'browser', 'main'],
-// 		sourceExts: process.env.RUNNING_E2E_TESTS ? ['mock.ts', ...defaultSourceExts] : defaultSourceExts
-// 	}
-// };
-
+const defaultSourceExts = require('metro-config/src/defaults/defaults').sourceExts;
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 /**
@@ -35,18 +8,9 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  * @type {import('metro-config').MetroConfig}
  */
 const config = {
-	// transformer: {
-	// 	getTransformOptions: () => ({
-	// 		transform: {
-	// 			// experimentalImportSupport: true,
-	// 			inlineRequires: true
-	// 		}
-	// 	})
-	// },
-	// maxWorkers: 2,
 	resolver: {
-		resolverMainFields: ['sbmodern', 'react-native', 'browser', 'main']
-		// sourceExts: process.env.RUNNING_E2E_TESTS ? ['mock.ts', ...defaultSourceExts] : defaultSourceExts
+		resolverMainFields: ['sbmodern', 'react-native', 'browser', 'main'],
+		sourceExts: process.env.RUNNING_E2E_TESTS ? ['mock.ts', ...defaultSourceExts] : defaultSourceExts
 	}
 };
 
