06a34fae4f18a91b9288708ea7226dffe67d8def
Diego Mello <diegolmello@gmail.com>
2023-07-25 15:59:14 -0300

    Autocomplete status

--- app/containers/MessageComposer/components/Autocomplete/Icon.tsx
+++ app/containers/MessageComposer/components/Autocomplete/Icon.tsx
@@ -1,13 +1,4 @@
-import { useEffect } from 'react';
-
-import { useUserStatus } from '../../../../lib/hooks';
-import { getUserPresence } from '../../../../lib/methods';
 import RoomTypeIcon from '../../../RoomTypeIcon';
 
-export const Icon = ({ item, type }) => {
-	console.log('🚀 ~ file: icon.tsx:8 ~ Icon ~ item:', item);
-	const { status } = useUserStatus(item.t, undefined, item.id);
-
-	console.log('🚀 ~ file: icon.tsx:12 ~ Icon ~ status ?? item.status:', item.status);
-	return type === '@' || type === '#' ? <RoomTypeIcon type={item.t} status={item.status} size={16} /> : null;
-};
+export const Icon = ({ item, type }) =>
+	type === '@' || type === '#' ? <RoomTypeIcon userId={item.id} type={item.t} status={item.status} size={16} /> : null;
