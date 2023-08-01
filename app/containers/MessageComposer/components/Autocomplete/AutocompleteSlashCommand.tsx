bacadbaaa77685a961bd3fae91aa80a95cbefed6
Diego Mello <diegolmello@gmail.com>
2023-08-01 15:57:39 -0300

    i18n for core commands

--- app/containers/MessageComposer/components/Autocomplete/AutocompleteSlashCommand.tsx
+++ app/containers/MessageComposer/components/Autocomplete/AutocompleteSlashCommand.tsx
@@ -3,6 +3,7 @@ import { View, Text } from 'react-native';
 import { useTheme } from '../../../../theme';
 import sharedStyles from '../../../../views/Styles';
 import { IAutocompleteSlashCommand } from '../../interfaces';
+import I18n from '../../../../i18n';
 
 export const AutocompleteSlashCommand = ({ item }: { item: IAutocompleteSlashCommand }) => {
 	const { colors } = useTheme();
@@ -17,7 +18,7 @@ export const AutocompleteSlashCommand = ({ item }: { item: IAutocompleteSlashCom
 				{item.subtitle ? (
 					<View style={{ flex: 1, flexDirection: 'row', alignItems: 'center' }}>
 						<Text style={[sharedStyles.textRegular, { fontSize: 14, color: colors.fontSecondaryInfo, flex: 1 }]}>
-							{item.subtitle}
+							{I18n.isTranslated(item.subtitle) ? I18n.t(item.subtitle) : item.subtitle}
 						</Text>
 					</View>
 				) : null}
