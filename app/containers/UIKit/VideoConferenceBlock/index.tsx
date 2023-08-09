cbbea733747b329c45d5d2cc1b9906738bcbcf12
Gleidson Daniel Silva <gleidson10daniel@hotmail.com>
2023-08-09 16:15:10 -0300

    feat: add new video conf block - Issue (#5150)
    * improve build-icon-set script
    
    * update mappedIcons with new icons, and orderBy name
    
    * add new icons fonts
    
    * add phone-issue icon
    
    * create VideoConferenceIssue block
    
    * verify error and return issue block

--- app/containers/UIKit/VideoConferenceBlock/index.tsx
+++ app/containers/UIKit/VideoConferenceBlock/index.tsx
@@ -5,9 +5,10 @@ import VideoConferenceDirect from './components/VideoConferenceDirect';
 import VideoConferenceEnded from './components/VideoConferenceEnded';
 import VideoConferenceOutgoing from './components/VideoConferenceOutgoing';
 import VideoConferenceSkeletonLoading from './components/VideoConferenceSkeletonLoading';
+import VideoConferenceIssue from './components/VideoConferenceIssue';
 
 export default function VideoConferenceBlock({ callId, blockId }: { callId: string; blockId: string }): React.ReactElement {
-	const { result } = useEndpointData('video-conference.info', { callId });
+	const { result, error } = useEndpointData('video-conference.info', { callId });
 
 	if (result?.success) {
 		const { users, type, status, createdBy, rid } = result;
@@ -18,6 +19,8 @@ export default function VideoConferenceBlock({ callId, blockId }: { callId: stri
 
 		return <VideoConferenceOutgoing blockId={blockId} users={users} />;
 	}
+
+	if (result?.error || error) return <VideoConferenceIssue />;
 
 	return <VideoConferenceSkeletonLoading />;
 }
