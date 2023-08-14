1639e9ac9624e2284a1e98bc8602a14a4257195f
Reinaldo Neto <yiweigao0226@gmail.com>
2023-08-14 15:11:45 -0300

    regression: attachment view is changing the color of status bar (#5167)

--- app/containers/StatusBar.tsx
+++ app/containers/StatusBar.tsx
@@ -1,8 +1,7 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { StatusBar as StatusBarRN } from 'react-native';
 
 import { useTheme } from '../theme';
-import { isAndroid } from '../lib/methods/helpers';
 
 const supportedStyles = {
 	'light-content': 'light-content',
@@ -14,23 +13,15 @@ interface IStatusBar {
 	backgroundColor?: string;
 }
 
-const StatusBar = React.memo(({ barStyle, backgroundColor }: IStatusBar) => {
+const StatusBar = ({ barStyle, backgroundColor }: IStatusBar) => {
 	const { theme, colors } = useTheme();
-
-	useEffect(() => {
-		if (!barStyle) {
-			barStyle = 'light-content';
-			if (theme === 'light') {
-				barStyle = 'dark-content';
-			}
+	if (!barStyle) {
+		barStyle = 'light-content';
+		if (theme === 'light') {
+			barStyle = 'dark-content';
 		}
-		if (isAndroid) {
-			StatusBarRN.setBackgroundColor(backgroundColor ?? colors.headerBackground);
-		}
-		StatusBarRN.setBarStyle(barStyle, true);
-	}, [theme, barStyle, backgroundColor]);
-
-	return <StatusBarRN />;
-});
+	}
+	return <StatusBarRN backgroundColor={backgroundColor ?? colors.headerBackground} barStyle={barStyle} animated />;
+};
 
 export default StatusBar;
