024088dca6bdb7973a937df2b310567e1f6b5277
Diego Mello <diegolmello@gmail.com>
2023-08-29 16:15:26 -0300

    feat: Refactor Status to make it handle user presence requests (#5148)

--- app/containers/Status/definition.ts
+++ app/containers/Status/definition.ts
@@ -5,5 +5,10 @@ import { TUserStatus } from '../../definitions';
 export interface IStatus extends TextProps {
 	id: string;
 	size: number;
-	status: TUserStatus;
+	status?: TUserStatus | null;
+}
+
+export interface IStatusComponentProps extends Omit<IStatus, 'id' | 'size' | 'status'> {
+	size?: number;
+	status?: TUserStatus;
 }
