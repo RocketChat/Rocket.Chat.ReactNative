0e16488a7e04739680e91ecf90715a49aaa9b7de
Diego Mello <diegolmello@gmail.com>
2023-09-11 11:14:09 -0300

    Base useMessages

---
+++ app/views/RoomView/List/useMessages.ts
@@ -0,0 +1,54 @@
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import { Q } from '@nozbe/watermelondb';
+
+import { TAnyMessageModel } from '../../../definitions';
+import database from '../../../lib/database';
+
+export const useMessages = ({
+	rid,
+	tmid,
+	showMessageInMainThread,
+	serverVersion
+}: {
+	rid: string;
+	tmid?: string;
+	showMessageInMainThread: boolean;
+	serverVersion: string | null;
+}): TAnyMessageModel[] => {
+	const [messages, setMessages] = useState<TAnyMessageModel[]>([]);
+
+	useLayoutEffect(() => {
+		console.count('RoomViewList useEffect');
+
+		if (!rid) {
+			return;
+		}
+
+		const db = database.active;
+		if (rid) {
+			const whereClause = [
+				Q.where('rid', rid),
+				Q.experimentalSortBy('ts', Q.desc)
+				// Q.experimentalSkip(0),
+				// Q.experimentalTake(this.count)
+			] as (Q.WhereDescription | Q.Or)[];
+			if (!showMessageInMainThread) {
+				whereClause.push(Q.or(Q.where('tmid', null), Q.where('tshow', Q.eq(true))));
+			}
+			const observable = db
+				.get('messages')
+				.query(...whereClause)
+				.observe();
+
+			const subscription = observable.subscribe(data => {
+				setMessages(data);
+			});
+
+			return () => {
+				subscription.unsubscribe();
+			};
+		}
+	}, [rid, tmid, showMessageInMainThread, serverVersion]);
+
+	return messages;
+};
