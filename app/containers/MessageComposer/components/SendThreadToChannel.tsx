8a967b2d12cb473a22c658964f37ed1758276b59
Diego Mello <diegolmello@gmail.com>
2023-09-25 17:44:18 -0300

    AlsoSendThreadToChannel user pref

--- app/containers/MessageComposer/components/SendThreadToChannel.tsx
+++ app/containers/MessageComposer/components/SendThreadToChannel.tsx
@@ -1,6 +1,8 @@
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 import { Text } from 'react-native';
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { Subscription } from 'rxjs';
+import { Q } from '@nozbe/watermelondb';
 
 import { useRoomContext } from '../../../views/RoomView/context';
 import { useAlsoSendThreadToChannel, useMessageComposerApi } from '../context';
@@ -8,12 +10,57 @@ import { CustomIcon } from '../../CustomIcon';
 import { useTheme } from '../../../theme';
 import sharedStyles from '../../../views/Styles';
 import I18n from '../../../i18n';
+import { useAppSelector } from '../../../lib/hooks';
+import database from '../../../lib/database';
+import { compareServerVersion } from '../../../lib/methods/helpers';
 
 export const SendThreadToChannel = (): React.ReactElement | null => {
 	const alsoSendThreadToChannel = useAlsoSendThreadToChannel();
 	const { setAlsoSendThreadToChannel } = useMessageComposerApi();
 	const { tmid } = useRoomContext();
 	const { colors } = useTheme();
+	const subscription = useRef<Subscription>();
+	const alsoSendThreadToChannelUserPref = useAppSelector(state => state.login.user.alsoSendThreadToChannel);
+	const serverVersion = useAppSelector(state => state.server.version);
+
+	useEffect(() => {
+		if (!tmid) {
+			return;
+		}
+
+		if (compareServerVersion(serverVersion, 'lowerThan', '5.0.0')) {
+			setAlsoSendThreadToChannel(false);
+			return;
+		}
+
+		if (alsoSendThreadToChannelUserPref === 'always') {
+			setAlsoSendThreadToChannel(true);
+			return;
+		}
+		if (alsoSendThreadToChannelUserPref === 'never') {
+			setAlsoSendThreadToChannel(false);
+			return;
+		}
+
+		/**
+		 * "default" sends a to channel only in the first message of the thread.
+		 * We check if the thread exists by observing/subscribing to the query with tmid.
+		 * If it doesn't exist, it means that this is the first message of the thread. So it's true.
+		 * Otherwise, it's false.
+		 *  */
+		if (alsoSendThreadToChannelUserPref === 'default') {
+			const db = database.active;
+			const observable = db.get('threads').query(Q.where('tmid', tmid)).observe();
+
+			subscription.current = observable.subscribe(result => {
+				setAlsoSendThreadToChannel(!result.length);
+			});
+		}
+
+		return () => {
+			subscription.current?.unsubscribe();
+		};
+	}, [tmid, alsoSendThreadToChannelUserPref, serverVersion, setAlsoSendThreadToChannel]);
 
 	if (!tmid) {
 		return null;
