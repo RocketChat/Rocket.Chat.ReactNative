dfa208fdac1b7b0b318d1b369f103e999906c02f
Diego Mello <yiweigao0226@gmail.com>
2023-10-03 17:54:00 -0300

    fix: Set size as lineHeight on CustomIcon (#5252)

--- app/containers/CustomIcon/index.tsx
+++ app/containers/CustomIcon/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createIconSetFromIcoMoon } from 'react-native-vector-icons';
-import { TextProps } from 'react-native';
+import { TextProps, StyleSheet } from 'react-native';
 
 import { mappedIcons } from './mappedIcons';
 
@@ -16,8 +16,8 @@ export interface ICustomIcon extends TextProps {
 	color: string;
 }
 
-const CustomIcon = ({ name, size, color, ...props }: ICustomIcon) => (
+const CustomIcon = ({ name, size, color, style, ...props }: ICustomIcon) => (
 	// @ts-ignore TODO remove this after update @types/react-native to 0.65.0
-	<IconSet name={name} size={size} color={color} {...props} />
+	<IconSet name={name} size={size} color={color} style={StyleSheet.flatten({ lineHeight: size }, style)} {...props} />
 );
 export { CustomIcon };
