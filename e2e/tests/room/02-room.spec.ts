80f4f682e3800427442f7559fa6ae3081a353bf4
Reinaldo Neto <reinaldonetof@hotmail.com>
2023-10-06 12:17:11 -0300

    fix action sheet swipe 02-room

--- e2e/tests/room/02-room.spec.ts
+++ e2e/tests/room/02-room.spec.ts
@@ -478,6 +478,8 @@ describe('Room screen', () => {
 					.toExist()
 					.withTimeout(2000);
 				await expect(element(by.id('action-sheet-handle'))).toBeVisible();
+				// open the action-sheet, then swipe the scroll inside the action-sheet
+				await element(by.id('action-sheet-handle')).swipe('up', 'fast', 0.5);
 				await element(by.id('action-sheet')).swipe('up', 'fast', 0.5);
 				await sleep(300); // wait for animation
 				await waitFor(element(by[textMatcher]('Delete')))
