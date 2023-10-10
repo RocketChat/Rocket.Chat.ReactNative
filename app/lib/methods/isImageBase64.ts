e722f517b66a31beea777e97de4696ed39fe4673
GleidsonDaniel <gleidson10daniel@hotmail.com>
2023-10-10 11:04:14 -0300

    chore: update regex

--- app/lib/methods/isImageBase64.ts
+++ app/lib/methods/isImageBase64.ts
@@ -1,4 +1,4 @@
-export const imageBase64RegExp = new RegExp(/^data:image\/[bmp,gif,ico,jpg,png,svg,webp,x\-icon,svg+xml]+;base64,/);
+export const imageBase64RegExp = new RegExp(/^data:image\/([a-zA-Z]*);base64,([^\"]*)$/);
 
 export function isImageBase64(data?: string): boolean {
 	return !!data && imageBase64RegExp.test(data);
