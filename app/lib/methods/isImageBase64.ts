bf1dd5b3e5ac5272441eb4c3e303bc528f362e83
Diego Mello <diegolmello@gmail.com>
2023-10-10 13:26:51 -0300

    Update app/lib/methods/isImageBase64.ts

--- app/lib/methods/isImageBase64.ts
+++ app/lib/methods/isImageBase64.ts
@@ -1,5 +1,5 @@
 export const imageBase64RegExp = new RegExp(/^data:image\/([a-zA-Z]*);base64,([^\"]*)$/);
 
 export function isImageBase64(data?: string): boolean {
-	return !!data && imageBase64RegExp.test(data);
+	return imageBase64RegExp.test(data);
 }
