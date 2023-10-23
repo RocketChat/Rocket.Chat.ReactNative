f1c21a8872b789045e408c1a59d52db728d3185d
GleidsonDaniel <gleidson10daniel@hotmail.com>
2023-10-23 17:26:07 -0300

    fix android import

--- app/lib/notifications/videoConf/getInitialNotification.ts
+++ app/lib/notifications/videoConf/getInitialNotification.ts
@@ -4,7 +4,7 @@ import { store } from '../../store/auxStore';
 
 export const getInitialNotification = async (): Promise<void> => {
 	if (isAndroid) {
-		const notifee = require('@notifee/react-native');
+		const notifee = require('@notifee/react-native').default;
 		const initialNotification = await notifee.getInitialNotification();
 		if (initialNotification?.notification?.data?.notificationType === 'videoconf') {
 			store.dispatch(deepLinkingClickCallPush(initialNotification?.notification?.data));
