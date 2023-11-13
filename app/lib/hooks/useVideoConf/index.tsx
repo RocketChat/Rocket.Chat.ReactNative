e9618dfa2fd85ecf453b3794f3dbc8cd5f948f72
GleidsonDaniel <gleidson10daniel@hotmail.com>
2023-11-13 09:59:13 -0300

    add better translation cheking

--- app/lib/hooks/useVideoConf/index.tsx
+++ app/lib/hooks/useVideoConf/index.tsx
@@ -20,7 +20,9 @@ const availabilityErrors = {
 
 const handleErrors = (isAdmin: boolean, error: keyof typeof availabilityErrors) => {
 	const key = isAdmin ? `admin-${error}` : error;
-	showErrorAlert(i18n.t(`${key}-body`), i18n.t(`${key}-header`));
+	const body = `${key}-body`;
+	const header = `${key}-header`;
+	if (i18n.isTranslated(body) && i18n.isTranslated(header)) showErrorAlert(i18n.t(body), i18n.t(header));
 };
 
 export const useVideoConf = (
