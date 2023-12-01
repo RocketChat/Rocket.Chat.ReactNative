6c0a17345c39ecf0871d40ed796159daa5ff1661
Dongwoo Gim <gimdongwoo@gmail.com>
2023-12-01 11:28:57 +0900

    Update userAgent.ts
    I'm trying to solve the problem of the userAgent being fixed and not knowing the actual device.
    
    Update userAgent.ts
    
    Revert "Update userAgent.ts"
    
    This reverts commit c9397f74fa91d13d5184f673a5f02796be105612.
    
    Update userAgent.ts
    
    Update userAgent.ts

--- app/lib/constants/userAgent.ts
+++ app/lib/constants/userAgent.ts
@@ -1,5 +1,12 @@
+import DeviceInfo from 'react-native-device-info';
+
 import { isIOS } from '../methods/helpers';
 
 export const userAgent = isIOS
-	? 'Mozilla/5.0 (iPhone; CPU iPhone OS 15_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.5 Mobile/15E148 Safari/604.1'
-	: 'Mozilla/5.0 (Linux; Android 12; SM-A315G) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Mobile Safari/537.36';
+	? `Mozilla/5.0 (iPhone; CPU iPhone OS ${DeviceInfo.getSystemVersion().replace(
+		'.',
+		'_'
+	)} like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/${DeviceInfo.getSystemVersion()} Mobile/15E148 Safari/604.1`
+	: `Mozilla/5.0 (Linux; Android ${
+		DeviceInfo.getSystemVersion().split('.')[0]
+	}; ${DeviceInfo.getModel()}) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36`;
