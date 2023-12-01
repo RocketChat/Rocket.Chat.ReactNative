360ec44cf9b289c6da74032a1699a436cc18d448
Reinaldo Neto <reinaldonetof@hotmail.com>
2023-12-01 15:18:00 -0300

    add the comments

--- app/lib/methods/helpers/buildUrlImage.ts
+++ app/lib/methods/helpers/buildUrlImage.ts
@@ -6,6 +6,7 @@ export const isValidLink = (link: string): boolean => {
 	}
 };
 
+// https://github.com/RocketChat/Rocket.Chat/blob/5c145e3170f04e341be93a2a60f09b6cbdc46c73/apps/meteor/client/components/message/content/urlPreviews/buildImageURL.ts#L3
 export const buildImageURL = (url: string, imageUrl: string): string => {
 	if (isValidLink(imageUrl)) {
 		return imageUrl;
