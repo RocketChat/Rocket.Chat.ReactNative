b03bcd2eb93fada1676fa258abb529f2bd770cfe
Reinaldo Neto <reinaldonetof@hotmail.com>
2023-12-12 17:40:22 -0300

    minor tweak unit test

--- app/lib/methods/helpers/buildImageURL.test.ts
+++ app/lib/methods/helpers/buildImageURL.test.ts
@@ -4,9 +4,9 @@ import { buildImageURL } from './buildImageURL';
 describe('buildImageURL', () => {
 	const testCases = [
 		[
-			'https://s2.glbimg.com/fXQKM_UZjF6I_3APIbPJzJTOUvw=/1200x/smart/filters:cover():strip_icc()/s04.video.glbimg.com/x720/11012523.jpg',
-			'https://s2.glbimg.com/fXQKM_UZjF6I_3APIbPJzJTOUvw=/1200x/smart/filters:cover():strip_icc()/s04.video.glbimg.com/x720/11012523.jpg',
-			'https://g1.globo.com/mundo/video/misseis-atingem-ponte-de-vidro-em-kiev-11012523.ghtml'
+			'https://open.rocket.chat/avatar/rocket.cat',
+			'https://open.rocket.chat/avatar/rocket.cat',
+			'https://open.rocket.chat/direct/NNNNnnnnNNNNnnnnfrocket.cat'
 		],
 		['https://open.rocket.chat/assets/favicon_512.png', 'assets/favicon_512.png', 'https://open.rocket.chat/channel/general'],
 		['https://open.rocket.chat/assets/favicon_512.png', '/assets/favicon_512.png', 'https://open.rocket.chat/channel/general'],
