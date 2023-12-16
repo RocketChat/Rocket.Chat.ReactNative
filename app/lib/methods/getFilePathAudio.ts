805c520327f5537bc3ba167c04523a4f67a459d6
Sathurshan <jsathu07@gmail.com>
2023-12-16 19:31:57 +0530

    refactor: fix test

--- app/lib/methods/getFilePathAudio.ts
+++ app/lib/methods/getFilePathAudio.ts
@@ -3,11 +3,11 @@ import { store } from '../store/auxStore';
 import { getFilePath } from './handleMediaDownload';
 import { IAttachment } from '../../definitions/IAttachment';
 
-const baseUrl = store.getState().server.server;
-const cdnPrefix = store.getState().settings.CDN_PREFIX;
-const user = getUserSelector(store.getState());
-
 export const getFilePathAudio = (file: IAttachment): string => {
+	const baseUrl = store.getState().server.server;
+	const cdnPrefix = store.getState().settings.CDN_PREFIX;
+	const user = getUserSelector(store.getState());
+
 	let url = file.audio_url;
 	if (url && !url.startsWith('http')) {
 		url = `${cdnPrefix || baseUrl}${url}`;
