053cda0bdbeb9e39ad37fd482143ab5deb5f8121
GleidsonDaniel <gleidson10daniel@hotmail.com>
2024-01-03 14:20:27 -0300

    feat: refactor Ringer component to improve sound loading and playing

--- app/containers/Ringer/index.tsx
+++ app/containers/Ringer/index.tsx
@@ -1,6 +1,5 @@
 import { Audio } from 'expo-av';
 import React, { useEffect, useRef } from 'react';
-import { View } from 'react-native';
 
 export enum ERingerSounds {
 	DIALTONE = 'dialtone',
@@ -8,34 +7,28 @@ export enum ERingerSounds {
 }
 
 const Ringer = React.memo(({ ringer }: { ringer: ERingerSounds }) => {
-	const sound = useRef<Audio.Sound | null>(null);
+	const sound = useRef(new Audio.Sound());
+
 	useEffect(() => {
-		(async () => {
-			let expo = null;
-			switch (ringer) {
-				case ERingerSounds.DIALTONE:
-					expo = await Audio.Sound.createAsync(require(`./dialtone.mp3`));
-					break;
-				case ERingerSounds.RINGTONE:
-					expo = await Audio.Sound.createAsync(require(`./ringtone.mp3`));
-					break;
-				default:
-					expo = await Audio.Sound.createAsync(require(`./dialtone.mp3`));
-					break;
+		const loadAndPlay = async () => {
+			try {
+				const soundFile = ringer === ERingerSounds.DIALTONE ? require(`./dialtone.mp3`) : require(`./ringtone.mp3`);
+				await sound.current.loadAsync(soundFile);
+				await sound.current.playAsync();
+				await sound.current.setIsLoopingAsync(true);
+			} catch (error) {
+				console.error('Error loading sound:', error);
 			}
-			sound.current = expo.sound;
-			await sound.current.playAsync();
-			await sound.current.setIsLoopingAsync(true);
-		})();
+		};
+
+		loadAndPlay();
+
+		return () => {
+			sound.current?.unloadAsync();
+		};
 	}, []);
 
-	useEffect(() => () => stopSound(), []);
-
-	const stopSound = () => {
-		sound?.current?.unloadAsync();
-	};
-
-	return <View />;
+	return null;
 });
 
 export default Ringer;
