e6b7e0cc8ab4049dc5721995e8a17223d347d0a4
Sathurshan <jsathu07@gmail.com>
2024-01-11 20:52:58 +0530

    optimise performance

--- app/containers/ImageViewer/ImageCarousal.tsx
+++ app/containers/ImageViewer/ImageCarousal.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import Animated, { useAnimatedStyle, useSharedValue } from 'react-native-reanimated';
+import Animated, { runOnJS, useAnimatedReaction, useAnimatedStyle, useSharedValue } from 'react-native-reanimated';
 
 import { ImageViewer } from './ImageViewer';
 
@@ -38,28 +38,42 @@ export const ImageCarousal = ({
 	const translateOuterX = useSharedValue(WIDTH_OFFSET * currItem.value);
 	const offsetOuterX = useSharedValue(WIDTH_OFFSET * currItem.value);
 
+	const [curr, setCurr] = useState(firstIndex);
+
 	const style = useAnimatedStyle(() => ({
 		transform: [{ translateX: translateOuterX.value }]
 	}));
+
+	useAnimatedReaction(
+		() => currItem.value,
+		currVal => {
+			runOnJS(setCurr)(currVal);
+		}
+	);
 
 	return (
 		<GestureHandlerRootView style={styles.container}>
 			<View style={[{ width: data.length * width, height }]}>
 				<Animated.View style={[{ flex: 1, flexDirection: 'row' }, style]}>
-					{data.map((item: IImageData) => (
-						<ImageViewer
-							key={item.uri}
-							item={item}
-							translateOuterX={translateOuterX}
-							offsetOuterX={offsetOuterX}
-							currItem={currItem}
-							size={data.length}
-							width={width}
-							height={height}
-							showHeader={showHeader}
-							{...props}
-						/>
-					))}
+					{data.map((item: IImageData, index) => {
+						if (index === curr || index === curr - 1 || index === curr + 1) {
+							return (
+								<ImageViewer
+									key={item.uri}
+									item={item}
+									translateOuterX={translateOuterX}
+									offsetOuterX={offsetOuterX}
+									currItem={currItem}
+									size={data.length}
+									width={width}
+									height={height}
+									showHeader={showHeader}
+									{...props}
+								/>
+							);
+						}
+						return <View key={item.uri} style={{ width, height }} />;
+					})}
 				</Animated.View>
 			</View>
 		</GestureHandlerRootView>
--- app/containers/ImageViewer/ImageViewer.tsx
+++ app/containers/ImageViewer/ImageViewer.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { memo, useState } from 'react';
 import { LayoutChangeEvent, StyleSheet, StyleProp, ViewStyle, ImageStyle, View, Text } from 'react-native';
 import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 import Animated, {
@@ -58,234 +58,232 @@ const styles = StyleSheet.create({
 	}
 });
 
-export const ImageViewer = ({
-	item,
-	imageComponentType,
-	width,
-	height,
-	translateOuterX,
-	offsetOuterX,
-	currItem,
-	size,
-	showHeader,
-	...props
-}: ImageViewerProps): React.ReactElement => {
-	console.log(props);
-	const [centerX, setCenterX] = useState(0);
-	const [centerY, setCenterY] = useState(0);
-	const [loading, setLoading] = useState(false);
-	const [isHeaderVisible, setIsHeaderVisible] = useState(showHeader);
+export const ImageViewer = memo(
+	({
+		item,
+		imageComponentType,
+		width,
+		height,
+		translateOuterX,
+		offsetOuterX,
+		currItem,
+		size,
+		showHeader,
+		...props
+	}: ImageViewerProps): React.ReactElement => {
+		const [centerX, setCenterX] = useState(0);
+		const [centerY, setCenterY] = useState(0);
+		const [loading, setLoading] = useState(false);
+		const [isHeaderVisible, setIsHeaderVisible] = useState(showHeader);
 
-	const WIDTH_OFFSET = -width;
-	const OUTER_EDGE_PAN = 100; // how much to translate when panned on both edges of the outer view
+		const WIDTH_OFFSET = -width;
+		const OUTER_EDGE_PAN = 100; // how much to translate when panned on both edges of the outer view
 
-	const onLayout = ({
-		nativeEvent: {
-			layout: { x, y, width, height }
-		}
-	}: LayoutChangeEvent) => {
-		setCenterX(x + width / 2);
-		setCenterY(y + height / 2);
-	};
-
-	const translationX = useSharedValue<number>(0);
-	const eventTranslationX = useSharedValue<number>(0);
-	const translationY = useSharedValue<number>(0);
-	const offsetX = useSharedValue<number>(0);
-	const offsetY = useSharedValue<number>(0);
-	const scale = useSharedValue<number>(1);
-	const scaleOffset = useSharedValue<number>(1);
-
-	const style = useAnimatedStyle(() => ({
-		transform: [
-			{
-				translateX: translationX.value
-			},
-			{
-				translateY: translationY.value
-			},
-			{
-				scale: scale.value
+		const onLayout = ({
+			nativeEvent: {
+				layout: { x, y, width, height }
 			}
-		]
-	}));
+		}: LayoutChangeEvent) => {
+			setCenterX(x + width / 2);
+			setCenterY(y + height / 2);
+		};
 
-	const resetScaleAnimation = () => {
-		'worklet';
+		const translationX = useSharedValue<number>(0);
+		const eventTranslationX = useSharedValue<number>(0);
+		const translationY = useSharedValue<number>(0);
+		const offsetX = useSharedValue<number>(0);
+		const offsetY = useSharedValue<number>(0);
+		const scale = useSharedValue<number>(1);
+		const scaleOffset = useSharedValue<number>(1);
 
-		scaleOffset.value = 1;
-		offsetOuterX.value = WIDTH_OFFSET * currItem.value;
-		offsetX.value = 0;
-		offsetY.value = 0;
-		scale.value = withSpring(1);
-		translationX.value = withSpring(0, {
-			overshootClamping: true
-		});
-		translateOuterX.value = withSpring(WIDTH_OFFSET * currItem.value, {
-			overshootClamping: true
-		});
-		translationY.value = withSpring(0, {
-			overshootClamping: true
-		});
-	};
-
-	const clamp = (value: number, min: number, max: number) => {
-		'worklet';
-
-		return Math.max(Math.min(value, max), min);
-	};
-
-	const pinchGesture = Gesture.Pinch()
-		.onUpdate(event => {
-			scale.value = clamp(scaleOffset.value * (event.scale > 0 ? event.scale : 1), 1, 4);
-		})
-		.onEnd(() => {
-			scaleOffset.value = scale.value > 0 ? scale.value : 1;
-		});
-
-	const panGesture = Gesture.Pan()
-		.maxPointers(2)
-		.onStart(() => {
-			translateOuterX.value = offsetOuterX.value;
-			translationX.value = offsetX.value;
-			translationY.value = offsetY.value;
-			eventTranslationX.value = 0;
-		})
-		.onUpdate(event => {
-			const scaleFactor = scale.value - 1;
-
-			translationX.value = clamp(event.translationX + offsetX.value, -scaleFactor * centerX, scaleFactor * centerX);
-			translationY.value = clamp(event.translationY + offsetY.value, -scaleFactor * centerY, scaleFactor * centerY);
-
-			// when edge is reached
-			if (translationX.value === scaleFactor * centerX || translationX.value === -scaleFactor * centerX) {
-				translateOuterX.value = clamp(
-					event.translationX - eventTranslationX.value + offsetOuterX.value,
-					WIDTH_OFFSET * (size - 1) - OUTER_EDGE_PAN,
-					OUTER_EDGE_PAN
-				);
-			} else {
-				eventTranslationX.value = event.translationX; // to get finger moved value after reaching inner edge
-			}
-		})
-		.onEnd(() => {
-			const diff = translateOuterX.value - WIDTH_OFFSET * currItem.value; // diff between outer position and outer translation
-			const scrollOffset = centerX / 2; // on how much pan go to next image
-
-			// only when outer translate value is changed
-			if (diff) {
-				let isChanged = true;
-				if (diff < -scrollOffset && currItem.value + 1 < size) {
-					currItem.value += 1;
-				} else if (diff > scrollOffset && currItem.value - 1 >= 0) {
-					currItem.value -= 1;
-				} else {
-					isChanged = false;
-				}
-				translateOuterX.value = withSpring(WIDTH_OFFSET * currItem.value, { overshootClamping: true });
-				offsetOuterX.value = WIDTH_OFFSET * currItem.value;
-				if (isChanged) {
-					resetScaleAnimation();
-					return;
-				}
-			}
-
-			offsetX.value = translationX.value;
-			offsetY.value = translationY.value;
-			if (scale.value === 1) resetScaleAnimation();
-		});
-
-	const doubleTapGesture = Gesture.Tap()
-		.numberOfTaps(2)
-		.maxDelay(120)
-		.maxDistance(70)
-		.onEnd(event => {
-			if (scaleOffset.value > 1) resetScaleAnimation();
-			else {
-				scale.value = withTiming(2, {
-					duration: 200
-				});
-				translationX.value = withTiming(centerX - event.x, {
-					duration: 200
-				});
-				offsetX.value = centerX - event.x;
-				scaleOffset.value = 2;
-			}
-		});
-
-	const toggleHeader = () => setIsHeaderVisible(s => !s);
-
-	const singleTapGesture = Gesture.Tap()
-		.requireExternalGestureToFail(doubleTapGesture)
-		.onEnd(() => {
-			if (showHeader) {
-				runOnJS(toggleHeader)();
-			}
-		});
-
-	const gesture = Gesture.Simultaneous(pinchGesture, panGesture, doubleTapGesture, singleTapGesture);
-
-	const Component = ImageComponent({ type: imageComponentType, uri: item.uri });
-
-	const { colors } = useTheme();
-
-	const navigation = useAppNavigation();
-
-	const { baseUrl, user, Allow_Save_Media_to_Gallery } = useAppSelector(
-		state => ({
-			baseUrl: state.server.server,
-			user: { id: getUserSelector(state).id, token: getUserSelector(state).token },
-			Allow_Save_Media_to_Gallery: (state.settings.Allow_Save_Media_to_Gallery as boolean) ?? true
-		}),
-		shallowEqual
-	);
-
-	return (
-		<View
-			style={[
-				styles.flex,
+		const style = useAnimatedStyle(() => ({
+			transform: [
 				{
+					translateX: translationX.value
+				},
+				{
+					translateY: translationY.value
+				},
+				{
+					scale: scale.value
+				}
+			]
+		}));
+
+		const resetScaleAnimation = () => {
+			'worklet';
+
+			scaleOffset.value = 1;
+			offsetOuterX.value = WIDTH_OFFSET * currItem.value;
+			offsetX.value = 0;
+			offsetY.value = 0;
+			scale.value = withSpring(1);
+			translationX.value = withSpring(0, {
+				overshootClamping: true
+			});
+			translateOuterX.value = withSpring(WIDTH_OFFSET * currItem.value, {
+				overshootClamping: true
+			});
+			translationY.value = withSpring(0, {
+				overshootClamping: true
+			});
+		};
+
+		const clamp = (value: number, min: number, max: number) => {
+			'worklet';
+
+			return Math.max(Math.min(value, max), min);
+		};
+
+		const pinchGesture = Gesture.Pinch()
+			.onUpdate(event => {
+				scale.value = clamp(scaleOffset.value * (event.scale > 0 ? event.scale : 1), 1, 4);
+			})
+			.onEnd(() => {
+				scaleOffset.value = scale.value > 0 ? scale.value : 1;
+			});
+
+		const panGesture = Gesture.Pan()
+			.maxPointers(2)
+			.onStart(() => {
+				translateOuterX.value = offsetOuterX.value;
+				translationX.value = offsetX.value;
+				translationY.value = offsetY.value;
+				eventTranslationX.value = 0;
+			})
+			.onUpdate(event => {
+				const scaleFactor = scale.value - 1;
+
+				translationX.value = clamp(event.translationX + offsetX.value, -scaleFactor * centerX, scaleFactor * centerX);
+				translationY.value = clamp(event.translationY + offsetY.value, -scaleFactor * centerY, scaleFactor * centerY);
+
+				// when edge is reached
+				if (translationX.value === scaleFactor * centerX || translationX.value === -scaleFactor * centerX) {
+					translateOuterX.value = clamp(
+						event.translationX - eventTranslationX.value + offsetOuterX.value,
+						WIDTH_OFFSET * (size - 1) - OUTER_EDGE_PAN,
+						OUTER_EDGE_PAN
+					);
+				} else {
+					eventTranslationX.value = event.translationX; // to get finger moved value after reaching inner edge
+				}
+			})
+			.onEnd(() => {
+				const diff = translateOuterX.value - WIDTH_OFFSET * currItem.value; // diff between outer position and outer translation
+				const scrollOffset = centerX / 2; // on how much pan go to next image
+
+				// only when outer translate value is changed
+				if (diff) {
+					let isChanged = true;
+					if (diff < -scrollOffset && currItem.value + 1 < size) {
+						currItem.value += 1;
+					} else if (diff > scrollOffset && currItem.value - 1 >= 0) {
+						currItem.value -= 1;
+					} else {
+						isChanged = false;
+					}
+					translateOuterX.value = withSpring(WIDTH_OFFSET * currItem.value, { overshootClamping: true });
+					offsetOuterX.value = WIDTH_OFFSET * currItem.value;
+					if (isChanged) {
+						resetScaleAnimation();
+						return;
+					}
+				}
+
+				offsetX.value = translationX.value;
+				offsetY.value = translationY.value;
+				if (scale.value === 1) resetScaleAnimation();
+			});
+
+		const doubleTapGesture = Gesture.Tap()
+			.numberOfTaps(2)
+			.maxDelay(120)
+			.maxDistance(70)
+			.onEnd(event => {
+				if (scaleOffset.value > 1) resetScaleAnimation();
+				else {
+					scale.value = withTiming(2, {
+						duration: 200
+					});
+					translationX.value = withTiming(centerX - event.x, {
+						duration: 200
+					});
+					offsetX.value = centerX - event.x;
+					scaleOffset.value = 2;
+				}
+			});
+
+		const toggleHeader = () => setIsHeaderVisible(s => !s);
+
+		const singleTapGesture = Gesture.Tap()
+			.requireExternalGestureToFail(doubleTapGesture)
+			.onEnd(() => {
+				if (showHeader) {
+					runOnJS(toggleHeader)();
+				}
+			});
+
+		const gesture = Gesture.Simultaneous(pinchGesture, panGesture, doubleTapGesture, singleTapGesture);
+
+		const Component = ImageComponent({ type: imageComponentType, uri: item.uri });
+
+		const { colors } = useTheme();
+
+		const navigation = useAppNavigation();
+
+		const { baseUrl, user, Allow_Save_Media_to_Gallery } = useAppSelector(
+			state => ({
+				baseUrl: state.server.server,
+				user: { id: getUserSelector(state).id, token: getUserSelector(state).token },
+				Allow_Save_Media_to_Gallery: (state.settings.Allow_Save_Media_to_Gallery as boolean) ?? true
+			}),
+			shallowEqual
+		);
+
+		return (
+			<View
+				style={{
 					width,
 					height,
 					overflow: 'hidden',
 					paddingHorizontal: 5,
 					backgroundColor: colors.previewBackground
-				}
-			]}
-		>
-			{isHeaderVisible && (
-				<View style={[styles.header, { width, backgroundColor: colors.previewBackground }]}>
-					<Touchable onPress={navigation.goBack}>
-						<CustomIcon name={'arrow-back'} size={25} color={colors.previewTintColor} />
-					</Touchable>
-
-					<Text numberOfLines={1} style={[styles.text, { color: colors.previewTintColor, maxWidth: '60%' }]}>
-						{decodeURI(item.title as string) || ''}
-					</Text>
-
-					{Allow_Save_Media_to_Gallery && !isImageBase64(item.uri) && (
-						<Touchable
-							onPress={() =>
-								handleSave({ image_url: item.uri, title: item.title, image_type: item.image_type }, user, baseUrl, setLoading)
-							}
-						>
-							<CustomIcon name={'download'} size={25} color={colors.previewTintColor} />
+				}}
+			>
+				{isHeaderVisible && (
+					<View style={[styles.header, { width, backgroundColor: colors.previewBackground }]}>
+						<Touchable onPress={navigation.goBack}>
+							<CustomIcon name={'arrow-back'} size={25} color={colors.previewTintColor} />
 						</Touchable>
-					)}
-				</View>
-			)}
-			<GestureDetector gesture={gesture}>
-				<Animated.View onLayout={onLayout} style={[styles.flex, style]}>
-					<Component
-						// @ts-ignore
-						style={styles.image}
-						resizeMode='contain'
-						source={{ uri: item.uri }}
-						{...props}
-					/>
-				</Animated.View>
-			</GestureDetector>
-			{loading ? <RCActivityIndicator absolute size='large' /> : null}
-		</View>
-	);
-};
+
+						<Text numberOfLines={1} style={[styles.text, { color: colors.previewTintColor, maxWidth: '60%' }]}>
+							{decodeURI(item.title as string) || ''}
+						</Text>
+
+						{Allow_Save_Media_to_Gallery && !isImageBase64(item.uri) && (
+							<Touchable
+								onPress={() =>
+									handleSave({ image_url: item.uri, title: item.title, image_type: item.image_type }, user, baseUrl, setLoading)
+								}
+							>
+								<CustomIcon name={'download'} size={25} color={colors.previewTintColor} />
+							</Touchable>
+						)}
+					</View>
+				)}
+				<GestureDetector gesture={gesture}>
+					<Animated.View onLayout={onLayout} style={[styles.flex, style]}>
+						<Component
+							// @ts-ignore
+							style={styles.image}
+							resizeMode='contain'
+							source={{ uri: item.uri }}
+							{...props}
+						/>
+					</Animated.View>
+				</GestureDetector>
+				{loading ? <RCActivityIndicator absolute size='large' /> : null}
+			</View>
+		);
+	}
+);
