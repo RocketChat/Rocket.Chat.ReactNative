043af156f338d5117efdef337cc659881d5f2b35
GleidsonDaniel <gleidson10daniel@hotmail.com>
2024-02-16 15:47:44 -0300

    fix: move to react-native-katex-v2 and add some memoization

--- app/containers/markdown/new/Katex.tsx
+++ app/containers/markdown/new/Katex.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { KaTeX as KaTeXProps } from '@rocket.chat/message-parser';
 // eslint-disable-next-line import/no-unresolved
 import MathView, { MathText } from 'react-native-math-view';
-import Katex from 'react-native-katex';
+import Katex from 'react-native-katex-v2';
 
 import { useTheme } from '../../../theme';
 import { DEFAULT_MESSAGE_HEIGHT } from '../../message/utils';
@@ -11,7 +11,7 @@ interface IKaTeXProps {
 	value: KaTeXProps['value'];
 }
 
-export const KaTeX = ({ value }: IKaTeXProps): React.ReactElement | null => {
+export const KaTeX = memo(({ value }: IKaTeXProps) => {
 	const { colors } = useTheme();
 	return (
 		<MathView
@@ -20,9 +20,9 @@ export const KaTeX = ({ value }: IKaTeXProps): React.ReactElement | null => {
 			renderError={() => <Katex expression={value} style={{ flex: 1, height: DEFAULT_MESSAGE_HEIGHT }} />}
 		/>
 	);
-};
+});
 
-export const InlineKaTeX = ({ value }: IKaTeXProps): React.ReactElement | null => {
+export const InlineKaTeX = memo(({ value }: IKaTeXProps) => {
 	const { colors } = useTheme();
 	return <MathText color value={`$$${value}$$`} direction='ltr' style={{ color: colors.bodyText }} />;
-};
+});
