69ce044e8b9cc578ea1baf2cf16e1597c8f55a61
Reinaldo Neto <yiweigao0226@gmail.com>
2024-03-04 08:27:24 -0300

    feat: mobile troubleshoot notifications (#5330)
    * feat: troubleshoot notification (#5198)
    
    * navigation done
    
    * create the icon inside roomslistview, navigation to push troubleshot and layout push troubleshoot
    
    * custom header
    
    * fix the rooms list view header icon
    
    * layout done
    
    * update the pt-br i18n
    
    * tweak on colors
    
    * feat: create notification in room view (#5250)
    
    * button and simple navigation done, missing master detail
    
    * navigation
    
    * add withTheme and colors to rightuttons
    
    * fix e2e test
    
    * feat: add troubleshooting to notifications pages (#5276)
    
    * feat: add troubleshooting to notifications pages
    
    * fix e2e test
    
    * feat: device notification settings (#5277)
    
    * iOS go to device notification setting to change the configuration
    
    * go to notification settings with android
    
    * add notifee
    
    * add the reducer and action
    
    * saga request done
    
    * add the setInAlert action
    
    * tweak at name and add focus to dispatch the request
    
    * use the foreground inside pushTroubleShoot to request the notification and fix the icon color
    
    * add the request at roomslistview didmount
    
    * remove the notification modulo from android
    
    * add patch
    
    * minor tweak
    
    * feat: test push notification (#5329)
    
    * feat: test push notification
    
    * restApi and definition
    
    * push.info and change properly the troubleshootingNotification
    
    * use the finally at try/catch
    
    * minor tweak
    
    * alert and push.info just for 6.6
    
    * fix the react-native.config
    
    * minor tweaks
    
    * minor tweak
    
    * push.test as rest api
    
    * change the name from inAlertNotification to highlightTroubleshooting
    
    * feat: push quota
    
    * refactor the percentage state
    
    * removed the push quota feature
    
    * minor tweaks
    
    * update the link to push notification
    
    * the notification icon in the room header will appear if notifications are disabled or highlight troubleshoot is true
    
    * remove push quota texts
    
    * updated some of the push quota texts
    
    * chore: rename highlightTroubleshooting
    
    * chore: better prop naming
    
    * wip
    
    * chore: fix function name
    
    * chore: fix colors
    
    * fix: copy
    
    * chore: 💅
    
    * chore: use fork
    
    * chore: naming
    
    * chore: fix init
    
    * chore: naming
    
    * chore: naming
    
    * Comment CE code
    
    * Use put on troubleshooting saga
    
    * Add db column
    
    * fix: check notification payload
    
    * action: organized translations
    
    * fix: push init
    
    ---------
    
    Co-authored-by: GleidsonDaniel <gleidson10daniel@hotmail.com>
    Co-authored-by: Diego Mello <diegolmello@gmail.com>
    Co-authored-by: GleidsonDaniel <GleidsonDaniel@users.noreply.github.com>

--- app/sagas/login.js
+++ app/sagas/login.js
@@ -17,6 +17,7 @@ import { inviteLinksRequest } from '../actions/inviteLinks';
 import { showErrorAlert } from '../lib/methods/helpers/info';
 import { localAuthenticate } from '../lib/methods/helpers/localAuthentication';
 import { encryptionInit, encryptionStop } from '../actions/encryption';
+import { initTroubleshootingNotification } from '../actions/troubleshootingNotification';
 import UserPreferences from '../lib/methods/userPreferences';
 import { inquiryRequest, inquiryReset } from '../ee/omnichannel/actions/inquiry';
 import { isOmnichannelStatusAvailable } from '../ee/omnichannel/lib';
@@ -236,6 +237,7 @@ const handleLoginSuccess = function* handleLoginSuccess({ user }) {
 			yield put(inviteLinksRequest(inviteLinkToken));
 		}
 		yield showSupportedVersionsWarning(server);
+		yield put(initTroubleshootingNotification());
 	} catch (e) {
 		log(e);
 	}
