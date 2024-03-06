f9fabf8f7dcee5d896e4038186c9dc2263f1363f
Timothy Roy <timothy@Timothys-MacBook-Pro.local>
2024-03-06 14:16:41 -0600

    Refactor getVirtualHappyHourChat for readability

--- app/views/HomeView/helpers.tsx
+++ app/views/HomeView/helpers.tsx
@@ -11,32 +11,31 @@ const CHAT247ROOMID = '24-7-chatroom';
 const VIRTUAL_HAPPY_HOUR_ROOMID = 'virtual-happy-hours';
 const TECH_SUPPORT_USERNAME = 'tech_support';
 
-
 export const getVirtualHappyHourChat = async (): Promise<TSubscriptionModel | undefined> => {
-   let chatRoom: TSubscriptionModel | undefined;
-   unsubscribeQuery();
-   const db = database.active;
-   const defaultWhereClause = [Q.where('archived', false), Q.where('open', true)] as (Q.WhereDescription | Q.SortBy)[];
-   defaultWhereClause.push(Q.experimentalSortBy('room_updated_at', Q.desc));
-   const observable = await db
-       .get('subscriptions')
-       .query(...defaultWhereClause)
-       .observeWithColumns(['on_hold']);
-   const subscriptionPromise = new Promise<void>((resolve, reject) => {
-       querySubscription = observable.subscribe(
-           data => {
-               chatRoom = data.find(chat => chat.name === VIRTUAL_HAPPY_HOUR_ROOMID);
-               resolve();
-           },
-           error => {
-               reject(error);
-           }
-       );
-   });
-   await subscriptionPromise;
-   return chatRoom;
-};
+  unsubscribeQuery();
 
+  const db = database.active;
+  const defaultWhereClause = [
+    Q.where('archived', false),
+    Q.where('open', true),
+    Q.experimentalSortBy('room_updated_at', Q.desc),
+  ] as (Q.WhereDescription | Q.SortBy)[];
+
+  return new Promise<TSubscriptionModel | undefined>((resolve, reject) => {
+    const observable = db
+      .get('subscriptions')
+      .query(...defaultWhereClause)
+      .observeWithColumns(['on_hold']);
+
+    const subscription = observable.subscribe({
+      next: data => {
+        const chatRoom = data.find(chat => chat.name === VIRTUAL_HAPPY_HOUR_ROOMID);
+        resolve(chatRoom);
+      },
+      error: reject,
+    });
+  });
+};
 
 export const navigateToVirtualHappyHour = async (Navigation: any, isMasterDetail: boolean) => {
    if (Navigation) {
