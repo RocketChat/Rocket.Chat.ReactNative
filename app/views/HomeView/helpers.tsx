4b3cca80681b432951da5d9309ab421b4852f8dd
Timothy Roy <timothy@Timothys-MacBook-Pro.local>
2024-03-07 13:38:50 -0600

    Refactor get247Chat and getVirtualHappyHourChat

--- app/views/HomeView/helpers.tsx
+++ app/views/HomeView/helpers.tsx
@@ -12,8 +12,6 @@ const VIRTUAL_HAPPY_HOUR_ROOMID = 'virtual-happy-hours';
 const TECH_SUPPORT_USERNAME = 'tech_support';
 
 export const getVirtualHappyHourChat = async (): Promise<TSubscriptionModel | undefined> => {
-  unsubscribeQuery();
-
   const db = database.active;
   const defaultWhereClause = [
     Q.where('archived', false),
@@ -29,10 +27,14 @@ export const getVirtualHappyHourChat = async (): Promise<TSubscriptionModel | un
 
     const subscription = observable.subscribe({
       next: data => {
+        subscription.unsubscribe();
         const chatRoom = data.find(chat => chat.name === VIRTUAL_HAPPY_HOUR_ROOMID);
         resolve(chatRoom);
       },
-      error: reject,
+      error: (error) => {
+        subscription.unsubscribe();
+        reject(error);
+      },
     });
   });
 };
@@ -42,7 +44,7 @@ export const navigateToVirtualHappyHour = async (Navigation: any, isMasterDetail
        try {
            const chatRoom = await getVirtualHappyHourChat();
            await Navigation.navigate('ChatsStackNavigator', {
-               screen: 'RoomsListView'
+               screen: 'RoomListView'
            });
            goRoom({ item: chatRoom, isMasterDetail });
        } catch (error) {
@@ -51,7 +53,7 @@ export const navigateToVirtualHappyHour = async (Navigation: any, isMasterDetail
    }
 };
 
- export const navToTechSupport= async (Navigation: any, isMasterDetail: boolean): Promise<void> => {
+ export const navToTechSupport = async (Navigation: any, isMasterDetail: boolean): Promise<void> => {
 	try {
 		const db = database.active;
 		const subsCollection = db.get('subscriptions');
@@ -59,14 +61,14 @@ export const navigateToVirtualHappyHour = async (Navigation: any, isMasterDetail
 		if (query.length > 0) {
 			const room = query[0]
 			await Navigation.navigate('ChatsStackNavigator', {
-				screen: 'RoomsListView'
+				screen: 'RoomListView'
 			});
 			handleGoRoom(room, isMasterDetail);
 		} else {
 			const result = await Services.createDirectMessage(TECH_SUPPORT_USERNAME);
 			if (result.success) {
 				await Navigation.navigate('ChatsStackNavigator', {
-					screen: 'RoomsListView'
+					screen: 'RoomListView'
 				});
 				handleGoRoom({ rid: result.room?._id as string, name: TECH_SUPPORT_USERNAME, t: SubscriptionType.DIRECT }, isMasterDetail);
 			}
@@ -81,43 +83,32 @@ const handleGoRoom = (item: TGoRoomItem, isMasterDetail: boolean): void => {
 };
 
 
-let querySubscription: Subscription;
-
-const unsubscribeQuery = () => {
-	if (querySubscription && querySubscription.unsubscribe) {
-		querySubscription.unsubscribe();
-	}
-};
-
 export const get247Chat = async (): Promise<TSubscriptionModel | undefined> => {
-	let chatRoom: TSubscriptionModel | undefined;
+  const db = database.active;
+  const defaultWhereClause = [
+    Q.where('archived', false),
+    Q.where('open', true),
+    Q.experimentalSortBy('room_updated_at', Q.desc),
+  ] as (Q.WhereDescription | Q.SortBy)[];
 
-	unsubscribeQuery();
+  return new Promise<TSubscriptionModel | undefined>((resolve, reject) => {
+    const observable = db
+      .get('subscriptions')
+      .query(...defaultWhereClause)
+      .observeWithColumns(['on_hold']);
 
-	const db = database.active;
-
-	const defaultWhereClause = [Q.where('archived', false), Q.where('open', true)] as (Q.WhereDescription | Q.SortBy)[];
-	defaultWhereClause.push(Q.experimentalSortBy('room_updated_at', Q.desc));
-
-	const observable = await db
-		.get('subscriptions')
-		.query(...defaultWhereClause)
-		.observeWithColumns(['on_hold']);
-
-	const subscriptionPromise = new Promise<void>((resolve, reject) => {
-		querySubscription = observable.subscribe(
-			data => {
-				chatRoom = data.find(chat => chat.name === CHAT247ROOMID);
-				resolve();
-			},
-			error => {
-				reject(error);
-			}
-		);
-	});
-
-	await subscriptionPromise;
-	return chatRoom;
+    const subscription = observable.subscribe({
+      next: data => {
+        subscription.unsubscribe();
+        const chatRoom = data.find(chat => chat.name === CHAT247ROOMID);
+        resolve(chatRoom);
+      },
+      error: (error) => {
+        subscription.unsubscribe();
+        reject(error);
+      },
+    });
+  });
 };
 
 export const navigateTo247Chat = async (Navigation: any, isMasterDetail: boolean) => {
