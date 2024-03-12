56d3b31730b442f53fe8ee7b55dc82d2252a1b96
Timothy Roy <timothy@Timothys-MacBook-Pro.local>
2024-03-11 19:34:04 -0500

    Add basic test for HomeView

---
+++ app/views/HomeView/HomeView.test.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { cleanup, render } from '@testing-library/react-native';
+import { Provider } from 'react-redux';
+
+import { mockedStore as store } from '../../reducers/mockedStore';
+import HomeView from './index';
+
+jest.mock('@react-navigation/native', () => {
+  return {
+    ...jest.requireActual('@react-navigation/native'),
+    useNavigation: () => ({
+      navigate: jest.fn(),
+      setOptions: jest.fn(),
+    }),
+  };
+});
+
+describe('HomeView', () => {
+  it('renders', async () => {
+    const { getByTestId } = render(
+      <Provider store={store}>
+        <HomeView />
+      </Provider>
+    );
+    expect(getByTestId('home-view')).toBeTruthy();
+  });
+});
