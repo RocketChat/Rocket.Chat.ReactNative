689235318e2e9e87ad4751992b9770eb0f68cfb8
Timothy Roy <timothy@Timothys-MacBook-Pro.local>
2024-03-11 20:05:18 -0500

    Use production URL by default

--- app/sagas/serverConfig.js
+++ app/sagas/serverConfig.js
@@ -7,9 +7,10 @@ const DEV_SERVER_URL_ANDROID = 'http://10.0.2.2:3000';
 // Production URL
 const PROD_SERVER_URL = "https://app.t1dreachout.com";
 
-const SERVER_URL = __DEV__ ? Platform.select({
-  ios: DEV_SERVER_URL_IOS,
-  android: DEV_SERVER_URL_ANDROID,
-}) : PROD_SERVER_URL;
+// uncomment and export as default to enable development with local backend
+// const SERVER_URL = __DEV__ ? Platform.select({
+//   ios: DEV_SERVER_URL_IOS,
+//   android: DEV_SERVER_URL_ANDROID,
+// }) : PROD_SERVER_URL;
 
-export default SERVER_URL;
+export default PROD_SERVER_URL;
