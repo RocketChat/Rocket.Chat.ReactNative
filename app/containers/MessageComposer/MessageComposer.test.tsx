73824fe7d991ec701753ae6687f955728fdf60e4
anirbanpaulcom <anirbanpaulcom@gmail.com>
2024-04-19 21:31:37 +0530

    test

--- app/containers/MessageComposer/MessageComposer.test.tsx
+++ app/containers/MessageComposer/MessageComposer.test.tsx
@@ -100,8 +100,10 @@ describe('MessageComposer', () => {
 		const onSendMessage = jest.fn();
 		render(<Render context={{ onSendMessage }} />);
 		expect(screen.getByTestId('message-composer-send-audio')).toBeOnTheScreen();
+		expect(screen.getByTestId('message-composer-send')).not.toBeOnTheScreen();
 
 		await user.type(screen.getByTestId('message-composer-input'), 'test');
+		expect(screen.getByTestId('message-composer-input').props.value.trim()).not.toBe("");
 		expect(screen.queryByTestId('message-composer-send-audio')).not.toBeOnTheScreen();
 		expect(screen.getByTestId('message-composer-send')).toBeOnTheScreen();
 
