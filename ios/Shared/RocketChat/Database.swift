079fd2387b47ac7ab600ea80d6d8d62677287b8b
Djorkaeff Alexandre <djorkaeff.unb@gmail.com>
2024-04-26 15:15:44 -0300

    Fix push reply

--- ios/Shared/RocketChat/Database.swift
+++ ios/Shared/RocketChat/Database.swift
@@ -10,41 +10,42 @@ import Foundation
 import WatermelonDB
 
 final class Database {
-  private let database: WatermelonDB.Database
-  
-  init(server: String) {
-    database = .init(name: server)
-  }
-  
-  func readRoomEncryptionKey(rid: String) -> String? {
-    if let results = try? database.queryRaw("select * from subscriptions where id == ? limit 1", [rid]) {
-      guard let record = results.next() else {
-        return nil
-      }
-      
-      if let room = record.resultDictionary as? [String: Any] {
-        if let e2eKey = room["e2e_key"] as? String {
-          return e2eKey
-        }
-      }
-    }
-    
-    return nil
-  }
-  
-  func readRoomEncrypted(rid: String) -> Bool {
-    if let results = try? database.queryRaw("select * from subscriptions where id == ? limit 1", [rid]) {
-      guard let record = results.next() else {
-        return false
-      }
-      
-      if let room = record.resultDictionary as? [String: Any] {
-        if let encrypted = room["encrypted"] as? Bool {
-          return encrypted
-        }
-      }
-    }
-    
-    return false
-  }
+	private let database: WatermelonDB.Database
+	
+	init(server: String) {
+		let domain = URL(string: server)?.domain ?? ""
+		database = .init(name: domain)
+	}
+	
+	func readRoomEncryptionKey(rid: String) -> String? {
+		if let results = try? database.queryRaw("select * from subscriptions where id == ? limit 1", [rid]) {
+			guard let record = results.next() else {
+				return nil
+			}
+			
+			if let room = record.resultDictionary as? [String: Any] {
+				if let e2eKey = room["e2e_key"] as? String {
+					return e2eKey
+				}
+			}
+		}
+		
+		return nil
+	}
+	
+	func readRoomEncrypted(rid: String) -> Bool {
+		if let results = try? database.queryRaw("select * from subscriptions where id == ? limit 1", [rid]) {
+			guard let record = results.next() else {
+				return false
+			}
+			
+			if let room = record.resultDictionary as? [String: Any] {
+				if let encrypted = room["encrypted"] as? Bool {
+					return encrypted
+				}
+			}
+		}
+		
+		return false
+	}
 }
