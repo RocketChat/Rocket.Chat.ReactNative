49109b6428e3364eccb3248c9b5d42c7f36b362d
GleidsonDaniel <gleidson10daniel@hotmail.com>
2024-05-20 14:35:39 -0400

    fix: update color

--- app/containers/message/Components/RightIcons/Pinned.tsx
+++ app/containers/message/Components/RightIcons/Pinned.tsx
@@ -2,14 +2,10 @@ import React from 'react';
 
 import { CustomIcon } from '../../../CustomIcon';
 import styles from '../../styles';
-import { useTheme } from '../../../../theme';
 
-const Pinned = React.memo(({ pinned, testID }: { pinned?: boolean; testID?: string }) => {
-	const { colors } = useTheme();
-	if (pinned) {
-		return <CustomIcon testID={testID} name='pin' color={colors.auxiliaryTintColor} size={16} style={styles.rightIcons} />;
-	}
+const Pinned = ({ pinned, testID }: { pinned?: boolean; testID?: string }): React.ReactElement | null => {
+	if (pinned) return <CustomIcon testID={testID} name='pin' size={16} style={styles.rightIcons} />;
 	return null;
-});
+};
 
 export default Pinned;
