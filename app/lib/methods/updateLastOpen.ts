0cb366563b845bd620582e994b30539eb4b8111f
GleidsonDaniel <gleidson10daniel@hotmail.com>
2024-06-11 09:48:29 -0300

    regression: add updateLastOpen function to update the last open date of a subscription

---
+++ app/lib/methods/updateLastOpen.ts
@@ -0,0 +1,21 @@
+import database from '../database';
+import log from './helpers/log';
+import { TSubscriptionModel } from '../../definitions';
+
+export async function updateLastOpen(rid: string, lastOpen = new Date()): Promise<void> {
+	try {
+		const db = database.active;
+		const subscription = await db.get('subscriptions').find(rid);
+		db.write(async () => {
+			try {
+				await subscription.update((s: TSubscriptionModel) => {
+					s.lastOpen = lastOpen;
+				});
+			} catch (e) {
+				log(e);
+			}
+		});
+	} catch (e) {
+		log(e);
+	}
+}
