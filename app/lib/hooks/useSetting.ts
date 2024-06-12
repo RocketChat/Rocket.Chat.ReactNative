35da322af7103164df201a7e4d660e17c2bfdc6e
GleidsonDaniel <gleidson10daniel@hotmail.com>
2024-06-12 16:53:29 -0300

    type

--- app/lib/hooks/useSetting.ts
+++ app/lib/hooks/useSetting.ts
@@ -1,6 +1,6 @@
 import { TSettingsValues, TSupportedSettings } from '../../reducers/settings';
 import { useAppSelector } from './useAppSelector';
 
-export function useSetting<TValue extends TSettingsValues>(key: TSupportedSettings): TValue | undefined {
+export function useSetting<TValue extends TSettingsValues>(key: TSupportedSettings): TValue {
 	return useAppSelector(state => state.settings[key]) as TValue;
 }
