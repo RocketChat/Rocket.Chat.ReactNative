10d5a946cd7d58a7d2aa1c166d3d48c1bfd88af0
Diego Mello <diegolmello@gmail.com>
2024-08-27 15:39:01 -0300

    Change MMKV logLevel to info

--- ios/RocketChatRN/AppDelegate.mm
+++ ios/RocketChatRN/AppDelegate.mm
@@ -18,7 +18,7 @@
   
   // AppGroup MMKV
   NSString *groupDir = [[NSFileManager defaultManager] containerURLForSecurityApplicationGroupIdentifier:[[NSBundle mainBundle] objectForInfoDictionaryKey:@"AppGroup"]].path;
-  [MMKV initializeMMKV:nil groupDir:groupDir logLevel:MMKVLogDebug];
+  [MMKV initializeMMKV:nil groupDir:groupDir logLevel:MMKVLogInfo];
   
   [RNNotifications startMonitorNotifications];
   [ReplyNotification configure];
