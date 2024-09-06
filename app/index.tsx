2f9bdc88885ad33d17b341af1361d2580c5f7051
Diego Mello <diegolmello@gmail.com>
2024-09-06 16:50:44 -0300

    Remove unnecessary clipboard

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,6 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider, initialWindowMetrics } from 'react-native-safe-area-context';
 import RNScreens from 'react-native-screens';
 import { Provider } from 'react-redux';
-import Clipboard from '@react-native-clipboard/clipboard';
 
 import AppContainer from './AppContainer';
 import { appInit, appInitLocalSettings, setMasterDetail as setMasterDetailAction } from './actions/app';
@@ -140,7 +139,6 @@ export default class Root extends React.Component<{}, IState> {
 		const deepLinking = await Linking.getInitialURL();
 		const parsedDeepLinkingURL = parseDeepLinking(deepLinking!);
 		if (parsedDeepLinkingURL) {
-			Clipboard.setString(JSON.stringify(parsedDeepLinkingURL));
 			store.dispatch(deepLinkingOpen(parsedDeepLinkingURL));
 			return;
 		}
