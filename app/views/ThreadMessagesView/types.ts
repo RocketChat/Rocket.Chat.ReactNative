2420dd21090f355ee61bed2bad5cf5f5dfc1baee
OtavioStasiak <otaviostasiakmusic@outlook.com>
2024-10-14 17:28:24 -0300

    chore: organization and performance changes

--- app/views/ThreadMessagesView/types.ts
+++ app/views/ThreadMessagesView/types.ts
@@ -4,23 +4,37 @@ import { ChatsStackParamList } from "../../stacks/types";
 import { TSupportedThemes } from "../../theme";
 
 
-export interface IThreadMessagesViewState {
+export type ISearchThreadMessages = {
+	isSearching: boolean;
+	searchText: string;
+};
+
+export type IThreadMessagesViewState = {
 	loading: boolean;
 	end: boolean;
 	messages: any[];
 	displayingThreads: TThreadModel[];
 	subscription: TSubscriptionModel;
 	currentFilter: Filter;
-	isSearching: boolean;
-	searchText: string;
+	search: ISearchThreadMessages;
 	offset: number;
-}
+};
 
-export interface IThreadMessagesViewProps extends IBaseScreen<ChatsStackParamList, 'ThreadMessagesView'> {
+export type IThreadMessagesViewProps = IBaseScreen<ChatsStackParamList, 'ThreadMessagesView'> & {
 	user: { id: string };
 	baseUrl: string;
 	useRealName: boolean;
 	theme: TSupportedThemes;
 	isMasterDetail: boolean;
-}
+};
 
+
+export type IThreadAction =
+	| { type: 'SET_LOADING'; payload: boolean }
+	| { type: 'SET_END'; payload: boolean }
+	| { type: 'SET_MESSAGES'; payload: any[] } 
+	| { type: 'SET_DISPLAYING_THREADS'; payload: TThreadModel[] } 
+	| { type: 'SET_SUBSCRIPTION'; payload: TSubscriptionModel } 
+	| { type: 'SET_FILTER'; payload: Filter } 
+	| { type: 'SET_SEARCH'; payload: ISearchThreadMessages }
+	| { type: 'SET_OFFSET'; payload: number };
