7fdf9567f7d743307f4f0e0aa73338e2c5970ae2
OtavioStasiak <otaviostasiakmusic@outlook.com>
2024-10-17 14:54:23 -0300

    chore: created hooks and separated components

---
+++ app/views/NewServerView/styles.ts
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+
+import sharedStyles from '../Styles';
+
+
+const styles = StyleSheet.create({
+	onboardingImage: {
+		alignSelf: 'center',
+		resizeMode: 'contain'
+	},
+	title: {
+		...sharedStyles.textBold,
+		letterSpacing: 0,
+		alignSelf: 'center'
+	},
+	subtitle: {
+		...sharedStyles.textRegular,
+		alignSelf: 'center'
+	},
+	description: {
+		...sharedStyles.textRegular,
+		textAlign: 'center'
+	},
+	connectButton: {
+		marginBottom: 0
+	}
+});
+
+export default styles;
