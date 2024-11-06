d9fb35b9a475d885dc226462250828b6186ee7bb
Diego Mello <diegolmello@gmail.com>
2024-11-06 17:53:36 -0300

    Fix undefined emails on rooms stream

--- app/lib/methods/subscriptions/rooms.ts
+++ app/lib/methods/subscriptions/rooms.ts
@@ -301,7 +301,7 @@ export default function subscribeRooms() {
 		const [, ev] = ddpMessage.fields.eventName.split('/');
 		if (/userData/.test(ev)) {
 			const [{ diff, unset }] = ddpMessage.fields.args;
-			if (diff.emails?.length > 0) {
+			if (diff?.emails?.length > 0) {
 				store.dispatch(setUser({ emails: diff.emails }));
 			}
 			if (diff?.statusLivechat) {
