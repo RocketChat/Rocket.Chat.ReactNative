32e037c421e4913507ab6b2a764c05b31ef1e1da
OtavioStasiak <otaviostasiakmusic@outlook.com>
2024-11-11 19:15:25 -0300

    fix: updated rules on add emoji on messageComposer

---
+++ app/containers/MessageComposer/helpers/insertEmojiAtCursor.ts
@@ -0,0 +1,25 @@
+export const insertEmojiAtCursor = (text: string, emojiText: string, cursor: number) => {
+	let updatedText = '';
+	let updatedCursor = cursor + emojiText.length;
+
+	const firstPart = text.substr(0, cursor);
+	const lastPart = text.substr(cursor);
+
+	if (firstPart[firstPart.length - 1] !== ' ') {
+		updatedText = `${firstPart} ${emojiText}`;
+		updatedCursor += 1;
+	} else {
+		updatedText = `${firstPart}${emojiText}`;
+	}
+
+	if (lastPart[0] !== ' ') {
+		updatedText = `${updatedText} ${lastPart}`;
+		updatedCursor += 1;
+	} else {
+		updatedText = `${updatedText}${lastPart}`;
+	}
+	return {
+		updatedCursor,
+		updatedText
+	};
+};
