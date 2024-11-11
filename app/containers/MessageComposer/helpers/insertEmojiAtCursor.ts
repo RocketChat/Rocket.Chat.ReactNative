119a11a235c0e028f9b84c2f7d3bceed855069c7
OtavioStasiak <otaviostasiakmusic@outlook.com>
2024-11-11 20:21:02 -0300

    chore: simplify insertEmojiAtCursor function

--- app/containers/MessageComposer/helpers/insertEmojiAtCursor.ts
+++ app/containers/MessageComposer/helpers/insertEmojiAtCursor.ts
@@ -1,30 +1,16 @@
 export const insertEmojiAtCursor = (text: string, emojiText: string, cursor: number) => {
-	let updatedText = '';
 	let updatedCursor = cursor + emojiText.length;
 
 	const firstPart = text.substr(0, cursor);
 	const lastPart = text.substr(cursor);
 
-	if (firstPart[firstPart.length - 1] !== ' ') {
-		updatedText = `${firstPart} ${emojiText}`;
-		updatedCursor += 1;
-	} else {
-		updatedText = `${firstPart}${emojiText}`;
-	}
+	const spaceBefore = firstPart.endsWith(' ') ? '' : ' ';
+	const spaceAfter = lastPart.startsWith(' ') || lastPart.length === 0 ? '' : ' ';
 
-	if (lastPart.length === 0) {
-		return {
-			updatedCursor,
-			updatedText
-		};
-	}
+	const updatedText = `${firstPart}${spaceBefore}${emojiText}${spaceAfter}${lastPart}`;
+	updatedCursor += spaceBefore ? 1 : 0;
+	updatedCursor += spaceAfter ? 1 : 0;
 
-	if (lastPart[0] !== ' ') {
-		updatedText = `${updatedText} ${lastPart}`;
-		updatedCursor += 1;
-	} else {
-		updatedText = `${updatedText}${lastPart}`;
-	}
 	return {
 		updatedCursor,
 		updatedText
