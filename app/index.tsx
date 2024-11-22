707513616d63ebda990f13bc1f64ab7803e02363
yash-rajpal <rajpal.yash03@gmail.com>
2024-11-23 00:08:25 +0530

    WIP: i18next

--- app/index.tsx
+++ app/index.tsx
@@ -35,6 +35,7 @@ import { initStore } from './lib/store/auxStore';
 import { TSupportedThemes, ThemeContext } from './theme';
 import ChangePasscodeView from './views/ChangePasscodeView';
 import ScreenLockedView from './views/ScreenLockedView';
+import './i18n/index';
 
 RNScreens.enableScreens();
 initStore(store);
