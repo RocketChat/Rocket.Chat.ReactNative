00cd041c327f7189c67ccb85d057dfe4c6d73c08
OtavioStasiak <otaviostasiakmusic@outlook.com>
2024-11-25 18:13:58 -0300

    fix: lint

--- app/views/RoomInfoEditView/index.tsx
+++ app/views/RoomInfoEditView/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Q } from '@nozbe/watermelondb';
 import { BlockContext } from '@rocket.chat/ui-kit';
 import { dequal } from 'dequal';
-import { Alert, Keyboard, ScrollView, Text, TextInput, View, StyleSheet } from 'react-native';
+import { Alert, Keyboard, ScrollView, Text, TextInput, View } from 'react-native';
 import { connect } from 'react-redux';
 import { Subscription } from 'rxjs';
 
