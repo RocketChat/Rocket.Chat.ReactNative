27d8790a7bed1aef609b4f401d58f186ed080fa2
OtavioStasiak <otaviostasiakmusic@outlook.com>
2024-11-27 12:41:19 -0300

    chore: started to migrate to hooks

--- app/views/RegisterView.tsx
+++ app/views/RegisterView.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
+import React, { useLayoutEffect, useRef, useState } from 'react';
 import { Keyboard, StyleSheet, Text, View, TextInput as RNTextInput } from 'react-native';
 import RNPickerSelect from 'react-native-picker-select';
-import { connect } from 'react-redux';
 import parse from 'url-parse';
+import { useForm, Controller } from 'react-hook-form';
 
 import { loginRequest } from '../actions/login';
-import { themes } from '../lib/constants';
 import Button from '../containers/Button';
 import FormContainer, { FormContainerInner } from '../containers/FormContainer';
 import * as HeaderButton from '../containers/HeaderButton';
 import LoginServices from '../containers/LoginServices';
 import { FormTextInput } from '../containers/TextInput';
-import { IApplicationState, IBaseScreen } from '../definitions';
+import { IBaseScreen } from '../definitions';
 import I18n from '../i18n';
 import { getShowLoginButton } from '../selectors/login';
 import { OutsideParamList } from '../stacks/types';
-import { withTheme } from '../theme';
+import { useTheme } from '../theme';
 import { showErrorAlert, isValidEmail } from '../lib/methods/helpers';
 import log, { events, logEvent } from '../lib/methods/helpers/log';
 import sharedStyles from './Styles';
 import { Services } from '../lib/services';
 import UGCRules from '../containers/UserGeneratedContentRules';
+import { useAppSelector } from '../lib/hooks';
 
 const styles = StyleSheet.create({
 	title: {
@@ -48,124 +48,119 @@ const styles = StyleSheet.create({
 	}
 });
 
-interface IProps extends IBaseScreen<OutsideParamList, 'RegisterView'> {
-	Site_Url: string;
-	Gitlab_URL: string;
-	CAS_enabled: boolean;
-	CAS_login_url: string;
-	Accounts_CustomFields: string;
-	Accounts_EmailVerification: boolean;
-	Accounts_ManuallyApproveNewUsers: boolean;
-	showLoginButton: boolean;
-}
+interface IProps extends IBaseScreen<OutsideParamList, 'RegisterView'> {}
 
-class RegisterView extends React.Component<IProps, any> {
-	private parsedCustomFields: any;
-	private usernameInput?: RNTextInput | null;
-	private passwordInput?: RNTextInput | null;
-	private emailInput?: RNTextInput | null;
-	private avatarUrl?: RNTextInput | null;
+const RegisterView = ({ navigation, route, dispatch }: IProps) => {
+	const getCustomFields = () => {
+		let customFields: any;
+		Object.keys(parsedCustomFields).forEach((key: string) => {
+			if (parsedCustomFields[key].defaultValue) {
+				customFields[key] = parsedCustomFields[key].defaultValue;
+			}
+		});
 
-	static navigationOptions = ({ route, navigation }: IProps) => ({
-		title: route?.params?.title ?? 'Rocket.Chat',
-		headerRight: () => <HeaderButton.Legal testID='register-view-more' navigation={navigation} />
-	});
+		return customFields;
+	};
 
-	constructor(props: IProps) {
-		super(props);
-		const customFields: any = {};
-		this.parsedCustomFields = {};
-		if (props.Accounts_CustomFields) {
+	const getParsedCustomFields = () => {
+		let parsedCustomFields: any = {};
+		if (Accounts_CustomFields) {
 			try {
-				this.parsedCustomFields = JSON.parse(props.Accounts_CustomFields);
+				parsedCustomFields = JSON.parse(Accounts_CustomFields);
 			} catch (e) {
 				log(e);
 			}
 		}
-		Object.keys(this.parsedCustomFields).forEach((key: string) => {
-			if (this.parsedCustomFields[key].defaultValue) {
-				customFields[key] = this.parsedCustomFields[key].defaultValue;
-			}
-		});
-		this.state = {
+
+		return parsedCustomFields;
+	};
+
+	const parsedCustomFields = getParsedCustomFields();
+	const customFields = getCustomFields();
+	const usernameInput = useRef<RNTextInput | null>(null);
+	const passwordInput = useRef<RNTextInput | null>(null);
+	const emailInput = useRef<RNTextInput | null>(null);
+	const avatarUrl = useRef<RNTextInput | null>(null);
+
+	const { colors } = useTheme();
+	const [saving, setSaving] = useState(false);
+	const { control, handleSubmit, setFocus, register } = useForm({
+		defaultValues: {
 			name: '',
 			email: '',
 			password: '',
-			username: '',
-			saving: false,
-			customFields
-		};
-	}
+			username: ''
+		}
+	});
 
-	login = () => {
-		const { navigation, Site_Url } = this.props;
+	const { Accounts_CustomFields, Site_Url, Accounts_EmailVerification, Accounts_ManuallyApproveNewUsers, showLoginButton } =
+		useAppSelector(state => {
+			return {
+				...state.settings,
+				showLoginButton: getShowLoginButton(state),
+				Site_Url: state.settings.Site_Url as string,
+				Accounts_CustomFields: state.settings.Accounts_CustomFields as string
+			};
+		});
+
+	const login = () => {
 		navigation.navigate('LoginView', { title: new parse(Site_Url).hostname });
 	};
 
-	valid = () => {
-		const { name, email, password, username, customFields } = this.state;
-		let requiredCheck = true;
-		Object.keys(this.parsedCustomFields).forEach((key: string) => {
-			if (this.parsedCustomFields[key].required) {
-				requiredCheck = requiredCheck && customFields[key] && Boolean(customFields[key].trim());
-			}
-		});
-		return name.trim() && email.trim() && password.trim() && username.trim() && isValidEmail(email) && requiredCheck;
-	};
-
-	submit = async () => {
+	const onSubmit = async (data: any) => {
 		logEvent(events.REGISTER_DEFAULT_SIGN_UP);
-		if (!this.valid()) {
+
+		const { name, email, password, username } = data;
+
+		/* if (!this.valid()) {
+			return;
+		} */
+
+		if (!isValidEmail(email)) {
+			showErrorAlert(I18n.t('Invalid_email'), I18n.t('Error'));
 			return;
 		}
-		this.setState({ saving: true });
+
+		setSaving(true);
 		Keyboard.dismiss();
 
-		const { name, email, password, username, customFields } = this.state;
-		const { dispatch, Accounts_EmailVerification, navigation, Accounts_ManuallyApproveNewUsers } = this.props;
-
 		try {
-			const user = await Services.register({
-				name,
-				email,
-				pass: password,
-				username
-			});
-			if (user.success) {
+			const response = await Services.register({ name, email, pass: password, username });
+
+			if (response.success) {
 				if (Accounts_EmailVerification) {
-					await navigation.goBack();
 					showErrorAlert(I18n.t('Verify_email_desc'), I18n.t('Registration_Succeeded'));
+					navigation.goBack();
 				} else if (Accounts_ManuallyApproveNewUsers) {
-					await navigation.goBack();
 					showErrorAlert(I18n.t('Wait_activation_warning'), I18n.t('Registration_Succeeded'));
+					navigation.goBack();
 				} else {
 					dispatch(loginRequest({ user: email, password }, false, false, customFields));
 				}
 			}
-		} catch (e: any) {
-			if (e.data?.errorType === 'username-invalid') {
+		} catch (error: any) {
+			if (error.data?.errorType === 'username-invalid') {
 				return dispatch(loginRequest({ user: email, password }));
 			}
-			if (e.data?.error) {
+			if (error.data?.error) {
 				logEvent(events.REGISTER_DEFAULT_SIGN_UP_F);
-				showErrorAlert(e.data.error, I18n.t('Oops'));
+				showErrorAlert(error.data.error, I18n.t('Oops'));
 			}
+		} finally {
+			setSaving(false);
 		}
-		this.setState({ saving: false });
 	};
 
-	renderCustomFields = () => {
-		const { customFields } = this.state;
-		const { Accounts_CustomFields } = this.props;
+	const renderCustomFields = () => {
 		if (!Accounts_CustomFields) {
 			return null;
 		}
 		try {
 			return (
 				<>
-					{Object.keys(this.parsedCustomFields).map((key, index, array) => {
-						if (this.parsedCustomFields[key].type === 'select') {
-							const options = this.parsedCustomFields[key].options.map((option: string) => ({ label: option, value: option }));
+					{Object.keys(parsedCustomFields).map((key, index, array) => {
+						if (parsedCustomFields[key].type === 'select') {
+							const options = parsedCustomFields[key].options.map((option: string) => ({ label: option, value: option }));
 							return (
 								<RNPickerSelect
 									key={key}
@@ -173,7 +168,7 @@ class RegisterView extends React.Component<IProps, any> {
 									onValueChange={value => {
 										const newValue: { [key: string]: string | number } = {};
 										newValue[key] = value;
-										this.setState({ customFields: { ...customFields, ...newValue } });
+										/* 		this.setState({ customFields: { ...customFields, ...newValue } }); */
 									}}
 									value={customFields[key]}>
 									<FormTextInput
@@ -202,14 +197,14 @@ class RegisterView extends React.Component<IProps, any> {
 								onChangeText={(value: string) => {
 									const newValue: { [key: string]: string | number } = {};
 									newValue[key] = value;
-									this.setState({ customFields: { ...customFields, ...newValue } });
+									/* this.setState({ customFields: { ...customFields, ...newValue } }); */
 								}}
 								onSubmitEditing={() => {
 									if (array.length - 1 > index) {
 										// @ts-ignore
 										return this[array[index + 1]].focus();
 									}
-									this.avatarUrl?.focus();
+									avatarUrl?.current?.focus();
 								}}
 								containerStyle={styles.inputContainer}
 							/>
@@ -222,112 +217,125 @@ class RegisterView extends React.Component<IProps, any> {
 		}
 	};
 
-	render() {
-		const { saving } = this.state;
-		const { theme, showLoginButton } = this.props;
-		return (
-			<FormContainer testID='register-view'>
-				<FormContainerInner>
-					<LoginServices separator />
-					<Text style={[styles.title, sharedStyles.textBold, { color: themes[theme].fontTitlesLabels }]}>
-						{I18n.t('Sign_Up')}
-					</Text>
-					<FormTextInput
-						label={I18n.t('Name')}
-						containerStyle={styles.inputContainer}
-						placeholder={I18n.t('Full_name')}
-						returnKeyType='next'
-						onChangeText={(name: string) => this.setState({ name })}
-						onSubmitEditing={() => {
-							this.usernameInput?.focus();
-						}}
-						testID='register-view-name'
-						textContentType='name'
-						autoComplete='name'
-					/>
-					<FormTextInput
-						label={I18n.t('Username')}
-						containerStyle={styles.inputContainer}
-						inputRef={e => {
-							this.usernameInput = e;
-						}}
-						returnKeyType='next'
-						onChangeText={(username: string) => this.setState({ username })}
-						onSubmitEditing={() => {
-							this.emailInput?.focus();
-						}}
-						testID='register-view-username'
-						textContentType='username'
-						autoComplete='username'
-					/>
-					<FormTextInput
-						label={I18n.t('Email')}
-						containerStyle={styles.inputContainer}
-						inputRef={e => {
-							this.emailInput = e;
-						}}
-						returnKeyType='next'
-						onChangeText={(email: string) => this.setState({ email })}
-						onSubmitEditing={() => {
-							this.passwordInput?.focus();
-						}}
-						testID='register-view-email'
-						keyboardType='email-address'
-						textContentType='emailAddress'
-						autoComplete='email'
-					/>
-					<FormTextInput
-						label={I18n.t('Password')}
-						containerStyle={styles.inputContainer}
-						inputRef={e => {
-							this.passwordInput = e;
-						}}
-						returnKeyType='send'
-						secureTextEntry
-						onChangeText={(value: string) => this.setState({ password: value })}
-						onSubmitEditing={this.submit}
-						testID='register-view-password'
-						textContentType='newPassword'
-						autoComplete='password-new'
-					/>
+	useLayoutEffect(() => {
+		navigation.setOptions({
+			title: route?.params?.title ?? 'Rocket.Chat',
+			headerRight: () => <HeaderButton.Legal testID='register-view-more' navigation={navigation} />
+		});
+	}, []);
 
-					{this.renderCustomFields()}
+	return (
+		<FormContainer testID='register-view'>
+			<FormContainerInner>
+				<LoginServices separator />
+				<Text style={[styles.title, sharedStyles.textBold, { color: colors.fontTitlesLabels }]}>{I18n.t('Sign_Up')}</Text>
 
-					<Button
-						title={I18n.t('Register')}
-						type='primary'
-						onPress={this.submit}
-						testID='register-view-submit'
-						disabled={!this.valid()}
-						loading={saving}
-						style={styles.registerButton}
-					/>
+				<Controller
+					name='name'
+					control={control}
+					render={({ field: { onChange, value, ref } }) => (
+						<FormTextInput
+							inputRef={ref}
+							testID='register-view-name'
+							textContentType='name'
+							autoComplete='name'
+							returnKeyType='next'
+							required
+							label={I18n.t('Full_name')}
+							value={value}
+							onChangeText={onChange}
+							onSubmitEditing={() => setFocus('username')}
+							containerStyle={styles.inputContainer}
+						/>
+					)}
+				/>
 
-					{showLoginButton ? (
-						<View style={styles.bottomContainer}>
-							<Text style={[styles.bottomContainerText, { color: themes[theme].fontSecondaryInfo }]}>
-								{I18n.t('Already_have_an_account')}
-							</Text>
-							<Button title={I18n.t('Login')} type='secondary' onPress={this.login} style={styles.loginButton} />
-						</View>
-					) : null}
+				<Controller
+					name='username'
+					control={control}
+					render={({ field: { onChange, value, ref } }) => (
+						<FormTextInput
+							inputRef={ref}
+							testID='register-view-username'
+							textContentType='username'
+							autoComplete='username'
+							returnKeyType='next'
+							required
+							label={I18n.t('Username')}
+							value={value}
+							onChangeText={onChange}
+							onSubmitEditing={() => {
+								setFocus('email');
+							}}
+							containerStyle={styles.inputContainer}
+						/>
+					)}
+				/>
 
-					<UGCRules />
-				</FormContainerInner>
-			</FormContainer>
-		);
-	}
-}
+				<Controller
+					name='email'
+					control={control}
+					render={({ field: { onChange, value, ref } }) => (
+						<FormTextInput
+							inputRef={ref}
+							testID='register-view-email'
+							keyboardType='email-address'
+							textContentType='emailAddress'
+							autoComplete='email'
+							returnKeyType='next'
+							required
+							label={I18n.t('Email')}
+							value={value}
+							onChangeText={onChange}
+							onSubmitEditing={() => {
+								setFocus('password');
+							}}
+							containerStyle={styles.inputContainer}
+						/>
+					)}
+				/>
 
-const mapStateToProps = (state: IApplicationState) => ({
-	Site_Url: state.settings.Site_Url as string,
-	Gitlab_URL: state.settings.API_Gitlab_URL as string,
-	CAS_enabled: state.settings.CAS_enabled as boolean,
-	CAS_login_url: state.settings.CAS_login_url as string,
-	Accounts_CustomFields: state.settings.Accounts_CustomFields as string,
-	Accounts_EmailVerification: state.settings.Accounts_EmailVerification as boolean,
-	Accounts_ManuallyApproveNewUsers: state.settings.Accounts_ManuallyApproveNewUsers as boolean,
-	showLoginButton: getShowLoginButton(state)
-});
+				<Controller
+					name='password'
+					control={control}
+					render={({ field: { onChange, value, ref } }) => (
+						<FormTextInput
+							inputRef={ref}
+							testID='register-view-password'
+							textContentType='newPassword'
+							autoComplete='password-new'
+							returnKeyType='next'
+							required
+							label={I18n.t('Password')}
+							value={value}
+							onChangeText={onChange}
+							secureTextEntry
+							containerStyle={styles.inputContainer}
+						/>
+					)}
+				/>
+				{renderCustomFields()}
 
-export default connect(mapStateToProps)(withTheme(RegisterView));
+				<Button
+					title={I18n.t('Register')}
+					type='primary'
+					onPress={handleSubmit(onSubmit)}
+					loading={saving}
+					style={styles.registerButton}
+				/>
+
+				{showLoginButton ? (
+					<View style={styles.bottomContainer}>
+						<Text style={[styles.bottomContainerText, { color: colors.fontSecondaryInfo }]}>
+							{I18n.t('Already_have_an_account')}
+						</Text>
+						<Button title={I18n.t('Login')} type='secondary' onPress={login} style={styles.loginButton} />
+					</View>
+				) : null}
+				<UGCRules />
+			</FormContainerInner>
+		</FormContainer>
+	);
+};
+
+export default RegisterView;
