62b2c9819631285f7ed4fbea3f35d5a3c83ffa97
Rohit Bansal <40559587+Rohit3523@users.noreply.github.com>
2024-11-29 01:50:18 +0530

    Android native code for handling theme change for modals

---
+++ android/app/src/main/java/chat/rocket/reactnative/ThemeManagerModule.kt
@@ -0,0 +1,24 @@
+package chat.rocket.reactnative
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.Callback
+import android.util.Log
+
+class ThemeManagerModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+    override fun getName() = "ThemeManager"
+
+    @ReactMethod
+    fun setAppTheme(isDarkMode: Boolean) {
+        if (isDarkMode) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+    }
+}
