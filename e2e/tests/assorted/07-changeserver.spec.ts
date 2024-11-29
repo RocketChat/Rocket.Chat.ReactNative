ed5234bcf6337e83cf0acd2d780dcf9049d5773d
OtavioStasiak <otaviostasiakmusic@outlook.com>
2024-11-28 23:34:24 -0300

    fix: changeserver e2e test

--- e2e/tests/assorted/07-changeserver.spec.ts
+++ e2e/tests/assorted/07-changeserver.spec.ts
@@ -83,7 +83,7 @@ describe('Change server', () => {
 		await element(by.id('register-view-email')).tapReturnKey();
 		await element(by.id('register-view-password')).replaceText(randomUser.password);
 		await element(by.id('register-view-password')).tapReturnKey();
-		await element(by.id('register-view-confirm-password')).replaceText(user.password);
+		await element(by.id('register-view-confirm-password')).replaceText(randomUser.password);
 		await element(by.id('register-view-confirm-password')).tapReturnKey();
 		await element(by.id('register-view')).swipe('up', 'fast', 0.5);
 		await element(by.id('register-view-submit')).tap();
