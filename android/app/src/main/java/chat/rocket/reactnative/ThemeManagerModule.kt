385674462d6a44343d7942663cd1f95dc6686c29
Rohit Bansal <40559587+Rohit3523@users.noreply.github.com>
2024-11-29 21:56:57 +0530

    Remove android.util.Log from ThemeManagerModule

--- android/app/src/main/java/chat/rocket/reactnative/ThemeManagerModule.kt
+++ android/app/src/main/java/chat/rocket/reactnative/ThemeManagerModule.kt
@@ -8,7 +8,6 @@ import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.Callback
-import android.util.Log
 
 class ThemeManagerModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
     override fun getName() = "ThemeManager"
