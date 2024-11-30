0c1fcd2f58a625b8b8653b01450ec8cc2ba90c6e
OtavioStasiak <otaviostasiakmusic@outlook.com>
2024-11-29 23:59:52 -0300

    fix: create team e2e test

--- e2e/tests/team/01-createteam.spec.ts
+++ e2e/tests/team/01-createteam.spec.ts
@@ -82,7 +82,7 @@ describe('Create team screen', () => {
 
 		it('should delete team', async () => {
 			await element(by.id('room-info-view-edit-button')).tap();
-			sleep(300);
+			await sleep(300);
 			await waitFor(element(by.id('room-info-edit-view-list')))
 				.toBeVisible()
 				.withTimeout(2000);
