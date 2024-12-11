cd4741048ed29d59dd754ac8daa0fb2e5def5af7
Diego Mello <diegolmello@gmail.com>
2024-12-11 14:24:49 -0300

    Refactored tests to cover async key exchange

--- e2e/tests/assorted/01-e2eencryption.spec.ts
+++ e2e/tests/assorted/01-e2eencryption.spec.ts
@@ -5,90 +5,16 @@ import {
 	login,
 	sleep,
 	tapBack,
-	logout,
 	platformTypes,
 	TTextMatcher,
 	tapAndWaitFor,
-	expectValidRegisterOrRetry,
 	mockMessage,
 	tryTapping,
 	navigateToRoom,
 	checkRoomTitle
 } from '../../helpers/app';
-import data from '../../data';
 import { createRandomUser, deleteCreatedUsers, IDeleteCreateUser, ITestUser } from '../../helpers/data_setup';
 import random from '../../helpers/random';
-
-const checkServer = async (server: string) => {
-	const label = `Connected to ${server}`;
-	await waitFor(element(by.id('rooms-list-view-sidebar')))
-		.toBeVisible()
-		.withTimeout(10000);
-	await element(by.id('rooms-list-view-sidebar')).tap();
-	await waitFor(element(by.id('sidebar-view')))
-		.toBeVisible()
-		.withTimeout(2000);
-	await waitFor(element(by.label(label)))
-		.toBeVisible()
-		.withTimeout(60000);
-	await element(by.id('sidebar-close-drawer')).tap();
-};
-
-const checkBanner = async () => {
-	// TODO: Assert 'Save Your Encryption Password'
-	await waitFor(element(by.id('listheader-encryption')))
-		.toExist()
-		.withTimeout(10000);
-};
-
-async function waitForToast() {
-	await sleep(300);
-}
-
-async function navigateSecurityPrivacy() {
-	await waitFor(element(by.id('rooms-list-view')))
-		.toBeVisible()
-		.withTimeout(2000);
-	await element(by.id('rooms-list-view-sidebar')).tap();
-	await waitFor(element(by.id('sidebar-view')))
-		.toBeVisible()
-		.withTimeout(2000);
-	await waitFor(element(by.id('sidebar-settings')))
-		.toBeVisible()
-		.withTimeout(2000);
-	await element(by.id('sidebar-settings')).tap();
-	await waitFor(element(by.id('settings-view')))
-		.toBeVisible()
-		.withTimeout(2000);
-	await element(by.id('settings-view-security-privacy')).tap();
-	await waitFor(element(by.id('security-privacy-view')))
-		.toBeVisible()
-		.withTimeout(2000);
-}
-
-// Prepare stuff
-// Login as UserB
-// Save e2ee password
-// change e2ee password ***
-// Login as UserA
-// Save e2ee password
-// change e2ee password ***
-
-// Create channel with UserA
-// Enviar mensagem
-// Login as UserB
-// Enviar mensagem
-
-// Login as UserA
-// Reset user key
-// Login as UserA
-// Reset room key
-// Send message
-// Login as UserB
-// Send message and read all the others
-
-// change password
-// banner stuff
 
 let alertButtonType: string;
 let textMatcher: TTextMatcher;
@@ -112,6 +38,10 @@ const loginAs = async (user: ITestUser, enterE2EEPassword = true) => {
 			.withTimeout(10000);
 	}
 };
+
+async function waitForToast() {
+	await sleep(300);
+}
 
 const navToE2EESecurity = async () => {
 	await waitFor(element(by.id('rooms-list-view')))
@@ -192,9 +122,9 @@ describe('E2E Encryption', () => {
 		await changeE2EEPassword();
 	});
 
-	// afterAll(async () => {
-	// 	await deleteCreatedUsers(deleteUsersAfterAll);
-	// });
+	afterAll(async () => {
+		await deleteCreatedUsers(deleteUsersAfterAll);
+	});
 
 	describe('Create room as UserA and send a message', () => {
 		it('should create encrypted room', async () => {
@@ -285,276 +215,58 @@ describe('E2E Encryption', () => {
 		it('should reset user E2EE key, login again and recreate keys', async () => {
 			await resetE2EEKey();
 			await loginAs(UserA, false);
+			await changeE2EEPassword();
+		});
+
+		it('should reset room E2EE key', async () => {
+			await device.launchApp({ permissions: { notifications: 'YES' } });
+			await navigateToRoom(room);
+			await waitFor(element(by.id('room-view-header-encryption')))
+				.toBeVisible()
+				.withTimeout(2000);
+			await element(by.id('room-view-header-encryption')).tap();
+			await waitFor(element(by.id('e2ee-toggle-room-view')))
+				.toBeVisible()
+				.withTimeout(2000);
+			await waitFor(element(by.id('e2ee-toggle-room-reset-key')))
+				.toBeVisible()
+				.withTimeout(2000);
+			await element(by.id('e2ee-toggle-room-reset-key')).tap();
+			await waitFor(element(by[textMatcher]('Reset encryption key')))
+				.toExist()
+				.withTimeout(2000);
+			await element(by[textMatcher]('Reset')).atIndex(0).tap();
+			await waitForToast();
+			await tapBack();
+			await checkRoomTitle(room);
+		});
+
+		it('should send message and be able to read it', async () => {
+			await mockMessage(getMessage(4));
+		});
+	});
+
+	describe('Login as UserB, accept new room key, send a message and read everything', () => {
+		beforeAll(async () => {
 			await loginAs(UserB);
 		});
 
-		// it('should reset room E2EE key', async () => {
-		// 	await navigateToRoom(room);
-		// 	// await waitFor(element(by.id('room-view-header-encryption')))
-		// 	// 	.toBeVisible()
-		// 	// 	.withTimeout(2000);
-		// 	// await element(by.id('room-view-header-encryption')).tap();
-		// 	// await waitFor(element(by.id('e2ee-toggle-room-view')))
-		// 	// 	.toBeVisible()
-		// 	// 	.withTimeout(2000);
-		// 	// await waitFor(element(by.id('e2ee-toggle-room-reset-key')))
-		// 	// 	.toBeVisible()
-		// 	// 	.withTimeout(2000);
-		// 	// await element(by.id('e2ee-toggle-room-reset-key')).tap();
-		// 	// await waitFor(element(by[textMatcher]('Reset encryption key')))
-		// 	// 	.toExist()
-		// 	// 	.withTimeout(2000);
-		// 	// await element(by[textMatcher]('Reset')).atIndex(0).tap();
-		// 	// await waitForToast();
-		// 	// await tapBack();
-		// 	// await checkRoomTitle(room);
-		// });
-
-		// it('should send message and be able to read it', async () => {
-		// 	await mockMessage(getMessage(4));
-		// });
+		it('should send message and be able to read it', async () => {
+			await navigateToRoom(room);
+			await mockMessage(getMessage(5));
+			await readMessages(4);
+		});
 	});
 
-	// describe('Security and Privacy', () => {
-	// 	it('should navigate to security privacy', async () => {
-	// 		await waitFor(element(by.id('rooms-list-view')))
-	// 			.toBeVisible()
-	// 			.withTimeout(2000);
-	// 		await element(by.id('rooms-list-view-sidebar')).tap();
-	// 		await waitFor(element(by.id('sidebar-view')))
-	// 			.toBeVisible()
-	// 			.withTimeout(2000);
-	// 		await waitFor(element(by.id('sidebar-settings')))
-	// 			.toBeVisible()
-	// 			.withTimeout(2000);
-	// 		await element(by.id('sidebar-settings')).tap();
-	// 		await waitFor(element(by.id('settings-view')))
-	// 			.toBeVisible()
-	// 			.withTimeout(2000);
-	// 		await element(by.id('settings-view-security-privacy')).tap();
-	// 		await waitFor(element(by.id('security-privacy-view')))
-	// 			.toBeVisible()
-	// 			.withTimeout(2000);
+	// describe('Login as UserA, accept new room key, send a message and read everything', () => {
+	// 	beforeAll(async () => {
+	// 		await loginAs(UserA);
 	// 	});
 
-	// 	it('render', async () => {
-	// 		await expect(element(by.id('security-privacy-view-e2e-encryption'))).toExist();
-	// 		await expect(element(by.id('security-privacy-view-screen-lock'))).toExist();
-	// 		await expect(element(by.id('security-privacy-view-analytics-events'))).toExist();
-	// 		await expect(element(by.id('security-privacy-view-crash-report'))).toExist();
+	// 	it('should send message and be able to read it', async () => {
+	// 		await navigateToRoom(room);
+	// 		await mockMessage(getMessage(6));
+	// 		await readMessages(5);
 	// 	});
 	// });
-
-	// describe('E2E Encryption Security', () => {
-	// 	it('should navigate to e2e encryption security', async () => {
-	// 		await element(by.id('security-privacy-view-e2e-encryption')).tap();
-	// 		await waitFor(element(by.id('e2e-encryption-security-view')))
-	// 			.toBeVisible()
-	// 			.withTimeout(2000);
-	// 	});
-
-	// 	describe('Render', () => {
-	// 		it('should have items', async () => {
-	// 			await waitFor(element(by.id('e2e-encryption-security-view')))
-	// 				.toBeVisible()
-	// 				.withTimeout(2000);
-	// 			await expect(element(by.id('e2e-encryption-security-view-password'))).toExist();
-	// 			await expect(element(by.id('e2e-encryption-security-view-change-password'))).toExist();
-	// 			await expect(element(by.id('e2e-encryption-security-view-reset-key'))).toExist();
-	// 		});
-	// 	});
-
-	// 	describe('Change password', () => {
-	// 		it('should change password', async () => {
-	// 			await element(by.id('e2e-encryption-security-view-password')).replaceText(newPassword);
-	// 			await element(by.id('e2e-encryption-security-view-change-password')).tap();
-	// 			await waitFor(element(by[textMatcher]('Are you sure?')))
-	// 				.toExist()
-	// 				.withTimeout(2000);
-	// 			await expect(element(by[textMatcher]("Make sure you've saved it carefully somewhere else."))).toExist();
-	// 			await element(by[textMatcher]('Yes, change it')).atIndex(0).tap();
-	// 			await waitForToast();
-	// 		});
-
-	// 		it('should navigate to the room and messages should remain decrypted', async () => {
-	// 			await waitFor(element(by.id('e2e-encryption-security-view')))
-	// 				.toBeVisible()
-	// 				.withTimeout(2000);
-	// 			await tapBack();
-	// 			await waitFor(element(by.id('security-privacy-view')))
-	// 				.toBeVisible()
-	// 				.withTimeout(2000);
-	// 			await tapBack();
-	// 			await waitFor(element(by.id('settings-view')))
-	// 				.toBeVisible()
-	// 				.withTimeout(2000);
-	// 			await element(by.id('settings-view-drawer')).tap();
-	// 			await waitFor(element(by.id('sidebar-view')))
-	// 				.toBeVisible()
-	// 				.withTimeout(2000);
-	// 			await element(by.id('sidebar-chats')).tap();
-	// 			await waitFor(element(by.id('rooms-list-view')))
-	// 				.toBeVisible()
-	// 				.withTimeout(2000);
-	// 			await navigateToRoom(room);
-	// 			await waitFor(element(by[textMatcher](mockedMessageText)).atIndex(0))
-	// 				.toExist()
-	// 				.withTimeout(2000);
-	// 		});
-
-	// 		it('should logout, login and messages should be encrypted', async () => {
-	// 			await tapBack();
-	// 			await waitFor(element(by.id('rooms-list-view')))
-	// 				.toBeVisible()
-	// 				.withTimeout(2000);
-	// 			await logout();
-	// 			await navigateToLogin();
-	// 			await login(UserA.username, UserA.password);
-	// 			await navigateToRoom(room);
-	// 			await waitFor(element(by[textMatcher](mockedMessageText)).atIndex(0))
-	// 				.not.toExist()
-	// 				.withTimeout(2000);
-	// 			await waitFor(element(by.id('room-view-encrypted-room')))
-	// 				.toBeVisible()
-	// 				.withTimeout(2000);
-	// 		});
-
-	// 		it('should enter new e2e password and messages should be decrypted', async () => {
-	// 			await tapBack();
-	// 			await waitFor(element(by.id('rooms-list-view')))
-	// 				.toBeVisible()
-	// 				.withTimeout(2000);
-	// 			// TODO: assert 'Enter E2EE Password'
-	// 			await waitFor(element(by.id('listheader-encryption')))
-	// 				.toBeVisible()
-	// 				.withTimeout(2000);
-	// 			await tapAndWaitFor(element(by.id('listheader-encryption')), element(by.id('e2e-enter-your-password-view')), 2000);
-	// 			await element(by.id('e2e-enter-your-password-view-password')).replaceText(newPassword);
-	// 			await element(by.id('e2e-enter-your-password-view-confirm')).tap();
-	// 			await waitFor(element(by.id('listheader-encryption')))
-	// 				.not.toExist()
-	// 				.withTimeout(10000);
-	// 			await navigateToRoom(room);
-	// 			await waitFor(element(by[textMatcher](mockedMessageText)).atIndex(0))
-	// 				.toExist()
-	// 				.withTimeout(2000);
-	// 		});
-	// 	});
-
-	// 	describe('Reset E2E key', () => {
-	// 		beforeAll(async () => {
-	// 			await tapBack();
-	// 			await waitFor(element(by.id('rooms-list-view')))
-	// 				.toBeVisible()
-	// 				.withTimeout(2000);
-	// 		});
-	// 		it('should reset e2e key', async () => {
-	// 			await navigateSecurityPrivacy();
-	// 			await element(by.id('security-privacy-view-e2e-encryption')).tap();
-	// 			await waitFor(element(by.id('e2e-encryption-security-view')))
-	// 				.toBeVisible()
-	// 				.withTimeout(2000);
-	// 			await element(by.id('e2e-encryption-security-view-reset-key')).tap();
-	// 			await waitFor(element(by[textMatcher]('Are you sure?')))
-	// 				.toExist()
-	// 				.withTimeout(2000);
-	// 			await expect(element(by[textMatcher]("You're going to be logged out."))).toExist();
-	// 			await element(by[textMatcher]('Yes, reset it').and(by.type(alertButtonType))).tap();
-	// 			await sleep(2000);
-
-	// 			// FIXME: The app isn't showing this alert anymore
-	// 			// await waitFor(element(by[textMatcher]("You've been logged out by the server. Please log in again.")))
-	// 			// 	.toExist()
-	// 			// 	.withTimeout(20000);
-	// 			// await element(by[textMatcher]('OK').and(by.type(alertButtonType))).tap();
-	// 			// await waitFor(element(by.id('workspace-view')))
-	// 			// 	.toBeVisible()
-	// 			// 	.withTimeout(10000);
-	// 			// await element(by.id('workspace-view-login')).tap();
-	// 			await navigateToLogin();
-	// 			await waitFor(element(by.id('login-view')))
-	// 				.toBeVisible()
-	// 				.withTimeout(2000);
-	// 			await login(UserA.username, UserA.password);
-	// 			// TODO: assert 'Save Your Encryption Password'
-	// 			await waitFor(element(by.id('listheader-encryption')))
-	// 				.toBeVisible()
-	// 				.withTimeout(5000);
-	// 		});
-	// 	});
-	// });
-
-	// describe('Persist Banner', () => {
-	// 	it('check save banner', async () => {
-	// 		await checkServer(data.server);
-	// 		await checkBanner();
-	// 	});
-
-	// 	it('should add server and create new user', async () => {
-	// 		await sleep(5000);
-	// 		await element(by.id('rooms-list-header-servers-list-button')).tap();
-	// 		await waitFor(element(by.id('rooms-list-header-servers-list')))
-	// 			.toBeVisible()
-	// 			.withTimeout(5000);
-	// 		await element(by.id('rooms-list-header-server-add')).tap();
-
-	// 		// TODO: refactor
-	// 		await waitFor(element(by.id('new-server-view')))
-	// 			.toBeVisible()
-	// 			.withTimeout(60000);
-	// 		await element(by.id('new-server-view-input')).replaceText(`${data.alternateServer}`);
-	// 		await element(by.id('new-server-view-input')).tapReturnKey();
-	// 		await waitFor(element(by.id('workspace-view')))
-	// 			.toBeVisible()
-	// 			.withTimeout(60000);
-	// 		await element(by.id('workspace-view-register')).tap();
-	// 		await waitFor(element(by.id('register-view')))
-	// 			.toBeVisible()
-	// 			.withTimeout(2000);
-
-	// 		// Register new user
-	// 		const randomUser = data.randomUser();
-	// 		await element(by.id('register-view-name')).replaceText(randomUser.name);
-	// 		await element(by.id('register-view-name')).tapReturnKey();
-	// 		await element(by.id('register-view-username')).replaceText(randomUser.username);
-	// 		await element(by.id('register-view-username')).tapReturnKey();
-	// 		await element(by.id('register-view-email')).replaceText(randomUser.email);
-	// 		await element(by.id('register-view-email')).tapReturnKey();
-	// 		await element(by.id('register-view-password')).replaceText(randomUser.password);
-	// 		await element(by.id('register-view-password')).tapReturnKey();
-	// 		await expectValidRegisterOrRetry(device.getPlatform());
-	// 		deleteUsersAfterAll.push({ server: data.alternateServer, username: randomUser.username });
-
-	// 		await checkServer(data.alternateServer);
-	// 	});
-
-	// 	it('should change back', async () => {
-	// 		await waitFor(element(by.id('rooms-list-header-servers-list-button')))
-	// 			.toExist()
-	// 			.withTimeout(2000);
-	// 		await element(by.id('rooms-list-header-servers-list-button')).tap();
-	// 		await waitFor(element(by.id('rooms-list-header-servers-list')))
-	// 			.toBeVisible()
-	// 			.withTimeout(5000);
-	// 		await element(by.id(`server-item-${data.server}`)).tap();
-	// 		await waitFor(element(by.id('rooms-list-view')))
-	// 			.toBeVisible()
-	// 			.withTimeout(10000);
-	// 		await checkServer(data.server);
-	// 		await checkBanner();
-	// 	});
-
-	// 	it('should reopen the app and have banner', async () => {
-	// 		await device.launchApp({
-	// 			permissions: { notifications: 'YES' },
-	// 			newInstance: true
-	// 		});
-	// 		await waitFor(element(by.id('rooms-list-view')))
-	// 			.toBeVisible()
-	// 			.withTimeout(10000);
-	// 		await checkBanner();
-	// 	});
-	// });
-
-	// // TODO: missing request e2ee room key
 });
