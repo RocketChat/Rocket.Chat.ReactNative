85b4578c8207d6f1ca67372bac7eb08d1c649fcb
OtavioStasiak <otaviostasiakmusic@outlook.com>
2024-12-12 18:44:35 -0300

    chore: updated textInput

--- app/containers/TextInput/FormTextInput.tsx
+++ app/containers/TextInput/FormTextInput.tsx
@@ -1,7 +1,8 @@
-import { BottomSheetTextInput } from '@discord/bottom-sheet';
 import React, { useState } from 'react';
 import { StyleProp, StyleSheet, Text, TextInput as RNTextInput, TextInputProps, TextStyle, View, ViewStyle } from 'react-native';
+import { BottomSheetTextInput } from '@discord/bottom-sheet';
 import Touchable from 'react-native-platform-touchable';
+import { A11y } from 'react-native-a11y-order';
 
 import i18n from '../../i18n';
 import { useTheme } from '../../theme';
@@ -9,6 +10,7 @@ import sharedStyles from '../../views/Styles';
 import ActivityIndicator from '../ActivityIndicator';
 import { CustomIcon, TIconsName } from '../CustomIcon';
 import { TextInput } from './TextInput';
+import { isIOS } from '../../lib/methods/helpers';
 
 const styles = StyleSheet.create({
 	error: {
@@ -97,99 +99,109 @@ export const FormTextInput = ({
 	const [showPassword, setShowPassword] = useState(false);
 	const showClearInput = onClearInput && value && value.length > 0;
 	const Input = bottomSheet ? BottomSheetTextInput : TextInput;
+	const accessibilityLabelRequired = required ? `, ${i18n.t('Required')}` : '';
+	const accessibilityInputValue = (!secureTextEntry && value && isIOS) || showPassword ? `, ${value}` : '';
 	return (
-		<View
-			accessible
-			accessibilityLabel={`${label} - ${required ? i18n.t('Required') : ''}`}
-			style={[styles.inputContainer, containerStyle]}>
-			{label ? (
-				<Text style={[styles.label, { color: colors.fontTitlesLabels }, error?.error && { color: colors.fontDanger }]}>
-					{label}{' '}
-					{required && <Text style={[styles.required, { color: colors.fontSecondaryInfo }]}>{`(${i18n.t('Required')})`}</Text>}
-				</Text>
-			) : null}
+		<A11y.Order>
+			<A11y.Index index={1}>
+				<View
+					accessible
+					accessibilityLabel={accessibilityLabel ?? `${label}${accessibilityLabelRequired}${accessibilityInputValue}`}
+					style={[styles.inputContainer, containerStyle]}>
+					{label ? (
+						<Text style={[styles.label, { color: colors.fontTitlesLabels }, error?.error && { color: colors.fontDanger }]}>
+							{label}{' '}
+							{required && (
+								<Text style={[styles.required, { color: colors.fontSecondaryInfo }]}>{`(${i18n.t('Required')})`}</Text>
+							)}
+						</Text>
+					) : null}
 
-			<View style={styles.wrap}>
-				<Input
-					style={[
-						styles.input,
-						iconLeft && styles.inputIconLeft,
-						(secureTextEntry || iconRight || showClearInput) && styles.inputIconRight,
-						{
-							backgroundColor: colors.surfaceRoom,
-							borderColor: colors.strokeLight,
-							color: colors.fontTitlesLabels
-						},
-						error?.error && {
-							color: colors.buttonBackgroundDangerDefault,
-							borderColor: colors.buttonBackgroundDangerDefault
-						},
-						inputStyle
-					]}
-					// @ts-ignore ref error
-					ref={inputRef}
-					autoCorrect={false}
-					autoCapitalize='none'
-					underlineColorAndroid='transparent'
-					secureTextEntry={secureTextEntry && !showPassword}
-					testID={testID}
-					placeholder={placeholder}
-					value={value}
-					placeholderTextColor={colors.fontAnnotation}
-					{...inputProps}
-				/>
-
-				{iconLeft ? (
-					<CustomIcon
-						name={iconLeft}
-						testID={testID ? `${testID}-icon-left` : undefined}
-						size={20}
-						color={colors.fontSecondaryInfo}
-						style={[styles.iconContainer, styles.iconLeft]}
-					/>
-				) : null}
-
-				{showClearInput ? (
-					<Touchable onPress={onClearInput} style={[styles.iconContainer, styles.iconRight]} testID='clear-text-input'>
-						<CustomIcon name='input-clear' size={20} color={colors.fontDefault} />
-					</Touchable>
-				) : null}
-
-				{iconRight && !showClearInput ? (
-					<CustomIcon
-						name={iconRight}
-						testID={testID ? `${testID}-icon-right` : undefined}
-						size={20}
-						color={colors.fontDefault}
-						style={[styles.iconContainer, styles.iconRight]}
-						accessible={false}
-					/>
-				) : null}
-
-				{secureTextEntry ? (
-					<Touchable
-						accessibilityLabel={showPassword ? i18n.t('Hide-Password') : i18n.t('Show-Password')}
-						onPress={() => setShowPassword(!showPassword)}
-						style={[styles.iconContainer, styles.iconRight]}>
-						<CustomIcon
-							name={showPassword ? 'unread-on-top' : 'unread-on-top-disabled'}
-							testID={testID ? `${testID}-icon-password` : undefined}
-							size={20}
-							color={colors.fontDefault}
+					<View accessible style={styles.wrap}>
+						<Input
+							style={[
+								styles.input,
+								iconLeft && styles.inputIconLeft,
+								(secureTextEntry || iconRight || showClearInput) && styles.inputIconRight,
+								{
+									backgroundColor: colors.surfaceRoom,
+									borderColor: colors.strokeLight,
+									color: colors.fontTitlesLabels
+								},
+								error?.error && {
+									color: colors.buttonBackgroundDangerDefault,
+									borderColor: colors.buttonBackgroundDangerDefault
+								},
+								inputStyle
+							]}
+							// @ts-ignore ref error
+							ref={inputRef}
+							autoCorrect={false}
+							autoCapitalize='none'
+							underlineColorAndroid='transparent'
+							secureTextEntry={secureTextEntry && !showPassword}
+							testID={testID}
+							placeholder={placeholder}
+							value={value}
+							placeholderTextColor={colors.fontAnnotation}
+							{...inputProps}
 						/>
-					</Touchable>
-				) : null}
 
-				{loading ? (
-					<ActivityIndicator
-						style={[styles.iconContainer, styles.iconRight]}
-						color={colors.fontDefault}
-						testID={testID ? `${testID}-loading` : undefined}
-					/>
-				) : null}
-				{left}
-			</View>
-			{error && error.reason ? <Text style={[styles.error, { color: colors.fontDanger }]}>{error.reason}</Text> : null}
-		</View>
+						{iconLeft ? (
+							<CustomIcon
+								name={iconLeft}
+								testID={testID ? `${testID}-icon-left` : undefined}
+								size={20}
+								color={colors.fontSecondaryInfo}
+								style={[styles.iconContainer, styles.iconLeft]}
+							/>
+						) : null}
+
+						{showClearInput ? (
+							<Touchable onPress={onClearInput} style={[styles.iconContainer, styles.iconRight]} testID='clear-text-input'>
+								<CustomIcon name='input-clear' size={20} color={colors.fontDefault} />
+							</Touchable>
+						) : null}
+
+						{iconRight && !showClearInput ? (
+							<CustomIcon
+								name={iconRight}
+								testID={testID ? `${testID}-icon-right` : undefined}
+								size={20}
+								color={colors.fontDefault}
+								style={[styles.iconContainer, styles.iconRight]}
+								accessible={false}
+							/>
+						) : null}
+
+						{secureTextEntry ? (
+							<A11y.Index style={[styles.iconContainer, styles.iconRight]} index={2}>
+								<Touchable
+									accessible
+									accessibilityLabel={showPassword ? i18n.t('Hide_Password') : i18n.t('Show_Password')}
+									onPress={() => setShowPassword(!showPassword)}>
+									<CustomIcon
+										name={showPassword ? 'unread-on-top' : 'unread-on-top-disabled'}
+										testID={testID ? `${testID}-icon-password` : undefined}
+										size={20}
+										color={colors.fontDefault}
+									/>
+								</Touchable>
+							</A11y.Index>
+						) : null}
+
+						{loading ? (
+							<ActivityIndicator
+								style={[styles.iconContainer, styles.iconRight]}
+								color={colors.fontDefault}
+								testID={testID ? `${testID}-loading` : undefined}
+							/>
+						) : null}
+						{left}
+					</View>
+					{error && error.reason ? <Text style={[styles.error, { color: colors.fontDanger }]}>{error.reason}</Text> : null}
+				</View>
+			</A11y.Index>
+		</A11y.Order>
 	);
 };
