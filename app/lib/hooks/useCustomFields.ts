f56d0ecc9ed28171469318ecd363f1ee6bbd62ac
OtavioStasiak <otaviostasiakmusic@outlook.com>
2025-01-09 15:19:24 -0300

    fix: type useCustomFields

--- app/lib/hooks/useCustomFields.ts
+++ app/lib/hooks/useCustomFields.ts
@@ -2,7 +2,30 @@ import { useMemo } from 'react';
 
 import log from '../methods/helpers/log';
 
-const useParsedCustomFields: any = (Accounts_CustomFields: string) => {
+interface IModifyRecordField {
+	array: boolean;
+	field: string;
+}
+
+interface ICustomField {
+	type: 'select' | 'text';
+	required: boolean;
+	defaultValue?: string;
+	options?: string[];
+	minLength?: number;
+	maxLength?: number;
+	modifyRecordField?: IModifyRecordField;
+}
+
+interface IParsedCustomFields {
+	[key: string]: ICustomField;
+}
+
+interface IUseParsedCustomFields {
+	(Accounts_CustomFields: string): { parsedCustomFields: IParsedCustomFields };
+}
+
+const useParsedCustomFields: IUseParsedCustomFields = (Accounts_CustomFields: string) => {
 	const parsedCustomFields = useMemo(() => {
 		let parsed = {};
 		if (Accounts_CustomFields) {
