3d8e86e0b13d9677a6c2cbb65545470719b16574
OtavioStasiak <otaviostasiakmusic@outlook.com>
2025-01-30 13:25:03 -0300

    chore: updated read receipt icon and color

--- app/containers/message/Components/RightIcons/ReadReceipt.tsx
+++ app/containers/message/Components/RightIcons/ReadReceipt.tsx
@@ -6,15 +6,11 @@ import { useTheme } from '../../../../theme';
 
 const ReadReceipt = React.memo(({ isReadReceiptEnabled, unread }: { isReadReceiptEnabled?: boolean; unread?: boolean }) => {
 	const { colors } = useTheme();
+	const iconName = !unread && unread !== null ? 'check-double' : 'check';
+	const iconColor = !unread && unread !== null ? colors.fontInfo : colors.fontAnnotation;
+
 	if (isReadReceiptEnabled) {
-		return (
-			<CustomIcon
-				name='check'
-				color={!unread && unread !== null ? colors.badgeBackgroundLevel2 : colors.fontHint}
-				size={16}
-				style={styles.rightIcons}
-			/>
-		);
+		return <CustomIcon name={iconName} color={iconColor} size={16} style={styles.rightIcons} />;
 	}
 	return null;
 });
