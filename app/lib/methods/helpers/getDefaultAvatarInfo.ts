8fe1fb592a7c09dac697366077fadb56cefd1482
OtavioStasiak <otaviostasiakmusic@outlook.com>
2025-01-31 18:05:59 -0300

    fix: lint

--- app/lib/methods/helpers/getDefaultAvatarInfo.ts
+++ app/lib/methods/helpers/getDefaultAvatarInfo.ts
@@ -1,6 +1,6 @@
-import { useAppSelector } from '../../../lib/hooks';
+import { store } from '../../store/auxStore';
 
-// borrowed from https://github.com/RocketChat/Rocket.Chat/blob/a4f2102af1c2e875c60cafebd0163105bdaca678/apps/meteor/server/routes/avatar/utils.ts#L133
+// Borrowed from https://github.com/RocketChat/Rocket.Chat/blob/a4f2102af1c2e875c60cafebd0163105bdaca678/apps/meteor/server/routes/avatar/utils.ts#L133
 
 const colors = ['#158D65', '#7F1B9F', '#B68D00', '#E26D0E', '#10529E', '#6C727A'];
 const MAX_SVG_AVATAR_INITIALS = 3;
@@ -14,7 +14,8 @@ const getInitials = (name: string) => name.split(' ').slice(0, MAX_SVG_AVATAR_IN
 
 export const getDefaultAvatarInfo = (username: string | undefined) => {
 	if (!username) return;
-	const { UI_Use_Name_Avatar } = useAppSelector(state => state.settings);
+	const { UI_Use_Name_Avatar } = store.getState().settings;
+
 	return {
 		backgroundColor: getAvatarBackgroundColor(username),
 		firstLetter: !UI_Use_Name_Avatar ? getAvatarFirstLetter(username) : getInitials(username),
