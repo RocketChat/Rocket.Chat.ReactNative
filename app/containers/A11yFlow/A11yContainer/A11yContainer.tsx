5b573bb66d9fcb6c55ac53047a5e7ed0892b4591
OtavioStasiak <otaviostasiakmusic@outlook.com>
2025-02-03 14:07:03 -0300

    fix: android a11yElement using view

--- app/containers/A11yFlow/A11yContainer/A11yContainer.tsx
+++ app/containers/A11yFlow/A11yContainer/A11yContainer.tsx
@@ -2,6 +2,6 @@ import { ViewProps } from 'react-native';
 
 interface IA11yContainer extends ViewProps {}
 
-const A11yContainer = ({ children }: IA11yContainer) => <>{children}</>;
+const A11yContainer = ({ children }: IA11yContainer) => children;
 
 export default A11yContainer;
