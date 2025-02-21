bfa82b3c4ab7c6da573f708a9f3d08bd2771b921
Diego Mello <diegolmello@gmail.com>
2025-02-21 13:53:24 -0300

    Stop fetching permissions for iOS library

--- app/lib/methods/helpers/ImagePicker/getPermissions.ts
+++ app/lib/methods/helpers/ImagePicker/getPermissions.ts
@@ -1,8 +1,14 @@
 import { Linking } from 'react-native';
 
 import ImagePicker from './ImagePicker';
+import { isIOS } from '../deviceInfo';
 
 export const getPermissions = async (type: 'camera' | 'library') => {
+	// Doesn't need permission to read
+	if (isIOS && type === 'library') {
+		return Promise.resolve();
+	}
+
 	const method = type === 'camera' ? 'requestCameraPermissionsAsync' : 'requestMediaLibraryPermissionsAsync';
 	const requestResult = await ImagePicker[method]();
 	if (!requestResult.canAskAgain) {
