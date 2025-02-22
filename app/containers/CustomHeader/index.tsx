7df5c86bf60c5fe52cc962a02e634484c47ba082
OtavioStasiak <otaviostasiakmusic@outlook.com>
2025-02-22 00:41:50 -0300

    chore: created customHeader component

---
+++ app/containers/CustomHeader/index.tsx
@@ -0,0 +1,61 @@
+import React, { ReactNode } from 'react';
+import { Text } from 'react-native';
+import { NativeStackHeaderProps } from '@react-navigation/native-stack';
+import { HeaderBackButton } from '@react-navigation/elements';
+
+import HeaderContainer from '../HeaderContainer';
+import { useAppSelector } from '../../lib/hooks';
+import { useTheme } from '../../theme';
+import { styles } from './styles';
+
+interface IHeaderTitle {
+	headerTitle?: string | ((props: { children: string; tintColor?: string }) => ReactNode);
+}
+
+const HeaderTitle = ({ headerTitle }: IHeaderTitle) => {
+	const { colors } = useTheme();
+	if (!headerTitle) return null;
+	if (typeof headerTitle === 'string') {
+		return (
+			<Text
+				style={{
+					...styles.title,
+					color: colors.fontTitlesLabels
+				}}>
+				{headerTitle}
+			</Text>
+		);
+	}
+
+	return headerTitle({ children: '', tintColor: colors.fontTitlesLabels });
+};
+
+interface IHeader extends NativeStackHeaderProps {}
+
+const CustomHeader = ({ options, navigation, route }: IHeader) => {
+	const { headerLeft, headerTitle, headerRight, title } = options;
+	const { colors } = useTheme();
+	const isMasterDetail = useAppSelector(state => state.app.isMasterDetail);
+
+	const isRoomViewMasterDetail = isMasterDetail && route.name !== 'RoomView' && route.name !== 'RoomsListView';
+
+	return (
+		<HeaderContainer increaseStatusBarPadding={!isRoomViewMasterDetail}>
+			{!!headerLeft ? (
+				headerLeft({ canGoBack: false })
+			) : (
+				<HeaderBackButton
+					labelVisible={false}
+					onPress={() => navigation.goBack()}
+					tintColor={colors.fontDefault}
+					testID='header-back'
+					style={styles.headerBackButton}
+				/>
+			)}
+			<HeaderTitle headerTitle={headerTitle ?? title} />
+			{!!headerRight ? headerRight({ canGoBack: false }) : <></>}
+		</HeaderContainer>
+	);
+};
+
+export default CustomHeader;
