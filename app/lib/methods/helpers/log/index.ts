2b0c253a84e628cdf7fd17e8e8d8a72f86f7d451
Diego Mello <diegolmello@gmail.com>
2025-03-06 14:05:24 -0300

    Remove most references

--- app/lib/methods/helpers/log/index.ts
+++ app/lib/methods/helpers/log/index.ts
@@ -1,32 +1,29 @@
 import firebaseAnalytics from '@react-native-firebase/analytics';
 
-import { isFDroidBuild } from '../../../constants/environment';
 import events from './events';
 
 const analytics = firebaseAnalytics || '';
 let bugsnag: any = '';
-let crashlytics: any;
 let reportCrashErrors = true;
 let reportAnalyticsEvents = true;
 
 export const getReportCrashErrorsValue = (): boolean => reportCrashErrors;
 export const getReportAnalyticsEventsValue = (): boolean => reportAnalyticsEvents;
 
-if (!isFDroidBuild) {
-	bugsnag = require('@bugsnag/react-native').default;
-	bugsnag.start({
-		onBreadcrumb() {
-			return reportAnalyticsEvents;
-		},
-		onError(error: { breadcrumbs: string[] }) {
-			if (!reportAnalyticsEvents) {
-				error.breadcrumbs = [];
-			}
-			return reportCrashErrors;
+bugsnag = require('@bugsnag/react-native').default;
+
+bugsnag.start({
+	onBreadcrumb() {
+		return reportAnalyticsEvents;
+	},
+	onError(error: { breadcrumbs: string[] }) {
+		if (!reportAnalyticsEvents) {
+			error.breadcrumbs = [];
 		}
-	});
-	crashlytics = require('@react-native-firebase/crashlytics').default;
-}
+		return reportCrashErrors;
+	}
+});
+const crashlytics = require('@react-native-firebase/crashlytics').default;
 
 export { analytics };
 export const loggerConfig = bugsnag.config;
@@ -42,20 +39,16 @@ export const logServerVersion = (serverVersion: string): void => {
 
 export const logEvent = (eventName: string, payload?: { [key: string]: any }): void => {
 	try {
-		if (!isFDroidBuild) {
-			analytics().logEvent(eventName, payload);
-			bugsnag.leaveBreadcrumb(eventName, payload);
-		}
+		analytics().logEvent(eventName, payload);
+		bugsnag.leaveBreadcrumb(eventName, payload);
 	} catch {
 		// Do nothing
 	}
 };
 
 export const setCurrentScreen = (currentScreen: string): void => {
-	if (!isFDroidBuild) {
-		analytics().logScreenView({ screen_class: currentScreen, screen_name: currentScreen });
-		bugsnag.leaveBreadcrumb(currentScreen, { type: 'navigation' });
-	}
+	analytics().logScreenView({ screen_class: currentScreen, screen_name: currentScreen });
+	bugsnag.leaveBreadcrumb(currentScreen, { type: 'navigation' });
 };
 
 export const toggleCrashErrorsReport = (value: boolean): boolean => {
@@ -73,9 +66,7 @@ export default (e: any): void => {
 		bugsnag.notify(e, (event: { addMetadata: (arg0: string, arg1: {}) => void }) => {
 			event.addMetadata('details', { ...metadata });
 		});
-		if (!isFDroidBuild) {
-			crashlytics().recordError(e);
-		}
+		crashlytics().recordError(e);
 	} else {
 		console.error(e);
 	}
