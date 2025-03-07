83558e24161e1f62c49560ecf96d8211551aa2ce
Diego Mello <diegolmello@gmail.com>
2025-03-07 16:25:29 -0300

    Auto link firebase libs

--- react-native.config.js
+++ react-native.config.js
@@ -1,20 +1,5 @@
 module.exports = {
 	dependencies: {
-		'@react-native-firebase/app': {
-			platforms: {
-				android: null
-			}
-		},
-		'@react-native-firebase/analytics': {
-			platforms: {
-				android: null
-			}
-		},
-		'@react-native-firebase/crashlytics': {
-			platforms: {
-				android: null
-			}
-		},
 		'@react-native-firebase/messaging': {
 			platforms: {
 				ios: null
