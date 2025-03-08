2009491f353aff93a94e2c77386f85b8ba826fd6
jjinendra3 <jjinendra3@gmail.com>
2025-03-08 11:20:56 +0530

    made subscription reponsive

--- app/containers/MessageComposer/hooks/useSubscription.ts
+++ app/containers/MessageComposer/hooks/useSubscription.ts
@@ -4,19 +4,30 @@ import { getSubscriptionByRoomId } from '../../../lib/database/services/Subscrip
 import { ISubscription } from '../../../definitions';
 
 // TODO: Not reactive. Should we work on an official version?
+// Please let me know if you'd like me to remove the above comment?
+
 export const useSubscription = (rid?: string) => {
 	const [subscription, setSubscription] = useState<ISubscription>();
+
 	useEffect(() => {
+		// eslint-disable-next-line no-undef
+		let interval: NodeJS.Timeout;
 		const loadRoom = async () => {
 			if (!rid) {
-				return setSubscription(undefined);
+				setSubscription(undefined);
+				return;
 			}
 			const result = await getSubscriptionByRoomId(rid);
 			if (result) {
 				setSubscription(result);
 			}
 		};
+
 		loadRoom();
+		if (rid) {
+			interval = setInterval(loadRoom, 50);
+		}
+		return () => clearInterval(interval);
 	}, [rid]);
 
 	return subscription;
