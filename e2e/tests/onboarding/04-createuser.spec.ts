9f9a5810baecc0fd00f944cf047d51e5c46d4d3e
OtavioStasiak <otaviostasiakmusic@outlook.com>
2025-03-10 13:19:17 -0300

    fix: e2e tests

--- e2e/tests/onboarding/04-createuser.spec.ts
+++ e2e/tests/onboarding/04-createuser.spec.ts
@@ -20,7 +20,9 @@ describe('Create user screen', () => {
 			await element(by.id('register-view-password')).replaceText(user.password);
 			await element(by.id('register-view-password')).tapReturnKey();
 			await element(by.id('register-view-confirm-password')).replaceText(user.password);
-			await element(by.id('register-view-confirm-password')).tapReturnKey();
+			await element(by.id('register-view')).swipe('down', 'fast');
+			await element(by.id('register-view')).swipe('up', 'fast');
+			await element(by.id('register-view-submit')).tap();
 
 			await expectValidRegisterOrRetry(device.getPlatform());
 		});
