8899ed7acc3c4f478f3f51a0ea7c61a15ed82e98
Diego Mello <diegolmello@gmail.com>
2025-03-13 13:21:12 -0300

    chore: Remove F-Droid flavor (#6209)

--- android/app/src/main/java/chat/rocket/reactnative/MainApplication.kt
+++ android/app/src/main/java/chat/rocket/reactnative/MainApplication.kt
@@ -1,6 +1,9 @@
 package chat.rocket.reactnative
 
 import android.app.Application
+import android.content.Context
+import android.content.res.Configuration
+import android.os.Bundle
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
@@ -9,18 +12,21 @@ import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.react.flipper.ReactNativeFlipper
 import com.facebook.soloader.SoLoader
 import com.nozbe.watermelondb.jsi.WatermelonDBJSIPackage;
 import com.facebook.react.bridge.JSIModulePackage;
-
+import com.wix.reactnativenotifications.core.AppLaunchHelper
+import com.wix.reactnativenotifications.core.AppLifecycleFacade
+import com.wix.reactnativenotifications.core.JsIOHelper
+import com.wix.reactnativenotifications.core.notification.INotificationsApplication
+import com.wix.reactnativenotifications.core.notification.IPushNotification
+import com.bugsnag.android.Bugsnag
 import expo.modules.ApplicationLifecycleDispatcher
-import expo.modules.ReactNativeHostWrapper
 import chat.rocket.reactnative.networking.SSLPinningPackage;
+import chat.rocket.reactnative.notification.CustomPushNotification;
 import com.reactnativecommunity.viewpager.RNCViewPagerPackage;
-import android.content.res.Configuration;
 
-open class MainApplication : Application(), ReactApplication {
+open class MainApplication : Application(), ReactApplication, INotificationsApplication {
 
   override val reactNativeHost: ReactNativeHost =
       object : DefaultReactNativeHost(this) {
@@ -29,7 +35,6 @@ open class MainApplication : Application(), ReactApplication {
               // Packages that cannot be autolinked yet can be added manually here, for example:
               add(RNCViewPagerPackage())
               add(SSLPinningPackage())
-              addAll(AdditionalModules().getAdditionalModules())
             }
 
         override fun getJSIModulePackage(): JSIModulePackage {
@@ -50,6 +55,7 @@ open class MainApplication : Application(), ReactApplication {
   override fun onCreate() {
     super.onCreate()
     SoLoader.init(this, false)
+    Bugsnag.start(this)
 
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
@@ -61,5 +67,20 @@ open class MainApplication : Application(), ReactApplication {
 	override fun onConfigurationChanged(newConfig: Configuration) {
     super.onConfigurationChanged(newConfig)
     ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig)
+  }
+
+  override fun getPushNotification(
+    context: Context,
+    bundle: Bundle,
+    defaultFacade: AppLifecycleFacade,
+    defaultAppLaunchHelper: AppLaunchHelper
+  ): IPushNotification {
+    return CustomPushNotification(
+      context,
+      bundle,
+      defaultFacade,
+      defaultAppLaunchHelper,
+      JsIOHelper()
+    )
   }
 }
