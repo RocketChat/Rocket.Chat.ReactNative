f5df1851c82a05944eb55c77593a6bf705280a7e
jasim0021 <skjasimuddin9064@gmail.com>
2025-03-23 21:53:42 +0530

    remove comments

--- app/containers/CustomHeader/components/HeaderTitle/index.tsx
+++ app/containers/CustomHeader/components/HeaderTitle/index.tsx
@@ -7,7 +7,7 @@ import { styles } from './styles';
 
 interface IHeaderTitle {
 	headerTitle?: string | ((props: { children: string; tintColor?: string }) => ReactNode);
-	style?: StyleProp<ViewStyle | TextStyle>; // Specify the type for style as an object
+	style?: StyleProp<ViewStyle | TextStyle>;
 }
 const HeaderTitle = ({ headerTitle, style }: IHeaderTitle) => {
 	const { colors } = useTheme();
