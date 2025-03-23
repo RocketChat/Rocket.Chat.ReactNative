3519de8eb17e83f5787ad9d4403cc5ab92e89a6b
jasim0021 <skjasimuddin9064@gmail.com>
2025-03-23 21:34:22 +0530

    markdown changes revert

--- app/containers/CustomHeader/components/HeaderTitle/index.tsx
+++ app/containers/CustomHeader/components/HeaderTitle/index.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from 'react';
-import { Text, View } from 'react-native';
+import { Text, TextStyle, View } from 'react-native';
 
 import { isAndroid } from '../../../../lib/methods/helpers';
 import { useTheme } from '../../../../theme';
@@ -7,7 +7,7 @@ import { styles } from './styles';
 
 interface IHeaderTitle {
 	headerTitle?: string | ((props: { children: string; tintColor?: string }) => ReactNode);
-	style?: object; // Specify the type for style as an object
+	style?: TextStyle | object; // Specify the type for style as an object
 }
 const HeaderTitle = ({ headerTitle, style }: IHeaderTitle) => {
 	const { colors } = useTheme();
