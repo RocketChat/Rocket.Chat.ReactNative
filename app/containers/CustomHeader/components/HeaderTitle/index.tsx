463b739b7dd7a7a5fff3943adcc396bb0bc2465d
jasim0021 <skjasimuddin9064@gmail.com>
2025-03-25 15:45:47 +0530

    feature request: iOS Quick Action to request feedback #6227

--- app/containers/CustomHeader/components/HeaderTitle/index.tsx
+++ app/containers/CustomHeader/components/HeaderTitle/index.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from 'react';
-import { Text, TextStyle, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 import { isAndroid } from '../../../../lib/methods/helpers';
 import { useTheme } from '../../../../theme';
@@ -7,9 +7,9 @@ import { styles } from './styles';
 
 interface IHeaderTitle {
 	headerTitle?: string | ((props: { children: string; tintColor?: string }) => ReactNode);
-	style?: TextStyle; // Specify the type for style as an object
 }
-const HeaderTitle = ({ headerTitle, style }: IHeaderTitle) => {
+
+const HeaderTitle = ({ headerTitle }: IHeaderTitle) => {
 	const { colors } = useTheme();
 	if (!headerTitle) {
 		return null;
@@ -18,14 +18,24 @@ const HeaderTitle = ({ headerTitle, style }: IHeaderTitle) => {
 	if (typeof headerTitle === 'string') {
 		if (isAndroid) {
 			return (
-				<Text numberOfLines={1} style={[style || styles.androidTitle, { color: colors.fontTitlesLabels }]}>
+				<Text
+					numberOfLines={1}
+					style={{
+						...styles.androidTitle,
+						color: colors.fontTitlesLabels
+					}}>
 					{headerTitle}
 				</Text>
 			);
 		}
 		return (
-			<View style={[styles.headerTitleContainer]}>
-				<Text numberOfLines={1} style={[style || styles.androidTitle, { color: colors.fontTitlesLabels }]}>
+			<View style={styles.headerTitleContainer}>
+				<Text
+					numberOfLines={1}
+					style={{
+						...styles.title,
+						color: colors.fontTitlesLabels
+					}}>
 					{headerTitle}
 				</Text>
 			</View>
