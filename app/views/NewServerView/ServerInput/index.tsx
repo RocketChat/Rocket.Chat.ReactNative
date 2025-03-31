164b7950251a3a425da9827ec8245e05a8f063df
OtavioStasiak <otaviostasiakmusic@outlook.com>
2025-03-31 19:28:24 -0300

    feat: adjusted new server input layout

--- app/views/NewServerView/ServerInput/index.tsx
+++ app/views/NewServerView/ServerInput/index.tsx
@@ -1,30 +1,22 @@
-import React, { useState } from 'react';
-import { FlatList, StyleSheet, TextInputProps, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, TextInputProps, View } from 'react-native';
 
 import { useTheme } from '../../../theme';
 import { FormTextInput } from '../../../containers/TextInput';
 import { TServerHistoryModel } from '../../../definitions';
-import * as List from '../../../containers/List';
 import I18n from '../../../i18n';
-import Item from './Item';
+import { CustomIcon } from '../../../containers/CustomIcon';
+import Touch from '../../../containers/Touch';
 
 const styles = StyleSheet.create({
 	container: {
-		zIndex: 1
+		flexDirection: 'row',
+		alignItems: 'flex-end',
+		gap: 4
 	},
 	inputContainer: {
 		marginTop: 0,
 		marginBottom: 0
-	},
-	serverHistory: {
-		maxHeight: 180,
-		width: '100%',
-		top: '100%',
-		zIndex: 1,
-		position: 'absolute',
-		borderWidth: StyleSheet.hairlineWidth,
-		borderRadius: 4,
-		borderTopWidth: 0
 	}
 });
 
@@ -44,35 +36,30 @@ const ServerInput = ({
 	onDelete,
 	onPressServerHistory
 }: IServerInput): JSX.Element => {
-	const [focused, setFocused] = useState(false);
 	const { colors } = useTheme();
 	return (
 		<View style={styles.container}>
-			<FormTextInput
-				label={I18n.t('Workspace_URL')}
-				containerStyle={styles.inputContainer}
-				value={text}
-				returnKeyType='send'
-				onChangeText={onChangeText}
-				testID='new-server-view-input'
-				onSubmitEditing={onSubmit}
-				clearButtonMode='while-editing'
-				keyboardType='url'
-				textContentType='URL'
-				required
-				onFocus={() => setFocused(true)}
-				onBlur={() => setFocused(false)}
-			/>
-			{focused && serversHistory?.length ? (
-				<View style={[styles.serverHistory, { backgroundColor: colors.surfaceRoom, borderColor: colors.strokeLight }]}>
-					<FlatList
-						data={serversHistory}
-						renderItem={({ item }) => <Item item={item} onPress={() => onPressServerHistory(item)} onDelete={onDelete} />}
-						ItemSeparatorComponent={List.Separator}
-						keyExtractor={item => item.id}
-					/>
-				</View>
-			) : null}
+			<View style={{ flex: 1 }}>
+				<FormTextInput
+					label={I18n.t('Workspace_URL')}
+					containerStyle={styles.inputContainer}
+					value={text}
+					returnKeyType='send'
+					onChangeText={onChangeText}
+					testID='new-server-view-input'
+					onSubmitEditing={onSubmit}
+					clearButtonMode='while-editing'
+					keyboardType='url'
+					textContentType='URL'
+					required
+				/>
+			</View>
+
+			<View style={{ width: 32, paddingVertical: 9 }}>
+				<Touch>
+					<CustomIcon name='clock' size={32} color={colors.fontInfo} />
+				</Touch>
+			</View>
 		</View>
 	);
 };
