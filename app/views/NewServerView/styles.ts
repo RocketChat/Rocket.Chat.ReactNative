770f2d924c845f57de9eba121d1d38e03979276b
Otávio Stasiak <91474186+OtavioStasiak@users.noreply.github.com>
2025-04-14 11:08:17 -0300

    chore: migrate NewServerView to hooks (#5919)
    * chore: migrate NewServerView to hooks
    
    * chore: updated types and migrate to useAppSelector
    
    * chore: created hooks and separated components
    
    * chore: created useConnectServer hook
    
    * minor changes
    
    * minor changes
    
    * minor changes
    
    * chore: adjusted code
    
    * fix: selectServerRequest missing version
    
    * chore: improved code
    
    * fix: removed empty spaces
    
    * fix: test
    
    * fix: unused useEffect deps
    
    * fix: certificate picker style and improved code
    
    * fix: comment
    
    * fix: improved code
    
    * fix: code adjustments on certificate picker
    
    * fix: added style to the component file
    
    * fix: added previouServer on useEffect deps to prevent header flickering
    
    * fix: certificate picker style
    
    * fix: added useDebounce on queryServerHistory

---
+++ app/views/NewServerView/styles.ts
@@ -0,0 +1,28 @@
+import { StyleSheet } from 'react-native';
+
+import sharedStyles from '../Styles';
+
+const styles = StyleSheet.create({
+	onboardingImage: {
+		alignSelf: 'center',
+		width: 250,
+		height: 50,
+		marginBottom: 32
+	},
+	title: {
+		...sharedStyles.textBold,
+		fontSize: 24,
+		lineHeight: 36,
+		marginBottom: 24
+	},
+	buttonPrompt: {
+		...sharedStyles.textRegular,
+		textAlign: 'center',
+		lineHeight: 20
+	},
+	connectButton: {
+		marginTop: 36
+	}
+});
+
+export default styles;
