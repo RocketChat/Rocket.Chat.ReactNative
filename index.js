ffcf433192669427c078721bf7a1773b87d9fa00
Diego Mello <diegolmello@gmail.com>
2025-04-15 10:23:46 -0300

    Remove LogBox

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 import 'react-native-console-time-polyfill';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, LogBox } from 'react-native';
 
 import { name as appName, share as shareName } from './app.json';
 import { isAndroid } from './app/lib/methods/helpers';
@@ -19,6 +19,8 @@ if (process.env.USE_STORYBOOK) {
 		console.error = () => {};
 		console.info = () => {};
 	}
+
+	LogBox.ignoreAllLogs();
 
 	if (isAndroid) {
 		require('./app/lib/notifications/videoConf/backgroundNotificationHandler');
