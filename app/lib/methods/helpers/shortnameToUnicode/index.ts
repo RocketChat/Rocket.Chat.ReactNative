4cf71af2f9124eb041b894ac80ef02399e282a53
OtavioStasiak <otaviostasiakmusic@outlook.com>
2025-04-28 15:08:26 -0300

    fix: markdown preview

--- app/lib/methods/helpers/shortnameToUnicode/index.ts
+++ app/lib/methods/helpers/shortnameToUnicode/index.ts
@@ -1,5 +1,7 @@
 import emojis from './emojis';
 import ascii, { asciiRegexp } from './ascii';
+import { useAppSelector } from '../../../../lib/hooks';
+import { getUserSelector } from '../../../../selectors/login';
 
 const shortnamePattern = new RegExp(/:[-+_a-z0-9]+:/, 'gi');
 const replaceShortNameWithUnicode = (shortname: string) => emojis[shortname] || shortname;
@@ -28,8 +30,10 @@ const unescapeHTML = (string: string) => {
 };
 
 const shortnameToUnicode = (str: string): string => {
-	str = str.replace(shortnamePattern, replaceShortNameWithUnicode);
+	const { settings } = useAppSelector(state => getUserSelector(state));
+	const convertAsciiEmoji = settings?.preferences.convertAsciiEmoji;
 
+	str = str.replace(shortnamePattern, replaceShortNameWithUnicode);
 	str = str.replace(regAscii, (entire, m1, m2, m3) => {
 		if (!m3 || !(unescapeHTML(m3) in ascii)) {
 			// if the ascii doesnt exist just return the entire match
@@ -37,9 +41,12 @@ const shortnameToUnicode = (str: string): string => {
 		}
 
 		m3 = unescapeHTML(m3);
+
+		if (!convertAsciiEmoji) {
+			return m3;
+		}
 		return ascii[m3];
 	});
-
 	return str;
 };
 
