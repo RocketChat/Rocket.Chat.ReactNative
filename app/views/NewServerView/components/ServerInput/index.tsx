e6967b3baa103b482e13ecb59a3dc25166cc7225
OtavioStasiak <otaviostasiakmusic@outlook.com>
2025-05-06 17:25:46 -0300

    fix: lint

--- app/views/NewServerView/components/ServerInput/index.tsx
+++ app/views/NewServerView/components/ServerInput/index.tsx
@@ -1,11 +1,9 @@
 import React, { useState } from 'react';
-import { FlatList, StyleSheet, TextInputProps, View } from 'react-native';
+import { StyleSheet, TextInputProps, View } from 'react-native';
 import { Control } from 'react-hook-form';
 
-import Item from './Item';
+import { useTheme } from '../../../../theme';
 import { ControlledFormTextInput } from '../../../../containers/TextInput';
-import * as List from '../../../../containers/List';
-import I18n from '../../../../i18n';
 import { TServerHistoryModel } from '../../../../definitions';
 import I18n from '../../../../i18n';
 import { CustomIcon } from '../../../../containers/CustomIcon';
@@ -22,12 +20,8 @@ const styles = StyleSheet.create({
 	inputContainer: {
 		flex: 1
 	},
-	input: {
-		marginTop: 0,
-		marginBottom: 0
-	},
 	serversHistoryButton: {
-		paddingVertical: 9
+		paddingVertical: 18
 	}
 });
 
@@ -39,7 +33,6 @@ interface IServerInput extends TextInputProps {
 		},
 		any
 	>;
-	text: string;
 	serversHistory: TServerHistoryModel[];
 	onSubmit(): void;
 	onDelete(item: TServerHistoryModel): void;
@@ -55,6 +48,7 @@ const ServerInput = ({
 	onDelete,
 	onPressServerHistory
 }: IServerInput): JSX.Element => {
+	const [focused, setFocused] = useState(false);
 	const { colors } = useTheme();
 
 	const handleDeleteServerHistory = (item: TServerHistoryModel) => {
@@ -81,33 +75,35 @@ const ServerInput = ({
 
 	return (
 		<View style={styles.container}>
-			<ControlledFormTextInput
-				name='workspaceUrl'
-				control={control}
-				label={I18n.t('Workspace_URL')}
-				containerStyle={styles.inputContainer}
-				inputStyle={error && !focused ? { borderColor: colors.fontDanger } : {}}
-				returnKeyType='send'
-				testID='new-server-view-input'
-				onSubmitEditing={onSubmit}
-				clearButtonMode='while-editing'
-				keyboardType='url'
-				textContentType='URL'
-				required
-				onChangeText={onChangeText}
-				onFocus={() => setFocused(true)}
-				onBlur={() => setFocused(false)}
-				error={error}
-			/>
-
-			{focused && serversHistory?.length ? (
-				<View style={[styles.serverHistory, { backgroundColor: colors.surfaceRoom, borderColor: colors.strokeLight }]}>
-					<FlatList
-						data={serversHistory}
-						renderItem={({ item }) => <Item item={item} onPress={() => onPressServerHistory(item)} onDelete={onDelete} />}
-						ItemSeparatorComponent={List.Separator}
-						keyExtractor={item => item.id}
-					/>
+			<View style={styles.inputContainer}>
+				<ControlledFormTextInput
+					name='workspaceUrl'
+					control={control}
+					label={I18n.t('Workspace_URL')}
+					containerStyle={styles.inputContainer}
+					inputStyle={error && !focused ? { borderColor: colors.fontDanger } : {}}
+					returnKeyType='send'
+					testID='new-server-view-input'
+					onSubmitEditing={onSubmit}
+					clearButtonMode='while-editing'
+					keyboardType='url'
+					textContentType='URL'
+					required
+					onChangeText={onChangeText}
+					onFocus={() => setFocused(true)}
+					onBlur={() => setFocused(false)}
+					error={error}
+				/>
+			</View>
+			{serversHistory?.length > 0 ? (
+				<View style={styles.serversHistoryButton}>
+					<Touch
+						accessible
+						accessibilityLabel={I18n.t('Open_servers_history')}
+						testID='servers-history-button'
+						onPress={openServersHistory}>
+						<CustomIcon name='clock' size={32} color={colors.fontInfo} />
+					</Touch>
 				</View>
 			) : null}
 		</View>
