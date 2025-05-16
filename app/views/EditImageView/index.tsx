b5c1e11bfe24ccea8c20cfa68eef7d0784ac07d6
OtavioStasiak <otaviostasiakmusic@outlook.com>
2025-05-16 16:01:41 -0300

    feat: finished pan animations

--- app/views/EditImageView/index.tsx
+++ app/views/EditImageView/index.tsx
@@ -81,58 +81,170 @@ const EditImageView = () => {
         justifyContent: 'center'
     }))
 
-    const leftCenter = Gesture.Pan().onChange(e => {
-        const offset = e.translationX - prevTranslationXValue.value;
-        const newWidth = clamp(sharedValueWidth.value - (e.translationX - prevTranslationXValue.value), 100, width) 
-        const newLeft =  clamp(left.value + offset, 0, width - newWidth) ;
+    
+    const topLeft = Gesture.Pan().onChange(e => {
+        const verticalOffset = e.translationY - prevTranslationYValue.value;
+        const newHeight = clamp(sharedValueHeight.value - (e.translationY - prevTranslationYValue.value), 30, height);
+        const newTop =  clamp(top.value + verticalOffset, 0, height - newHeight);
+        
+        const horizontalOffset = e.translationX - prevTranslationXValue.value;
+        const newWidth = clamp(sharedValueWidth.value - horizontalOffset, 100, width);
+        const newLeft =  clamp(left.value + horizontalOffset, 0, width - newWidth);
+       
+        if(newTop > 0) {
+            sharedValueHeight.value = newHeight > height * 0.999 ? height: newHeight;
+            top.value = newHeight > height * 0.999 ? 0 : newTop;
 
-        if(newWidth > 100) {
-            left.value = newLeft;
-            if(newLeft > 0) {
-                sharedValueWidth.value = newWidth > width * 0.99 ? width: newWidth;
-            }
         }
+
+        if(newLeft > 0) {
+            sharedValueWidth.value = newWidth > width * 0.999 ? width: newWidth;
+            left.value =  newWidth > width * 0.999 ? 0 : newLeft;
+        }
+    
+        prevTranslationYValue.value = e.translationY;
         prevTranslationXValue.value = e.translationX;
-    }).onFinalize(() => {
-        prevTranslationXValue.value = 0; 
-      
+
+    }).onFinalize(() => { 
+        prevTranslationYValue.value = 0
+        prevTranslationXValue.value = 0
     })
-
-    const rightCenter = Gesture.Pan().onChange(e => {
-        const offset = (e.translationX * -1) + prevTranslationXValue.value;
-        const newWidth = clamp( sharedValueWidth.value - offset, 100, width)
-        if(newWidth + left.value < width) {
-             sharedValueWidth.value = newWidth > width * 0.99 ? width: newWidth;
-        }
-        prevTranslationXValue.value = e.translationX;
-    }).onFinalize(() => {prevTranslationXValue.value = 0})
-
     const topCenter = Gesture.Pan().onChange(e => {
         const offset = e.translationY - prevTranslationYValue.value;
         const newHeight = clamp(sharedValueHeight.value - (e.translationY - prevTranslationYValue.value), 30, height);
         const newTop =  clamp(top.value + offset, 0, height - newHeight);
 
-        if(newHeight > 30) {
-            top.value = newTop;
-            if(newTop > 0) {
-                sharedValueHeight.value = newHeight > height * 0.99 ? height: newHeight;
-            }
+        if(newTop > 0) {
+            sharedValueHeight.value = newHeight;
+            top.value =  newTop;
+
         }
 
         prevTranslationYValue.value = e.translationY;
 
     }).onFinalize(() => { prevTranslationYValue.value = 0})
+    const topRight = Gesture.Pan().onChange(e => {
+        const verticalOffset = e.translationY - prevTranslationYValue.value;
+        const newHeight = clamp(sharedValueHeight.value - (e.translationY - prevTranslationYValue.value), 30, height);
+        const newTop =  clamp(top.value + verticalOffset, 0, height - newHeight);
+        const horizontalOffset = (e.translationX * -1) + prevTranslationXValue.value;
+        const newWidth = clamp( sharedValueWidth.value - horizontalOffset, 100, width);
 
+        if(newTop > 0) {
+            sharedValueHeight.value = newHeight > height * 0.999 ? height: newHeight;
+            top.value = newHeight > height * 0.999 ? 0 : newTop;
+        }
+    
+        if(newWidth + left.value < width) {
+            sharedValueWidth.value = newWidth > width * 0.999 ? width: newWidth;
+        }
+
+        prevTranslationYValue.value = e.translationY;
+        prevTranslationXValue.value = e.translationX;
+    }).onFinalize(() => {
+        prevTranslationYValue.value = 0
+        prevTranslationXValue.value = 0
+    });
+
+
+    const leftCenter = Gesture.Pan().onChange(e => {
+        const offset = e.translationX - prevTranslationXValue.value;
+        const newWidth = clamp(sharedValueWidth.value - (e.translationX - prevTranslationXValue.value), 100, width) 
+        const newLeft =  clamp(left.value + offset, 0, width - newWidth) ;
+
+        if(newLeft > 0) {
+            sharedValueWidth.value = newWidth > width * 0.999 ? width: newWidth;
+            left.value =  newWidth > width * 0.999 ? 0 : newLeft;
+        }
+    
+        prevTranslationXValue.value = e.translationX;
+    }).onFinalize(() => {
+        prevTranslationXValue.value = 0; 
+      
+    })
+     const moveGrid = Gesture.Pan().onChange(e => {
+        const offset = e.translationX - prevTranslationXValue.value;
+        const verticalOffset = e.translationY - prevTranslationYValue.value;
+
+        const newLeft =  clamp((left.value + offset)/2, 0, width - sharedValueWidth.value);
+        const newTop =  clamp((top.value + verticalOffset)/2, 0, height - sharedValueHeight.value);
+
+        if(sharedValueWidth.value < width) {
+            left.value = newLeft
+        }
+
+        if(sharedValueHeight.value < height) {
+            top.value = newTop
+        }
+
+    }).onFinalize(() => {
+        prevTranslationXValue.value = 0; 
+        prevTranslationYValue.value = 0;
+    })
+    const rightCenter = Gesture.Pan().onChange(e => {
+        const offset = (e.translationX * -1) + prevTranslationXValue.value;
+        const newWidth = clamp( sharedValueWidth.value - offset, 100, width)
+        if(newWidth + left.value < width) {
+             sharedValueWidth.value = newWidth > width * 0.999 ? width: newWidth;
+        }
+        prevTranslationXValue.value = e.translationX;
+    }).onFinalize(() => {prevTranslationXValue.value = 0})
    
+
+    const bottomLeft = Gesture.Pan().onChange(e => {
+        const horizontalOffset = e.translationX - prevTranslationXValue.value;
+        const newWidth = clamp(sharedValueWidth.value - horizontalOffset, 100, width);
+        const newLeft =  clamp(left.value + horizontalOffset, 0, width - newWidth);
+       
+        const offset = (e.translationY * -1) + prevTranslationYValue.value;
+        const newHeight = clamp( sharedValueHeight.value - offset, 30, height)
+       
+        if(newHeight + top.value < height) {
+            sharedValueHeight.value = newHeight > height * 0.999 ? height: newHeight;
+        }
+        
+        if(newLeft > 0) {
+            sharedValueWidth.value = newWidth > width * 0.999 ? width: newWidth;
+            left.value =  newWidth > width * 0.999 ? 0 : newLeft;
+        }
+    
+        prevTranslationYValue.value = e.translationY;
+        prevTranslationXValue.value = e.translationX;
+
+    }).onFinalize(() => { 
+        prevTranslationYValue.value = 0
+        prevTranslationXValue.value = 0
+    });
     const bottomCenter = Gesture.Pan().onChange(e => {
         const offset = (e.translationY * -1) + prevTranslationYValue.value;
         const newHeight = clamp( sharedValueHeight.value - offset, 30, height)
         if(newHeight + top.value < height) {
-             sharedValueHeight.value = newHeight > height * 0.99 ? height: newHeight;
+            sharedValueHeight.value = newHeight > height * 0.999 ? height: newHeight;
         }
         prevTranslationYValue.value = e.translationY;
     }).onFinalize(() => {prevTranslationYValue.value = 0})
+    const bottomRight = Gesture.Pan().onChange(e => {
+        const horizontalOffset = (e.translationX * -1) + prevTranslationXValue.value;
+        const newWidth = clamp( sharedValueWidth.value - horizontalOffset, 100, width);
+        const offset = (e.translationY * -1) + prevTranslationYValue.value;
+        const newHeight = clamp( sharedValueHeight.value - offset, 30, height)
+        if(newHeight + top.value < height) {
+            sharedValueHeight.value = newHeight > height * 0.999 ? height: newHeight;
+        }
+        if(newWidth + left.value < width) {
+            sharedValueWidth.value = newWidth > width * 0.999 ? width: newWidth;
+        }
 
+        prevTranslationYValue.value = e.translationY;
+        prevTranslationXValue.value = e.translationX;
+    }).onFinalize(() => {
+        prevTranslationYValue.value = 0
+        prevTranslationXValue.value = 0
+    });
+
+   
+
+    const renderSquare = () => (<View style={{ borderColor, borderWidth: 1, backgroundColor: 'transparent', flex: 1 / 3, height: '100%'}}/>)
     return(  
         <SafeAreaView>
             
@@ -153,60 +265,47 @@ const EditImageView = () => {
                         flexDirection: 'row', width: '100%', flex: 1 / 3, backgroundColor: 'transparent'
                     }}
                     >
-                        <View style={{
-                            borderColor, borderWidth: 1, backgroundColor: 'transparent', flex: 1 / 3, height: '100%'
-                        }}
-                        />
-                        <GestureDetector gesture={topCenter}>
-                        <View style={{
-                            borderColor, borderWidth: 1, backgroundColor:'transparent', flex: 1 / 3, height: '100%'
-                        }}
-                        />
+                        <GestureDetector gesture={topLeft}>
+                            {renderSquare()}
                         </GestureDetector>
-                        <View style={{
-                            borderColor, borderWidth: 1, backgroundColor: 'transparent', flex: 1 / 3, height: '100%'
-                        }}
-                        />
+                       
+                        <GestureDetector gesture={topCenter}>
+                            {renderSquare()}
+                        </GestureDetector>
+
+                       <GestureDetector gesture={topRight}>
+                            {renderSquare()}
+                       </GestureDetector>
                     </View>
+
                     <View style={{flexDirection: 'row', width: '100%', flex: 1 / 3, backgroundColor: 'transparent'}}>
                        <GestureDetector gesture={leftCenter}>
-                            <View style={{
-                                borderColor, borderWidth: 1, backgroundColor: 'transparent', flex: 1 / 3, height: '100%'
-                                }}
-                            />
+                            {renderSquare()}
                        </GestureDetector>
                       
-                        <View style={{
-                            borderColor, borderWidth: 1, backgroundColor:'transparent', flex: 1 / 3, height: '100%'
-                        }}
-                        />
-
+                        <GestureDetector gesture={moveGrid}>
+                            {renderSquare()}
+                        </GestureDetector>
                         <GestureDetector gesture={rightCenter}>  
-                            <View style={{
-                                borderColor, borderWidth: 1, backgroundColor: 'transparent', flex: 1 / 3, height: '100%'
-                            }}
-                            />
+                            {renderSquare()}
                         </GestureDetector>
                     </View>
+
                     <View style={{
                         flexDirection: 'row', width: '100%', flex: 1 / 3, backgroundColor: 'transparent'
                     }}
                     >
-                        <View style={{
-                            borderColor, borderWidth: 1, backgroundColor: 'transparent', flex: 1 / 3, height: '100%'
-                        }}
-                        />
+                       <GestureDetector gesture={bottomLeft}>
+                            {renderSquare()}
+                       </GestureDetector>
                         <GestureDetector gesture={bottomCenter}>
-                            <View style={{
-                                borderColor, borderWidth: 1, backgroundColor:'transparent', flex: 1 / 3, height: '100%'
-                            }}
-                            />
+                            {renderSquare()}
                         </GestureDetector>
-                        <View style={{
-                            borderColor, borderWidth: 1, backgroundColor: 'transparent', flex: 1 / 3, height: '100%'
-                        }}
-                        />
+                        <GestureDetector gesture={bottomRight}>
+                            {renderSquare()}
+                        </GestureDetector>
                     </View>
+
                 </Animated.View>
                     : null
                 }
@@ -231,8 +330,8 @@ const EditImageView = () => {
                     <Text>RotRight</Text>
                 </Touch>
 
-                 <Touch onPress={() => setCrop(true)}>
-                    <Text>crop</Text>
+                 <Touch style={{backgroundColor: 'gray'}} onPress={() => setCrop(true)}>
+                    <Text >crop</Text>
                 </Touch>
 
 
