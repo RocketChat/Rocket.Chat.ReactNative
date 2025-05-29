fc2ff15fdc711868fa62a38fcce17bfbc877643d
OtavioStasiak <otaviostasiakmusic@outlook.com>
2025-05-29 16:45:30 -0300

    fix: prevent scale as 0

--- app/views/EditImageView/methods/getValueBasedOnOriginal.ts
+++ app/views/EditImageView/methods/getValueBasedOnOriginal.ts
@@ -1,4 +1,5 @@
 const getValueBasedOnOriginal = (cuttedValue: number, originalSize: number, screenScale: number) => {
+	if (screenScale === 0) throw new Error('screenScale cannot be zero');
 	const scale = originalSize / screenScale;
 	return cuttedValue * scale;
 };
