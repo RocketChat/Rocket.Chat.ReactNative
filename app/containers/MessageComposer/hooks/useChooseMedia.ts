302fe9bd493165867beb155b471ba08da3579540
Otávio Stasiak <91474186+OtavioStasiak@users.noreply.github.com>
2025-06-03 11:29:06 -0300

    regression: rollback react-native-crop-picker (#6385)
    * chore: install lib and rollback on type and helpers of composer
    
    * rollback changeAvatarView
    
    * chore: rollback useChooseMedia
    
    * fix: remove unused helpers
    
    * fix: rollback translations
    
    * fix: action sheet did not close on android
    
    * feat: remove expo-image-picker
    
    * fix: image with low quality after take photo
    
    * chore: change commit reference of react-native-crop-picker
    
    * chore: podfile

--- app/containers/MessageComposer/hooks/useChooseMedia.ts
+++ app/containers/MessageComposer/hooks/useChooseMedia.ts
@@ -2,6 +2,7 @@ import { Alert } from 'react-native';
 import * as DocumentPicker from 'expo-document-picker';
 
 import { IMAGE_PICKER_CONFIG, LIBRARY_PICKER_CONFIG, VIDEO_PICKER_CONFIG } from '../constants';
+import { forceJpgExtension } from '../helpers';
 import I18n from '../../../i18n';
 import { canUploadFile } from '../../../lib/methods/helpers';
 import log from '../../../lib/methods/helpers/log';
@@ -10,10 +11,7 @@ import { getThreadById } from '../../../lib/database/services/Thread';
 import Navigation from '../../../lib/navigation/appNavigation';
 import { useAppSelector } from '../../../lib/hooks';
 import { useRoomContext } from '../../../views/RoomView/context';
-import ImagePicker from '../../../lib/methods/helpers/ImagePicker/ImagePicker';
-import { mapMediaResult } from '../../../lib/methods/helpers/ImagePicker/mapMediaResult';
-import { getPermissions } from '../../../lib/methods/helpers/ImagePicker/getPermissions';
-import { IShareAttachment } from '../../../definitions';
+import ImagePicker, { ImageOrVideo } from '../../../lib/methods/helpers/ImagePicker/ImagePicker';
 
 export const useChooseMedia = ({
 	rid,
@@ -28,23 +26,25 @@ export const useChooseMedia = ({
 	const { action, setQuotesAndText, selectedMessages, getText } = useRoomContext();
 	const allowList = FileUpload_MediaTypeWhiteList as string;
 	const maxFileSize = FileUpload_MaxFileSize as number;
+	const libPickerLabels = {
+		cropperChooseText: I18n.t('Choose'),
+		cropperCancelText: I18n.t('Cancel'),
+		loadingLabelText: I18n.t('Processing')
+	};
 
 	const takePhoto = async () => {
 		try {
-			await getPermissions('camera');
-			const result = await ImagePicker.launchCameraAsync(IMAGE_PICKER_CONFIG);
-			if (result.canceled) {
-				return;
-			}
-			const media = mapMediaResult(result.assets);
+			let image = await ImagePicker.openCamera({ ...IMAGE_PICKER_CONFIG, ...libPickerLabels });
+			image = forceJpgExtension(image);
+			const file = image as any; // FIXME: unify those types to remove the need for any
 			const canUploadResult = canUploadFile({
-				file: media[0],
+				file,
 				allowList,
 				maxFileSize,
 				permissionToUploadFile: permissionToUpload
 			});
 			if (canUploadResult.success) {
-				return openShareView(media);
+				return openShareView([image]);
 			}
 
 			handleError(canUploadResult.error);
@@ -55,20 +55,16 @@ export const useChooseMedia = ({
 
 	const takeVideo = async () => {
 		try {
-			await getPermissions('camera');
-			const result = await ImagePicker.launchCameraAsync(VIDEO_PICKER_CONFIG);
-			if (result.canceled) {
-				return;
-			}
-			const media = mapMediaResult(result.assets);
+			const video = await ImagePicker.openCamera({ ...VIDEO_PICKER_CONFIG, ...libPickerLabels });
+			const file = video as any; // FIXME: unify those types to remove the need for any
 			const canUploadResult = canUploadFile({
-				file: media[0],
+				file,
 				allowList,
 				maxFileSize,
 				permissionToUploadFile: permissionToUpload
 			});
 			if (canUploadResult.success) {
-				return openShareView(media);
+				return openShareView([video]);
 			}
 
 			handleError(canUploadResult.error);
@@ -79,13 +75,13 @@ export const useChooseMedia = ({
 
 	const chooseFromLibrary = async () => {
 		try {
-			await getPermissions('library');
-			const result = await ImagePicker.launchImageLibraryAsync(LIBRARY_PICKER_CONFIG);
-			if (result.canceled) {
-				return;
-			}
-			const media = mapMediaResult(result.assets);
-			openShareView(media);
+			// The type can be video or photo, however the lib understands that it is just one of them.
+			let attachments = (await ImagePicker.openPicker({
+				...LIBRARY_PICKER_CONFIG,
+				...libPickerLabels
+			})) as unknown as ImageOrVideo[]; // FIXME: type this
+			attachments = attachments.map(att => forceJpgExtension(att));
+			openShareView(attachments);
 		} catch (e) {
 			log(e);
 		}
@@ -128,7 +124,7 @@ export const useChooseMedia = ({
 
 	const finishShareView = (text = '', quotes = []) => setQuotesAndText?.(text, quotes);
 
-	const openShareView = async (attachments: IShareAttachment[]) => {
+	const openShareView = async (attachments: any) => {
 		if (!rid) return;
 		const room = await getSubscriptionByRoomId(rid);
 		let thread;
