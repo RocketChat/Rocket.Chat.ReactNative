4d4a42decabdca208a51ffe5d0c258b7f2615e9e
Diego Mello <diegolmello@gmail.com>
2025-06-05 10:17:29 -0300

    able to receive calls while killed

--- index.js
+++ index.js
@@ -2,8 +2,93 @@ import 'react-native-gesture-handler';
 import 'react-native-console-time-polyfill';
 import { AppRegistry, LogBox } from 'react-native';
 
+import RNCallKeep from 'react-native-callkeep';
+import VoipPushNotification from 'react-native-voip-push-notification';
+
 import { name as appName } from './app.json';
 import { isAndroid } from './app/lib/methods/helpers';
+
+const options = {
+	ios: {
+		appName: 'Rocket.Chat',
+		supportsVideo: true,
+		maximumCallGroups: 1,
+		maximumCallsPerCallGroup: 1,
+		includesCallsInRecents: false
+	}
+};
+
+RNCallKeep.setup(options).then(accepted => {
+	console.log('CallKeep setup completed:', accepted);
+});
+
+// CallKeep event listeners
+RNCallKeep.addEventListener('answerCall', ({ callUUID }) => {
+	console.log('Call answered:', callUUID);
+	// Handle call answer - you can dispatch to your Redux store or handle app navigation
+});
+
+RNCallKeep.addEventListener('endCall', ({ callUUID }) => {
+	console.log('Call ended:', callUUID);
+	// Handle call end
+});
+
+RNCallKeep.addEventListener('didPerformSetMutedCallAction', ({ muted, callUUID }) => {
+	console.log('Call muted:', { muted, callUUID });
+	// Handle mute action
+});
+
+RNCallKeep.addEventListener('didPerformDTMFAction', ({ dtmf, callUUID }) => {
+	console.log('DTMF:', { dtmf, callUUID });
+	// Handle DTMF if needed
+});
+
+RNCallKeep.addEventListener('didActivateAudioSession', () => {
+	console.log('Audio session activated');
+	// Handle audio session activation
+});
+
+RNCallKeep.addEventListener('didDeactivateAudioSession', () => {
+	console.log('Audio session deactivated');
+	// Handle audio session deactivation
+});
+
+VoipPushNotification.addEventListener('register', token => {
+	console.log('VoIP Token:', token);
+	// Send token to your server
+});
+
+function onVoipPushNotificationiReceived(data) {
+	console.log('VoIP Notification Received:', data);
+	RNCallKeep.displayIncomingCall('E26B14F7-2CDF-48D0-9925-532199AE7C45', 'handle', 'callerName');
+}
+
+VoipPushNotification.addEventListener('notification', notification => {
+	console.log('VoIP Notification Received:', notification);
+	const { uuid, callerName, handle } = notification;
+	onVoipPushNotificationiReceived(notification);
+
+	VoipPushNotification.onVoipNotificationCompleted(uuid);
+});
+
+VoipPushNotification.addEventListener('didLoadWithEvents', events => {
+	// --- this will fire when there are events occured before js bridge initialized
+	// --- use this event to execute your event handler manually by event type
+
+	if (!events || !Array.isArray(events) || events.length < 1) {
+		return;
+	}
+	for (const voipPushEvent of events) {
+		const { name, data } = voipPushEvent;
+		if (name === VoipPushNotification.RNVoipPushRemoteNotificationsRegisteredEvent) {
+			console.log('didLoadWithEvents VoIP Token: ', data);
+		} else if (name === VoipPushNotification.RNVoipPushRemoteNotificationReceivedEvent) {
+			onVoipPushNotificationiReceived(data);
+		}
+	}
+});
+
+VoipPushNotification.registerVoipToken();
 
 if (process.env.USE_STORYBOOK) {
 	AppRegistry.registerComponent(appName, () => require('./.storybook/index').default);
--- ios/AppDelegate.mm
+++ ios/AppDelegate.mm
@@ -7,6 +7,8 @@
 #import <Firebase.h>
 #import <Bugsnag/Bugsnag.h>
 #import <MMKV/MMKV.h>
+#import <RNCallKeep/RNCallKeep.h>
+#import <RNVoipPushNotification/RNVoipPushNotificationManager.h>
 
 @implementation AppDelegate
 
@@ -16,6 +18,8 @@
     [FIRApp configure];
   }
   [Bugsnag start];
+    
+    [RNVoipPushNotificationManager voipRegistration];
   
   // AppGroup MMKV
   NSString *groupDir = [[NSFileManager defaultManager] containerURLForSecurityApplicationGroupIdentifier:[[NSBundle mainBundle] objectForInfoDictionaryKey:@"AppGroup"]].path;
@@ -31,12 +35,129 @@
   self.initialProps = @{};
   [super application:application didFinishLaunchingWithOptions:launchOptions];
   [RNBootSplash initWithStoryboard:@"LaunchScreen" rootView:self.window.rootViewController.view];
-  [[[SSLPinning alloc] init] migrate];
+//  [[[SSLPinning alloc] init] migrate];
+    
+    [RNCallKeep setup:@{@"appName": @"Rocket.Chat"}];
+//      [RNVoipPushNotificationManager voipRegistration];
+    
   
   self.watchConnection = [[WatchConnection alloc] initWithSession:[WCSession defaultSession]];
 
   return YES;
 }
+
+// --- Handle updated push credentials
+- (void)pushRegistry:(PKPushRegistry *)registry didUpdatePushCredentials:(PKPushCredentials *)credentials forType:(PKPushType)type {
+  // Register VoIP push token (a property of PKPushCredentials) with server
+  [RNVoipPushNotificationManager didUpdatePushCredentials:credentials forType:(NSString *)type];
+}
+
+- (void)pushRegistry:(PKPushRegistry *)registry didInvalidatePushTokenForType:(PKPushType)type
+{
+  // --- The system calls this method when a previously provided push token is no longer valid for use. No action is necessary on your part to reregister the push type. Instead, use this method to notify your server not to send push notifications using the matching push token.
+}
+
+// --- Handle incoming pushes
+- (void)pushRegistry:(PKPushRegistry *)registry didReceiveIncomingPushWithPayload:(PKPushPayload *)payload forType:(PKPushType)type withCompletionHandler:(void (^)(void))completion {
+  
+
+  // --- NOTE: apple forced us to invoke callkit ASAP when we receive voip push
+  // --- see: react-native-callkeep
+
+  // --- Extract information from your voip push payload
+  NSDictionary *payloadDict = payload.dictionaryPayload;
+  NSLog(@"[VoIP] Received payload: %@", payloadDict);
+  
+  // Generate unique UUID for each call
+  NSString *uuid = [[NSUUID UUID] UUIDString];
+  
+  // Extract call information from payload, with fallbacks
+  NSString *callerName = @"Unknown Caller";
+  NSString *handle = @"Unknown";
+  BOOL hasVideo = NO;
+  
+  // Try to extract from common payload structures
+  if (payloadDict[@"caller_name"]) {
+    callerName = payloadDict[@"caller_name"];
+  } else if (payloadDict[@"callerName"]) {
+    callerName = payloadDict[@"callerName"];
+  } else if (payloadDict[@"name"]) {
+    callerName = payloadDict[@"name"];
+  } else if (payloadDict[@"aps"][@"alert"][@"title"]) {
+    callerName = payloadDict[@"aps"][@"alert"][@"title"];
+  }
+  
+  if (payloadDict[@"caller_id"]) {
+    handle = payloadDict[@"caller_id"];
+  } else if (payloadDict[@"callerId"]) {
+    handle = payloadDict[@"callerId"];
+  } else if (payloadDict[@"handle"]) {
+    handle = payloadDict[@"handle"];
+  } else if (payloadDict[@"phone"]) {
+    handle = payloadDict[@"phone"];
+  }
+  
+  if (payloadDict[@"has_video"]) {
+    hasVideo = [payloadDict[@"has_video"] boolValue];
+  } else if (payloadDict[@"hasVideo"]) {
+    hasVideo = [payloadDict[@"hasVideo"] boolValue];
+  } else if (payloadDict[@"video"]) {
+    hasVideo = [payloadDict[@"video"] boolValue];
+  }
+  
+  // Use UUID from payload if provided
+  if (payloadDict[@"uuid"]) {
+    uuid = payloadDict[@"uuid"];
+  } else if (payloadDict[@"call_id"]) {
+    uuid = payloadDict[@"call_id"];
+  } else if (payloadDict[@"callId"]) {
+    uuid = payloadDict[@"callId"];
+  }
+  
+  NSLog(@"[VoIP] Processing call - UUID: %@, Caller: %@, Handle: %@, Video: %d", uuid, callerName, handle, hasVideo);
+
+  // --- this is optional, only required if you want to call `completion()` on the js side
+  [RNVoipPushNotificationManager addCompletionHandler:uuid completionHandler:completion];
+
+  // --- Process the received push
+  [RNVoipPushNotificationManager didReceiveIncomingPushWithPayload:payload forType:(NSString *)type];
+
+  // --- You should make sure to report to callkit BEFORE execute `completion()`
+  [RNCallKeep reportNewIncomingCall:uuid 
+                             handle:handle 
+                         handleType:@"generic" 
+                           hasVideo:hasVideo 
+                localizedCallerName:callerName 
+                    supportsHolding:false 
+                       supportsDTMF:false 
+                   supportsGrouping:false 
+                 supportsUngrouping:false 
+                        fromPushKit:true 
+                            payload:payloadDict 
+              withCompletionHandler:completion];
+  
+  // --- You don't need to call it if you stored `completion()` and will call it on the js side.
+//  completion();
+}
+
+//- (void)pushRegistry:(PKPushRegistry *)registry didReceiveIncomingPushWithPayload:(PKPushPayload *)payload forType:(PKPushType)type withCompletionHandler:(void (^)(void))completion {
+//  [RNVoipPushNotificationManager didReceiveIncomingPushWithPayload:payload forType:(NSString *)type];
+//  NSString *uuid = [[[NSUUID UUID] UUIDString] lowercaseString];
+//  NSString *callerName = @"Caller Name";
+//  NSString *handle = @"Caller Number";
+//  [RNCallKeep reportNewIncomingCall:uuid
+//                             handle:handle
+//                         handleType:@"generic"
+//                           hasVideo:NO
+//                localizedCallerName:callerName
+//                    supportsHolding:YES
+//                       supportsDTMF:YES
+//                   supportsGrouping:YES
+//                 supportsUngrouping:YES
+//                        fromPushKit:YES
+//                            payload:nil
+//              withCompletionHandler:completion];
+//}
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
