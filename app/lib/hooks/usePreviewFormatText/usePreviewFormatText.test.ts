57fd0bf073c0f9415e7232ea0163dea8953a9dd0
Rohit Bansal <yiweigao0226@gmail.com>
2025-06-06 20:43:11 +0530

    fix: message preview markdown improvements (#6365)

--- app/lib/hooks/usePreviewFormatText/usePreviewFormatText.test.ts
+++ app/lib/hooks/usePreviewFormatText/usePreviewFormatText.test.ts
@@ -53,4 +53,76 @@ describe('Format preview message', () => {
 		const formattedText = usePreviewFormatText('You: [ ](https://open.rocket.chat/group/channel?msg=nrTDSw96IhtF3iN4K) \nTest');
 		expect(formattedText).toBe('You: Test');
 	});
+	test('Format a bold message as last message "You: **Test**" to be "You: Test"', () => {
+		const formattedText = usePreviewFormatText('You: **Test**');
+		expect(formattedText).toBe('You: Test');
+	});
+	test('Format a italic message as last message "You: _Test_" to be "You: Test"', () => {
+		const formattedText = usePreviewFormatText('You: _Test_');
+		expect(formattedText).toBe('You: Test');
+	});
+	test('Format a strike message as last message "You: ~Test~" to be "You: Test"', () => {
+		const formattedText = usePreviewFormatText('You: ~Test~');
+		expect(formattedText).toBe('You: Test');
+	});
+	test('Format a quote message as last message "You: > Test" to be "You: Test"', () => {
+		const formattedText = usePreviewFormatText('You: > Test');
+		expect(formattedText).toBe('You: Test');
+	});
+	test('Format a bold italic message as last message "You: *_Test_*" to be "You: Test"', () => {
+		const formattedText = usePreviewFormatText('You: *_Test_*');
+		expect(formattedText).toBe('You: Test');
+	});
+	test('Format a bold strike message as last message "You: *~Test~*" to be "You: Test"', () => {
+		const formattedText = usePreviewFormatText('You: *~Test~*');
+		expect(formattedText).toBe('You: Test');
+	});
+});
+
+describe('convertAsciiEmoji = true', () => {
+    beforeAll(() => {
+        mockedStore.dispatch(
+            setUser({
+                settings: {
+                    preferences: {
+                        convertAsciiEmoji: true
+                    }
+                }
+            })
+        );
+    });
+
+    test('Format unicode :)', () => {
+        const formattedText = usePreviewFormatText(':)');
+        expect(formattedText).toBe('🙂');
+    });
+
+    test('Format unicode :) with text', () => {
+        const formattedText = usePreviewFormatText('Hello World :)');
+        expect(formattedText).toBe('Hello World 🙂');
+    });
+});
+
+describe('convertAsciiEmoji = false', () => {
+    beforeAll(() => {
+        mockedStore.dispatch(
+            setUser({
+                settings: {
+                    preferences: {
+                        convertAsciiEmoji: false
+                    }
+                }
+            })
+        );
+    });
+
+    test('Keep unicode :)', () => {
+        const formattedText = usePreviewFormatText(':)');
+        expect(formattedText).toBe(':)');
+    });
+
+    test('Keep unicode :) with text', () => {
+        const formattedText = usePreviewFormatText('Hello World :)');
+        expect(formattedText).toBe('Hello World :)');
+    });
 });
