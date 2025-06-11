b29afa4ee1d8a1333ab886003ce62c5c8ca6d146
Diego Mello <diegolmello@gmail.com>
2025-06-11 14:41:28 -0300

    Fix elements for RN 0.79

--- e2e/tests/assorted/05-joinpublicroom.spec.ts
+++ e2e/tests/assorted/05-joinpublicroom.spec.ts
@@ -1,16 +1,7 @@
 import { device, waitFor, element, by, expect } from 'detox';
 
 import data from '../../data';
-import {
-	navigateToLogin,
-	login,
-	tapBack,
-	platformTypes,
-	TTextMatcher,
-	mockMessage,
-	navigateToRoom,
-	sleep
-} from '../../helpers/app';
+import { navigateToLogin, login, tapBack, platformTypes, TTextMatcher, mockMessage, navigateToRoom } from '../../helpers/app';
 import { createRandomUser, ITestUser } from '../../helpers/data_setup';
 import random from '../../helpers/random';
 
@@ -130,7 +121,6 @@ describe('Join public room', () => {
 			await waitFor(element(by.id('room-view-join-button')))
 				.not.toBeVisible()
 				.withTimeout(2000);
-			await sleep(1000);
 			await tapBack();
 			await navigateToRoom(room);
 			await waitFor(element(by.id('message-composer')))
@@ -140,11 +130,11 @@ describe('Join public room', () => {
 			await expect(element(by.id('room-view-join'))).toBeNotVisible();
 		});
 
-		it.skip('should send message', async () => {
+		it('should send message', async () => {
 			await mockMessage(random(5));
 		});
 
-		it.skip('should have notifications and leave channel', async () => {
+		it('should have notifications and leave channel', async () => {
 			await navigateToRoomActions();
 			await expect(element(by.id('room-actions-view'))).toBeVisible();
 			await expect(element(by.id('room-actions-info'))).toBeVisible();
@@ -159,7 +149,7 @@ describe('Join public room', () => {
 			await expect(element(by.id('room-actions-leave-channel'))).toBeVisible();
 		});
 
-		it.skip('should leave room', async () => {
+		it('should leave room', async () => {
 			await element(by.id('room-actions-leave-channel')).tap();
 			await waitFor(element(by[textMatcher]('Yes, leave it!').and(by.type(alertButtonType))))
 				.toBeVisible()
