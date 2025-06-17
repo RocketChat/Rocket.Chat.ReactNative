d62d1e847a48a9ccc4e8ba2e746ad12ba85fea84
Rohit Bansal <40559587+Rohit3523@users.noreply.github.com>
2025-06-17 21:10:16 +0530

    Lint should fail

--- app/views/AuthLoadingView.tsx
+++ app/views/AuthLoadingView.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
-
 import I18n from '../i18n';
 import StatusBar from '../containers/StatusBar';
 import { useTheme } from '../theme';
