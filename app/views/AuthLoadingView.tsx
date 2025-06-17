4ccf33535ee73e43029865b3c830124f4d1eee7b
Rohit Bansal <40559587+Rohit3523@users.noreply.github.com>
2025-06-17 21:12:03 +0530

    Fixed lint error :skull:

--- app/views/AuthLoadingView.tsx
+++ app/views/AuthLoadingView.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+
 import I18n from '../i18n';
 import StatusBar from '../containers/StatusBar';
 import { useTheme } from '../theme';
