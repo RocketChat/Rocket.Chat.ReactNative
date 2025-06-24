b3105bec890295a7fbcc325ddfc3723ba5b854de
OtavioStasiak <otaviostasiakmusic@outlook.com>
2025-06-24 12:11:57 -0300

    fix: remove input text color

--- app/containers/TextInput/FormTextInput.tsx
+++ app/containers/TextInput/FormTextInput.tsx
@@ -150,7 +150,6 @@ export const FormTextInput = ({
 								},
 								inputError
 									? {
-											color: colors.buttonBackgroundDangerDefault,
 											borderColor: colors.buttonBackgroundDangerDefault
 									  }
 									: {},
