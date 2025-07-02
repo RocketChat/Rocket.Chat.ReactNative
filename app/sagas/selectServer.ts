68822166dfa618d52c678ada3a1ca955fe1b85b6
OtavioStasiak <otaviostasiakmusic@outlook.com>
2025-07-02 17:17:59 -0300

    fix: remove duplicated a11y announcement error

--- app/sagas/selectServer.ts
+++ app/sagas/selectServer.ts
@@ -1,5 +1,4 @@
 import { put, takeLatest } from 'redux-saga/effects';
-import { AccessibilityInfo } from 'react-native';
 import { sanitizedRaw } from '@nozbe/watermelondb/RawRecord';
 import { Q } from '@nozbe/watermelondb';
 import valid from 'semver/functions/valid';
@@ -104,13 +103,11 @@ const getServerInfoSaga = function* getServerInfoSaga({ server, raiseError = tru
 		const serverInfoResult = yield* call(getServerInfo, server);
 		if (raiseError) {
 			if (!serverInfoResult.success) {
-				AccessibilityInfo.announceForAccessibility(I18n.t('Invalid_URL'));
 				yield put(serverFailure(I18n.t('Invalid_URL')));
 				return;
 			}
 			const websocketInfo = yield* call(Services.getWebsocketInfo, { server });
 			if (!websocketInfo.success) {
-				AccessibilityInfo.announceForAccessibility(I18n.t('Invalid_URL'));
 				yield put(serverFailure(I18n.t('Invalid_URL')));
 				return;
 			}
