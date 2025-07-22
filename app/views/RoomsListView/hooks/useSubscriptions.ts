317f5f6309bb7ad4702905f685008a3a4ed5e381
Diego Mello <diegolmello@gmail.com>
2025-07-22 16:07:20 -0300

    basic rendering

---
+++ app/views/RoomsListView/hooks/useSubscriptions.ts
@@ -0,0 +1,51 @@
+import { useEffect, useRef, useState } from 'react';
+import { Q } from '@nozbe/watermelondb';
+import { Subscription } from 'rxjs';
+
+import { SortBy } from '../../../lib/constants';
+import database from '../../../lib/database';
+import { useAppSelector } from '../../../lib/hooks';
+import { TSubscriptionModel } from '../../../definitions';
+
+export const useSubscriptions = ({ isGrouping, sortBy }: { isGrouping: boolean; sortBy: SortBy }) => {
+	console.count(`${useSubscriptions.name}.render calls`);
+	const useRealName = useAppSelector(state => state.settings.UI_Use_Real_Name);
+	const subscriptionRef = useRef<Subscription>(null);
+	const [subscriptions, setSubscriptions] = useState<TSubscriptionModel[]>([]);
+
+	useEffect(() => {
+		const getSubscriptions = async () => {
+			const db = database.active;
+
+			const whereClause = [Q.where('archived', false), Q.where('open', true)] as (Q.WhereDescription | Q.SortBy)[];
+
+			if (sortBy === SortBy.Alphabetical) {
+				whereClause.push(Q.sortBy(`${useRealName ? 'fname' : 'name'}`, Q.asc));
+			} else {
+				whereClause.push(Q.sortBy('room_updated_at', Q.desc));
+			}
+
+			const observeWithColumns = isGrouping ? ['alert', 'on_hold', 'f'] : ['on_hold'];
+
+			const observable = await db
+				.get('subscriptions')
+				.query(...whereClause)
+				.observeWithColumns(observeWithColumns);
+
+			subscriptionRef.current = observable.subscribe(data => {
+				setSubscriptions(data);
+			});
+		};
+
+		getSubscriptions();
+
+		return () => {
+			console.countReset(`${useSubscriptions.name}.render calls`);
+			subscriptionRef.current?.unsubscribe();
+		};
+	}, [isGrouping, sortBy, useRealName]);
+
+	return {
+		subscriptions
+	};
+};
