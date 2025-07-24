871b2d3d45b0f088b8316aaa937b97c9264a0568
Diego Mello <diegolmello@gmail.com>
2025-07-24 10:58:47 -0300

    Migrate useSearch to useReducer

--- app/views/RoomsListView/hooks/useSearch.ts
+++ app/views/RoomsListView/hooks/useSearch.ts
@@ -1,38 +1,81 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useReducer } from 'react';
 
 import { search as searchLib } from '../../../lib/methods';
 import { IRoomItem } from '../definitions';
 import { useDebounce } from '../../../lib/methods/helpers/debounce';
 
+interface SearchState {
+	searchEnabled: boolean;
+	searching: boolean;
+	searchResults: IRoomItem[];
+}
+
+type SearchAction =
+	| { type: 'START_SEARCH' }
+	| { type: 'SEARCH_SUCCESS'; payload: IRoomItem[] }
+	| { type: 'STOP_SEARCH' }
+	| { type: 'SET_SEARCHING' };
+
+const initialState: SearchState = {
+	searchEnabled: false,
+	searching: false,
+	searchResults: []
+};
+
+const searchReducer = (state: SearchState, action: SearchAction): SearchState => {
+	switch (action.type) {
+		case 'START_SEARCH':
+			return {
+				...state,
+				searchEnabled: true,
+				searching: true
+			};
+		case 'SEARCH_SUCCESS':
+			return {
+				...state,
+				searching: false,
+				searchResults: action.payload
+			};
+		case 'STOP_SEARCH':
+			return {
+				...state,
+				searchEnabled: false,
+				searching: false,
+				searchResults: []
+			};
+		case 'SET_SEARCHING':
+			return {
+				...state,
+				searching: true
+			};
+		default:
+			return state;
+	}
+};
+
 export const useSearch = () => {
-	const [searchEnabled, setSearchEnabled] = useState(false);
-	const [searching, setSearching] = useState(false);
-	const [searchResults, setSearchResults] = useState<IRoomItem[]>([]);
+	const [state, dispatch] = useReducer(searchReducer, initialState);
 
 	const search = useDebounce(async (text: string) => {
-		if (!searchEnabled) return;
-		setSearching(true);
+		if (!state.searchEnabled) return;
+		dispatch({ type: 'SET_SEARCHING' });
 		const result = await searchLib({ text });
-		setSearchResults(result as IRoomItem[]);
-		setSearching(false);
+		dispatch({ type: 'SEARCH_SUCCESS', payload: result as IRoomItem[] });
 	}, 500);
 
 	const startSearch = useCallback(() => {
-		setSearchEnabled(true);
-		setSearching(true);
+		dispatch({ type: 'START_SEARCH' });
 		search('');
 	}, [search]);
 
 	const stopSearch = useCallback(() => {
-		setSearchEnabled(false);
-		setSearching(false);
-		setSearchResults([]);
+		dispatch({ type: 'STOP_SEARCH' });
 	}, []);
 
 	return {
-		searching,
-		searchEnabled,
-		searchResults,
+		searching: state.searching,
+		searchEnabled: state.searchEnabled,
+		searchResults: state.searchResults,
 		startSearch,
 		stopSearch,
 		search
