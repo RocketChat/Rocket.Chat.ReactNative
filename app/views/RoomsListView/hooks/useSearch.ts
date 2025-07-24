1cc24e8b5b6821f851262c1efd1c81cf1c001258
Diego Mello <diegolmello@gmail.com>
2025-07-24 10:49:30 -0300

    Remove search room from redux

--- app/views/RoomsListView/hooks/useSearch.ts
+++ app/views/RoomsListView/hooks/useSearch.ts
@@ -1,19 +1,15 @@
 import { useCallback, useState } from 'react';
-import { useDispatch } from 'react-redux';
 
-import { closeSearchHeader, openSearchHeader } from '../../../actions/rooms';
 import { search as searchLib } from '../../../lib/methods';
 import { IRoomItem } from '../definitions';
 import { useDebounce } from '../../../lib/methods/helpers/debounce';
 
 export const useSearch = () => {
-	const dispatch = useDispatch();
 	const [searchEnabled, setSearchEnabled] = useState(false);
 	const [searching, setSearching] = useState(false);
 	const [searchResults, setSearchResults] = useState<IRoomItem[]>([]);
 
 	const search = useDebounce(async (text: string) => {
-		console.log('search', text);
 		if (!searchEnabled) return;
 		setSearching(true);
 		const result = await searchLib({ text });
@@ -25,15 +21,13 @@ export const useSearch = () => {
 		setSearchEnabled(true);
 		setSearching(true);
 		search('');
-		dispatch(openSearchHeader());
-	}, [dispatch, search]);
+	}, [search]);
 
 	const stopSearch = useCallback(() => {
 		setSearchEnabled(false);
 		setSearching(false);
 		setSearchResults([]);
-		dispatch(closeSearchHeader());
-	}, [dispatch]);
+	}, []);
 
 	return {
 		searching,
