11bbe100ce17619bfaf6365d277ae1e139de4598
OtavioStasiak <otaviostasiakmusic@outlook.com>
2025-07-28 14:10:44 -0300

    fix: new copy

--- app/views/CreateDiscussionView/index.tsx
+++ app/views/CreateDiscussionView/index.tsx
@@ -31,7 +31,7 @@ import handleSubmitEvent from './utils/handleSubmitEvent';
 import useA11yErrorAnnouncement from '../../lib/hooks/useA11yErrorAnnouncement';
 
 const schema = yup.object().shape({
-	name: yup.string().required(I18n.t('Name_is_required'))
+	name: yup.string().required(I18n.t('Discussion_name_required'))
 });
 
 const CreateDiscussionView = ({ route, navigation }: ICreateChannelViewProps) => {
