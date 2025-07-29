06af315be36e8e7ef84e8713e5c5b88bca94c0f5
Diego Mello <diegolmello@gmail.com>
2025-07-29 16:45:19 -0300

    Profile

--- app/views/SidebarView/index.tsx
+++ app/views/SidebarView/index.tsx
@@ -1,29 +1,20 @@
 import { memo, useEffect, useState } from 'react';
 import { DrawerNavigationProp } from '@react-navigation/drawer';
-import { ScrollView, Text, TouchableWithoutFeedback, View } from 'react-native';
-import { shallowEqual } from 'react-redux';
+import { ScrollView } from 'react-native';
 
-import Avatar from '../../containers/Avatar';
 import scrollPersistTaps from '../../lib/methods/helpers/scrollPersistTaps';
-import { useTheme } from '../../theme';
-import { getUserSelector } from '../../selectors/login';
 import SafeAreaView from '../../containers/SafeAreaView';
 import styles from './styles';
 import { DrawerParamList } from '../../stacks/types';
-import * as List from '../../containers/List';
 import SupportedVersionsWarnItem from './components/SupportedVersionsWarnItem';
 import { useAppSelector } from '../../lib/hooks';
 import CustomStatus from './components/CustomStatus';
 import Stacks from './components/Stacks';
 import Admin from './components/Admin';
+import Profile from './components/Profile';
 
-const Sidebar = ({ navigation }: { navigation: DrawerNavigationProp<DrawerParamList> }) => {
+const SidebarView = ({ navigation }: { navigation: DrawerNavigationProp<DrawerParamList> }) => {
 	const isMasterDetail = useAppSelector(state => state.app.isMasterDetail);
-	const { username, name } = useAppSelector(getUserSelector, shallowEqual);
-	const { colors } = useTheme();
-	const useRealName = useAppSelector(state => state.settings.UI_Use_Real_Name);
-	const server = useAppSelector(state => state.server.server);
-	const siteName = useAppSelector(state => state.settings.Site_Name) as string;
 	const [currentScreen, setCurrentScreen] = useState<string | null>(null);
 
 	useEffect(() => {
@@ -34,37 +25,10 @@ const Sidebar = ({ navigation }: { navigation: DrawerNavigationProp<DrawerParamL
 		return unsubscribe;
 	}, [navigation]);
 
-	const onPressUser = () => {
-		if (isMasterDetail) {
-			return;
-		}
-		navigation.closeDrawer();
-	};
-
 	return (
 		<SafeAreaView testID='sidebar-view' vertical={isMasterDetail}>
 			<ScrollView style={styles.container} {...scrollPersistTaps}>
-				<List.Separator />
-				<TouchableWithoutFeedback onPress={onPressUser} testID='sidebar-close-drawer'>
-					<View style={[styles.header, { backgroundColor: colors.surfaceRoom }]}>
-						<Avatar text={username} style={styles.avatar} size={30} />
-						<View style={styles.headerTextContainer}>
-							<View style={styles.headerUsername}>
-								<Text numberOfLines={1} style={[styles.username, { color: colors.fontTitlesLabels }]}>
-									{useRealName ? name : username}
-								</Text>
-							</View>
-							<Text
-								style={[styles.currentServerText, { color: colors.fontTitlesLabels }]}
-								numberOfLines={1}
-								accessibilityLabel={`Connected to ${server}`}>
-								{siteName}
-							</Text>
-						</View>
-					</View>
-				</TouchableWithoutFeedback>
-				<List.Separator />
-
+				<Profile navigation={navigation} />
 				<SupportedVersionsWarnItem />
 				<CustomStatus />
 				<Stacks currentScreen={currentScreen} />
@@ -74,4 +38,4 @@ const Sidebar = ({ navigation }: { navigation: DrawerNavigationProp<DrawerParamL
 	);
 };
 
-export default memo(Sidebar);
+export default memo(SidebarView);
