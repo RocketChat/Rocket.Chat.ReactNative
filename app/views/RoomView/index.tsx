152eb25f85913f55d3c5c71a5dd16bd707002df5
Diego Mello <diegolmello@gmail.com>
2025-08-05 14:44:06 -0300

    Cleanup

--- app/views/RoomView/index.tsx
+++ app/views/RoomView/index.tsx
@@ -102,7 +102,6 @@ import UserPreferences from '../../lib/methods/userPreferences';
 import { IRoomViewProps, IRoomViewState } from './definitions';
 import { roomAttrsUpdate, stateAttrsUpdate } from './constants';
 import { EncryptedRoom, MissingRoomE2EEKey } from './components';
-import { EmojiKeyboardProvider } from '../../containers/MessageComposer/hooks/useEmojiKeyboard';
 
 class RoomView extends React.Component<IRoomViewProps, IRoomViewState> {
 	private rid?: string;
@@ -1520,51 +1519,49 @@ class RoomView extends React.Component<IRoomViewProps, IRoomViewState> {
 		}
 
 		return (
-			<EmojiKeyboardProvider>
-				<RoomContext.Provider
-					value={{
-						rid,
-						t,
-						tmid: this.tmid,
-						sharing: false,
-						action,
-						selectedMessages,
-						updateAutocompleteVisible: this.updateAutocompleteVisible,
-						isAutocompleteVisible,
-						onRemoveQuoteMessage: this.onRemoveQuoteMessage,
-						editCancel: this.onEditCancel,
-						editRequest: this.onEditRequest,
-						onSendMessage: this.handleSendMessage,
-						setQuotesAndText: this.setQuotesAndText,
-						getText: this.getText
-					}}>
-					<SafeAreaView style={{ backgroundColor: themes[theme].surfaceRoom }} testID='room-view'>
-						{!this.tmid ? (
-							<Banner
-								title={I18n.t('Announcement')}
-								text={announcement}
-								bannerClosed={bannerClosed}
-								closeBanner={this.closeBanner}
-							/>
-						) : null}
-						<List
-							ref={this.list}
-							listRef={this.flatList}
-							rid={rid}
-							tmid={this.tmid}
-							renderRow={this.renderItem}
-							loading={loading}
-							hideSystemMessages={this.hideSystemMessages}
-							showMessageInMainThread={user.showMessageInMainThread ?? false}
-							serverVersion={serverVersion}
+			<RoomContext.Provider
+				value={{
+					rid,
+					t,
+					tmid: this.tmid,
+					sharing: false,
+					action,
+					selectedMessages,
+					updateAutocompleteVisible: this.updateAutocompleteVisible,
+					isAutocompleteVisible,
+					onRemoveQuoteMessage: this.onRemoveQuoteMessage,
+					editCancel: this.onEditCancel,
+					editRequest: this.onEditRequest,
+					onSendMessage: this.handleSendMessage,
+					setQuotesAndText: this.setQuotesAndText,
+					getText: this.getText
+				}}>
+				<SafeAreaView style={{ backgroundColor: themes[theme].surfaceRoom }} testID='room-view'>
+					{!this.tmid ? (
+						<Banner
+							title={I18n.t('Announcement')}
+							text={announcement}
+							bannerClosed={bannerClosed}
+							closeBanner={this.closeBanner}
 						/>
-						{this.renderFooter()}
-						{this.renderActions()}
-						<UploadProgress rid={rid} user={user} baseUrl={baseUrl} width={width} />
-						<JoinCode ref={this.joinCode} onJoin={this.onJoin} rid={rid} t={t} theme={theme} />
-					</SafeAreaView>
-				</RoomContext.Provider>
-			</EmojiKeyboardProvider>
+					) : null}
+					<List
+						ref={this.list}
+						listRef={this.flatList}
+						rid={rid}
+						tmid={this.tmid}
+						renderRow={this.renderItem}
+						loading={loading}
+						hideSystemMessages={this.hideSystemMessages}
+						showMessageInMainThread={user.showMessageInMainThread ?? false}
+						serverVersion={serverVersion}
+					/>
+					{this.renderFooter()}
+					{this.renderActions()}
+					<UploadProgress rid={rid} user={user} baseUrl={baseUrl} width={width} />
+					<JoinCode ref={this.joinCode} onJoin={this.onJoin} rid={rid} t={t} theme={theme} />
+				</SafeAreaView>
+			</RoomContext.Provider>
 		);
 	}
 }
