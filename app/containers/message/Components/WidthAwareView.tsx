86500e3bb56c2a4db8748b5e6f723901fa0e29fa
Diego Mello <diegolmello@gmail.com>
2025-08-05 11:43:46 -0300

    Rollback WidthAwareView

--- app/containers/message/Components/WidthAwareView.tsx
+++ app/containers/message/Components/WidthAwareView.tsx
@@ -1,30 +1,25 @@
-import { createContext, ReactElement, useState, useRef, useLayoutEffect } from 'react';
+import { createContext, ReactElement, useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 
 export const WidthAwareContext = createContext(0);
 
 const styles = StyleSheet.create({
 	container: {
-		flexDirection: 'row'
+		flex: 1
 	}
 });
 
 export const WidthAwareView = ({ children }: { children: ReactElement }) => {
 	const [width, setWidth] = useState(0);
-	const viewRef = useRef<View>(null);
-
-	useLayoutEffect(() => {
-		if (viewRef.current) {
-			viewRef.current.measure((x, y, w) => {
-				if (w) {
-					setWidth(w);
-				}
-			});
-		}
-	}, [viewRef]);
 
 	return (
-		<View ref={viewRef} style={styles.container}>
+		<View
+			style={styles.container}
+			onLayout={ev => {
+				if (ev.nativeEvent.layout.width) {
+					setWidth(ev.nativeEvent.layout.width);
+				}
+			}}>
 			<WidthAwareContext.Provider value={width}>{children}</WidthAwareContext.Provider>
 		</View>
 	);
