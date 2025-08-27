5b94cf89ac5642fb22c056d9b178f11bb41c3971
Rohit Bansal <40559587+Rohit3523@users.noreply.github.com>
2025-08-27 20:32:21 +0530

    More changes

--- app/lib/methods/helpers/review.ts
+++ app/lib/methods/helpers/review.ts
@@ -86,7 +86,7 @@ class ReviewApp {
 	positiveEventCount = 0;
 
 	pushPositiveEvent = () => {
-		if (process.env.RUNNING_E2E_TESTS === 'true') {
+        if ('true' === 'true') {
 			return;
 		}
 		if (this.positiveEventCount >= numberOfPositiveEvent) {
