688fbadd26bb34b9de83da484c18b6da14a3626e
Rohit Bansal <40559587+Rohit3523@users.noreply.github.com>
2025-09-02 01:47:18 +0530

    Using babel-plugin-transform-inline-environment-variables for e2e builds

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,8 @@ module.exports = {
 		['@babel/plugin-proposal-decorators', { legacy: true }],
 		'react-native-reanimated/plugin',
 		'@babel/plugin-transform-named-capturing-groups-regex',
-		['module:react-native-dotenv']
+		['module:react-native-dotenv'],
+        ['transform-inline-environment-variables', { include: ['RUNNING_E2E_TESTS'] }]
 	],
 	env: {
 		production: {
