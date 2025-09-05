405bbc94bbe4e0782fc491b6c3654e64bb354d2a
Diego Mello <yiweigao0226@gmail.com>
2025-09-05 13:57:26 -0300

    feat(Android): Migrate SSLPinning to new arch (#6627)

---
+++ android/app/src/main/java/chat/rocket/reactnative/networking/NativeSSLPinningSpec.java
@@ -0,0 +1,32 @@
+/**
+ * This file was manually created to replace the auto-generated codegen spec
+ * for Android-only SSLPinning TurboModule with 2-parameter signature.
+ */
+
+package chat.rocket.reactnative.networking;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.turbomodule.core.interfaces.TurboModule;
+
+public abstract class NativeSSLPinningSpec extends ReactContextBaseJavaModule implements TurboModule {
+
+  public static final String NAME = "SSLPinning";
+
+  public NativeSSLPinningSpec(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @Override
+  public String getName() {
+    return NAME;
+  }
+
+  @ReactMethod
+  public abstract void setCertificate(String server, Promise promise);
+
+  @ReactMethod
+  public abstract void pickCertificate(Promise promise);
+}
