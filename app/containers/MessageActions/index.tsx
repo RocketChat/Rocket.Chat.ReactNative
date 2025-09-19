e3ae6703cbaa60d2ca08f27aa97b63312896c68d
Rohit Bansal <40559587+Rohit3523@users.noreply.github.com>
2025-09-19 22:28:09 +0530

    passing messageId and starred as params in handleStar instead of watermelon active object

--- app/containers/MessageActions/index.tsx
+++ app/containers/MessageActions/index.tsx
@@ -291,12 +291,12 @@ const MessageActions = React.memo(
 				}
 			};
 
-			const handleStar = async (message: TAnyMessageModel) => {
-				logEvent(message.starred ? events.ROOM_MSG_ACTION_UNSTAR : events.ROOM_MSG_ACTION_STAR);
+			const handleStar = async (messageId: string, starred: boolean) => {
+				logEvent(starred ? events.ROOM_MSG_ACTION_UNSTAR : events.ROOM_MSG_ACTION_STAR);
 				try {
-					await Services.toggleStarMessage(message.id, message.starred as boolean); // TODO: reevaluate `message.starred` type on IMessage
+					await Services.toggleStarMessage(messageId, starred);
 
-					EventEmitter.emit(LISTENER, { message: message.starred ? I18n.t('Message_starred') : I18n.t('Message_unstarred') });
+					EventEmitter.emit(LISTENER, { message: starred ? I18n.t('Message_unstarred') : I18n.t('Message_starred') });
 				} catch (e) {
 					logEvent(events.ROOM_MSG_ACTION_STAR_F);
 					log(e);
@@ -486,7 +486,7 @@ const MessageActions = React.memo(
 					options.push({
 						title: I18n.t(message.starred ? 'Unstar' : 'Star'),
 						icon: message.starred ? 'star-filled' : 'star',
-						onPress: () => handleStar(message)
+						onPress: () => handleStar(message.id, message.starred)
 					});
 				}
 
