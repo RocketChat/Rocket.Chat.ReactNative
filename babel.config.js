9241fb974ed01465c5e7b3db18306426a425b08d
Rohit Bansal <40559587+Rohit3523@users.noreply.github.com>
2025-09-24 21:02:46 +0530

    Added react-compiler :)

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
 	presets: ['babel-preset-expo'],
 	plugins: [
+        'babel-plugin-react-compiler',
 		['@babel/plugin-proposal-decorators', { legacy: true }],
 		'react-native-reanimated/plugin',
 		'@babel/plugin-transform-named-capturing-groups-regex',
