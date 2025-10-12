abab68f9a772d1f0d086672c70811f2ed7de9fa6
Rohit Bansal <40559587+Rohit3523@users.noreply.github.com>
2025-10-13 02:46:10 +0530

    remove unused

--- app/containers/AudioPlayer/Seek.tsx
+++ app/containers/AudioPlayer/Seek.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { LayoutChangeEvent, View, TextInput, TextInputProps, TouchableNativeFeedback } from 'react-native';
-import { Gesture, GestureDetector, PanGestureHandlerGestureEvent } from 'react-native-gesture-handler';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 import Animated, {
     SharedValue,
     useAnimatedProps,
