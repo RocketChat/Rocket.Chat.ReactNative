c0a4d2586e5baab266652d1d135a7809af5b4df8
Rohit3523 <Rohit3523@users.noreply.github.com>
2025-10-12 21:21:17 +0000

    chore: format code with Prettier [skip ci]

--- app/containers/AudioPlayer/Seek.tsx
+++ app/containers/AudioPlayer/Seek.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { LayoutChangeEvent, View, TextInput, TextInputProps, TouchableNativeFeedback } from 'react-native';
 import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 import Animated, {
-    SharedValue,
-    useAnimatedProps,
-    useAnimatedStyle,
-    useDerivedValue,
-    useSharedValue,
-    withTiming
+	SharedValue,
+	useAnimatedProps,
+	useAnimatedStyle,
+	useDerivedValue,
+	useSharedValue,
+	withTiming
 } from 'react-native-reanimated';
 import { scheduleOnRN } from 'react-native-worklets';
 
@@ -19,118 +19,117 @@ Animated.addWhitelistedNativeProps({ text: true });
 const AnimatedTextInput = Animated.createAnimatedComponent(TextInput);
 
 interface ISeek {
-    duration: SharedValue<number>;
-    currentTime: SharedValue<number>;
-    loaded: boolean;
-    onChangeTime: (time: number) => Promise<void>;
+	duration: SharedValue<number>;
+	currentTime: SharedValue<number>;
+	loaded: boolean;
+	onChangeTime: (time: number) => Promise<void>;
 }
 
 function clamp(value: number, min: number, max: number) {
-    'worklet';
+	'worklet';
 
-    return Math.min(Math.max(value, min), max);
+	return Math.min(Math.max(value, min), max);
 }
 
 // https://docs.swmansion.com/react-native-reanimated/docs/2.x/fundamentals/worklets/
 const formatTime = (ms: number) => {
-    'worklet';
+	'worklet';
 
-    const minutes = Math.floor(ms / 60);
-    const remainingSeconds = Math.floor(ms % 60);
-    const formattedMinutes = String(minutes).padStart(2, '0');
-    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
-    return `${formattedMinutes}:${formattedSeconds}`;
+	const minutes = Math.floor(ms / 60);
+	const remainingSeconds = Math.floor(ms % 60);
+	const formattedMinutes = String(minutes).padStart(2, '0');
+	const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+	return `${formattedMinutes}:${formattedSeconds}`;
 };
 
 const Seek = ({ currentTime, duration, loaded = false, onChangeTime }: ISeek) => {
-    const { colors } = useTheme();
+	const { colors } = useTheme();
 
-    const maxWidth = useSharedValue(1);
-    const translateX = useSharedValue(0);
-    const timeLabel = useSharedValue(DEFAULT_TIME_LABEL);
-    const scale = useSharedValue(1);
-    const isPanning = useSharedValue(false);
-    const contextX = useSharedValue(0);
+	const maxWidth = useSharedValue(1);
+	const translateX = useSharedValue(0);
+	const timeLabel = useSharedValue(DEFAULT_TIME_LABEL);
+	const scale = useSharedValue(1);
+	const isPanning = useSharedValue(false);
+	const contextX = useSharedValue(0);
 
-    const styleLine = useAnimatedStyle(() => ({
-        width: translateX.value
-    }));
+	const styleLine = useAnimatedStyle(() => ({
+		width: translateX.value
+	}));
 
-    const styleThumb = useAnimatedStyle(() => ({
-        transform: [{ translateX: translateX.value - THUMB_SEEK_SIZE / 2 }, { scale: scale.value }]
-    }));
+	const styleThumb = useAnimatedStyle(() => ({
+		transform: [{ translateX: translateX.value - THUMB_SEEK_SIZE / 2 }, { scale: scale.value }]
+	}));
 
-    const onLayout = (event: LayoutChangeEvent) => {
-        const { width } = event.nativeEvent.layout;
-        maxWidth.value = width;
-    };
+	const onLayout = (event: LayoutChangeEvent) => {
+		const { width } = event.nativeEvent.layout;
+		maxWidth.value = width;
+	};
 
-    const panGesture = Gesture.Pan()
-        .enabled(loaded)
-        .activeOffsetX([-ACTIVE_OFFSET_X, ACTIVE_OFFSET_X])
-        .onBegin(() => {
-            isPanning.value = true;
-        })
-        .onStart(() => {
-            contextX.value = translateX.value;
-        })
-        .onUpdate((event) => {
-            const newX = contextX.value + event.translationX;
-            translateX.value = clamp(newX, 0, maxWidth.value);
-            scale.value = withTiming(1.3, { duration: 150 });
-        })
-        .onEnd(() => {
-            scale.value = withTiming(1, { duration: 150 });
-            isPanning.value = false;
-            scheduleOnRN(onChangeTime, Math.round(currentTime.value * 1000));
-        });
+	const panGesture = Gesture.Pan()
+		.enabled(loaded)
+		.activeOffsetX([-ACTIVE_OFFSET_X, ACTIVE_OFFSET_X])
+		.onBegin(() => {
+			isPanning.value = true;
+		})
+		.onStart(() => {
+			contextX.value = translateX.value;
+		})
+		.onUpdate(event => {
+			const newX = contextX.value + event.translationX;
+			translateX.value = clamp(newX, 0, maxWidth.value);
+			scale.value = withTiming(1.3, { duration: 150 });
+		})
+		.onEnd(() => {
+			scale.value = withTiming(1, { duration: 150 });
+			isPanning.value = false;
+			scheduleOnRN(onChangeTime, Math.round(currentTime.value * 1000));
+		});
 
+	useDerivedValue(() => {
+		if (isPanning.value) {
+			// When the user is panning, always the currentTime.value is been set different from the currentTime provided by
+			// the audio in progress
+			currentTime.value = (translateX.value * duration.value) / maxWidth.value || 0;
+		} else {
+			translateX.value = (currentTime.value * maxWidth.value) / duration.value || 0;
+		}
+		timeLabel.value = formatTime(currentTime.value);
+	}, [translateX, maxWidth, duration, isPanning, currentTime]);
 
-    useDerivedValue(() => {
-        if (isPanning.value) {
-            // When the user is panning, always the currentTime.value is been set different from the currentTime provided by
-            // the audio in progress
-            currentTime.value = (translateX.value * duration.value) / maxWidth.value || 0;
-        } else {
-            translateX.value = (currentTime.value * maxWidth.value) / duration.value || 0;
-        }
-        timeLabel.value = formatTime(currentTime.value);
-    }, [translateX, maxWidth, duration, isPanning, currentTime]);
+	const timeLabelAnimatedProps = useAnimatedProps(() => {
+		if (currentTime.value !== 0) {
+			return {
+				text: timeLabel.value
+			} as TextInputProps;
+		}
+		return {
+			text: formatTime(duration.value)
+		} as TextInputProps;
+	}, [timeLabel, duration, currentTime]);
 
-    const timeLabelAnimatedProps = useAnimatedProps(() => {
-        if (currentTime.value !== 0) {
-            return {
-                text: timeLabel.value
-            } as TextInputProps;
-        }
-        return {
-            text: formatTime(duration.value)
-        } as TextInputProps;
-    }, [timeLabel, duration, currentTime]);
+	const thumbColor = loaded ? colors.buttonBackgroundPrimaryDefault : colors.buttonBackgroundPrimaryDisabled;
 
-    const thumbColor = loaded ? colors.buttonBackgroundPrimaryDefault : colors.buttonBackgroundPrimaryDisabled;
-
-    // TouchableNativeFeedback is avoiding do a long press message when seeking the audio
-    return (
-        <TouchableNativeFeedback>
-            <View style={styles.seekContainer}>
-                <AnimatedTextInput
-                    defaultValue={DEFAULT_TIME_LABEL}
-                    editable={false}
-                    style={[styles.duration, { color: colors.fontDefault }]}
-                    animatedProps={timeLabelAnimatedProps}
-                />
-                <View style={styles.seek} onLayout={onLayout}>
-                    <View style={[styles.line, { backgroundColor: colors.strokeLight }]}>
-                        <Animated.View style={[styles.line, styleLine, { backgroundColor: colors.buttonBackgroundPrimaryDefault }]} />
-                    </View>
-                    <GestureDetector gesture={panGesture}>
-                        <Animated.View hitSlop={SEEK_HIT_SLOP} style={[styles.thumbSeek, { backgroundColor: thumbColor }, styleThumb]} />
-                    </GestureDetector>
-                </View>
-            </View>
-        </TouchableNativeFeedback>
-    );
+	// TouchableNativeFeedback is avoiding do a long press message when seeking the audio
+	return (
+		<TouchableNativeFeedback>
+			<View style={styles.seekContainer}>
+				<AnimatedTextInput
+					defaultValue={DEFAULT_TIME_LABEL}
+					editable={false}
+					style={[styles.duration, { color: colors.fontDefault }]}
+					animatedProps={timeLabelAnimatedProps}
+				/>
+				<View style={styles.seek} onLayout={onLayout}>
+					<View style={[styles.line, { backgroundColor: colors.strokeLight }]}>
+						<Animated.View style={[styles.line, styleLine, { backgroundColor: colors.buttonBackgroundPrimaryDefault }]} />
+					</View>
+					<GestureDetector gesture={panGesture}>
+						<Animated.View hitSlop={SEEK_HIT_SLOP} style={[styles.thumbSeek, { backgroundColor: thumbColor }, styleThumb]} />
+					</GestureDetector>
+				</View>
+			</View>
+		</TouchableNativeFeedback>
+	);
 };
 
 export default Seek;
