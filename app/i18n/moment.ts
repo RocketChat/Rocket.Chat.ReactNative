d3f820c44024611ff8a5b743a9f9682590ed5f07
Rohit Bansal <40559587+Rohit3523@users.noreply.github.com>
2025-10-14 02:08:02 +0530

    if locale is missing in json, return the same

--- app/i18n/moment.ts
+++ app/i18n/moment.ts
@@ -1,22 +1,10 @@
 const localeKeys: { [key: string]: string } = {
-	en: 'en',
-	ar: 'ar',
-	de: 'de',
 	'es-ES': 'es',
-	fi: 'fi',
-	fr: 'fr',
-	it: 'it',
-	ja: 'ja',
-	nl: 'nl',
 	'pt-BR': 'pt-br',
 	'pt-PT': 'pt',
-	ru: 'ru',
 	'sl-SI': 'sl',
-	sv: 'sv',
-	tr: 'tr',
 	'zh-CN': 'zh-cn',
 	'zh-TW': 'zh-tw',
-    'hi': 'hi'
 };
 
-export const toMomentLocale = (locale: string): string => localeKeys[locale];
+export const toMomentLocale = (locale: string): string => localeKeys[locale] || locale;
