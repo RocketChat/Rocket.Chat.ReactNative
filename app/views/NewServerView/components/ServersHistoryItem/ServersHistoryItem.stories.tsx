85a927530f9d0b45d1bc8e451ebde03b22e43f53
OtavioStasiak <otaviostasiakmusic@outlook.com>
2025-10-15 19:39:47 -0300

    chore: serversHistoryItem stories

---
+++ app/views/NewServerView/components/ServersHistoryItem/ServersHistoryItem.stories.tsx
@@ -0,0 +1,79 @@
+import React from 'react';
+
+import { themes } from '../../../../lib/constants/colors';
+import ServersHistoryItemComponent, { IServersHistoryItem } from '.';
+import { ThemeContext, TSupportedThemes } from '../../../../theme';
+import { TServerHistoryModel } from '../../../../definitions';
+
+export default {
+	title: 'ServersHistoryItem'
+};
+
+const defaultItem = {
+	id: '1',
+	url: 'https://open.rocket.chat',
+	username: 'john.doe',
+	updatedAt: new Date(),
+	iconURL: 'https://open.rocket.chat/images/logo/android-chrome-512x512.png'
+} as TServerHistoryModel;
+
+const ServersHistoryItem = ({
+	item,
+	theme = 'light',
+	onPress = () => alert('Press'),
+	onDeletePress
+}: {
+	item?: Partial<TServerHistoryModel>;
+	theme?: TSupportedThemes;
+	onPress?: IServersHistoryItem['onPress'];
+	onDeletePress?: IServersHistoryItem['onDeletePress'];
+}) => (
+	<ThemeContext.Provider
+		value={{
+			theme,
+			colors: themes[theme]
+		}}>
+		<ServersHistoryItemComponent
+			item={{ ...defaultItem, ...item } as TServerHistoryModel}
+			onPress={onPress}
+			onDeletePress={onDeletePress || (() => alert('Delete'))}
+		/>
+	</ThemeContext.Provider>
+);
+
+export const Content = () => (
+	<>
+		<ServersHistoryItem />
+		<ServersHistoryItem
+			item={{
+				url: 'https://superlongservername.tologintoasuperlongservername.rocket.chat',
+				username: 'very.long.username.here'
+			}}
+		/>
+		<ServersHistoryItem
+			item={{
+				url: 'https://stable.rocket.chat',
+				username: 'admin',
+				iconURL: undefined
+			}}
+		/>
+	</>
+);
+
+export const SwipeActions = () => (
+	<>
+		<ServersHistoryItem onDeletePress={() => alert('Delete Server History')} />
+		<ServersHistoryItem
+			item={{ url: 'https://example.com', username: 'user123' }}
+			onDeletePress={() => alert('Delete Server History')}
+		/>
+	</>
+);
+
+export const Themes = () => (
+	<>
+		<ServersHistoryItem theme={'light'} />
+		<ServersHistoryItem theme={'dark'} />
+		<ServersHistoryItem theme={'black'} />
+	</>
+);
