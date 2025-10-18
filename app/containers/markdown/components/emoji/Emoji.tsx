7bbf773edd14d2c2e4fb172a2d16ad500459b26e
Rohit Bansal <40559587+Rohit3523@users.noreply.github.com>
2025-10-19 01:11:14 +0530

    Lint fix

--- app/containers/markdown/components/emoji/Emoji.tsx
+++ app/containers/markdown/components/emoji/Emoji.tsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import { Text, View, useWindowDimensions } from 'react-native';
-import { Emoji as EmojiProps } from '@rocket.chat/message-parser';
+import { type Emoji as EmojiProps } from '@rocket.chat/message-parser';
 
 import Plain from '../Plain';
 import useShortnameToUnicode from '../../../../lib/hooks/useShortnameToUnicode';
