3bb2339bba8f3fce4f973093b5cbfe1de9d52b73
Rohit Bansal <40559587+Rohit3523@users.noreply.github.com>
2025-11-04 01:21:37 +0530

    Fix unistyle index file

--- app/unistyles.ts
+++ app/unistyles.ts
@@ -4,15 +4,16 @@ import { initialTheme } from './lib/methods/helpers/theme';
 import { themes } from './lib/constants/colors';
 
 type AppThemes = typeof themes;
-// type AppBreakpoints = typeof breakpoints
 
 declare module 'react-native-unistyles' {
 	export interface UnistylesThemes extends AppThemes {}
-	// export interface UnistylesBreakpoints extends AppBreakpoints {}
 }
 
 const settings = {
-	initialTheme: 'black' as keyof UnistylesThemes
+	initialTheme: () => {
+        const theme = initialTheme();
+        return theme.currentTheme === 'dark' ? theme.darkLevel : (theme.currentTheme as keyof UnistylesThemes) 
+    }
 };
 
 StyleSheet.configure({
