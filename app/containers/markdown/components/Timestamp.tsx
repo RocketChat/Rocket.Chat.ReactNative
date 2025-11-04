93048fac6beede376108fae64b22d679aff39cc5
Rohit Bansal <40559587+Rohit3523@users.noreply.github.com>
2025-11-04 21:31:44 +0530

    Format update

--- app/containers/markdown/components/Timestamp.tsx
+++ app/containers/markdown/components/Timestamp.tsx
@@ -17,27 +17,27 @@ const Timestamp = ({ value }: ITimestampProps): React.ReactElement => {
 		const timestamp = parseInt(value.timestamp) * 1000;
 
 		if (value.format === 't') {
-			return moment(timestamp).format('hh:mm A');
+			return moment(timestamp).format('LT');
 		}
 
 		if (value.format === 'T') {
-			return moment(timestamp).format('hh:mm:ss A');
+			return moment(timestamp).format('LTS');
 		}
 
 		if (value.format === 'd') {
-			return moment(timestamp).format('MM/DD/YYYY');
+			return moment(timestamp).format('L');
 		}
 
 		if (value.format === 'D') {
-			return moment(timestamp).format('dddd, MMM DD, YYYY');
+			return moment(timestamp).format('LL');
 		}
 
 		if (value.format === 'f') {
-			return moment(timestamp).format('dddd, MMM DD, YYYY hh:mm A');
+			return moment(timestamp).format('LLL');
 		}
 
 		if (value.format === 'F') {
-			return moment(timestamp).format('dddd, MMM DD, YYYY hh:mm:ss A');
+			return moment(timestamp).format('LLLL');
 		}
 
 		if (value.format === 'R') {
