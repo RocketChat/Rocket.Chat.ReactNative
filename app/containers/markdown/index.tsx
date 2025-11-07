3629d176123b6f25b3ba650fff3c9bf98317dfe1
Divyanshu patil <work.divyanshupatil@gmail.com>
2025-11-08 02:10:49 +0530

    fix: unique key prop warnings for all components

--- app/containers/markdown/index.tsx
+++ app/containers/markdown/index.tsx
@@ -69,32 +69,35 @@ const Markdown: React.FC<IMarkdownProps> = ({
 					getCustomEmoji,
 					onLinkPress
 				}}>
-				{tokens?.map(block => {
+				{tokens?.map((block, index) => {
+					// sliced to avoid very long keys
+					// key example: PARAGRAPH-this is value of 20 -3
+					const key = `${block.type}-${block.value?.toString().slice(0, 20)}-${index}`;
 					switch (block.type) {
 						case 'BIG_EMOJI':
-							return <BigEmoji value={block.value} />;
+							return <BigEmoji key={key} value={block.value} />;
 						case 'UNORDERED_LIST':
-							return <UnorderedList value={block.value} />;
+							return <UnorderedList key={key} value={block.value} />;
 						case 'ORDERED_LIST':
-							return <OrderedList value={block.value} />;
+							return <OrderedList key={key} value={block.value} />;
 						case 'TASKS':
-							return <TaskList value={block.value} />;
+							return <TaskList key={key} value={block.value} />;
 						case 'QUOTE':
-							return <Quote value={block.value} />;
+							return <Quote key={key} value={block.value} />;
 						case 'PARAGRAPH':
-							return <Paragraph value={block.value} />;
+							return <Paragraph key={key} value={block.value} />;
 						case 'CODE':
-							return <Code value={block.value} />;
+							return <Code key={key} value={block.value} />;
 						case 'HEADING':
-							return <Heading value={block.value} level={block.level} />;
+							return <Heading key={key} value={block.value} level={block.level} />;
 						case 'LINE_BREAK':
-							return <LineBreak />;
+							return <LineBreak key={key} />;
 						// This prop exists, but not even on the web it is treated, so...
 						// https://github.com/RocketChat/Rocket.Chat/blob/develop/packages/gazzodown/src/Markup.tsx
 						// case 'LIST_ITEM':
 						// 	return <View />;
 						case 'KATEX':
-							return <KaTeX value={block.value} />;
+							return <KaTeX key={key} value={block.value} />;
 						default:
 							return null;
 					}
