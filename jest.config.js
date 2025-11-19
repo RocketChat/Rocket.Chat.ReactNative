bbda8b8c5933f7002d669313e397c4c6e5c6108b
Rohit Bansal <40559587+Rohit3523@users.noreply.github.com>
2025-11-20 02:03:17 +0530

    Added unistyle mock in jest

--- jest.config.js
+++ jest.config.js
@@ -10,5 +10,5 @@ module.exports = {
 		'.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
 		'.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js'
 	},
-	setupFilesAfterEnv: ['./jest.setup.js', './node_modules/react-native-gesture-handler/jestSetup.js']
+	setupFilesAfterEnv: ['./jest.setup.js', './node_modules/react-native-gesture-handler/jestSetup.js', 'react-native-unistyles/mocks']
 };
