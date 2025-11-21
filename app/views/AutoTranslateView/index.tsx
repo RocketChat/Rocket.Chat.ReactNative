d46b6df5d66b1c6c940aa54eddd43fca41eb29b5
OtavioStasiak <otaviostasiakmusic@outlook.com>
2025-11-21 12:58:51 -0300

    chore: code improvements

--- app/views/AutoTranslateView/index.tsx
+++ app/views/AutoTranslateView/index.tsx
@@ -28,7 +28,6 @@ const AutoTranslateView = (): React.ReactElement => {
 	const [languages, setLanguages] = useState<{ language: string; name: string }[]>([]);
 	const [selectedLanguage, setSelectedLanguage] = useState<string | undefined>(room?.autoTranslateLanguage);
 	const [enableAutoTranslate, setEnableAutoTranslate] = useState<boolean | undefined>(room?.autoTranslate);
-
 	const subscription = useRef<Subscription | null>(null);
 
 	useLayoutEffect(() => {
@@ -51,7 +50,6 @@ const AutoTranslateView = (): React.ReactElement => {
 	useEffect(() => {
 		let letSelectedLanguage = selectedLanguage;
 		let letAutoTranslate = enableAutoTranslate;
-
 		subscription.current = room.observe().subscribe((changes: ISubscription) => {
 			if (letSelectedLanguage !== changes.autoTranslateLanguage) {
 				setSelectedLanguage(changes.autoTranslateLanguage);
@@ -110,6 +108,7 @@ const AutoTranslateView = (): React.ReactElement => {
 			additionalAcessibilityLabelCheck
 		/>
 	));
+
 	return (
 		<SafeAreaView>
 			<FlatList
