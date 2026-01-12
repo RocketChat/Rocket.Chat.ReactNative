252ae5c386be7b002e2e216e92c974d33e4dbf39
Divyanshu patil <work.divyanshupatil@gmail.com>
2026-01-12 20:17:36 +0530

    cleanup: removed outdated code on MainActivity.kt

--- android/app/src/main/java/chat/rocket/reactnative/MainActivity.kt
+++ android/app/src/main/java/chat/rocket/reactnative/MainActivity.kt
@@ -6,16 +6,14 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnable
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 import android.os.Bundle
+import android.os.Build
+import android.os.PersistableBundle
 import com.zoontek.rnbootsplash.RNBootSplash
 import android.content.Intent
 import android.content.res.Configuration
-import android.net.Uri
-import chat.rocket.reactnative.notification.VideoConfModule
-import chat.rocket.reactnative.notification.VideoConfNotification
-import com.google.gson.GsonBuilder
-
 import chat.rocket.reactnative.notification.NotificationIntentHandler
- 
+import androidx.core.net.toUri
+
 class MainActivity : ReactActivity() {
 
   /**
@@ -32,6 +30,7 @@ class MainActivity : ReactActivity() {
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
 
   override fun onCreate(savedInstanceState: Bundle?) {
+    // quick actions handling
     intent?.let {
       embedQuickActionIntoLinking(it)
     }
@@ -39,66 +38,23 @@ class MainActivity : ReactActivity() {
     RNBootSplash.init(this, R.style.BootTheme)
     super.onCreate(null)
 
-    // Handle video conf action from notification
-    intent?.let {
-      handleVideoConfIntent(it)
-    }
-    
     // Handle notification intents
     intent?.let { NotificationIntentHandler.handleIntent(this, it) }
   }
 
   public override fun onNewIntent(intent: Intent) {
     super.onNewIntent(intent)
-    // Handle video conf action when activity is already running
-    embedQuickActionIntoLinking(intent)
-    handleVideoConfIntent(intent)
-  }
-
-  private fun handleVideoConfIntent(intent: Intent) {
-    if (intent.getBooleanExtra("videoConfAction", false)) {
-      val notificationId = intent.getIntExtra("notificationId", 0)
-      val event = intent.getStringExtra("event") ?: return
-      val rid = intent.getStringExtra("rid") ?: ""
-      val callerId = intent.getStringExtra("callerId") ?: ""
-      val callerName = intent.getStringExtra("callerName") ?: ""
-      val host = intent.getStringExtra("host") ?: ""
-      val callId = intent.getStringExtra("callId") ?: ""
-
-      android.util.Log.d("RocketChat.MainActivity", "Handling video conf intent - event: $event, rid: $rid, host: $host, callId: $callId")
-
-      // Cancel the notification
-      if (notificationId != 0) {
-        VideoConfNotification.cancelById(this, notificationId)
-      }
-
-      // Store action for JS to pick up - include all required fields
-      val data = mapOf(
-        "notificationType" to "videoconf",
-        "rid" to rid,
-        "event" to event,
-        "host" to host,
-        "callId" to callId,
-        "caller" to mapOf(
-          "_id" to callerId,
-          "name" to callerName
-        )
-      )
-
-      val gson = GsonBuilder().create()
-      val jsonData = gson.toJson(data)
-
-      android.util.Log.d("RocketChat.MainActivity", "Storing video conf action: $jsonData")
-
-      VideoConfModule.storePendingAction(this, jsonData)
-
-      // Clear the video conf flag to prevent re-processing
-      intent.removeExtra("videoConfAction")
-    }
     setIntent(intent)
-    
+
+    // quick actions handling
+    embedQuickActionIntoLinking(intent)
+
     // Handle notification intents when activity is already running
     NotificationIntentHandler.handleIntent(this, intent)
+  }
+
+  override fun invokeDefaultOnBackPressed() {
+    moveTaskToBack(true)
   }
 
   private fun embedQuickActionIntoLinking(intent: Intent) {
@@ -109,27 +65,38 @@ class MainActivity : ReactActivity() {
     // Handle Expo quick actions
     if (action == "expo.modules.quickactions.SHORTCUT") {
       // Get the PersistableBundle
-      val shortcutData = intent.getParcelableExtra<android.os.PersistableBundle>("shortcut_data")
+      val shortcutData: PersistableBundle? =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+          intent.getParcelableExtra(
+            "shortcut_data",
+            PersistableBundle::class.java
+          )
+        } else {
+          @Suppress("DEPRECATION")
+          intent.getParcelableExtra("shortcut_data")
+        }
 
       if (shortcutData != null) {
-        // Log all keys in the bundle
-        // kept for debugging later
-        // android.util.Log.d("RocketChat.QuickAction", "=== Shortcut Data Bundle ===")
-        // for (key in shortcutData.keySet()) {
-        //    val value = shortcutData.get(key)
-        //    android.util.Log.d("RocketChat.QuickAction", "Key: $key, Value: $value")
-        //  }
-        //  android.util.Log.d("RocketChat.QuickAction", "============================")
+        /**
+         * Log all keys in the bundle
+         * kept for debugging later
+         */
+        /*
+        android.util.Log.d("RocketChat.QuickAction", "=== Shortcut Data Bundle ===")
+            for (key in shortcutData.keySet()) {
+              val value = shortcutData.get(key)
+              android.util.Log.d("RocketChat.QuickAction", "Key: $key, Value: $value")
+            }
+         android.util.Log.d("RocketChat.QuickAction", "============================")
+         */
 
         // Try to get the shortcut ID from various possible keys
         val shortcutId = shortcutData.getString("id")
           ?: shortcutData.getString("shortcutId")
           ?: shortcutData.getString("android.intent.extra.shortcut.ID")
 
-        android.util.Log.d("RocketChat.QuickAction", "Expo shortcut ID: $shortcutId")
-
         if (shortcutId != null) {
-          val uri = Uri.parse("rocketchat://quick-action/$shortcutId")
+          val uri = "rocketchat://quick-action/$shortcutId".toUri()
 
           android.util.Log.d("RocketChat.QuickAction", "Converted to: $uri")
 
@@ -145,9 +112,7 @@ class MainActivity : ReactActivity() {
       return
     }
 
-    // Handle non-Expo quick actions
-    // we does not need this as currently we are setting it from expo only
-    // TODO: remove later
+    // skip for non-Expo quick actions (app launches)
     if (!action.startsWith("chat.rocket.reactnative.")) {
       android.util.Log.d("RocketChat.QuickAction", "Not a quick action, skipping")
       return
@@ -158,18 +123,12 @@ class MainActivity : ReactActivity() {
       .lowercase()
       .replace('_', '-')
 
-    val uri = Uri.parse("rocketchat://quick-action/$quickAction")
-
-    android.util.Log.d("RocketChat.QuickAction", "Converted to: $uri")
+    val uri = "rocketchat://quick-action/$quickAction".toUri()
 
     intent.action = Intent.ACTION_VIEW
     intent.data = uri
     setIntent(intent)
 
     android.util.Log.d("RocketChat.QuickAction", "Intent set with data: ${getIntent().data}")
-  }
-
-  override fun invokeDefaultOnBackPressed() {
-    moveTaskToBack(true)
   }
 }
