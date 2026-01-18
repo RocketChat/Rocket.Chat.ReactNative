3f8a600107ec82f69c5dad98c1c74ec7d670db5e
Rohit Bansal <40559587+Rohit3523@users.noreply.github.com>
2026-01-18 16:33:59 +0530

    Lint fix

--- app/views/CannedResponsesListView/DepartmentFilter/index.tsx
+++ app/views/CannedResponsesListView/DepartmentFilter/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FlatList, View } from 'react-native';
+import { FlatList } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { BottomSheetView } from '@gorhom/bottom-sheet';
 
