312daf8fcb32a2b21db29c25b4746484b2e0f67a
Rohit3523 <Rohit3523@users.noreply.github.com>
2026-01-22 15:37:06 +0000

    chore: format code and fix lint issues [skip ci]

--- app/views/RoomView/index.tsx
+++ app/views/RoomView/index.tsx
@@ -850,7 +850,7 @@ class RoomView extends React.Component<IRoomViewProps, IRoomViewState> {
 						reactionClose={this.onReactionClose}
 					/>
 				),
-                snaps: ['50%'],
+				snaps: ['50%'],
 				enableContentPanningGesture: false,
 				onClose: this.resetAction
 			});
