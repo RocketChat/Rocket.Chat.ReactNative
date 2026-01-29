4837b1a5201325334be314328ae31dfc968ff856
OtavioStasiak <OtavioStasiak@users.noreply.github.com>
2026-01-29 20:37:02 +0000

    chore: format code and fix lint issues [skip ci]

--- app/views/RoomView/List/components/InvertedScrollView.tsx
+++ app/views/RoomView/List/components/InvertedScrollView.tsx
@@ -17,15 +17,14 @@ const COMMAND_SCROLL_TO = 1;
 const COMMAND_SCROLL_TO_END = 2;
 const COMMAND_FLASH_SCROLL_INDICATORS = 3;
 
-
 type ScrollViewPropsWithRef = ScrollViewProps & React.RefAttributes<NativeScrollInstance | null>;
 type NativeScrollInstance = React.ComponentRef<NonNullable<typeof NativeInvertedScrollView>>;
 interface IScrollableMethods {
-    scrollTo(options?: { x?: number; y?: number; animated?: boolean }): void;
-    scrollToEnd(options?: { animated?: boolean }): void;
-    flashScrollIndicators(): void;
-    getScrollRef(): NativeScrollInstance | null;
-    setNativeProps(props: object): void;
+	scrollTo(options?: { x?: number; y?: number; animated?: boolean }): void;
+	scrollToEnd(options?: { animated?: boolean }): void;
+	flashScrollIndicators(): void;
+	getScrollRef(): NativeScrollInstance | null;
+	setNativeProps(props: object): void;
 }
 
 export type InvertedScrollViewRef = NativeScrollInstance & IScrollableMethods;
@@ -37,89 +36,89 @@ const NativeInvertedScrollContentView = isAndroid
 	: null;
 
 const InvertedScrollView = forwardRef<InvertedScrollViewRef, ScrollViewProps>((props, externalRef) => {
-	const internalRef = useRef<NativeScrollInstance | null>(null);  
+	const internalRef = useRef<NativeScrollInstance | null>(null);
 
-    useLayoutEffect(() => {
-        const node = internalRef.current as any;
-        
-        if (node) {
+	useLayoutEffect(() => {
+		const node = internalRef.current as any;
 
-            // 1. Implementation of scrollTo
-            node.scrollTo = (options?: { x?: number; y?: number; animated?: boolean }) => {
-                const tag = findNodeHandle(node);
-                if (tag!= null) {
-                    const x = options?.x || 0;
-                    const y = options?.y || 0;
-                    const animated = options?.animated !== false;
-                    UIManager.dispatchViewManagerCommand(tag, COMMAND_SCROLL_TO, [x, y, animated]);
-                }
-            };
+		if (node) {
+			// 1. Implementation of scrollTo
+			node.scrollTo = (options?: { x?: number; y?: number; animated?: boolean }) => {
+				const tag = findNodeHandle(node);
+				if (tag != null) {
+					const x = options?.x || 0;
+					const y = options?.y || 0;
+					const animated = options?.animated !== false;
+					UIManager.dispatchViewManagerCommand(tag, COMMAND_SCROLL_TO, [x, y, animated]);
+				}
+			};
 
-            // 2. Implementation of scrollToEnd
-            node.scrollToEnd = (options?: { animated?: boolean }) => {
-                const tag = findNodeHandle(node);
-                if (tag!= null) {
-                    const animated = options?.animated !== false;
-                    UIManager.dispatchViewManagerCommand(tag, COMMAND_SCROLL_TO_END, [animated]);
-                }
-            };
+			// 2. Implementation of scrollToEnd
+			node.scrollToEnd = (options?: { animated?: boolean }) => {
+				const tag = findNodeHandle(node);
+				if (tag != null) {
+					const animated = options?.animated !== false;
+					UIManager.dispatchViewManagerCommand(tag, COMMAND_SCROLL_TO_END, [animated]);
+				}
+			};
 
-            // 3. Implementation of flashScrollIndicators
-            node.flashScrollIndicators = () => {
-                const tag = findNodeHandle(node as any);
-                if (tag !== null) {
-                    UIManager.dispatchViewManagerCommand(tag, COMMAND_FLASH_SCROLL_INDICATORS, []);
-                }
-            };
+			// 3. Implementation of flashScrollIndicators
+			node.flashScrollIndicators = () => {
+				const tag = findNodeHandle(node as any);
+				if (tag !== null) {
+					UIManager.dispatchViewManagerCommand(tag, COMMAND_FLASH_SCROLL_INDICATORS, []);
+				}
+			};
 
-            node.getScrollRef = () => node;
-            
-            if (typeof node.setNativeProps!== 'function') {
-                node.setNativeProps = (nativeProps: object) => {
-                     // Check again if the underlying node has the method hidden
-                     if (node && typeof (node as any).setNativeProps === 'function') {
-                         (node as any).setNativeProps(nativeProps);
-                     }
-                };
-            }
-        }
-    });
+			node.getScrollRef = () => node;
 
-    // Callback Ref to handle merging internal and external refs
-    const setRef = (node: NativeScrollInstance | null) => {
-        internalRef.current = node;
-        
-        if (typeof externalRef === 'function') {
-            externalRef(node as InvertedScrollViewRef);
-        } else if (externalRef) {
-            (externalRef as React.MutableRefObject<NativeScrollInstance | null>).current = node;
-        }
-    };
-	
-	const { 
-        children,
-        contentContainerStyle,
-        onContentSizeChange,
-        removeClippedSubviews,
-        maintainVisibleContentPosition,
-        snapToAlignment,
-        stickyHeaderIndices,
-       ...rest
-    } = props;
+			if (typeof node.setNativeProps !== 'function') {
+				node.setNativeProps = (nativeProps: object) => {
+					// Check again if the underlying node has the method hidden
+					if (node && typeof (node as any).setNativeProps === 'function') {
+						(node as any).setNativeProps(nativeProps);
+					}
+				};
+			}
+		}
+	});
 
-    const preserveChildren = maintainVisibleContentPosition != null || (isAndroid && snapToAlignment != null);
-    const hasStickyHeaders = Array.isArray(stickyHeaderIndices) && stickyHeaderIndices.length > 0;
+	// Callback Ref to handle merging internal and external refs
+	const setRef = (node: NativeScrollInstance | null) => {
+		internalRef.current = node;
 
-    const contentContainerStyleArray = [props.horizontal ? { flexDirection: 'row' as const } : null, contentContainerStyle];
+		if (typeof externalRef === 'function') {
+			externalRef(node as InvertedScrollViewRef);
+		} else if (externalRef) {
+			(externalRef as React.MutableRefObject<NativeScrollInstance | null>).current = node;
+		}
+	};
 
-    const contentSizeChangeProps = onContentSizeChange == null
-       ? undefined
-        : {
-            onLayout: (e: LayoutChangeEvent) => {
-                const { width, height } = e.nativeEvent.layout;
-                onContentSizeChange(width, height);
-            }
-        };
+	const {
+		children,
+		contentContainerStyle,
+		onContentSizeChange,
+		removeClippedSubviews,
+		maintainVisibleContentPosition,
+		snapToAlignment,
+		stickyHeaderIndices,
+		...rest
+	} = props;
+
+	const preserveChildren = maintainVisibleContentPosition != null || (isAndroid && snapToAlignment != null);
+	const hasStickyHeaders = Array.isArray(stickyHeaderIndices) && stickyHeaderIndices.length > 0;
+
+	const contentContainerStyleArray = [props.horizontal ? { flexDirection: 'row' as const } : null, contentContainerStyle];
+
+	const contentSizeChangeProps =
+		onContentSizeChange == null
+			? undefined
+			: {
+					onLayout: (e: LayoutChangeEvent) => {
+						const { width, height } = e.nativeEvent.layout;
+						onContentSizeChange(width, height);
+					}
+			  };
 
 	const horizontal = !!props.horizontal;
 	const baseStyle = horizontal ? styles.baseHorizontal : styles.baseVertical;
