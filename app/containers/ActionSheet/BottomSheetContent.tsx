7fb67b63db452e1cd4e304453ccf81009ccc6349
OtavioStasiak <otaviostasiakmusic@outlook.com>
2026-02-10 16:38:43 -0300

    fix: android custom content behavior

--- app/containers/ActionSheet/BottomSheetContent.tsx
+++ app/containers/ActionSheet/BottomSheetContent.tsx
@@ -61,7 +61,7 @@ const BottomSheetContent = React.memo(({ options, hasCancel, hide, children, onL
 		);
 	}
 	return (
-		<View testID='action-sheet' style={styles.contentContainer} onLayout={onLayout}>
+		<View testID='action-sheet' style={[styles.contentContainer]} onLayout={onLayout}>
 			{children}
 		</View>
 	);
