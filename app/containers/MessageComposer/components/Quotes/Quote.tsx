14113e59c3b5e111b241bf122d6463ea0182290b
Tasso Evangelista <tasso.evangelista@rocket.chat>
2026-02-13 22:40:37 -0300

    chore(eslint): enable some React Native rules

--- app/containers/MessageComposer/components/Quotes/Quote.tsx
+++ app/containers/MessageComposer/components/Quotes/Quote.tsx
@@ -49,6 +49,7 @@ export const Quote = ({ messageId }: { messageId: string }) => {
 					testID={`composer-quote-remove-${message.id}`}
 				/>
 			</View>
+			{/* eslint-disable-next-line react-native/no-single-element-style-arrays */}
 			<MarkdownPreview style={[styles.message]} numberOfLines={1} msg={msg} />
 		</View>
 	);
--- app/views/ThreadMessagesView/Item.tsx
+++ app/views/ThreadMessagesView/Item.tsx
@@ -88,6 +88,7 @@ const Item = ({ item, useRealName, user, badgeColor, onPress, toggleFollowThread
 						<Text style={[styles.time, { color: themes[theme].fontSecondaryInfo }]}>{time}</Text>
 					</View>
 					<View style={styles.messageContainer}>
+						{/* eslint-disable-next-line react-native/no-single-element-style-arrays */}
 						<MarkdownPreview msg={makeThreadName(item)} numberOfLines={2} style={[styles.markdown]} />
 						{badgeColor ? <View style={[styles.badge, { backgroundColor: badgeColor }]} /> : null}
 					</View>
