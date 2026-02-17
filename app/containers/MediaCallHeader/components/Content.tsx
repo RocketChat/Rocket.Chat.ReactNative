5efdc7bd29a94d811f45f147bc1e39ffea6e8a3b
Diego Mello <diegolmello@gmail.com>
2026-02-17 17:29:07 -0300

    Add nav to room mock to MediaCallHeader

--- app/containers/MediaCallHeader/components/Content.tsx
+++ app/containers/MediaCallHeader/components/Content.tsx
@@ -1,11 +1,22 @@
-import { View } from 'react-native';
+import { Pressable, StyleSheet, View } from 'react-native';
 
 import Title from './Title';
 import Subtitle from './Subtitle';
 
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		flexDirection: 'column',
+		justifyContent: 'space-between',
+		alignItems: 'center'
+	}
+});
+
 export const Content = () => (
-	<View style={{ flex: 1, flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center' }}>
-		<Title />
-		<Subtitle />
-	</View>
+	<Pressable onPress={() => alert('nav to call room')}>
+		<View style={styles.container}>
+			<Title />
+			<Subtitle />
+		</View>
+	</Pressable>
 );
