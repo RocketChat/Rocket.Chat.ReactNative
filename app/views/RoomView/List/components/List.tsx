4db3a04670a18cca851798d5c2b0c33b04f1ce0c
OtavioStasiak <otaviostasiakmusic@outlook.com>
2026-03-05 13:11:41 -0300

    rollback List

--- app/views/RoomView/List/components/List.tsx
+++ app/views/RoomView/List/components/List.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Platform, StyleSheet, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Animated, { runOnJS, useAnimatedScrollHandler } from 'react-native-reanimated';
 
 import { isIOS } from '../../../../lib/methods/helpers';
@@ -22,39 +22,6 @@ const styles = StyleSheet.create({
 const List = ({ listRef, jumpToBottom, ...props }: IListProps) => {
 	const [visible, setVisible] = useState(false);
 	const { isAutocompleteVisible } = useRoomContext();
-	const { data, renderItem, ...flatListProps } = props;
-
-	const renderItemWithFocus: IListProps['renderItem'] = info => {
-		if (!renderItem) {
-			return null as any;
-		}
-
-		if (Platform.OS !== 'android') {
-			return renderItem(info);
-		}
-
-		const total = data?.length || 0;
-		const { index } = info;
-
-		const nextFocusUp = index < total - 1 ? index + 1 : undefined;
-		const nextFocusDown = index > 0 ? index + 1 : undefined;
-		return (
-			<View
-				nativeID={`${index}`}
-				focusable
-				{...(Platform.OS === 'android'
-					? {
-							// @ts-ignore Android-only props not in ViewProps types; accepts native component refs
-							nextFocusUp,
-							// @ts-ignore Android-only props not in ViewProps types; accepts native component refs
-							nextFocusDown
-					  }
-					: null)}>
-				{renderItem(info)}
-			</View>
-		);
-	};
-
 	const scrollHandler = useAnimatedScrollHandler({
 		onScroll: event => {
 			if (event.contentOffset.y > SCROLL_LIMIT) {
@@ -69,18 +36,15 @@ const List = ({ listRef, jumpToBottom, ...props }: IListProps) => {
 		<View style={styles.list}>
 			{/* @ts-ignore */}
 			<Animated.FlatList
-				{...flatListProps}
 				accessibilityElementsHidden={isAutocompleteVisible}
 				importantForAccessibility={isAutocompleteVisible ? 'no-hide-descendants' : 'yes'}
 				testID='room-view-messages'
 				ref={listRef}
 				keyExtractor={item => item.id}
-				data={data}
-				renderItem={renderItemWithFocus}
 				contentContainerStyle={styles.contentContainer}
 				style={styles.list}
-				inverted={props.inverted || true}
-				renderScrollComponent={isIOS ? undefined : scrollProps => <InvertedScrollView {...scrollProps} />}
+				inverted
+				renderScrollComponent={isIOS ? undefined : props => <InvertedScrollView {...props} />}
 				removeClippedSubviews={isIOS}
 				initialNumToRender={7}
 				onEndReachedThreshold={0.5}
@@ -88,6 +52,7 @@ const List = ({ listRef, jumpToBottom, ...props }: IListProps) => {
 				windowSize={10}
 				scrollEventThrottle={16}
 				onScroll={scrollHandler}
+				{...props}
 				{...scrollPersistTaps}
 			/>
 			<NavBottomFAB visible={visible} onPress={jumpToBottom} />
