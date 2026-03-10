15588e28d567e2e7be7b5fd688d5e25040635e54
Rohit <40559587+Rohit3523@users.noreply.github.com>
2026-03-10 17:56:24 +0530

    space revert

--- android/app/src/main/java/chat/rocket/reactnative/MainActivity.kt
+++ android/app/src/main/java/chat/rocket/reactnative/MainActivity.kt
@@ -1,5 +1,5 @@
 package chat.rocket.reactnative
-
+ 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -13,13 +13,13 @@ import chat.rocket.reactnative.notification.NotificationIntentHandler
 import chat.rocket.reactnative.notification.CustomPushNotification
 
 class MainActivity : ReactActivity() {
-
+ 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
   override fun getMainComponentName(): String = "RocketChatRN"
-
+ 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
@@ -30,7 +30,7 @@ class MainActivity : ReactActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     RNBootSplash.init(this, R.style.BootTheme)
     super.onCreate(null)
-
+    
     // Handle notification intents
     intent?.let { NotificationIntentHandler.handleIntent(this, it) }
   }
@@ -50,7 +50,7 @@ class MainActivity : ReactActivity() {
   public override fun onNewIntent(intent: Intent) {
     super.onNewIntent(intent)
     setIntent(intent)
-
+    
     // Handle notification intents when activity is already running
     NotificationIntentHandler.handleIntent(this, intent)
   }
