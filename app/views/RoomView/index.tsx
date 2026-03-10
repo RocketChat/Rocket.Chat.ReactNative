9feee82b0f83f6ee8e479c591d691a1e0570a100
OtavioStasiak <otaviostasiakmusic@outlook.com>
2026-03-09 23:20:52 -0300

    fix: e2e tests

--- app/views/RoomView/index.tsx
+++ app/views/RoomView/index.tsx
@@ -915,7 +915,8 @@ class RoomView extends React.Component<IRoomViewProps, IRoomViewState> {
 		this.handleCloseEmoji(showActionSheet, {
 			children: <ReactionsList reactions={message?.reactions} getCustomEmoji={this.getCustomEmoji} />,
 			snaps: ['50%'],
-			enableContentPanningGesture: false
+			enableContentPanningGesture: false,
+			fullContainer: true
 		});
 	};
 
