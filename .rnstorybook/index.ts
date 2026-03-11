1a060cfa40c01d47356539b52904af550589a3ca
Rohit <40559587+Rohit3523@users.noreply.github.com>
2026-03-11 19:45:59 +0530

    storybook ui test

--- .rnstorybook/index.ts
+++ .rnstorybook/index.ts
@@ -1,12 +1,46 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { view } from './storybook.requires';
 import RNBootSplash from 'react-native-bootsplash';
+import { Linking } from 'react-native';
 
 const StorybookUIRoot = view.getStorybookUI({
 	storage: {
 		getItem: AsyncStorage.getItem,
 		setItem: AsyncStorage.setItem
 	}
+});
+
+function openStoryFromUrl(url: string | null) {
+	if (!url) return
+
+	try {
+		const parsed = new URL(url)
+		const path = parsed.searchParams.get('path')
+
+		if (!path) return
+
+		const storyId = path.replace('/story/', '')
+
+		const channel = view._channel
+
+		const emit = () => {
+			channel.emit('setCurrentStory', { storyId })
+		}
+
+		if (channel) {
+			emit()
+		} else {
+			setTimeout(emit, 500)
+		}
+	} catch (e) {
+		console.log(e)
+	}
+}
+
+Linking.getInitialURL().then(openStoryFromUrl);
+
+Linking.addEventListener('url', event => {
+	openStoryFromUrl(event.url)
 });
 
 RNBootSplash.hide();
