c892c17fea8d1365fbd0aa0b748f68c5cda2daff
Divyanshu Patil <work.divyanshupatil@gmail.com>
2026-03-12 23:57:43 +0530

    add excel support

--- app/lib/methods/getFileIcon.ts
+++ app/lib/methods/getFileIcon.ts
@@ -4,7 +4,7 @@ export const getFileIcon = (filename: string): TIconsName => {
 	const lowerFilename = filename.toLowerCase();
 	if (lowerFilename.endsWith('.pdf')) return 'adobe-reader-monochromatic';
 	// if (lowerFilename.endsWith('.doc') || lowerFilename.endsWith('.docx')) return 'another';
-	// if (lowerFilename.endsWith('.xls') || lowerFilename.endsWith('.xlsx')) return 'another';
+	if (lowerFilename.endsWith('.xls') || lowerFilename.endsWith('.xlsx')) return 'file-sheet';
 	// if (lowerFilename.endsWith('.zip')) return 'another';
-	return 'document';
+	return 'file-document';
 };
