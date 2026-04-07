409965f043ca9849665c379786f581bec6422ec8
OtavioStasiak <otaviostasiakmusic@outlook.com>
2026-04-07 16:55:40 -0300

    fix: iOS native module

--- ios/ExternalInputModule.m
+++ ios/ExternalInputModule.m
@@ -1,7 +1,12 @@
 #import "ExternalInputModule.h"
 #import <GameController/GameController.h>
+#import <stdarg.h>
 
 @implementation ExternalInputModule
+{
+  BOOL _hasExternalKeyboard;
+  BOOL _hasInitializedKeyboardState;
+}
 
 RCT_EXPORT_MODULE(ExternalInput);
 
@@ -10,9 +15,48 @@ RCT_EXPORT_MODULE(ExternalInput);
   return NO;
 }
 
+- (instancetype)init
+{
+  self = [super init];
+  if (self) {
+    GCKeyboard *coalescedKeyboard = [GCKeyboard coalescedKeyboard];
+    _hasExternalKeyboard = coalescedKeyboard != nil;
+    _hasInitializedKeyboardState = YES;
+
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(handleKeyboardDidConnect:)
+                                                 name:GCKeyboardDidConnectNotification
+                                               object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(handleKeyboardDidDisconnect:)
+                                                 name:GCKeyboardDidDisconnectNotification
+                                               object:nil];
+  }
+  return self;
+}
+
+- (void)dealloc
+{
+  [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (void)handleKeyboardDidConnect:(NSNotification *)notification
+{
+  _hasExternalKeyboard = YES;
+  _hasInitializedKeyboardState = YES;
+}
+
+- (void)handleKeyboardDidDisconnect:(NSNotification *)notification
+{
+  _hasExternalKeyboard = NO;
+  _hasInitializedKeyboardState = YES;
+}
+
 RCT_EXPORT_BLOCKING_SYNCHRONOUS_METHOD(isExternalKeyboardConnected)
 {
-  return @([GCKeyboard coalescedKeyboard] != nil);
+  BOOL fallbackFromCoalesced = [GCKeyboard coalescedKeyboard] != nil;
+  BOOL hasExternalKeyboard = _hasInitializedKeyboardState ? _hasExternalKeyboard : fallbackFromCoalesced;
+  return @(hasExternalKeyboard);
 }
 
 @end
