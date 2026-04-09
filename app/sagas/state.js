9bec59b083b5440f8b034f7f42db6c0ecae108cb
Rohit <40559587+Rohit3523@users.noreply.github.com>
2026-04-10 02:10:21 +0530

    refresh user presence

--- app/sagas/state.js
+++ app/sagas/state.js
@@ -1,3 +1,4 @@
+import { Q } from '@nozbe/watermelondb';
 import { select, takeLatest } from 'redux-saga/effects';
 
 import log from '../lib/methods/helpers/log';
@@ -7,11 +8,33 @@ import { RootEnum } from '../definitions';
 import { checkAndReopen } from '../lib/services/connect';
 import { setUserPresenceOnline, setUserPresenceAway } from '../lib/services/restApi';
 import { checkPendingNotification } from '../lib/notifications';
+import database from '../lib/database';
+import { getUsersPresence } from '../lib/methods/getUsersPresence';
 
 const isAuthAndConnected = function* isAuthAndConnected() {
 	const login = yield select(state => state.login);
 	const meteor = yield select(state => state.meteor);
 	return login.isAuthenticated && meteor.connected;
+};
+
+const getDirectMessageUserIds = async () => {
+	try {
+		const db = database.active;
+		const subscriptionsCollection = db.get('subscriptions');
+		// Query for open direct message subscriptions that are not archived
+		const subscriptions = await subscriptionsCollection
+			.query(Q.where('t', 'd'), Q.where('open', true), Q.where('archived', false))
+			.fetch();
+		// Extract user IDs from uids field (direct messages store the other user's ID in uids)
+		const userIds = subscriptions
+			.map(sub => sub.uids?.[0])
+			.filter(Boolean);
+		// Remove duplicates
+		return [...new Set(userIds)];
+	} catch (e) {
+		log('[state.js] Error getting DM user IDs:', e);
+		return [];
+	}
 };
 
 const appHasComeBackToForeground = function* appHasComeBackToForeground() {
@@ -31,6 +54,11 @@ const appHasComeBackToForeground = function* appHasComeBackToForeground() {
 		checkPendingNotification().catch(e => {
 			log('[state.js] Error checking pending notification:', e);
 		});
+		// Refresh presence for DM users to ensure status is up-to-date after background
+		const dmUserIds = yield getDirectMessageUserIds();
+		if (dmUserIds.length > 0) {
+			yield getUsersPresence(dmUserIds);
+		}
 		return yield setUserPresenceOnline();
 	} catch (e) {
 		log(e);
