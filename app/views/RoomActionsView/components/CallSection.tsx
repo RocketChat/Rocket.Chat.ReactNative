80663c7c31b97c8677357b9dcbb071eceeee00ee
Diego Mello <diegolmello@gmail.com>
2026-04-16 15:01:20 -0300

    feat(voip): display video conf provider as subtitle (#7160)

--- app/views/RoomActionsView/components/CallSection.tsx
+++ app/views/RoomActionsView/components/CallSection.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import * as List from '../../../containers/List';
 import { useVideoConf } from '../../../lib/hooks/useVideoConf';
 import type { TSubscriptionModel } from '../../../definitions';
 import { useNewMediaCall } from '../../../lib/hooks/useNewMediaCall';
+import { videoConferenceGetCapabilities } from '../../../lib/services/restApi';
 
 export default function CallSection({
 	room,
@@ -14,6 +15,15 @@ export default function CallSection({
 }): React.ReactElement | null {
 	const { callEnabled, showInitCallActionSheet, disabledTooltip } = useVideoConf(room.rid);
 	const { openNewMediaCall, hasMediaCallPermission } = useNewMediaCall(room.rid);
+	const [providerName, setProviderName] = useState<string>();
+
+	useEffect(() => {
+		if (callEnabled) {
+			videoConferenceGetCapabilities()
+				.then((res: any) => setProviderName(res.providerName))
+				.catch(() => {});
+		}
+	}, [callEnabled]);
 
 	if (!hasMediaCallPermission && !callEnabled) {
 		return null;
@@ -39,6 +49,8 @@ export default function CallSection({
 				<>
 					<List.Item
 						title={'Video_call'}
+						subtitle={providerName ? `(${providerName})` : undefined}
+						translateSubtitle={false}
 						onPress={showInitCallActionSheet}
 						testID='room-actions-call'
 						left={() => <List.Icon name='video' />}
