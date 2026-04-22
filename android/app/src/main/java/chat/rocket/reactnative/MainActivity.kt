31d37c4070e0f1a6869dbe4692ef09961863cc62
OtavioStasiak <otaviostasiakmusic@outlook.com>
2026-04-22 17:04:29 -0300

    rollback mainActivity

--- android/app/src/main/java/chat/rocket/reactnative/MainActivity.kt
+++ android/app/src/main/java/chat/rocket/reactnative/MainActivity.kt
@@ -4,6 +4,7 @@ import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+
 import android.os.Bundle
 import com.zoontek.rnbootsplash.RNBootSplash
 import android.content.Intent
