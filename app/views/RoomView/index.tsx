f91620b6ee505a5a846b72c4299ba996823829c7
Rohit <40559587+Rohit3523@users.noreply.github.com>
2026-04-29 00:31:15 +0530

    fix emoji scroll in reaction

--- app/views/RoomView/index.tsx
+++ app/views/RoomView/index.tsx
@@ -854,6 +854,7 @@ class RoomView extends React.Component<IRoomViewProps, IRoomViewState> {
 					/>
 				),
 				snaps: ['50%'],
+				enableContentPanningGesture: false,
 				onClose: this.resetAction,
 				fullContainer: true
 			});
@@ -914,6 +915,7 @@ class RoomView extends React.Component<IRoomViewProps, IRoomViewState> {
 		this.handleCloseEmoji(showActionSheet, {
 			children: <ReactionsList reactions={message?.reactions} getCustomEmoji={this.getCustomEmoji} />,
 			snaps: ['50%'],
+			enableContentPanningGesture: false,
 			fullContainer: true
 		});
 	};
