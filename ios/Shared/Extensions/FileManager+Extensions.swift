3e3a98df35e2721ccc31571180d312afba788f8f
Rohit <40559587+Rohit3523@users.noreply.github.com>
2026-05-19 00:05:38 +0530

    fix new line

--- ios/Shared/Extensions/FileManager+Extensions.swift
+++ ios/Shared/Extensions/FileManager+Extensions.swift
@@ -3,11 +3,10 @@ import Foundation
 extension FileManager {
   func groupDir() -> String {
     let applicationGroupIdentifier = Bundle.main.string(forKey: "AppGroupIdentifier")
-
     guard let path = containerURL(forSecurityApplicationGroupIdentifier: applicationGroupIdentifier)?.path else {
       return ""
     }
-
+    
     return path
   }
 }
