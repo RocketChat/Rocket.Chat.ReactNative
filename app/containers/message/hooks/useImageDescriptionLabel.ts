770d173c5280eaa0200b414a1202dcec26fb62c1
OtavioStasiak <otaviostasiakmusic@outlook.com>
2026-05-19 15:59:17 -0300

    refactor: simplify message accessibility label composition

--- app/containers/message/hooks/useImageDescriptionLabel.ts
+++ app/containers/message/hooks/useImageDescriptionLabel.ts
@@ -1,10 +1,16 @@
 import i18n from '../../../i18n';
+import { useAltTextSupported } from '../../../lib/hooks/useAltTextSupported';
 import { type IAttachment } from '../../../definitions';
 
 export const useImageDescriptionLabel = (attachments: IAttachment[] | undefined, msg: string | undefined): string => {
+	const isAltTextSupported = useAltTextSupported();
+	if (isAltTextSupported) {
+		return '';
+	}
+
 	const imageAltText = attachments
 		?.filter(attachment => attachment.image_url)
-		.map(attachment => (attachment.altText ?? attachment.description)?.trim())
+		.map(attachment => (attachment.altText || attachment.description)?.trim())
 		.find(text => !!text);
 
 	if (!imageAltText || imageAltText === msg?.trim()) {
