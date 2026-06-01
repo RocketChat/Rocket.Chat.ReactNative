37201f3967d7394aed9f8af009a85b8c07bb97bd
yash-rajpal <rajpal.yash03@gmail.com>
2026-06-01 22:10:14 +0530

    update snapshots

--- app/containers/LoginServices/LoginServices.test.tsx
+++ app/containers/LoginServices/LoginServices.test.tsx
@@ -1,4 +1,6 @@
 import { generateSnapshots } from '../../../.rnstorybook/generateSnapshots';
 import * as stories from './LoginServices.stories';
 
+jest.mock('../../lib/services/connect', () => ({}));
+
 generateSnapshots(stories);
