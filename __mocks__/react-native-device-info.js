4cf875f20515029088ee047c69d00693a4ce3ba2
Rohit <40559587+Rohit3523@users.noreply.github.com>
2026-06-09 02:23:11 +0530

    Added mock device-info for versions to fix header snapshot

--- __mocks__/react-native-device-info.js
+++ __mocks__/react-native-device-info.js
@@ -1,3 +1,7 @@
 import mockDeviceInfo from 'react-native-device-info/jest/react-native-device-info-mock';
 
+mockDeviceInfo.getSystemVersion = jest.fn(() => '14.0');
+mockDeviceInfo.getVersion = jest.fn(() => '1.0.0');
+mockDeviceInfo.getBuildNumber = jest.fn(() => '1');
+
 export default mockDeviceInfo;
