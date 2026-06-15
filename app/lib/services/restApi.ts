de8ee2edda9ae1abcabe4b5a4a1fa447c101adb8
Rohit <40559587+Rohit3523@users.noreply.github.com>
2026-06-15 23:48:10 +0530

    use rest api instead of methodcall for set status

--- app/lib/services/restApi.ts
+++ app/lib/services/restApi.ts
@@ -329,14 +329,8 @@ export const setUserPreferences = (userId: string, data: Partial<INotificationPr
 	// RC 0.62.0
 	sdk.post('users.setPreferences', { userId, data });
 
-export const setUserStatus = (status: string, message: string, expiresAt?: string | null) => {
-	if (compareServerVersion(reduxStore.getState().server.version, 'greaterThanOrEqualTo', '8.6.0')) {
-		return sdk.post('users.setStatus', { status, message, ...(expiresAt !== undefined && { expiresAt }) });
-	}
-
-	// RC 1.2.0
-	return sdk.methodCall('setUserStatus', status, message);
-};
+export const setUserStatus = (status: string, message: string, expiresAt?: string | null) =>
+	sdk.post('users.setStatus', { status, message, ...(expiresAt !== undefined && { expiresAt }) });
 
 export const setReaction = (emoji: string, messageId: string) =>
 	// RC 0.62.2
