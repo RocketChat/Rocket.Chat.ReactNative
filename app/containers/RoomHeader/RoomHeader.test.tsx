78ae8e36a80419ef42edc927b9af6a6206a48a49
Rohit <40559587+Rohit3523@users.noreply.github.com>
2026-06-16 02:47:11 +0530

    fake time in jest

--- app/containers/RoomHeader/RoomHeader.test.tsx
+++ app/containers/RoomHeader/RoomHeader.test.tsx
@@ -1,4 +1,6 @@
 import { generateSnapshots } from '../../../.rnstorybook/generateSnapshots';
 import * as stories from './RoomHeader.stories';
 
+jest.useFakeTimers({ now: new Date('2026-06-15T12:00:00.000Z') });
+
 generateSnapshots(stories);
