3cfb86734dc78459bbef5ecb7bf22b8f619ed840
Rohit <40559587+Rohit3523@users.noreply.github.com>
2026-06-17 00:57:41 +0530

    fix lint

--- app/containers/RoomHeader/RoomHeader.stories.tsx
+++ app/containers/RoomHeader/RoomHeader.stories.tsx
@@ -102,11 +102,11 @@ export const Themes = () => (
 );
 
 const futureExpiry = '2030-06-15T19:00:00.000Z';
-const now = new Date('2026-06-15T12:00:00.000Z');
+const now = new Date('2026-06-15T12:00:00.000Z').getTime();
+const hourLaterExpiry = new Date(now + 3600000).toISOString();
+const expiredExpiry = new Date(now - 3600000).toISOString();
 
 export const DM_Status = () => {
-	const hourLaterExpiry = new Date(now.getTime() + 3600000).toISOString();
-	const expiredExpiry = new Date(now.getTime() - 3600000).toISOString();
 	return (
 		<>
 			<HeaderExample
