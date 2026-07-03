b7620674717851d191d2bad2859f85fb8c5e9046
Shevilll <ahmadfaraz00710@gmail.com>
2026-07-03 23:57:31 +0530

    refactor(protectedFunction): handle async/promise rejections safely

--- app/lib/methods/helpers/protectedFunction.ts
+++ app/lib/methods/helpers/protectedFunction.ts
@@ -1,7 +1,12 @@
 export default <T extends (...args: any[]) => any>(fn: T): ((...params: Parameters<T>) => void) =>
 	(...params: Parameters<T>) => {
 		try {
-			fn(...params);
+			const result = fn(...params);
+			if (result && typeof result.catch === 'function') {
+				result.catch((e: any) => {
+					console.log(e);
+				});
+			}
 		} catch (e) {
 			console.log(e);
 		}
