bf54234f2d77e605d51a33958c4d5f11d991e0e9
Diego Mello <diegolmello@gmail.com>
2026-08-24 14:56:15 -0300

    refactor: type the logged-in server lookup with TServerModel

--- app/lib/methods/loggedInServer.ts
+++ app/lib/methods/loggedInServer.ts
@@ -1,11 +1,13 @@
+import { type TServerModel } from '../../definitions';
 import { TOKEN_KEY } from '../constants/keys';
 import database from '../database';
+import { SERVERS_TABLE } from '../database/model';
 import UserPreferences from './userPreferences';
 
 export const hasStoredLoginToken = (serverId: string): boolean => !!UserPreferences.getString(`${TOKEN_KEY}-${serverId}`);
 
-export const findLoggedInServer = function* findLoggedInServer(): Generator<any, { id: string; version: string } | undefined> {
-	const serversCollection = database.servers.get('servers');
-	const servers = (yield serversCollection.query().fetch()) as { id: string; version: string }[];
+export const findLoggedInServer = function* findLoggedInServer(): Generator<any, TServerModel | undefined> {
+	const serversCollection = database.servers.get(SERVERS_TABLE);
+	const servers = (yield serversCollection.query().fetch()) as TServerModel[];
 	return servers.find(({ id }) => hasStoredLoginToken(id));
 };
