e61ccda164d90037237bf0bc9a40f0d85e12b634
OtavioStasiak <otaviostasiakmusic@outlook.com>
2026-08-26 20:28:55 -0300

    chore: trim Separator barrel

--- app/containers/Separator/index.ts
+++ app/containers/Separator/index.ts
@@ -1,3 +1,2 @@
-export { default as DateSeparator, DateSeparatorLabel } from './DateSeparator';
 export { default as FloatingDateSeparator } from './FloatingDateSeparator';
 export { default as MessageSeparator } from './MessageSeparator';
