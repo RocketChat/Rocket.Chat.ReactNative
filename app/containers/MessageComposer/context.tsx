45b964c5dcb9bb2a8ee324edd9b7169528a78708
Diego Mello <diegolmello@gmail.com>
2026-09-01 18:19:43 -0300

    chore(MessageComposer): update stale TODO referent

--- app/containers/MessageComposer/context.tsx
+++ app/containers/MessageComposer/context.tsx
@@ -84,7 +84,7 @@ export const useComposerAttachments = (): State['attachments'] => useComposerSto
 type TMessageInnerContext = {
 	sendMessage(): void;
 	onEmojiSelected(emoji: IEmoji): void;
-	// TODO: action should be required
+	// TODO: onClosed should be required
 	closeEmojiKeyboardAndAction(onClosed?: Function, params?: any): void;
 	focus(): void;
 };
