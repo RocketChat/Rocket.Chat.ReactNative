59890bd68a4b56ad17e3825bb57809ceba17243c
Diego Mello <diegolmello@gmail.com>
2026-09-02 13:45:12 -0300

    test(RoomView): replace LoadMore snapshot with state assertions

--- app/views/RoomView/LoadMore/LoadMore.test.tsx
+++ app/views/RoomView/LoadMore/LoadMore.test.tsx
@@ -1,6 +1,51 @@
-import { generateSnapshots } from '../../../../.rnstorybook/generateSnapshots';
-import * as stories from './LoadMore.stories';
+import { ActivityIndicator } from 'react-native';
+import { Provider } from 'react-redux';
+import { render, screen, userEvent } from '@testing-library/react-native';
+
+import { createMockedStore } from '../../../reducers/mockedStore';
+import { MessageTypeLoad } from '../../../lib/constants/messageTypeLoad';
+import { type MessageType } from '../../../definitions';
+import LoadMore from '.';
 
 jest.unmock('../../../lib/hooks/useResponsiveLayout/useResponsiveLayout');
 
-generateSnapshots(stories);
+const renderLoadMore = ({
+	type = MessageTypeLoad.MORE,
+	runOnRender = false
+}: { type?: MessageType; runOnRender?: boolean } = {}) => {
+	const store = createMockedStore();
+	render(
+		<Provider store={store}>
+			<LoadMore rid='rid' t='c' loaderId='loaderId' type={type} runOnRender={runOnRender} />
+		</Provider>
+	);
+	return store;
+};
+
+describe('LoadMore', () => {
+	test.each([
+		[MessageTypeLoad.MORE, 'Load more'],
+		[MessageTypeLoad.PREVIOUS_CHUNK, 'Load older'],
+		[MessageTypeLoad.NEXT_CHUNK, 'Load newer']
+	])('renders the %s label', (type, label) => {
+		renderLoadMore({ type });
+
+		expect(screen.getByText(label)).toBeOnTheScreen();
+	});
+
+	test('shows the loading indicator instead of the label after being pressed', async () => {
+		renderLoadMore();
+
+		await userEvent.press(screen.getByText('Load more'));
+
+		expect(screen.queryByText('Load more')).not.toBeOnTheScreen();
+		expect(screen.UNSAFE_queryByType(ActivityIndicator)).not.toBeNull();
+	});
+
+	test('requests history on mount when runOnRender is set', () => {
+		const store = renderLoadMore({ runOnRender: true });
+
+		expect(store.getState().room.historyLoaders).toEqual(['loaderId']);
+		expect(screen.UNSAFE_queryByType(ActivityIndicator)).not.toBeNull();
+	});
+});
