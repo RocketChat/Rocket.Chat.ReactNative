471cb292d8bdef0695ed5702b356837520faa21f
Rohit <40559587+Rohit3523@users.noreply.github.com>
2026-09-02 19:19:21 +0530

    added safe area in video view and move style to it's own file

--- app/containers/VideoPlayer/index.tsx
+++ app/containers/VideoPlayer/index.tsx
@@ -1,12 +1,14 @@
 import { useVideoPlayer, VideoView } from 'expo-video';
 import { useEffect, useRef, type Dispatch, type SetStateAction, type ReactElement } from 'react';
-import { Alert } from 'react-native';
+import { Alert, View } from 'react-native';
 import { useEventListener } from 'expo';
 
+import { styles } from './styles';
 import { useAppNavigation } from '../../lib/hooks/navigation';
 import I18n from '../../i18n';
 import { type IAttachment } from '../../definitions';
 import { formatAttachmentUrl, encodeAttachmentUrl } from '../../lib/methods/helpers';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 interface IVideoPlayerProps {
 	attachment: IAttachment;
@@ -18,6 +20,7 @@ interface IVideoPlayerProps {
 const VideoPlayer = ({ attachment, user, baseUrl, setLoading }: IVideoPlayerProps): ReactElement => {
 	const navigation = useAppNavigation();
 	const hasHandledErrorRef = useRef(false);
+	const { bottom } = useSafeAreaInsets();
 
 	const url = formatAttachmentUrl(attachment.title_link || attachment.video_url, user.id, user.token, baseUrl);
 	const uri = encodeAttachmentUrl(url);
@@ -51,7 +54,9 @@ const VideoPlayer = ({ attachment, user, baseUrl, setLoading }: IVideoPlayerProp
 	}, [navigation, player]);
 
 	return (
-		<VideoView player={player} style={{ flex: 1 }} contentFit='contain' nativeControls allowsFullscreen allowsPictureInPicture />
+		<View style={[styles.container, { paddingBottom: bottom }]}>
+			<VideoView player={player} style={styles.container} contentFit='contain' nativeControls allowsFullscreen allowsPictureInPicture />
+		</View>
 	);
 };
 
