c3bf3e05b24243cae82bdedace06be0b99e86958
Rohit Bansal <40559587+Rohit3523@users.noreply.github.com>
2026-09-11 18:08:37 +0530

    tests: Add CAS and SAML login E2E Test (#7659)

--- .maestro/scripts/e2e_account.example.js
+++ .maestro/scripts/e2e_account.example.js
@@ -1,6 +1,14 @@
 const account = {
 	adminUser: 'Change_here',
-	adminPassword: 'Change_here'
+	adminPassword: 'Change_here',
+	saml: {
+		username: 'Change_here',
+		password: 'Change_here'
+	},
+	cas: {
+		username: 'Change_here',
+		password: 'Change_here'
+	}
 };
 
 output.account = account;
