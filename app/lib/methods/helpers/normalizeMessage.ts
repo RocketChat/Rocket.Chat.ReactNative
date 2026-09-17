5f638635c57d29df02aa6f19caac1203415169b8
Rohit Bansal <40559587+Rohit3523@users.noreply.github.com>
2026-09-18 02:01:16 +0530

    test: add unit tests for buildMessage and normalizeMessage (#7700)

--- app/lib/methods/helpers/normalizeMessage.ts
+++ app/lib/methods/helpers/normalizeMessage.ts
@@ -44,7 +44,7 @@ export default (msg: any): IMessage | IThreadResult | null => {
 			names: msg.reactions ? msg.reactions[key].names : []
 		}));
 	}
-	if (msg.translations && Object.keys(msg.translations).length) {
+	if (!Array.isArray(msg.translations) && msg.translations && Object.keys(msg.translations).length) {
 		msg.translations = Object.keys(msg.translations).map(key => ({
 			_id: `${msg._id}${key}`,
 			language: key,
